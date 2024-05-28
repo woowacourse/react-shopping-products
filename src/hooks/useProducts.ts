7a5cd3655f8c9c03c8cfe5e092039ffd73965e91
jaeml06 <jaeml0630@gmail.com>
2024-05-28 19:44:18 +0900

    feat : 마지막 페이지 접근 후, fetch 호출 막는 기능 구현
    Co-Authored-By: river <130737187+0jenn0@users.noreply.github.com>

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
 
 import { fetchProducts } from '../api/index';
+import {
+  INITIAL_DATA_LOAD_COUNT,
+  SUBSEQUENT_DATA_LOAD_COUNT,
+} from '../constants';
 
 interface Product {
   id: number;
@@ -23,13 +27,19 @@ export default function useProducts(): UseProductsResult {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<unknown>(null);
   const [page, setPage] = useState(1);
+  const [isLast, setIsLast] = useState(false);
 
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true);
       try {
-        const data = await fetchProducts(page, 4);
-        setProducts((prevProducts) => [...prevProducts, ...data]);
+        const limit =
+          page === 1 ? INITIAL_DATA_LOAD_COUNT : SUBSEQUENT_DATA_LOAD_COUNT;
+        const data = await fetchProducts(page, limit);
+        if (data.last) {
+          setIsLast(true);
+        }
+        setProducts((prevProducts) => [...prevProducts, ...data.content]);
       } catch (error) {
         setError(error);
       } finally {
@@ -42,10 +52,8 @@ export default function useProducts(): UseProductsResult {
 
   const fetchNextPage = () => {
     setPage((prevPage) => {
-      if (prevPage < 21) {
-        return prevPage + 1;
-      }
-      return prevPage;
+      if (isLast) return prevPage;
+      return prevPage + 1;
     });
   };
 
