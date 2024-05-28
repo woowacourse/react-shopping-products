ceac36e76c4e171bed44cba37a0e1029cfa93ef1
badahertz52 <badahertz52@gmail.com>
2024-05-28 12:23:54 +0900

    ci: prettier, eslint 설정 추가
    Co-Authored-By: JinHo Kim <81083461+jinhokim98@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
@@ -13,6 +14,51 @@ module.exports = {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
+    ],
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        allowSeparatedGroups: true,
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        pathGroups: [
+          {
+            pattern: "next",
+            group: "builtin",
+          },
+          {
+            pattern: "react",
+            group: "builtin",
+          },
+          {
+            pattern: "@MyDesignSystem/**",
+            group: "internal",
+          },
+          {
+            pattern: "src/**",
+            group: "internal",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["src/**", "@MyDesignSystem/**"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
     ],
   },
 };
