191c0a2ab6113e8fc41bc6c893a13c7840a63eaa
rbgksqkr <rbgks1937@naver.com>
2024-05-28 16:44:05 +0900

    test(productList): 다음 페이지에서 4개의 상품 목록을 추가로 불러오는 테스트 작성
    Co-authored-by: dle234 <213402@jnu.ac.kr>

--- src/__tests__/productList.test.ts
+++ src/__tests__/productList.test.ts
@@ -1,4 +1,5 @@
 import { renderHook, waitFor } from '@testing-library/react';
+import { act } from 'react';
 
 import useProductList from '@/hooks/useProductList';
 
@@ -9,6 +10,24 @@ describe('productList', () => {
     await waitFor(() => {
       expect(result.current.productList.length).toBe(20);
       expect(result.current.page).toBe(0);
+    });
+  });
+
+  it('다음 페이지에서는 4개의 상품 목록을 추가로 불러와야 한다.', async () => {
+    const { result } = renderHook(() => useProductList());
+
+    await waitFor(() => {
+      expect(result.current.productList.length).toBe(20);
+      expect(result.current.page).toBe(0);
+    });
+
+    act(() => {
+      result.current.fetchNextPage();
+    });
+
+    await waitFor(() => {
+      expect(result.current.productList.length).toBe(24);
+      expect(result.current.page).toBe(1);
     });
   });
 });
