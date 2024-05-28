94f6d77f9a485a9e21e03e48208fb716476f0836
anttiey <hanuuny@gmail.com>
2024-05-28 20:07:05 +0900

    feat: MSW 모킹 서버 초기 설정 및 상품 아이템 불러오기 모킹 추가
    Co-authored-by: 최진실 <chlwlstlf@users.noreply.github.com>

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,6 +1,6 @@
 import { generateBasicToken } from '../utils/auth';
 
-const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD;
 const USER_ID = import.meta.env.VITE_USER_ID;
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { worker } from './mocks/browser.ts';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />,
-  </React.StrictMode>,
+const startServer = async () => {
+  if (import.meta.env.DEV) {
+    return await worker.start({
+      onUnhandledRequest: 'bypass',
+    });
+  }
+};
+
+startServer().then(() =>
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />,
+    </React.StrictMode>,
+  ),
 );
