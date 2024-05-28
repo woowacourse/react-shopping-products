02e806e006f00b085488b11f85becd636cf35138
lurgi <wahoo9040@naver.com>
2024-05-28 19:27:24 +0900

    docs: 테스트 기능 구현 목록 작성
    Co-authored-by: 헤인 <Hain-tain@users.noreply.github.com>

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,10 +1,10 @@
-const CATEGORY = {
+export const CATEGORY = {
   fashion: "패션",
   beverage: "음료",
   electronics: "전자",
   kitchen: "주방용품",
   fitness: "피트니스",
   books: "책",
-};
+} as const;
 
-export default CATEGORY;
+export const CATEGORY_LIST = Object.values(CATEGORY);
