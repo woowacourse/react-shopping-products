c402e5b05f9f8f60eb14e87baf4c9e4d43908efe
손진영 <sm099@naver.com>
2024-05-28 15:58:04 +0900

    feat: useFetch hook 구현
    Co-authored-by: ss0526100 <ss0526100@gmail.com>

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,39 @@
+import APIClient from '@apis/APIClient';
+import { useState, useEffect } from 'react';
+
+const useFetch = <T>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState<unknown>(null);
+
+  useEffect(() => {
+    async function fetchData(endpoint: string) {
+      const response = await APIClient.get(endpoint);
+      const data = await response.json();
+
+      return data;
+    }
+
+    let ignore = false;
+
+    try {
+      // setIsLoading(true);
+
+      fetchData(url).then((data: T) => {
+        if (!ignore) setData(data);
+      });
+    } catch (error) {
+      // setError(error);
+    } finally {
+      // setIsLoading(false);
+    }
+
+    return () => {
+      ignore = true;
+    };
+  }, [url]);
+
+  return { data };
+};
+
+export default useFetch;
