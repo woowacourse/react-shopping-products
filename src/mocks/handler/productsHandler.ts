2dc02b6bb79c2c66b2b45679c85317bd56ab6970
ooherin <555ohr@naver.com>
2024-05-28 16:53:31 +0900

    feat(mock): mock 데이터 생성
    Co-authored-by: pp449 <mma7710@naver.com>

--- src/mocks/handler/productsHandler.ts
+++ src/mocks/handler/productsHandler.ts
@@ -1,6 +1,6 @@
 import { http, HttpResponse } from "msw";
-import SERVER_URL from "../../config/serverUrl";
-import products from "../products.json";
+import SERVER_URL from "@/config/serverUrl";
+import products from "@/mocks/mockResponse/products.json";
 
 export const productsHandler = [
   http.get(SERVER_URL.apiUrl, () => {
