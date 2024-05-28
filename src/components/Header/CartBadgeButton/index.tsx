06122bc3668cbb7a55b5afadf978f0d479a1a110
badahertz52 <badahertz52@gmail.com>
2024-05-28 21:39:20 +0900

    feat: CartBadgeButton의 수량관련 퍼블리싱
    Co-Authored-By: JinHo Kim <81083461+jinhokim98@users.noreply.github.com>

--- src/components/Header/CartBadgeButton/index.tsx
+++ src/components/Header/CartBadgeButton/index.tsx
@@ -2,10 +2,19 @@ import CartIcon from '@assets/cart.svg';
 
 import style from './style.module.css';
 
-function CartBadgeButton() {
+const BADGE_MAX_QUANTITY = 50;
+
+
+function CartBadgeButton({ handleClick }: CartActionButtonProps) {
+  const quantity = 51;
+  const quantityClassName = `${style.quantity} ${quantity > BADGE_MAX_QUANTITY ? style.over : ''}`;
+
   return (
     <button className={style.button}>
       <img src={CartIcon} alt="장바구니 아이콘(장바구니 페이지 이동)" />
+      {!!quantity && (
+        <div className={quantityClassName}>{quantity > BADGE_MAX_QUANTITY ? BADGE_MAX_QUANTITY : quantity}</div>
+      )}
     </button>
   );
 }
