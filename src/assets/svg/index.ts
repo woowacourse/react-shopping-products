e19a1b6ddc94f5b0487a4aad2a06961091299fba
손진영 <sm099@naver.com>
2024-05-28 15:07:33 +0900

    chore: svg 사용을 위한 설정

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,5 @@
+export { default as AddShoppingCart } from './add-shopping-cart.svg?react';
+export { default as RemoveShoppingCart } from './remove-shopping-cart.svg?react';
+export { default as DownArrow } from './down-arrow.svg?react';
+export { default as UpArrow } from './up-arrow.svg?react';
+export { default as ShoppingCart } from './shopping-cart.svg?react';
