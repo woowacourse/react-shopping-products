f9880497c387f2a5fbbe5d7ad61d8feda14f18c4
ooherin <555ohr@naver.com>
2024-05-28 21:48:53 +0900

    feat: selectBox 및 title 반영
    Co-authored-by: pp449 <mma7710@naver.com>

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -2,6 +2,9 @@ import CartBadge from "@/components/CartBadge";
 import Header from "@/components/Header";
 import TextBox from "@/components/_common/TextBox";
 import styled from "styled-components";
+import SelectBox from "@/components/SelectBox";
+
+const menu = ["메뉴1", "메뉴2", "메뉴3"];
 
 const ProductListPage = () => {
   return (
@@ -11,7 +14,13 @@ const ProductListPage = () => {
         <CartBadge />
       </Header>
       <Wrapper>
-        <TextBox type="xLarge" text="bpple 상품 목록" />
+        <ItemInfoWrapper>
+          <TextBox type="xLarge" text="bpple 상품 목록" />
+          <SelectBoxWrapper>
+            <SelectBox optionsContents={menu} />
+            <SelectBox optionsContents={menu} />
+          </SelectBoxWrapper>
+        </ItemInfoWrapper>
       </Wrapper>
     </>
   );
@@ -21,4 +30,15 @@ export default ProductListPage;
 
 const Wrapper = styled.div`
   padding: 36px 23.5px 36px;
+`;
+
+const SelectBoxWrapper = styled.div`
+  gap: 132px;
+  display: flex;
+`;
+
+const ItemInfoWrapper = styled.div`
+  gap: 24px;
+  display: flex;
+  flex-direction: column;
 `;
