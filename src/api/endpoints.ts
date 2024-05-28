8fd8cd7a092f332372c22b059e714491af4cf26a
jaeml06 <jaeml0630@gmail.com>
2024-05-28 15:36:18 +0900

    feat : 상품 목록 fetch 함수 구현
    Co-Authored-By: river <130737187+0jenn0@users.noreply.github.com>

---
+++ src/api/endpoints.ts
@@ -0,0 +1,4 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const USERNAME = import.meta.env.VITE_USERNAME;
+const PASSWORD = import.meta.env.VITE_PASSWORD;
+export const PRODUCTS_ENDPOINT = `${BASE_URL}/products`;
