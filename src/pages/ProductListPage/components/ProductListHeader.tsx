6e09f8caf68c67f54de8ce24026b1eb60b4b847b
jayming66 <mjw8941@gmail.com>
2024-05-28 16:44:39 +0900

    feat(ProductListHeader.tsx): cartIcon 추가
    Co-Authored-By: Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>

--- src/pages/ProductListPage/components/ProductListHeader.tsx
+++ src/pages/ProductListPage/components/ProductListHeader.tsx
@@ -1,12 +1,15 @@
 import Button from '../../../components/Button/Button';
 import Header from '../../../components/Header/Header';
+import cartIcon from '../../../assets/cartIcon.png';
 
 const ProductListHeader = () => {
   return (
     <div>
       <Header>
         <Button>SHOP</Button>
-        <Button>CART</Button>
+        <Button>
+          <img src={cartIcon} width={20} height={24} />
+        </Button>
       </Header>
     </div>
   );
