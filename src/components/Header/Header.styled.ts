a7f45b128fdc220fa06d1e056c3028b5290de1f5
00kang <kdb070970@gmail.com>
2024-05-28 22:44:52 +0900

    fix : Header 컴포넌트 위치 고정
    Co-Authored-By: soosoo22 <80167893+soosoo22@users.noreply.github.com>

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -1,11 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledHeader = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  position: fixed;
+  top: 0;
+  z-index: 1;
 
-  width: 100%;
+  width: inherit;
   height: 64px;
   padding: 0px 24px;
   background: rgba(0, 0, 0, 1);
