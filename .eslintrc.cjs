c43ea200bb72b41b16ce0810eff26463b9fa0d18
greetings1012 <ahnqwerty@gmail.com>
2024-05-28 13:53:50 +0900

    chore: eslint 및 npm 초기 설정
    Co-authored-by: Todari <rhymint@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,49 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "import"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
+    ],
+
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "type",
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "unknown",
+        ],
+        pathGroups: [
+          {
+            pattern: "react*",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@hooks/*",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@pages/*",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@components/*",
+            group: "internal",
+            position: "after",
+          },
+        ],
+      },
     ],
   },
 };
