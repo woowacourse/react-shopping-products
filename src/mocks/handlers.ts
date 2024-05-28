732816e46e76439e9b1508822b37d65b92728395
rbgksqkr <rbgks1937@naver.com>
2024-05-28 15:55:28 +0900

    feat: 첫 페이지 상품 목록을 불러오는 핸들러 구현
    Co-authored-by: dle234 <213402@jnu.ac.kr>

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,18 @@
 import { http, HttpResponse } from 'msw';
 
+import productList from './products.json';
+
 import { PRODUCTS } from '@/api/endpoints';
 
 export const handlers = [
-  http.get(PRODUCTS, () => {
-    return HttpResponse.json();
+  http.get(PRODUCTS, ({ request }) => {
+    const url = new URL(request.url);
+
+    const page = url.searchParams.get('page') || '0';
+    const start = 0;
+    const end = 20;
+    const result = page === '0' && productList.slice(start, end);
+
+    return HttpResponse.json(result);
   }),
 ];
