472651519c3103a0647669b71aa8ec1f230c38b1
jayming66 <mjw8941@gmail.com>
2024-05-28 15:54:37 +0900

    feat(ProductListHeader): ProductListHeader 컴포넌트 생성
    Co-Authored-By: Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>

---
+++ src/pages/ProductListPage/components/ProductListHeader.tsx
@@ -0,0 +1,15 @@
+import Button from '../../../components/Button/Button';
+import Header from '../../../components/Header/Header';
+
+const ProductListHeader = () => {
+  return (
+    <div>
+      <Header>
+        <Button>SHOP</Button>
+        <Button>CART</Button>
+      </Header>
+    </div>
+  );
+};
+
+export default ProductListHeader;
