76f119e080e9b08047cefda749e231935eb3f00f
00kang <kdb070970@gmail.com>
2024-05-28 22:38:13 +0900

    refactor : BaseButton 컴포넌트 불필요한 props 제거

--- src/components/Button/BaseButton.tsx
+++ src/components/Button/BaseButton.tsx
@@ -1,13 +1,8 @@
 interface BaseButtonProps {
   onClick?: () => void;
-  disabled?: boolean;
   children: React.ReactNode;
 }
 
-export const BaseButton: React.FC<BaseButtonProps> = ({ onClick, disabled, children }) => {
-  return (
-    <button onClick={onClick} disabled={disabled}>
-      {children}
-    </button>
-  );
+export const BaseButton: React.FC<BaseButtonProps> = ({ onClick, children }) => {
+  return <button onClick={onClick}>{children}</button>;
 };
