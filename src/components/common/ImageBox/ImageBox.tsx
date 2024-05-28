f58dc5167f91c5673a6dcf8724dfc52b85e16f59
brgndy <brgndy96@gmail.com>
2024-05-28 23:12:15 +0900

    feat: 공용 Image 박스 컴포넌트 생성
    Co-authored-by: pakxe <pigkill40@naver.com>

---
+++ src/components/common/ImageBox/ImageBox.tsx
@@ -0,0 +1,27 @@
+import { HTMLAttributes } from "react";
+
+interface ImageBoxProps extends HTMLAttributes<HTMLImageElement> {
+  src?: string;
+  radius?: "s" | "m" | "l" | number;
+  width: number;
+  height: number;
+  border?: string;
+  backgroundColor?: string;
+}
+
+const ImageBox = ({ src = "", width, height, ...rest }: ImageBoxProps) => {
+  return (
+    <div {...rest}>
+      <img
+        src={src}
+        alt="image"
+        style={{
+          width,
+          height,
+        }}
+      />
+    </div>
+  );
+};
+
+export default ImageBox;
