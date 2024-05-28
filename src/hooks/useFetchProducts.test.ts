e58f0ef2e2b26009ff11833d6a812fee78992cfa
skiende74 <skiende74@gmail.com>
2024-05-28 16:13:19 +0900

    test(useFetchProducts): 상품 목록 단순조회 성공/실패 테스트
    Co-authored-by: chosim-dvlpr <bmj05378@gmail.com>

---
+++ src/hooks/useFetchProducts.test.ts
@@ -0,0 +1,29 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import { HttpResponse, http } from 'msw';
+import { ENDPOINTS_PRODUCTS } from '../api/endpoints';
+import { server } from '../mocks/node';
+import useFetchProducts from './useFetchProducts';
+
+describe('fetchProducts', () => {
+  it('상품 목록 조회 성공', async () => {
+    const { result } = renderHook(useFetchProducts);
+
+    await waitFor(() => expect(result.current.products).toHaveLength(100));
+  });
+
+  it('상품 목록 조회 시 에러', async () => {
+    renderHook(() => {
+      server.use(
+        http.get(ENDPOINTS_PRODUCTS, () => {
+          return new HttpResponse(null, { status: 500 });
+        }),
+      );
+    });
+
+    const { result } = renderHook(useFetchProducts);
+
+    await waitFor(() => {
+      expect(result.current.isError).toBe(true);
+    });
+  });
+});
