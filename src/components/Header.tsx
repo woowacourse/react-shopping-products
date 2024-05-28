4ff37eb679074ec56d33e9785bb34d7dcb0ef21c
lurgi <wahoo9040@naver.com>
2024-05-28 17:08:50 +0900

    feat: Header 컴포넌트 구현

---
+++ src/components/Header.tsx
@@ -0,0 +1,68 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+import COLOR_PALETTE from "../style/colorPalette";
+import Logo from "./icons/Logo";
+import CartIcon from "./icons/CartIcon";
+
+// 스타일드 컴포넌트 정의
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 64px;
+
+  padding: 24px;
+  background-color: ${COLOR_PALETTE.black};
+  color: ${COLOR_PALETTE.white};
+  box-sizing: border-box;
+`;
+
+const CartButton = styled.button`
+  position: relative;
+
+  background: none;
+  border: none;
+  color: ${COLOR_PALETTE.white};
+  font-size: 16px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+`;
+
+const ItemCount = styled.span`
+  position: absolute;
+  width: 19px;
+  aspect-ratio: 1/1;
+  right: 4.5px;
+  bottom: 0px;
+
+  font-size: 10px;
+  font-weight: 700;
+  padding: 0.5px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${COLOR_PALETTE.white};
+  color: ${COLOR_PALETTE.black};
+  border-radius: 100%;
+  font-size: 14px;
+`;
+
+function Header() {
+  const [itemCount, setItemCount] = useState(13);
+
+  return (
+    <HeaderContainer>
+      <Logo />
+      <CartButton>
+        <CartIcon />
+        {itemCount > 0 && <ItemCount>{itemCount}</ItemCount>}
+      </CartButton>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
