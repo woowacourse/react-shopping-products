e297bc5cd55769c022ba159c85f740f225b71fa0
brgndy <brgndy96@gmail.com>
2024-05-28 23:15:11 +0900

    feat: 상품 목록 페칭 관련 msw 핸들러 코드 작성
    Co-authored-by: pakxe <pigkill40@naver.com>

---
+++ src/apis/endpoints.ts
@@ -0,0 +1,3 @@
+export const ENDPOINT = {
+  PRODUCT_LIST: "/products",
+};
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,63 @@
+import { http, HttpResponse } from "msw";
+import { BASE_URL } from "@/apis/baseUrl";
+import { ENDPOINT } from "@/apis/endpoints";
+import products from "./productList.json";
+
+type Category =
+  | "all"
+  | "fashion"
+  | "beverage"
+  | "electronics"
+  | "kitchen"
+  | "fitness"
+  | "books"
+  | "wooteco";
+
+type SortBase = "price";
+type SortOrder = "asce" | "desc";
+
+const FIRST_LENGTH = 20;
+
+export const handlers = [
+  http.get(`${BASE_URL}${ENDPOINT.PRODUCT_LIST}`, ({ request }) => {
+    const url = new URL(request.url);
+
+    // TODO as 타입 선언 대체하기
+    const category: Category = url.searchParams.get("category") as Category;
+    const page = Number(url.searchParams.get("page") || "1");
+    const size = Number(url.searchParams.get("size") || "4");
+    const [sortBase, sortOrder]: [SortBase, SortOrder] = url.searchParams
+      .get("sort")
+      ?.split(",");
+
+    const productListFilteredCategory =
+      category === "all"
+        ? products
+        : products.filter((product) => product.category === category);
+
+    const sortedProductList = productListFilteredCategory.sort((a, b) =>
+      sortOrder === "asce" ? a.price - b.price : b.price - a.price
+    );
+
+    const limit = page === 1 ? FIRST_LENGTH : size; // 첫 시도에는 20개, 그 이후부턴 4개씩
+    const start = page === 1 ? 0 : (page - 2) * size + FIRST_LENGTH; // 2페이지부터는 4씩 커서 증가
+    const end = start + limit; // 마지막 좌표
+
+    const paginatedProducts = sortedProductList.slice(start, end);
+
+    const totalProducts = sortedProductList.length;
+
+    const remainingProducts = totalProducts - FIRST_LENGTH;
+    const subsequentPages = Math.ceil(remainingProducts / size);
+
+    const totalPages = 1 + subsequentPages;
+
+    return HttpResponse.json({
+      content: paginatedProducts,
+      totalPages,
+      pageable: {
+        pageNumber: page,
+      },
+    });
+  }),
+];
