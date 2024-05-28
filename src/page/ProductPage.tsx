06467a5e242adaef883462f23bb651340e430f45
jaeml06 <jaeml0630@gmail.com>
2024-05-28 21:46:57 +0900

    feat : 무한 스크롤 기능 구현
    Co-Authored-By: river <130737187+0jenn0@users.noreply.github.com>

--- src/page/ProductPage.tsx
+++ src/page/ProductPage.tsx
@@ -7,6 +7,30 @@ import { Container } from './ProductPage.style';
 
 function ProductPage() {
   const { products, fetchNextPage, loading } = useProducts();
+  const observer = useRef<IntersectionObserver | null>(null);
+  const lastProductElementRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (observer.current) observer.current.disconnect();
+
+    const options = {
+      threshold: 1.0,
+    };
+
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        fetchNextPage();
+      }
+    }, options);
+
+    if (lastProductElementRef.current) {
+      observer.current.observe(lastProductElementRef.current);
+    }
+
+    return () => {
+      if (observer.current) observer.current.disconnect();
+    };
+  }, [products, loading, fetchNextPage]);
 
   return (
     <Container>
