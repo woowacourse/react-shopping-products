1701210c54604b089812ceaad0835e4a40df723c
greetings1012 <ahnqwerty@gmail.com>
2024-05-29 00:10:33 +0900

    fix: 브라우저 모킹 환경 설정 제거
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
 
-async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
-
-  const { worker } = await import("./mocks/browser");
-  return worker.start();
-}
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-});
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
