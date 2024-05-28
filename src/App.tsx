b457eb37c1b02e8927559757bb32902ecffea2d5
badahertz52 <badahertz52@gmail.com>
2024-05-28 20:39:38 +0900

    feat: App에 Layout ,ProductListPage 추가
    Co-Authored-By: JinHo Kim <81083461+jinhokim98@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import '@styles/App.css';
 import '@styles/reset.css';
 import '@styles/global.module.css';
-import { Header } from './components';
+import { Header, Layout } from './components';
+import { ProductListPage } from './pages';
 
 function App() {
   return (
     <>
       <Header />
+      <Layout>
+        <ProductListPage />
+      </Layout>
     </>
   );
 }
