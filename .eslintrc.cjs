b491f650c431ee417432df5164a449e3f072e186
rbgksqkr <rbgks1937@naver.com>
2024-05-28 14:18:57 +0900

    chore: prettier 및 eslint 설정
    Co-authored-by: dle234 <213402@jnu.ac.kr>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,11 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    "plugin:prettier/recommended",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:import/recommended"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
@@ -14,5 +16,51 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
+
+    "import/order": [
+      "error",
+      {
+        "groups": ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        "pathGroups": [
+          {
+            "pattern": "react*",
+            "group": "builtin",
+            "position": "before"
+          },
+          {
+            "pattern": "@/pages/*",
+            "group": "internal",
+            "position": "after"
+          },
+          {
+            "pattern": "@/components/*",
+            "group": "internal",
+            "position": "after"
+          },
+          {
+            "pattern": "@/hooks/*",
+            "group": "internal",
+            "position": "after"
+          },
+          {
+            "pattern": "src/**",
+            "group": "internal"
+          },
+        ],
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        },
+        "newlines-between": "always"
+      }
+    ],
   },
+  settings: {
+    "import/resolver": {
+      "typescript": {},
+      "node": {
+        "extensions": [".ts", ".tsx", ".js", ".jsx"]
+      }
+    }
+  }
 };
