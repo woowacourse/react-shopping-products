36a518f0143b9fad155cee4d5c39858d9dd27838
badahertz52 <badahertz52@gmail.com>
2024-05-28 21:40:25 +0900

    feat: Header에 CartAction fallback 적용
    Co-Authored-By: JinHo Kim <81083461+jinhokim98@users.noreply.github.com>

--- src/components/Header/CartBadgeButton/index.tsx
+++ src/components/Header/CartBadgeButton/index.tsx
@@ -4,13 +4,16 @@ import style from './style.module.css';
 
 const BADGE_MAX_QUANTITY = 50;
 
+interface CartActionButtonProps {
+  handleClick: () => void;
+}
 
 function CartBadgeButton({ handleClick }: CartActionButtonProps) {
   const quantity = 51;
   const quantityClassName = `${style.quantity} ${quantity > BADGE_MAX_QUANTITY ? style.over : ''}`;
 
   return (
-    <button className={style.button}>
+    <button onClick={handleClick} className={style.button}>
       <img src={CartIcon} alt="장바구니 아이콘(장바구니 페이지 이동)" />
       {!!quantity && (
         <div className={quantityClassName}>{quantity > BADGE_MAX_QUANTITY ? BADGE_MAX_QUANTITY : quantity}</div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
+
+import { CartAction } from '../Fallbacks';
+
 import CartBadgeButton from './CartBadgeButton';
 import Logo from './Logo';
 import style from './style.module.css';
 
 function Header() {
+  const [error, setError] = useState(false);
+
+  const handleClick = () => {
+    setError((prev) => !prev);
+  };
+
   return (
-    <header className={style.header}>
-      <Logo />
-      <CartBadgeButton />
-    </header>
+    <>
+      <header className={style.header}>
+        <Logo />
+        <CartBadgeButton handleClick={handleClick} />
+      </header>
+      {error && <CartAction />}
+    </>
   );
 }
 
