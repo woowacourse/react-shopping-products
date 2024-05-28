c022731a217b4bc1757cf10725ff274954473cd4
skiende74 <skiende74@gmail.com>
2024-05-28 17:34:33 +0900

    test(useFetchProducts): 마지막 페이지 도달시 더이상 요청하지 않는 기능 테스트 구현
    Co-authored-by: chosim-dvlpr <bmj05378@gmail.com>

--- src/hooks/useFetchProducts.test.ts
+++ src/hooks/useFetchProducts.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, waitFor } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 import { HttpResponse, http } from 'msw';
 import { ENDPOINTS_PRODUCTS } from '../api/endpoints';
 import { server } from '../mocks/node';
@@ -29,5 +29,40 @@ describe('fetchProducts', () => {
     });
   });
 
-  //
+  it('첫 상품목록 조회 이후부터는, 이전 결과에 이어 4개씩 아이템을 추가하여 가져온다.', async () => {
+    const { result } = renderHook(() => useFetchProducts());
+
+    await waitFor(() => {
+      expect(result.current.products).toHaveLength(20);
+    });
+
+    act(() => result.current.fetchNextPage());
+
+    await waitFor(() => {
+      expect(result.current.products).toHaveLength(24);
+    });
+  });
+
+  it('마지막 페이지 도달 시 더 이상 요청하지 않는다.', async () => {
+    const { result } = renderHook(() => useFetchProducts());
+
+    await waitFor(() => {
+      expect(result.current.products).toHaveLength(20);
+    });
+
+    act(() => result.current.fetchNextPage());
+    for (let i = 2; i <= 21; i++) {
+      act(() => result.current.fetchNextPage());
+    }
+
+    await waitFor(() => {
+      expect(result.current.products).toHaveLength(100);
+    });
+
+    act(() => result.current.fetchNextPage());
+
+    await waitFor(() => {
+      expect(result.current.products).toHaveLength(100);
+    });
+  });
 });
