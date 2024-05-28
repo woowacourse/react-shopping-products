88b6b047afe61a3ba6c922634c2a68755ab83543
ooherin <555ohr@naver.com>
2024-05-28 15:37:50 +0900

    test(msw): 브라우저 msw 설정 및 테스트
    Co-authored-by: pp449 <mma7710@naver.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-// process.env.NODE_ENV === 'development'
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const { worker } = await import("./mocks/browser.ts");
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
