d760ea0abcf1e7d9e4dd424c156cf0d9ce518594
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-28 16:30:47 +0900

    feat: 목록 조회 커스텀 작업
    Co-Authored-By: Yoonkyoung Kim <dev.yoonkyoung@gmail.com>

---
+++ src/hooks/useProductList.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+import { Product } from '@/types/product.type';
+import { getProductList } from '@/api/product';
+
+// TODO: 상수처리 파일로 옮기기
+const FIRST_PAGE_ITEM_COUNT = 20;
+const MORE_LOAD_ITEM_COUNT = 4;
+
+const useProductList = () => {
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(FIRST_PAGE_ITEM_COUNT);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (page > 1) setSize(MORE_LOAD_ITEM_COUNT);
+
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const data = await getProductList({ page, size });
+        setProducts((prev) => [...prev, ...data.content]);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [page]);
+
+  const fetchNextPage = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  return { page, products, loading, error, fetchNextPage };
+};
+
+export default useProductList;
