b9a265ea0afb86217da7240981bde26c50cfc360
simhorim <hori5020@gmail.com>
2024-05-30 00:07:14 +0900

    refactor(Header): export 수정

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -2,6 +2,8 @@ import * as S from './style';
 
 import { PropsWithChildren } from 'react';
 
-export const Header = ({ children }: PropsWithChildren) => {
+const Header = ({ children }: PropsWithChildren) => {
   return <S.Header>{children}</S.Header>;
 };
+
+export default Header;
