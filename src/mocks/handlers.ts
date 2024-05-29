3bc82ab04191db3d785e007a0413f16b44ce9e17
00kang <kdb070970@gmail.com>
2024-05-29 13:57:37 +0900

    feat : URL 상수처리
    Co-Authored-By: soosoo22 <80167893+soosoo22@users.noreply.github.com>

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,9 @@
 import { http, HttpResponse } from "msw";
+import { PRODUCTS_ENDPOINT } from "../api/endpoints";
 import products from "./products.json";
 
 export const handlers = [
-  http.get("URL", () => {
+  http.get(PRODUCTS_ENDPOINT, () => {
     return HttpResponse.json(products);
   }),
 ];
