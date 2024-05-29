1f5b84738b1c442fdaf8f9bb21753e02423f5ed7
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-29 16:57:57 +0900

    feat: 카테고리 필터링 드롭다운 작업
    Co-Authored-By: Yoonkyoung Kim <dev.yoonkyoung@gmail.com>

---
+++ src/components/dropdown/FilterCategory.tsx
@@ -0,0 +1,19 @@
+import BaseDropDown from './BaseDropDown';
+import { CATEGORY_LIST } from '@/constants/category';
+
+interface Props {
+  onChangeCategory: (category: string) => void;
+}
+
+const FilterCategory = ({ onChangeCategory }: Props) => {
+  const categoryOptions = CATEGORY_LIST;
+  return (
+    <BaseDropDown
+      initialValue="전체"
+      options={categoryOptions}
+      onChangeSelect={onChangeCategory}
+    />
+  );
+};
+
+export default FilterCategory;
