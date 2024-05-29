1d3aee2942efd32834440fff8e475605a5ebec7c
greetings1012 <ahnqwerty@gmail.com>
2024-05-29 20:26:57 +0900

    feat(Header): 헤더 구현
    Co-authored-by: Todari <rhymint@gmail.com>

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import { MainLogo, ShoppingCartIcon } from "../../assets";
+import useCartItemList from "../../hooks/useCartItemList";
+import * as S from "./Header.style";
+
+function Header() {
+  const { quantity } = useCartItemList();
+  return (
+    <S.HeaderWrapper>
+      <S.MainLogo src={MainLogo} alt="메인 로고" />
+      <S.ShoppingCartButton>
+        <S.ShoppingCartIconContainer
+          src={ShoppingCartIcon}
+        ></S.ShoppingCartIconContainer>
+        {quantity > 0 && (
+          <S.ShoppingCartQuantityContainer>
+            <S.ShoppingCartQuantity>{quantity}</S.ShoppingCartQuantity>
+          </S.ShoppingCartQuantityContainer>
+        )}
+      </S.ShoppingCartButton>
+    </S.HeaderWrapper>
+  );
+}
+
+export default Header;
