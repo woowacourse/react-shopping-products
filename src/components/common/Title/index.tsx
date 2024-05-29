d3b7e9029bc1c846c53fa387f2373c0d9356520f
simhorim <hori5020@gmail.com>
2024-05-30 00:02:53 +0900

    design(Title): UI 구현

---
+++ src/components/common/Title/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './style';
+
+const Title = () => {
+  return <S.Title>bpple 상품 목록</S.Title>;
+};
+
+export default Title;
---
+++ src/components/common/Title/style.tsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const Title = styled.h1`
+  ${(props) => props.theme.typography.title}
+  color: ${(props) => props.theme.color.black};
+`;
