7152c0f5c2a0da62e04fa21c15bdcac423cd8d3c
hain-tain <hain7ce@gmail.com>
2024-05-29 17:40:15 +0900

    feat: useToggleCartItem hook 구현

--- src/hooks/useToggleCartItem.ts
+++ src/hooks/useToggleCartItem.ts
@@ -1,14 +1,70 @@
+import { deleteCartItems, getCartItems, postCartItems } from "../api/cartItems";
+import { useEffect, useState } from "react";
+
+import { CartItem } from "../types/cartItems";
+
 const useToggleCartItem = () => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<unknown>(null);
+
+  const fetchCartItems = async () => {
+    try {
+      setIsLoading(true);
+
+      const data = await getCartItems();
+      setCartItems(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCartItems();
+  }, []);
+
+  const addToCart = async (productId: number) => {
+    try {
+      setIsLoading(true);
+      await postCartItems({ productId, quantity: 1 });
+      await fetchCartItems();
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const removeFromCart = async (productId: number) => {
+    try {
+      setIsLoading(true);
+      const targetCartItemIndex = cartItems.findIndex(
+        (item) => item.product.id === productId
+      );
+      const targetCartItemId = cartItems[targetCartItemIndex].id;
+      await deleteCartItems(targetCartItemId);
+
+      setCartItems((cartItems) => {
+        const newCartItems = cartItems.filter(
+          (cartItem) => cartItem.id !== targetCartItemId
+        );
+        return newCartItems;
+      });
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return {
-    cartItems: [],
-    addToCart: (id: number) => {
-      id;
-    },
-    removeFromCart: (id: number) => {
-      id;
-    },
-    isLoading: false,
-    error: null,
+    cartItems,
+    addToCart,
+    removeFromCart,
+    isLoading,
+    error,
   };
 };
 
