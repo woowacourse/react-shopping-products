ebf329740c4f2c0703cef96ae9d9d51fb0976266
greetings1012 <ahnqwerty@gmail.com>
2024-05-29 22:49:25 +0900

    feat: 정렬 및 필터링 타입 구현
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/interfaces/Product.ts
+++ src/interfaces/Product.ts
@@ -6,10 +6,11 @@ export interface Product {
   category: string;
 }
 
-export type Category =
-  | "books"
-  | "electronics"
-  | "kitchen"
-  | "fashion"
-  | "fitness"
-  | "beverage";
+export enum Category {
+  books = "도서",
+  fitness = "운동용품",
+  beverage = "음료",
+  electronics = "전자제품",
+  kitchen = "주방용품",
+  fashion = "패션잡화",
+}
