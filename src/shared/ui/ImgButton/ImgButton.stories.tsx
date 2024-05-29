37be303bfda25a13f3fa22f43113745d2cf6844b
vi-wolhwa <cerezo00@naver.com>
2024-05-29 21:23:19 +0900

    feat(ImgButton)

--- src/shared/ui/ImgButton/ImgButton.stories.tsx
+++ src/shared/ui/ImgButton/ImgButton.stories.tsx
@@ -1,18 +1,42 @@
-import { assets } from '@/shared';
+import { assets } from '../../assets/assets';
+import { Text } from '../Text/Text';
 
 import { ImgButton } from './ImgButton';
 
-export default {
-  title: '5. Shared/ImgButton',
+import type { Meta, StoryObj } from '@storybook/react';
+import css from './ImgButton.storibook.module.css';
+
+const meta: Meta<typeof ImgButton> = {
+  title: '5. shared/ImgButton',
   component: ImgButton,
+  args: {
+    alt: 'button',
+    src: assets.cart,
+    type: 'button',
+    onClick: () => {},
+  },
 };
 
-export const Default = () => (
-  <ImgButton
-    className='example-class'
-    alt='example'
-    src={assets.cart}
-    type='button'
-    onClick={() => alert('Button clicked')}
-  />
-);
+export default meta;
+
+type Story = StoryObj<typeof ImgButton>;
+
+export const OnlyImage: Story = {};
+
+export const WithText: Story = {
+  args: {
+    children: (
+      <Text tag={'p'} type={'h1'}>
+        text
+      </Text>
+    ),
+    className: css.imgButton,
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ border: '1px solid white' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
--- src/shared/ui/ImgButton/ImgButton.tsx
+++ src/shared/ui/ImgButton/ImgButton.tsx
@@ -7,18 +7,19 @@ import css from './ImgButton.module.css';
 type ButtonType = 'button' | 'submit' | 'reset';
 
 interface ImgButtonProps {
-  children: ReactElement<TextProps>;
-  className: string;
+  children?: ReactElement<TextProps>;
+  className?: string;
   alt: string;
   src: string;
   type: ButtonType;
   onClick: () => void;
 }
 
-export const ImgButton = ({ className, alt, src, type, onClick }: ImgButtonProps) => {
+export const ImgButton = ({ children, className, alt, src, type, onClick }: ImgButtonProps) => {
   return (
     <button className={`${css.button} ${className}`} type={type} onClick={onClick}>
       <img className={css.img} alt={alt} src={src}></img>
+      {children}
     </button>
   );
 };
