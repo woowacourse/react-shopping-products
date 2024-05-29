eeaabd866ab0aaecbba2facfd973fafe49beaac5
greetings1012 <ahnqwerty@gmail.com>
2024-05-29 22:47:01 +0900

    feat: PageTitle 구현
    Co-authored-by: Todari <rhymint@gmail.com>

---
+++ src/components/common/PageTitle/PageTitle.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import PageTitle from "./PageTitle";
+
+const meta = {
+  title: "Components/PageTitle",
+  component: PageTitle,
+  tags: ["autodocs"],
+} satisfies Meta<typeof PageTitle>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Playground: Story = {};
