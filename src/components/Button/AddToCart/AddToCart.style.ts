29f4196d755ba54a84a0833e161822c4006d9848
greetings1012 <ahnqwerty@gmail.com>
2024-05-29 20:26:03 +0900

    feat(AddToCart): 장바구니에 상품 담는 버튼 구현
    Co-authored-by: Todari <rhymint@gmail.com>

---
+++ src/components/Button/AddToCart/AddToCart.style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Icon = styled.img`
+  width: 16px;
+  height: 16px;
+`;
+
+export const Content = styled.p`
+  font-family: Noto Sans;
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 15px;
+  text-align: left;
+`;
