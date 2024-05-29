3c9cc59671949f2ea4bb7a526e4d4ddff828abb2
simhorim <hori5020@gmail.com>
2024-05-29 22:41:11 +0900

    chore: img export

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as CART } from './cart.svg';
+export { default as LOGO } from './logo.svg';
+export { default as ADD_TO_CART } from './addToCart.svg';
+export { default as REMOVE_TO_CART } from './removeToCart.svg';
