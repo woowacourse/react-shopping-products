358c9529f995d0ceb6808a16f46d308aa8dbc699
vi-wolhwa <cerezo00@naver.com>
2024-05-29 21:24:09 +0900

    feat(main): add main basic css

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from '@/app/App';
+import '@/shared/styles/reset.css';
+import '@/shared/styles/color.css';
+import '@/shared/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
