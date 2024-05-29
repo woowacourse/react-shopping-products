6b86556bf00bfdd0e9d68de0b1abac6906895833
vi-wolhwa <cerezo00@naver.com>
2024-05-30 02:03:45 +0900

    feat(ProductCard)

---
+++ src/entities/product/ui/ProductCard/ProductCard.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react';
+import { Text } from '@/shared';
+import { Product } from '../../model/types';
+
+import css from './ProductCard.module.css';
+
+interface ProductCardProps {
+  actionSlot: ReactNode;
+  product: Product;
+}
+
+export const ProductCard = ({ actionSlot, product }: ProductCardProps) => {
+  return (
+    <div className={css.productCardContainer}>
+      <div className={css.imageSlot}>
+        <img className={css.image} src={product.imageUrl} alt={product.name} />
+      </div>
+      <div className={css.contentContainer}>
+        <div className={css.detailSlot}>
+          <Text tag='p' type='h2'>
+            {product.name}
+          </Text>
+          <Text tag='p' type='b1'>{`${product.price.toLocaleString()}원`}</Text>
+        </div>
+        <div className={css.actionSlot}>{actionSlot}</div>
+      </div>
+    </div>
+  );
+};
