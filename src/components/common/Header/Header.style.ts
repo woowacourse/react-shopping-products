ac13013d21c3a16647dbdd3b6b19defcce7a5726
jaeml06 <jaeml0630@gmail.com>
2024-05-29 18:00:34 +0900

    style : header 스타일 조정
    Co-Authored-By: river <130737187+0jenn0@users.noreply.github.com>

--- src/components/common/Header/Header.style.ts
+++ src/components/common/Header/Header.style.ts
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 
 export const HeaderContainer = styled.header`
+  position: sticky;
+  top: 0;
   display: flex;
   align-items: center;
   justify-content: space-between;
