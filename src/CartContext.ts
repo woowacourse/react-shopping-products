ec901c55df15208dcabe8dbe02275fcaa88fdc8d
bmj0053 <bmj05378@gmail.com>
2024-05-29 19:37:59 +0900

    remove: Context 관련 주석 제거 및 사용하지 않는 파일 삭제
    Co-authored-by: skiende74 <skiende74@gmail.com>

--- src/CartContext.ts
+++ src/CartContext.ts
@@ -1,10 +1,7 @@
 import  { createContext } from 'react';
 import useFetchAddCart from './hooks/useFetchAddCart';
 
-// interface CartState {
-//   cartIdSet: Set<number>;
-//   setCartIdSet: React.Dispatch<React.SetStateAction<Set<number>>>;
-// }
+
 export const CartContext = createContext<ReturnType<typeof useFetchAddCart>>({
   cartIdSet: new Set(),
   setCartIdSet: () => {},
