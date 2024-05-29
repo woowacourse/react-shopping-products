987121d49831032e984f32a642d339117ce676cf
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-29 13:47:11 +0900

    chore: emotion style 설치 및 설정 세팅
    Co-Authored-By: Yoonkyoung Kim <dev.yoonkyoung@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
   base: '/react-shopping-products/dist',
   resolve: {
     alias: {
