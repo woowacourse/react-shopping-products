aa3d0655f66162052d312f418b2d75500508476d
ooherin <555ohr@naver.com>
2024-05-29 20:10:50 +0900

    feat(page): useSelect 사용해서 선택한 필터로 패칭 수행
    Co-authored-by: pp449 <mma7710@naver.com>

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -3,10 +3,22 @@ import Header from "@/components/Header";
 import TextBox from "@/components/_common/TextBox";
 import styled from "styled-components";
 import SelectBox from "@/components/SelectBox";
-import { CATEGORY, SORT } from "@/constants/selectOption";
+import { CATEGORY, Category, SORT, Sort } from "@/constants/selectOption";
 import ItemCardList from "@/components/ItemCardList";
+import useSelect from "@/hooks/useSelect";
+import { useEffect } from "react";
+import useProducts from "@/hooks/useProducts";
 
 const ProductListPage = () => {
+  const useCategorySelect = useSelect<Category>("전체");
+  const useSortSelect = useSelect<Sort>("낮은 가격순");
+
+  const { products, fetchFirstPage } = useProducts();
+
+  useEffect(() => {
+    fetchFirstPage(useCategorySelect.selected, 0, useSortSelect.selected);
+  }, [useCategorySelect.selected, useSortSelect.selected]);
+
   return (
     <>
       <Header>
@@ -17,11 +29,11 @@ const ProductListPage = () => {
         <ItemInfoWrapper>
           <TextBox type="xLarge" text="bpple 상품 목록" />
           <SelectBoxWrapper>
-            <SelectBox optionsContents={Object.values(CATEGORY)} />
-            <SelectBox optionsContents={Object.values(SORT)} />
+            <SelectBox useSelector={useCategorySelect} optionsContents={Object.keys(CATEGORY)} />
+            <SelectBox useSelector={useSortSelect} optionsContents={Object.keys(SORT)} />
           </SelectBoxWrapper>
         </ItemInfoWrapper>
-        <ItemCardList />
+        <ItemCardList products={products} />
       </Wrapper>
     </>
   );
