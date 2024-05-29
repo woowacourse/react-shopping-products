f866f73846e04a05b8e6a379e9d3bc87beed3c2a
이태훈 <rhymint@gmail.com>
2024-05-30 03:44:55 +0900

    fix: DropdownProp의 optionList 타입 변경
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- src/components/common/Dropdown/Dropdown.tsx
+++ src/components/common/Dropdown/Dropdown.tsx
@@ -4,20 +4,21 @@ import chevronDown from "../../../assets/images/chevronDown.svg";
 import * as S from "./Dropdown.style";
 
 interface DropdownProps {
-  optionList: string[];
+  optionList: string[][];
   type: "category" | "sort";
 }
 
-const Dropdown = ({ optionList = [], type }: DropdownProps) => {
-  const [value, setValue] = useState(optionList[0]);
+const Dropdown = ({ optionList = [[]], type }: DropdownProps) => {
+  
+
   return (
     <S.DropdownLabel htmlFor={type}>
       <S.Dropdown>
         <S.Select id={type} name={type}>
           {optionList.map((option) => {
             return (
-              <S.Option key={option} value={option}>
-                {option}
+              <S.Option key={option[0]} value={option[1]}>
+                {option[1]}
               </S.Option>
             );
           })}
