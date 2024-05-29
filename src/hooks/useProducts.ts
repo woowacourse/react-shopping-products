345be90d939b3a201424f71e4c92594af4837e61
simhorim <hori5020@gmail.com>
2024-05-29 17:42:20 +0900

    feat(useProducts): 커스텀 훅으로 분리

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,59 +1,19 @@
-import { useEffect, useState } from 'react';
-
-import { fetchProducts } from '../api/products';
+import { useState } from 'react';
 
 import { Product } from '../types/product';
-import {
-  FIRST_PAGE,
-  FIRST_PAGE_SIZE,
-  GAP_WITH_FIRST_PAGE,
-  SIZE_PER_PAGE,
-} from '../constants/pagination';
 
-export default function useProducts() {
-  const [page, setPage] = useState(0);
-  const [category, setCategory] = useState('');
+const useProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<unknown>(null);
-  const [isLastPage, setIsLastPage] = useState(false);
 
-  useEffect(() => {
-    const getProducts = async () => {
-      try {
-        setLoading(true);
-        const size = page === FIRST_PAGE ? FIRST_PAGE_SIZE : SIZE_PER_PAGE;
-        const data = await fetchProducts(page, size, category);
+  const addProducts = (newProducts: Product[]) => {
+    setProducts((prevProducts) => [...prevProducts, ...newProducts]);
+  };
 
-        if (data.last) setIsLastPage(true);
-
-        setProducts((prevProducts) => [...prevProducts, ...data.content]);
-      } catch (error) {
-        setPage((prevPage) =>
-          page === FIRST_PAGE + GAP_WITH_FIRST_PAGE ? prevPage - GAP_WITH_FIRST_PAGE : prevPage - 1,
-        );
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (!isLastPage && !error) getProducts();
-  }, [page, category]);
-
-  const filterByCategory = (selectedCategory: string) => {
-    setPage(0);
+  const resetProducts = () => {
     setProducts([]);
-    setIsLastPage(false);
-    setCategory(selectedCategory);
   };
 
-  const fetchNextPage = () => {
-    if (!isLastPage)
-      setPage((prevPage) =>
-        prevPage === FIRST_PAGE ? prevPage + GAP_WITH_FIRST_PAGE : prevPage + 1,
-      );
-  };
+  return { products, addProducts, resetProducts };
+};
 
-  return { products, loading, error, page, fetchNextPage, filterByCategory };
-}
+export default useProducts;
