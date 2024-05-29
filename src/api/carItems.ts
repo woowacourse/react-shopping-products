890a7c506ba728e0e9d4920019b86722bfead537
greetings1012 <ahnqwerty@gmail.com>
2024-05-29 17:36:09 +0900

    refactor: api 함수 이름 변경
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/api/carItems.ts
+++ src/api/carItems.ts
@@ -5,8 +5,8 @@ const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD;
 const USER_ID = import.meta.env.VITE_USER_ID;
 const token = generateBasicToken(USER_ID, USER_PASSWORD);
 
-export async function fetchCartItemList() {
-  const response = await fetch(`${CART_ITEMS_ENDPOINT}`, {
+export async function requestFetchCartItemList() {
+  const response = await fetch(`${CART_ITEMS_ENDPOINT}?size=2000`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -22,7 +22,7 @@ export async function fetchCartItemList() {
   return data;
 }
 
-export async function addCartItem(productId: number, quantity: number) {
+export async function requestAddCartItem(productId: number, quantity: number) {
   const response = await fetch(`${CART_ITEMS_ENDPOINT}`, {
     method: "POST",
     headers: {
@@ -37,14 +37,13 @@ export async function addCartItem(productId: number, quantity: number) {
   }
 }
 
-export async function deleteCartItem(productId: number) {
-  const response = await fetch(`${CART_ITEMS_ENDPOINT}`, {
+export async function requestDeleteCartItem(cartItemId: number | undefined) {
+  const response = await fetch(`${CART_ITEMS_ENDPOINT}/${cartItemId}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
       Authorization: token,
     },
-    body: JSON.stringify({ productId }),
   });
 
   if (!response.ok) {
