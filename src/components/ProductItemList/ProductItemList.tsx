83e618f1cc166a062985eb6bc18a15bb25df6d41
이태훈 <rhymint@gmail.com>
2024-05-30 03:44:01 +0900

    fix: 주석이 제대로 달리지 않은 오류 해결
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- src/components/ProductItemList/ProductItemList.tsx
+++ src/components/ProductItemList/ProductItemList.tsx
@@ -4,12 +4,14 @@ import * as S from "./ProductItemList.style";
 
 function ProductItemList() {
   const { productList } = useProductList();
+  // prodoct fetch 확인용
+  console.log(productList)
   return (
     <S.Container>
       {productList.map((product, idx) => {
         return <ProductItem key={idx} product={product} />;
       })}
-      // 여기는 그냥 warning때문에 key를 product.id에서 idx로 바꿔줬어
+      {/* <>여기는 그냥 warning때문에 key를 product.id에서 idx로 바꿔줬어</> */}
     </S.Container>
   );
 }
