15c6024571b2cdca062742dc509567bae79af002
Kim Da Eun <midekuna@gmail.com>
2024-05-29 19:45:48 +0900

    feat: 컴포넌트 변경사항 App.tsx에 적용
    Co-Authored-By: Fe <64690761+chysis@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import Header from './components/product/Header';
+import Dropdown from './components/common/Dropdown';
 import Title from './components/common/Title';
 import Layout from './components/layout';
-import Dropdown from './components/common/Dropdown';
+import Header from './components/product/Header';
 
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import IntersectionArea from './components/common/IntersectionArea';
+import useProducts, { SortType } from './components/hooks/useProducts';
+import ProductItem from './components/product/ProductItem';
 import ProductList from './components/product/ProductList';
-import products from './mocks/products.json';
+import { CartItemsProvider } from './context/CartItemsProvider';
 
 const Wrapper = styled.div`
   display: flex;
@@ -15,44 +17,70 @@ const Wrapper = styled.div`
 
 const FilterContainer = styled.div`
   width: 100%;
-
   display: flex;
   align-items: center;
   justify-content: space-between;
 `;
 
 function App() {
-  const [category, setCategory] = useState('전체');
-  const [sort, setSort] = useState('낮은 가격 순');
+  const { products, setCategory, setSort, fetchNextPage, error, loading } =
+    useProducts();
 
   const onCategorySelect = (value: string) => {
     setCategory(value);
   };
   const onSortSelect = (value: string) => {
-    setSort(value);
+    setSort(value as SortType);
   };
 
   return (
     <Wrapper>
-      <Layout header={<Header />}>
-        <Title content={'bpple 상품 목록'} />
-        <FilterContainer>
-          <Dropdown
-            size="small"
-            value={category}
-            options={['전체', '옵션1', '옵션2', '옵션3']}
-            onSelect={onCategorySelect}
-          />
-          <Dropdown
-            size="small"
-            value={sort}
-            options={['낮은 가격순', '높은 가격순']}
-            onSelect={onSortSelect}
-          />
-        </FilterContainer>
+      <CartItemsProvider>
+        <Layout header={<Header />}>
+          <Title content={'bpple 상품 목록'} />
+          <FilterContainer>
+            <Dropdown
+              size="small"
+              defaultContent={'전체'}
+              // TODO: 상수화
+              options={[
+                { content: '전체', value: '' },
+                { content: '패션', value: 'fashion' },
+                { content: '음료', value: 'beverage' },
+                { content: '전자제품', value: 'electronics' },
+                { content: '주방가전', value: 'kitchen' },
+                { content: '운동', value: 'fitness' },
+                { content: '도서', value: 'books' },
+              ]}
+              onSelect={onCategorySelect}
+            />
+            <Dropdown
+              size="small"
+              defaultContent="낮은 가격 순"
+              options={[
+                { content: '낮은 가격 순', value: 'asc' },
+                { content: '높은 가격 순', value: 'desc' },
+              ]}
+              onSelect={onSortSelect}
+            />
+          </FilterContainer>
 
-        <ProductList items={products.slice(0, 10)} />
-      </Layout>
+          <ProductList loading={loading} error={error}>
+            {products.map((product, idx) => {
+              return idx + 1 !== products.length ? (
+                <ProductItem product={product} key={`${product.id}_${idx}`} />
+              ) : (
+                <IntersectionArea
+                  onImpression={fetchNextPage}
+                  key={`${product.id}_${idx}`}
+                >
+                  <ProductItem product={product} />
+                </IntersectionArea>
+              );
+            })}
+          </ProductList>
+        </Layout>
+      </CartItemsProvider>
     </Wrapper>
   );
 }
