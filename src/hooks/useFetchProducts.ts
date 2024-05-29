6a6f4ae897e07ca1c9d98b68c5b0a46369597f6e
simhorim <hori5020@gmail.com>
2024-05-29 20:40:58 +0900

    refactor(useFetchProducts): useProducts 훅 사용

--- src/hooks/useFetchProducts.ts
+++ src/hooks/useFetchProducts.ts
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
 
 import { fetchProducts } from '../api/products';
-
-import { Order, Product, Sort } from '../types/product';
-import { FIRST_PAGE, FIRST_PAGE_SIZE, SIZE_PER_PAGE } from '../constants/pagination';
 import usePage from './usePage';
+import useProducts from './useProducts';
+
+import { Order, Sort } from '../types/product';
 
 const useFetchProducts = () => {
-  const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<unknown>(null);
   const [isLastPage, setIsLastPage] = useState(false);
+
+  const { products, addProducts, resetProducts } = useProducts();
   const { page, increasePage, decreasePage, resetPage } = usePage();
 
   const [category, setCategory] = useState('');
@@ -18,30 +19,29 @@ const useFetchProducts = () => {
     price: 'asc',
   });
 
+  const getProducts = async () => {
+    try {
+      setLoading(true);
+      const data = await fetchProducts(page, category, sort);
+
+      if (data.last) setIsLastPage(true);
+
+      addProducts(data.content);
+    } catch (error) {
+      decreasePage();
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const getProducts = async () => {
-      try {
-        setLoading(true);
-        const size = page === FIRST_PAGE ? FIRST_PAGE_SIZE : SIZE_PER_PAGE;
-        const data = await fetchProducts(page, size, category, sort);
-
-        if (data.last) setIsLastPage(true);
-
-        setProducts((prevProducts) => [...prevProducts, ...data.content]);
-      } catch (error) {
-        decreasePage();
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     if (!isLastPage && !error) getProducts();
   }, [page, category, sort]);
 
   const filterByCategory = (selectedCategory: string) => {
     resetPage();
-    setProducts([]);
+    resetProducts();
     setIsLastPage(false);
     setCategory(selectedCategory);
   };
