07e1d0980ade22d5b27818dd428b49d355ced3d2
simhorim <hori5020@gmail.com>
2024-05-30 01:09:18 +0900

    design(ProductsContent): tag ul로 수정

--- src/components/ProductsContent/style.tsx
+++ src/components/ProductsContent/style.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const ProductsContent = styled.div`
+export const ProductsContent = styled.ul`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 20px 16px;
