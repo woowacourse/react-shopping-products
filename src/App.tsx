9712b53f256fbd91a84e13796e988a94aa97075c
rbgksqkr <rbgks1937@naver.com>
2024-05-29 15:22:23 +0900

    feat: 무한 스크롤 구현
    Co-authored-by: dle234 <213402@jnu.ac.kr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useRef } from 'react';
+
+import useProductList from './hooks/useProductList';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const {
+    productList,
+    isLoading,
+    fetchNextPage,
+    handleChangeCategory,
+    handleChangeSort,
+    order,
+    category,
+  } = useProductList();
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const onIntersect = (entries: IntersectionObserverEntry[]) => {
+      const target = entries[0];
+      if (target.isIntersecting && !isLoading) {
+        fetchNextPage();
+      }
+    };
+
+    const options = {
+      rootMargin: '0px 0px 30% 0px',
+    };
+
+    const io = new IntersectionObserver(onIntersect, options);
+
+    if (bottomRef.current) {
+      io.observe(bottomRef.current);
+    }
+
+    return () => {
+      if (bottomRef.current) {
+        io.unobserve(bottomRef.current);
+      }
+    };
+  }, [isLoading, fetchNextPage]);
 
   return (
     <>
       <h1>React Shopping Products</h1>
+      <div ref={bottomRef} style={{ height: 100, backgroundColor: 'red' }}></div>
     </>
   );
 }
