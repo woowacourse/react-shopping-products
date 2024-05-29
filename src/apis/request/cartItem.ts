bb3ad229622af0487f8af847290b2115dcc52830
brgndy <brgndy96@gmail.com>
2024-05-30 02:47:55 +0900

    refactor: 장바구니 api 관련 타입 수정
    Co-authored-by: pakxe <pigkill40@naver.com>

--- src/apis/request/cartItem.ts
+++ src/apis/request/cartItem.ts
@@ -3,10 +3,53 @@ import { BASE_URL } from '../baseUrl';
 import { ENDPOINT } from '../endpoints';
 import { requestGet, requestPost, requestDelete } from '../fetcher';
 
-export const requestCartItemList = async (): Promise<CartItem[]> => {
+type ResponseCartItemList = {
+  content: CartItem[];
+  pageable: {
+    sort: {
+      sorted: false;
+      unsorted: true;
+      empty: true;
+    };
+    pageNumber: 0;
+    pageSize: 20;
+    offset: 0;
+    paged: true;
+    unpaged: false;
+  };
+  last: true;
+  totalPages: 1;
+  totalElements: 6;
+  sort: {
+    sorted: false;
+    unsorted: true;
+    empty: true;
+  };
+  first: true;
+  number: 0;
+  numberOfElements: 6;
+  size: 20;
+  empty: false;
+};
+
+type QueryParams = {
+  page: number;
+  size: number;
+};
+
+export const requestCartItemList = async (
+  page: number = 0,
+  size: number = 20,
+): Promise<CartItem[]> => {
+  const queryParams: QueryParams = {
+    page,
+    size,
+  };
+
   const data = await requestGet<ResponseCartItemList>({
     baseUrl: BASE_URL.SHOP,
     endpoint: ENDPOINT.CART_LIST,
+    queryParams,
   });
 
   return data.content;
