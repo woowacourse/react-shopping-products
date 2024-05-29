a0936aebf078fc5e6ecf411a7d28122c173445fb
badahertz52 <badahertz52@gmail.com>
2024-05-29 15:10:43 +0900

    feat: 상품 목록 핉터링 기능 추가
    Co-Authored-By: JinHo Kim <81083461+jinhokim98@users.noreply.github.com>

--- src/pages/ProductListPage/index.tsx
+++ src/pages/ProductListPage/index.tsx
@@ -1,15 +1,30 @@
 import { fetchProduct } from '@apis/index';
+import { Filtering } from '@appTypes/index';
 import { Dropdown } from '@components/index';
+import { CATEGORY_OPTIONS, PRICE_SORT_OPTIONS } from '@constants/index';
+import { useEffect, useState } from 'react';
 
 import ProductList from './ProductList';
 import style from './style.module.css';
 
 function ProductListPage() {
+  const [filtering, setFiltering] = useState<Filtering>({ category: '', sort: 'price,asc' });
+
   const getProducts = async () => {
     const result = await fetchProduct(filtering);
 
     return result;
   };
+
+  const handleChangeOption = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = event.target;
+
+    setFiltering((prev) => ({ ...prev, [name]: value }));
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [filtering]);
 
   return (
     <div>
