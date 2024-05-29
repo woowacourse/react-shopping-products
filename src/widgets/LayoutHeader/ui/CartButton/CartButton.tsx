7ae0e82bee10480aa3b1d2556281af89907f9c62
vi-wolhwa <cerezo00@naver.com>
2024-05-29 17:37:40 +0900

    refactor(CartButton): add temporary click event handler

--- src/widgets/LayoutHeader/ui/CartButton/CartButton.tsx
+++ src/widgets/LayoutHeader/ui/CartButton/CartButton.tsx
@@ -7,9 +7,14 @@ interface CartButtonProps {
 }
 
 export const CartButton = ({ cartItemCount }: CartButtonProps) => {
+  // FIXME: This will route to home page
+  const handleOnClick = () => {
+    console.log('clicked');
+  };
+
   return (
     <div className={css.cartButtonContainer}>
-      <ImgButton className={css.button} alt='cart' src={assets.cart} type={'button'} onClick={() => {}} />;
+      <ImgButton className={css.button} alt='cart' src={assets.cart} type={'button'} onClick={handleOnClick} />;
       <div className={css.countWrapper}>
         <p className={css.count}>{cartItemCount}</p>
       </div>
