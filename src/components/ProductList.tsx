17c1de97b7cbc047305b993671e0b136d984e0e0
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-29 22:28:14 +0900

    style: 아이템 리스트 내 overflow scroll 추가
    Co-Authored-By: Yoonkyoung Kim <dev.yoonkyoung@gmail.com>

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,26 +1,16 @@
 import { Product } from '@/types/product.type';
 import ProductItem from './ProductItem';
 import styled from '@emotion/styled';
-import { useState } from 'react';
 
 interface Props {
   products: Product[];
 }
 
 const ProductList = ({ products }: Props) => {
-  // TODO: id 당으로 변경
-  const [isSelected, setIsSelected] = useState<boolean[]>(
-    Array(products.length).fill(true)
-  );
-
   return (
     <S.ListContainer>
-      {products.map((product, index) => (
-        <ProductItem
-          key={product.id}
-          item={product}
-          isSelected={isSelected[index]}
-        />
+      {products.map((product) => (
+        <ProductItem key={product.id} item={product} />
       ))}
     </S.ListContainer>
   );
@@ -30,15 +20,15 @@ export default ProductList;
 
 const S = {
   ListContainer: styled.div`
-    width: 100%;
     display: grid;
     grid-template-columns: 1fr 1fr;
     flex-grow: 1;
     gap: 20px;
     justify-items: center;
+    width: 100%;
     height: calc(100vh - 200px);
-    overflow-y: auto;
-    z-index: -1;
     margin-top: 24px;
+    z-index: -1;
+    overflow-y: auto;
   `,
 };
