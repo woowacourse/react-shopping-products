0ceb0fb3291790983b5ae24052d4f057cfefe75c
hanyoung <phanyoung@naver.com>
2024-05-29 20:37:22 +0900

    feat(useIntersectionObserver): 커스텀 훅 생성
    Co-authored-by: ImxYJL <allensain14@gmail.com>

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+interface UseIntersectionObserver<T> {
+  setTarget: (target: T) => void;
+}
+
+const useIntersectionObserver = <T extends Element>(
+  callback: IntersectionObserverCallback
+): UseIntersectionObserver<T> => {
+  const [target, setTarget] = useState<Element | null>(null);
+
+  useEffect(() => {
+    if (!target) return;
+
+    const observer = new IntersectionObserver(callback);
+    observer.observe(target);
+
+    return () => observer.unobserve(target);
+  }, [callback, target]);
+
+  return { setTarget: (target: T) => setTarget(target) };
+};
+
+export default useIntersectionObserver;
