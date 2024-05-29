070b966f0054ce06c1d0f1a38a51b87cfd42822a
ooherin <555ohr@naver.com>
2024-05-29 15:00:46 +0900

    fix(selectBox): selectBox style 분리
    Co-authored-by: pp449 <mma7710@naver.com>

--- src/components/SelectBox/index.tsx
+++ src/components/SelectBox/index.tsx
@@ -1,6 +1,5 @@
 import useSelect from "@/hooks/useSelect";
-import styled from "styled-components";
-
+import * as S from "@/components/SelectBox/style";
 interface SelectBoxProps {
   optionsContents: string[];
 }
@@ -9,85 +8,22 @@ const SelectBox = ({ optionsContents }: SelectBoxProps) => {
   const { isDropdown, handleDropdown, selected, handleSelected } = useSelect(optionsContents[0]);
 
   return (
-    <Select onClick={handleDropdown}>
-      <Arrow isDropdown={!!isDropdown} />
-      <SelectedCardWrapper>
-        <SelectedCardText isSelected={!!selected}>{selected}</SelectedCardText>
-      </SelectedCardWrapper>
+    <S.Select onClick={handleDropdown}>
+      <S.Arrow isDropdown={!!isDropdown} />
+      <S.SelectedCardWrapper>
+        <S.SelectedCardText isSelected={!!selected}>{selected}</S.SelectedCardText>
+      </S.SelectedCardWrapper>
       {isDropdown && (
-        <OptionWrapper>
+        <S.OptionWrapper>
           {optionsContents.map((optionContent) => (
-            <Option key={optionContent} onClick={handleSelected}>
+            <S.Option key={optionContent} onClick={handleSelected}>
               {optionContent}
-            </Option>
+            </S.Option>
           ))}
-        </OptionWrapper>
+        </S.OptionWrapper>
       )}
-    </Select>
+    </S.Select>
   );
 };
 
 export default SelectBox;
-
-const Select = styled.ul`
-  position: relative;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  height: 36px;
-  font-size: 10px;
-
-  box-sizing: border-box;
-  border-radius: 3px;
-  padding: 8px;
-  margin-bottom: 3px;
-  border: 1px solid ${({ theme }) => theme.COLOR["grey2"]};
-  border-radius: 8px;
-`;
-
-const SelectedCardWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const SelectedCardText = styled.p<{ isSelected: boolean }>`
-  display: flex;
-  align-items: center;
-  color: ${(props) => (props.isSelected ? "black" : "grey")};
-`;
-
-const OptionWrapper = styled.div`
-  position: absolute;
-  left: 0;
-  top: 38px;
-  background: white;
-  width: 100%;
-  border: 1px solid grey;
-  border-radius: 5px;
-  max-height: 20vh;
-  overflow: scroll;
-`;
-
-const Option = styled.li`
-  padding: 8px;
-
-  &:hover {
-    background: grey;
-    transition: 0.1s;
-  }
-`;
-
-const Arrow = styled.div<{ isDropdown: boolean }>`
-  position: absolute;
-  top: ${({ isDropdown }) => (isDropdown ? "40%" : "30%")};
-  right: 10px;
-  transform: translate(-50%, -50%);
-  width: 8px;
-  height: 8px;
-  box-sizing: border-box;
-  border-top: 2px solid black;
-  border-right: 2px solid black;
-  transform: ${(props) => (props.isDropdown ? "rotate(315deg)" : "rotate(135deg)")};
-  transition: 0.3s;
-`;
