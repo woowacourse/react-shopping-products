15854a5d25997c379a18f1fe052e539499d702ec
00kang <kdb070970@gmail.com>
2024-05-29 17:45:02 +0900

    feat : 상품 목록 조회 중 에러 발생 상태 useProducts 커스텀 훅에 추가
    Co-Authored-By: soosoo22 <80167893+soosoo22@users.noreply.github.com>

--- src/hooks/useProducts.test.ts
+++ src/hooks/useProducts.test.ts
@@ -1,4 +1,7 @@
 import { renderHook, waitFor } from "@testing-library/react";
+import { HttpResponse, http } from "msw";
+import { PRODUCTS_ENDPOINT } from "../api/endpoints";
+import { server } from "../mocks/server";
 import useProducts from "./useProducts";
 
 describe("useProducts", () => {
@@ -15,6 +18,22 @@ describe("useProducts", () => {
       const { result } = renderHook(() => useProducts());
 
       expect(result.current.isLoading).toBe(true);
+    });
+
+    it.each([[400], [404], [500]])("상품 목록 조회 중 %i 에러가 발생한다.", async (status) => {
+      server.use(
+        http.get(PRODUCTS_ENDPOINT, () => {
+          return new HttpResponse(null, { status });
+        })
+      );
+
+      const { result } = renderHook(() => useProducts());
+
+      await waitFor(() => {
+        expect(result.current.products).toEqual([]);
+        expect(result.current.isLoading).toBe(false);
+        expect(result.current.error).toBeTruthy();
+      });
     });
   });
 });
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,25 +1,38 @@
 import { useEffect, useState } from "react";
 import { PRODUCTS_ENDPOINT } from "../api/endpoints";
+import { useError } from "../context/errorContext";
 
 interface UseProductsResult {
   products: ProductProps[];
   isLoading: boolean;
+  error: Error | null;
 }
 
 export default function useProducts(): UseProductsResult {
   const [products, setProducts] = useState<ProductProps[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+  const { setErrorStatus } = useError();
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch(PRODUCTS_ENDPOINT);
-      const data = await response.json();
-      setProducts(data.content);
-      setIsLoading(false);
+      try {
+        const response = await fetch(PRODUCTS_ENDPOINT);
+        if (!response.ok) {
+          setErrorStatus(response.status);
+          throw new Error("에러 발생");
+        }
+        const data = await response.json();
+        setProducts(data.content);
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     fetchProducts();
-  }, []);
+  }, [setErrorStatus]);
 
-  return { products, isLoading };
+  return { products, isLoading, error };
 }
