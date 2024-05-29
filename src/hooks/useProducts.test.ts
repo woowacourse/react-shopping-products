8dbae247752c1463d98744f0d5dd0b13ec3f10b4
ooherin <555ohr@naver.com>
2024-05-29 21:30:03 +0900

    test(useProducts): useProducts 훅 테스트 완료
    Co-authored-by: pp449 <mma7710@naver.com>

---
+++ src/hooks/useProducts.test.ts
@@ -0,0 +1,60 @@
+import { END_POINT } from "@/config/endPoint";
+import SERVER_URL from "@/config/serverUrl";
+import useProducts from "@/hooks/useProducts";
+import { server } from "@/mocks/servers";
+import { renderHook, waitFor } from "@testing-library/react";
+import { HttpResponse, http } from "msw";
+import { act } from "react";
+
+describe("useProducts 테스트", () => {
+  it("상품 목록을 조회한다.", async () => {
+    const { result } = renderHook(() => useProducts());
+
+    await waitFor(() => {
+      expect(result.current.products).toHaveLength(20);
+    });
+  });
+
+  it("상품 목록 조회 중 로딩 상태", () => {
+    const { result } = renderHook(() => useProducts());
+
+    expect(result.current.loading).toBe(true);
+  });
+
+  it("상품 목록 조회 중 에러 상태", async () => {
+    server.use(
+      http.get(SERVER_URL.apiUrl + END_POINT.products, () => {
+        return new HttpResponse(JSON.stringify({ ok: false }), { status: 500 });
+      })
+    );
+
+    const { result } = renderHook(() => useProducts());
+
+    act(() => {
+      result.current.fetchFirstPage("건강", 1, "낮은 가격순");
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.error).toBeTruthy();
+    });
+  });
+
+  it("다음 페이지의 상품 4개를 추가로 불러온다", async () => {
+    const { result } = renderHook(() => useProducts());
+
+    await waitFor(() => {
+      expect(result.current.products).toHaveLength(20);
+      expect(result.current.currentPage).toBe(1);
+    });
+
+    act(() => {
+      result.current.fetchNextPage("도서", 1, "낮은 가격순");
+    });
+
+    await waitFor(() => {
+      expect(result.current.products).toHaveLength(24);
+      expect(result.current.currentPage).toBe(2);
+    });
+  });
+});
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -6,23 +6,51 @@ import { Category, Sort } from "@/constants/selectOption";
 const useProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [currentPage, setCurrentPage] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
 
   const fetchFirstPage = async (category: Category, page: number, sort: Sort) => {
-    const res = await getProducts({ category, page, sort });
-    setProducts(res.content);
+    try {
+      setLoading(true);
+
+      const size = currentPage === 0 ? 20 : 4;
+      const res = await getProducts({ category, page, size, sort });
+
+      setProducts(res.content);
+      setLoading(false);
+      setCurrentPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error);
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   const fetchNextPage = async (category: Category, page: number, sort: Sort) => {
-    setCurrentPage((prevPage) => prevPage + 1);
-    const res = await getProducts({ category, page, sort });
-    setProducts((prevProducts) => [...prevProducts, ...res.content]);
+    try {
+      setLoading(true);
+
+      const size = currentPage === 0 ? 20 : 4;
+
+      const res = await getProducts({ category, page, size, sort });
+      setProducts((prevProducts) => [...prevProducts, ...res.content]);
+      setCurrentPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error);
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     fetchFirstPage("전체", currentPage, "낮은 가격순");
   }, []);
 
-  return { products, fetchFirstPage, fetchNextPage };
+  return { products, fetchFirstPage, fetchNextPage, loading, error, currentPage };
 };
 
 export default useProducts;
