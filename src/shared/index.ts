570a89aa63351d79aee2f2a45b17daa58eb16b17
vi-wolhwa <cerezo00@naver.com>
2024-05-29 23:11:23 +0900

    feat(Select)

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,4 +1,5 @@
 export { assets } from './assets/assets';
 
 export { ImgButton } from './ui/ImgButton/ImgButton';
+export { Select, type SelectOption } from './ui/Select/Select';
 export { Text, type TextProps } from './ui/Text/Text';
---
+++ src/shared/ui/Select/Select.tsx
@@ -0,0 +1,29 @@
+export interface SelectOption {
+  value: string;
+  label: string;
+}
+
+interface SelectProps {
+  className?: string;
+  options: SelectOption[];
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export const Select = ({ className, options, value, onChange }: SelectProps) => {
+  const handleOnChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(event.target.value);
+  };
+
+  return (
+    <select className={className} value={value} onChange={handleOnChange}>
+      {options.map((option, index) => (
+        <option key={index} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Select;
