34a8fc641366059652b9cb9b6181e2026cf97733
simhorim <hori5020@gmail.com>
2024-05-29 14:53:29 +0900

    feat(handlers): 카테고리 별 mock data 반환하는 기능 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,13 @@
 import { HttpResponse, http } from 'msw';
+
 import products from './products.json';
+import electronics from './electronics.json';
+import beverages from './beverages.json';
+import fashions from './fashions.json';
+import books from './books.json';
+import fitness from './fitness.json';
+import kitchens from './kitchens.json';
+
 import { PRODUCTS_ENDPOINT } from '../api/endpoints';
 import {
   FIRST_PAGE,
@@ -8,13 +16,36 @@ import {
   MOCK_PRODUCTS_LAST_PAGE,
   SIZE_PER_PAGE,
 } from '../constants/pagination';
+import { Category } from '../types/product';
+
+const filterByCategory = (category: Category) => {
+  switch (category) {
+    case 'electronics':
+      return electronics;
+    case 'beverage':
+      return beverages;
+    case 'fashion':
+      return fashions;
+    case 'books':
+      return books;
+    case 'fitness':
+      return fitness;
+    case 'kitchen':
+      return kitchens;
+
+    default:
+      return products;
+  }
+};
 
 export const handlers = [
   http.get(PRODUCTS_ENDPOINT, ({ request }) => {
     const url = new URL(request.url);
 
     const page = Number(url.searchParams.get('page') || '0');
+    const category = (url.searchParams.get('category') || '') as Category;
     const size = page === FIRST_PAGE ? FIRST_PAGE_SIZE : SIZE_PER_PAGE;
+
     const start =
       page === FIRST_PAGE
         ? FIRST_PAGE
@@ -22,7 +53,9 @@ export const handlers = [
     const end = start + size;
     const last = page === MOCK_PRODUCTS_LAST_PAGE;
 
-    const paginatedProducts = products.slice(start, end);
+    const filteredProducts = filterByCategory(category);
+
+    const paginatedProducts = filteredProducts.slice(start, end);
 
     return HttpResponse.json({ content: paginatedProducts, last });
   }),
