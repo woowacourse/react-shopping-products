d8276981393e09bf55e87ae72543c26e92aa5c40
simhorim <hori5020@gmail.com>
2024-05-29 15:02:26 +0900

    feat(fetchProducts): params에 카테고리 추가

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,14 +1,23 @@
 import { ProductsResponseData } from '../types/product';
 import { PRODUCTS_ENDPOINT } from './endpoints';
 
-export async function fetchProducts(page: number, size: number): Promise<ProductsResponseData> {
-  const response = await fetch(`${PRODUCTS_ENDPOINT}?page=${page}&size=${size}`);
+export async function fetchProducts(
+  page: number,
+  size: number,
+  category = '',
+): Promise<ProductsResponseData> {
+  const params = new URLSearchParams();
+  if (category !== '') params.append('category', category);
+
+  params.append('page', String(page));
+  params.append('size', String(size));
+
+  const response = await fetch(`${PRODUCTS_ENDPOINT}?${params.toString()}`);
 
   if (!response.ok) {
     throw new Error('Failed to fetch products');
   }
 
   const data = await response.json();
-
   return data;
 }
