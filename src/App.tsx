2f2732b5443502163afe35c3507bfbbd6f4322c4
greetings1012 <ahnqwerty@gmail.com>
2024-05-30 17:19:07 +0900

    fix: 빌드 오류 수정
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import Product from "./pages/Product";
 import GlobalStyles from "./styles/Global.style";
