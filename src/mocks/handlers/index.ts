042b0698612dc3293bb9d799d571e6e5db5ccbd8
badahertz52 <badahertz52@gmail.com>
2024-05-30 14:46:49 +0900

    test: 장바구니 담기/빼기 테스트
    Co-Authored-By: JinHo Kim <81083461+jinhokim98@users.noreply.github.com>

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,5 +1,6 @@
+import cartHandler from './cart';
 import productsHandler from './products';
 
-const handlers = [...productsHandler];
+const handlers = [...productsHandler, ...cartHandler];
 
 export default handlers;
