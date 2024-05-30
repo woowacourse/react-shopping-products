25dfa12900d7647dbcac1c8fe48207830b8690a5
손진영 <sm099@naver.com>
2024-05-30 15:26:05 +0900

    chore: vite.config.ts 내 svgr 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [
       { find: '@src', replacement: resolve(__dirname, 'src') },
