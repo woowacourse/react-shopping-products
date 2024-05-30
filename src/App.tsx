b92496029cb929fbf4cd6d55fa67969f2d133cf9
손진영 <sm099@naver.com>
2024-05-30 10:52:50 +0900

    refactor: ToastProvider를 App에 감싸는 형태로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ProductPage from '@pages/ProductPage/ProductPage';
 import { resetCSS } from '@styles/resetCSS';
 
 import useCheckedIds from '@hooks/product/useCheckedId';
+
 function App() {
   const { length, getIsCheckedId, toggleId } = useCheckedIds();
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import ToastProvider from '@components/common/Toast/provider/ToastProvider.tsx';
+import { SIZE } from '@styles/style.constant.ts';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <div
+    style={{
+      width: SIZE.layoutWidth,
+      margin: 'auto',
+    }}
+  >
+    <ToastProvider>
+      <App />
+    </ToastProvider>
+  </div>
 );
