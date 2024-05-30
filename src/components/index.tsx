b9088890dc2b6ffade9edf6e5b6d2e019e3fa68e
badahertz52 <badahertz52@gmail.com>
2024-05-30 15:41:07 +0900

    feat: 모달 기능 추가
    Co-Authored-By: JinHo Kim <81083461+jinhokim98@users.noreply.github.com>

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
+export { default as ToastModal } from './Modal/ToastModal';
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
 export { default as Dropdown } from './Dropdown';
