7c8a38fdcc15c6cce30ce8d4e7e2f67ac6f03f1c
jayming66 <mjw8941@gmail.com>
2024-05-30 17:40:16 +0900

    test(useProducts.test.ts): 초기 렌더링 시, 전체 목록 중 20개를 가져오는 테스트 작성
    Co-Authored-By: Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>

--- src/hooks/useProducts.test.ts
+++ src/hooks/useProducts.test.ts
@@ -1,10 +1,18 @@
+import { ToastProvider } from './../context/ToastProvider';
 import { renderHook, waitFor } from '@testing-library/react';
-import useProducts from './useProducts';
+import { useProductFetch } from './useProductFetch';
 
 describe('useProducts', () => {
   describe('상품 목록 조회', () => {
     it('초기 렌더링 시, 전체 목록 중 20개의 상품 목록을 가져온다.', async () => {
-      const { result } = renderHook(() => useProducts());
+      const selectBarCondition = {
+        category: 'all',
+        sort: 'priceAsc',
+      };
+      const { result } = renderHook(() => useProductFetch({ selectBarCondition }), {
+        wrapper: ToastProvider,
+      });
+
       await waitFor(() => {
         expect(result.current.products).toHaveLength(20);
       });
