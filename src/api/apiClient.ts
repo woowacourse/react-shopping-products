2960201b7f7531029a00eb432518251d41329036
anttiey <hanuuny@gmail.com>
2024-05-30 10:50:34 +0900

    refactor: api 함수 구조 분리
    Co-authored-by: 최진실 <chlwlstlf@users.noreply.github.com>

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,6 +1,6 @@
 import { generateBasicToken } from '../utils/auth';
+import { API_URL } from '../constants/api';
 
-export const API_URL = import.meta.env.VITE_API_URL;
 const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD;
 const USER_ID = import.meta.env.VITE_USER_ID;
 
@@ -17,46 +17,44 @@ interface RequestProps extends ApiProps {
   method: Method;
 }
 
+const createRequestInit = (method: Method, headers: Record<string, string>, body: object | null): RequestInit => {
+  const token = generateBasicToken(USER_ID, USER_PASSWORD);
+
+  return {
+    method,
+    headers: { ...headers, Authorization: token, 'Content-Type': 'application/json' },
+    body: body ? JSON.stringify(body) : null,
+  };
+};
+
+const fetchWithErrorHandling = async (endpoint: string, requestInit: RequestInit, errorMessage: string = '') => {
+  const response = await fetch(`${API_URL}${endpoint}`, requestInit);
+
+  if (!response.ok) {
+    throw new Error(errorMessage);
+  }
+
+  const text = await response.text();
+
+  return text ? JSON.parse(text) : response;
+};
+
 const apiClient = {
-  get({ endpoint, headers = {}, errorMessage = '' }: ApiProps) {
-    return this.request({ method: 'GET', endpoint, headers, errorMessage });
+  get: ({ endpoint, headers = {}, errorMessage = '' }: ApiProps) => {
+    return apiClient.request({ method: 'GET', endpoint, headers, errorMessage });
   },
-  post({ endpoint, headers = {}, body = {}, errorMessage = '' }: ApiProps) {
-    return this.request({ method: 'POST', endpoint, headers, body, errorMessage });
+  post: ({ endpoint, headers = {}, body = {}, errorMessage = '' }: ApiProps) => {
+    return apiClient.request({ method: 'POST', endpoint, headers, body, errorMessage });
   },
-  patch({ endpoint, headers = {}, body = {}, errorMessage = '' }: ApiProps) {
-    return this.request({ method: 'PATCH', endpoint, headers, body, errorMessage });
+  patch: ({ endpoint, headers = {}, body = {}, errorMessage = '' }: ApiProps) => {
+    return apiClient.request({ method: 'PATCH', endpoint, headers, body, errorMessage });
   },
-  delete({ endpoint, headers = {}, errorMessage = '' }: ApiProps) {
-    return this.request({ method: 'DELETE', endpoint, headers, errorMessage });
+  delete: ({ endpoint, headers = {}, errorMessage = '' }: ApiProps) => {
+    return apiClient.request({ method: 'DELETE', endpoint, headers, errorMessage });
   },
-
-  request({ method, endpoint, headers = {}, body = null, errorMessage = '' }: RequestProps) {
-    const token = generateBasicToken(USER_ID, USER_PASSWORD);
-
-    const requestInit = {
-      method,
-      headers: { ...headers, Authorization: token, 'Content-Type': 'application/json' },
-      body: body ? JSON.stringify(body) : null,
-    };
-
-    return this.fetchWithErrorHandling(endpoint, requestInit, errorMessage);
-  },
-
-  async fetchWithErrorHandling(endpoint: string, requestInit: RequestInit, errorMessage: string) {
-    const response = await fetch(`${API_URL}${endpoint}`, requestInit);
-
-    if (!response.ok) {
-      throw new Error(errorMessage);
-    }
-
-    const text = await response.text();
-
-    if (text === '') return;
-
-    const data = await JSON.parse(text);
-
-    return data;
+  request: async ({ method, endpoint, headers = {}, body = null, errorMessage = '' }: RequestProps) => {
+    const requestInit = createRequestInit(method, headers, body);
+    return await fetchWithErrorHandling(endpoint, requestInit, errorMessage);
   },
 };
 
--- src/api/product.ts
+++ src/api/product.ts
@@ -6,24 +6,27 @@ interface ProductApi {
   isLast: boolean;
 }
 
-export const fetchProducts = async (
-  category: string,
-  page: number,
-  size: number,
-  sort: string,
-): Promise<ProductApi> => {
-  const queryParameters = new URLSearchParams({
+const makeParams = (category: string, page: number, size: number, sort: string): string => {
+  const params = new URLSearchParams({
     page: page.toString(),
     size: (page === 0 ? 20 : size).toString(),
     sort: `price,${sort}`,
   });
 
   if (category && category !== 'all') {
-    queryParameters.append('category', category);
+    params.append('category', category);
   }
 
-  const endpoint = `/products?${queryParameters.toString()}`;
+  return params.toString();
+};
 
+export const fetchProducts = async (
+  category: string,
+  page: number,
+  size: number,
+  sort: string,
+): Promise<ProductApi> => {
+  const endpoint = `/products?${makeParams(category, page, size, sort)}`;
   const data = await apiClient.get({ endpoint, errorMessage: '상품 목록을 불러오는 중 에러가 발생했습니다.' });
 
   return { data: data.content, isLast: data.last };
