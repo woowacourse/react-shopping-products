eb57c1fdfb20b44cbe95aa72d035dba22b44fbbb
00kang <kdb070970@gmail.com>
2024-05-30 17:05:21 +0900

    feat : CartButton 컴포넌트에 카운트 보여주기
    Co-Authored-By: soosoo22 <80167893+soosoo22@users.noreply.github.com>

--- src/components/Button/CartButton.tsx
+++ src/components/Button/CartButton.tsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
+import { getCartCounts } from "../../api/cart";
 import { ShoppingCartIcon } from "../../assets";
 import { BaseButton } from "./BaseButton";
 import { StyledCartButtonImg, StyledCartCount, StyledContainer } from "./CartButton.styled";
 
-export const CartButton = ({ onClick }: { onClick: () => void }) => {
-  const countNumber = 0;
+interface CartButtonProps {
+  onClick?: () => void;
+}
+
+export const CartButton = ({ onClick = () => {} }: CartButtonProps) => {
+  const [productCount, setProductCount] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchCartCounts = async () => {
+      try {
+        const count = await getCartCounts();
+        setProductCount(count);
+      } catch (error) {
+        console.error("Failed to fetch cart counts", error);
+        setProductCount(null);
+      }
+    };
+
+    fetchCartCounts();
+  }, [productCount]);
 
   return (
     <BaseButton onClick={onClick}>
       <StyledContainer>
         <StyledCartButtonImg src={ShoppingCartIcon} />
-        {countNumber && <StyledCartCount>{countNumber}</StyledCartCount>}
+        {productCount !== null && <StyledCartCount>{productCount}</StyledCartCount>}
       </StyledContainer>
     </BaseButton>
   );
