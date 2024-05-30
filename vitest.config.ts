8cdf36838495d6535ae484bb8d0ccef5934a01fe
badahertz52 <badahertz52@gmail.com>
2024-05-30 16:08:47 +0900

    fix: vitest.config.ts 오류 수정

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,26 @@
-import { mergeConfig } from 'vite';
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 
-import viteConfig from './vite.config.ts';
-
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['./vitest.setup.ts'],
-    },
-  }),
-);
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+  },
+  resolve: {
+    alias: [
+      { find: '@src', replacement: resolve(__dirname, 'src') },
+      { find: '@components', replacement: resolve(__dirname, 'src/components') },
+      { find: '@contexts', replacement: resolve(__dirname, 'src/contexts') },
+      { find: '@styles', replacement: resolve(__dirname, 'src/styles') },
+      { find: '@assets', replacement: resolve(__dirname, 'src/assets') },
+      { find: '@constants', replacement: resolve(__dirname, 'src/constants') },
+      { find: '@utils', replacement: resolve(__dirname, 'src/utils') },
+      { find: '@mocks', replacement: resolve(__dirname, 'src/mocks') },
+      { find: '@hooks', replacement: resolve(__dirname, 'src/hooks') },
+      { find: '@pages', replacement: resolve(__dirname, 'src/pages') },
+      { find: '@apis', replacement: resolve(__dirname, 'src/apis') },
+      { find: '@appTypes', replacement: resolve(__dirname, 'src/appTypes') },
+    ],
+  },
+});
