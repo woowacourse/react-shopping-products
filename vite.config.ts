5c2d00f3f089d103fde873688d44b7e103718e2f
novice0840 <magnolia0840@gmail.com>
2024-05-30 17:02:08 +0900

    chore: github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
+  base: "/react-shopping-products/",
 });
