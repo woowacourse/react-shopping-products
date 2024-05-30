ea57971d29a43fda876521859675ab0119ec4d92
손진영 <sm099@naver.com>
2024-05-30 15:35:56 +0900

    fix: 드롭다운 내 같은 옵션을 선택했을 때 무한 로딩이 걸려버리는 문제 해결

--- src/hooks/product/useSelectProductDropdown.ts
+++ src/hooks/product/useSelectProductDropdown.ts
@@ -1,20 +1,26 @@
-import { Product } from '@appTypes/product';
 import { PRODUCT_CATEGORY_MAP } from '@components/product/CategoryDropdown/CategoryDropdown.constant';
 import { PRODUCT_SORT_MAP } from '@components/product/SortDropdown/SortDropdown.constant';
+import { Product } from '@appTypes/product';
 import { useState } from 'react';
 
 const useSelectProductDropdown = (onResetPage: () => void, onResetProducts: () => void) => {
   const [sortType, setSortType] = useState<keyof typeof PRODUCT_SORT_MAP>('asc');
   const [category, setCategory] = useState<Product['category'] | 'all'>('all');
 
-  const onSelectSortTypeOption = (sortType: keyof typeof PRODUCT_SORT_MAP) => {
-    setSortType(sortType);
+  const onSelectSortTypeOption = (selectSortType: keyof typeof PRODUCT_SORT_MAP) => {
+    setSortType(selectSortType);
+
+    if (selectSortType === sortType) return;
+
     onResetPage();
     onResetProducts();
   };
 
-  const onSelectCategoryOption = (category: keyof typeof PRODUCT_CATEGORY_MAP) => {
-    setCategory(category);
+  const onSelectCategoryOption = (selectCategory: keyof typeof PRODUCT_CATEGORY_MAP) => {
+    setCategory(selectCategory);
+
+    if (selectCategory === category) return;
+
     onResetPage();
     onResetProducts();
   };
