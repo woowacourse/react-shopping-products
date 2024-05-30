5ccbbafd394b09406dd607c05ee7942148e265a7
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-30 15:21:01 +0900

    feat: 에러 토스트 컴포넌트
    Co-Authored-By: Yoonkyoung Kim <dev.yoonkyoung@gmail.com>

---
+++ src/components/Toast.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+import { theme } from '@/style/theme.style';
+
+interface Props {
+  message: string;
+}
+
+const Toast = ({ message }: Props) => {
+  return <S.Toast>{message}</S.Toast>;
+};
+
+export default Toast;
+
+const S = {
+  Toast: styled.div`
+    position: absolute;
+    top: 60px;
+    width: 100%;
+    padding: 10px;
+    background-color: ${theme.color.lightRed};
+    text-align: center;
+    box-sizing: border-box;
+    transition: opacity 0.5s ease-in-out;
+    font-size: ${theme.fontSize.xsmall};
+    font-weight: ${theme.fontWeight.normal};
+  `,
+};
