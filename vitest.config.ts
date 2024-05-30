bb8d17f0707cf2e148ed0e6b9141f656d78d1acb
brgndy <brgndy96@gmail.com>
2024-05-30 16:28:10 +0900

    chore: 패키지 및 config 수정
    Co-authored-by: pakxe <pigkill40@naver.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,15 @@ import { mergeConfig } from 'vite';
 import { defineConfig } from 'vitest/config';
 import viteConfig from './vite.config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['./vitest.setup.ts'],
-    },
-  }),
+export default defineConfig((env) =>
+  mergeConfig(
+    viteConfig(env),
+    defineConfig({
+      test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./vitest.setup.ts'],
+      },
+    }),
+  ),
 );
