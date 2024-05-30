d9921b7e37cf913cb03c7c1f3a30c8db24dbd08f
simhorim <hori5020@gmail.com>
2024-05-30 15:37:29 +0900

    feat(ProductItem): 장바구니 담기 빼기 기능 구현

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -1,19 +1,42 @@
 import * as S from './style';
 
 import { ADD_TO_CART, REMOVE_TO_CART } from '../../assets/images';
+import useCart from '../../hooks/useCart';
+import { useEffect, useState } from 'react';
 
 interface ProductItemProps {
-  isInCart: boolean;
+  id: number;
   imageUrl: string;
   name: string;
   price: number;
+  useCartProp: () => ReturnType<typeof useCart>;
 }
 
-const ProductItem = ({ isInCart, imageUrl, name, price }: ProductItemProps) => {
-  const TOGGLE_BUTTON_ICON = isInCart ? REMOVE_TO_CART : ADD_TO_CART;
-  const BUTTON_TEXT = isInCart ? '담기' : '빼기';
+const ProductItem = ({ id, useCartProp, imageUrl, name, price }: ProductItemProps) => {
+  const { cartItems, isInCarts, addCart, deleteCart, CartItemsLoading, CartItemsError } =
+    useCartProp();
+  const cartItem = cartItems.find(({ product }) => id === product.id);
 
-  const onClick = () => console.log('click toggle button');
+  const [isInCart, setIsInCart] = useState(false);
+
+  useEffect(() => {
+    if (cartItem) {
+      setIsInCart(true);
+    }
+  }, [cartItems]);
+
+  const TOGGLE_BUTTON_ICON = isInCart ? REMOVE_TO_CART : ADD_TO_CART;
+  const BUTTON_TEXT = isInCart ? '빼기' : '담기';
+
+  const onToggle = () => {
+    if (cartItem && isInCart) {
+      deleteCart(cartItem.id);
+      setIsInCart(false);
+    } else {
+      addCart(id);
+      setIsInCart(true);
+    }
+  };
 
   return (
     <S.ProductItem>
@@ -24,7 +47,7 @@ const ProductItem = ({ isInCart, imageUrl, name, price }: ProductItemProps) => {
           <S.Price>{price.toLocaleString('ko-KR')}원</S.Price>
         </S.Information>
         <S.ButtonContainer>
-          <S.ToggleButton onClick={onClick} $isInCart={isInCart}>
+          <S.ToggleButton onClick={onToggle} $isInCart={isInCart}>
             <S.ButtonImage src={TOGGLE_BUTTON_ICON} />
             <span>{BUTTON_TEXT}</span>
           </S.ToggleButton>
