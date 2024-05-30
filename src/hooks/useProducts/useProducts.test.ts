485cab1a198a1fa8ceb1c1b14e1a234dd30c7f47
anttiey <hanuuny@gmail.com>
2024-05-30 16:07:48 +0900

    refactor: 커스텀 훅 파일 이름 변경
    Co-authored-by: 최진실 <chlwlstlf@users.noreply.github.com>

--- src/hooks/useProducts/useProducts.test.ts
+++ src/hooks/useProducts/useProducts.test.ts
@@ -1,7 +1,7 @@
 import { server } from '../../mocks/server';
 import { http, HttpResponse } from 'msw';
 import { act, renderHook, waitFor } from '@testing-library/react';
-import useProduct from './useProduct';
+import useProduct from './useProducts';
 import { CATEGORY_LIST, SORTING_LIST } from '../../constants/optionList';
 import { API_URL } from '../../constants/api';
 
