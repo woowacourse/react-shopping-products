0e88846013e80ecd961b83554224a4cc107ece7d
Soyeon Choe <soy2302ten@gmail.com>
2024-05-30 16:41:46 +0900

    fix: 첫 렌더링 시, cart-items에 담겼는지 확인하는 로직 수정

--- src/components/ProductList/ProductItem/ProductItem.tsx
+++ src/components/ProductList/ProductItem/ProductItem.tsx
@@ -1,61 +1,53 @@
-import { forwardRef, useContext, useState } from "react";
-import * as PI from "./ProductItem.style";
-import CartControlButton from "../../Button/CartControlButton";
-import { deleteProductInCart, postProductInCart } from "../../../api";
-import { useError } from "../../../hooks/useError";
-import { CartItemsContext } from "../../../context/CartItemsContext";
+import { forwardRef, useContext, useEffect } from 'react';
+import * as PI from './ProductItem.style';
+import CartControlButton from '../../Button/CartControlButton';
+import { deleteProductInCart, postProductInCart } from '../../../api';
+import { useError } from '../../../hooks/useError';
+import { CartItemsContext } from '../../../context/CartItemsContext';
 
 interface ProductProps {
-  product: Product;
+	product: Product;
 }
 
-const ProductItem = forwardRef<HTMLDivElement, ProductProps>(
-  ({ product }, ref) => {
-    const { cartItems, refreshCartItems } = useContext(CartItemsContext);
+const ProductItem = forwardRef<HTMLDivElement, ProductProps>(({ product }, ref) => {
+	const { cartItems, refreshCartItems } = useContext(CartItemsContext);
 
-    const cartItemIds = cartItems.map((item) => item.product.id);
-    const initialIsInCart = cartItemIds.includes(product.id);
-    const [isInCart, setIsInCart] = useState(initialIsInCart);
+	const cartItemIds = cartItems.map((item) => item.product.id);
+	const isInCart = cartItemIds.includes(product.id);
 
-    const { showError } = useError();
+	const { showError } = useError();
 
-    const handleIsInCart = async () => {
-      try {
-        if (isInCart) {
-          const filtered = cartItems.filter(
-            (item) => item.product.id === product.id
-          );
-          await deleteProductInCart(filtered[0].id);
-          refreshCartItems();
-          setIsInCart(!isInCart);
-        } else {
-          await postProductInCart(product.id);
-          refreshCartItems();
-          setIsInCart(!isInCart);
-        }
-      } catch (error) {
-        if (error instanceof Error) {
-          showError(error.message);
-        }
-      }
-    };
+	useEffect(() => {}, []);
 
-    return (
-      <PI.ProductItemStyle ref={ref}>
-        <PI.ProductImg
-          src={`${product.imageUrl}`}
-          alt={`${product.name} 상품 이미지`}
-        />
-        <PI.ProductGroup>
-          <PI.ProductContent>
-            <PI.ProductName>{product.name}</PI.ProductName>
-            <span>{product.price.toLocaleString("ko-kr")}원</span>
-          </PI.ProductContent>
-          <CartControlButton onClick={handleIsInCart} isInCart={isInCart} />
-        </PI.ProductGroup>
-      </PI.ProductItemStyle>
-    );
-  }
-);
+	const handleIsInCart = async () => {
+		try {
+			if (isInCart) {
+				const filtered = cartItems.filter((item) => item.product.id === product.id);
+				await deleteProductInCart(filtered[0].id);
+				refreshCartItems();
+			} else {
+				await postProductInCart(product.id);
+				refreshCartItems();
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				showError(error.message);
+			}
+		}
+	};
+
+	return (
+		<PI.ProductItemStyle ref={ref}>
+			<PI.ProductImg src={`${product.imageUrl}`} alt={`${product.name} 상품 이미지`} />
+			<PI.ProductGroup>
+				<PI.ProductContent>
+					<PI.ProductName>{product.name}</PI.ProductName>
+					<span>{product.price.toLocaleString('ko-kr')}원</span>
+				</PI.ProductContent>
+				<CartControlButton onClick={handleIsInCart} isInCart={isInCart} />
+			</PI.ProductGroup>
+		</PI.ProductItemStyle>
+	);
+});
 
 export default ProductItem;
