72613099292113086f3c3413d022cf2b37a27564
rbgksqkr <rbgks1937@naver.com>
2024-05-30 14:53:20 +0900

    feat: Dropdown 컴포넌트 구현
    Co-authored-by: dle234 <213402@jnu.ac.kr>

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,11 @@
 import ADD_SHOPPING_CART from './add_shopping_cart.svg';
+import ARROW_DOWN from './arrow_down.svg';
+import ARROW_UP from './arrow_up.svg';
 import REMOVE_SHOPPING_CART from './remove_shopping_cart.svg';
 
 export const IMAGES = {
   ADD_SHOPPING_CART,
   REMOVE_SHOPPING_CART,
+  ARROW_UP,
+  ARROW_DOWN,
 };
