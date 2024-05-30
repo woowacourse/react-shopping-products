8f8fdbf0d033b83adbcc6d8eb2dd0995bd213229
vi-wolhwa <cerezo00@naver.com>
2024-05-30 11:12:32 +0900

    rename(ProductPage)

--- src/pages/product/ui/ProductPage/ProductPage.tsx
+++ src/pages/product/ui/ProductPage/ProductPage.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
+
+import { Product } from '@/entities/product';
 import { Layout } from '@/shared';
+import products from '@/shared/mocks/products.json';
 import { ContentHeader } from '@/widgets/ContentHeader';
 import { HeaderCartButton, HeaderLogoButton, LayoutHeader } from '@/widgets/LayoutHeader';
 import { ProductList } from '@/widgets/ProductList';
-import products from '@/shared/mocks/products.json';
-import { Product } from '@/entities/product';
-import css from './ProductPage.module.css';
 
 export const ProductPage = () => {
   const [cartItemCount, setCartItemCount] = useState(0);
+  setCartItemCount;
 
   return (
     <Layout
@@ -17,7 +18,7 @@ export const ProductPage = () => {
       }
       contentHeaderSlot={<ContentHeader title={'상품 목록'} />}
       contentBodySlot={
-        <div className={css.contentBodySlot}>
+        <div>
           <ProductList products={products as Product[]} />
         </div>
       }
