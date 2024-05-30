9045e92f698cef1cb5c8e6ef756d7e0654181970
badahertz52 <badahertz52@gmail.com>
2024-05-30 13:25:42 +0900

    test : useFilterProducts 테스트
    Co-Authored-By: JinHo Kim <81083461+jinhokim98@users.noreply.github.com>

--- src/mocks/handlers/products.ts
+++ src/mocks/handlers/products.ts
@@ -1,4 +1,4 @@
-import { Product, ServerResponse } from '@appTypes/index';
+import { Category, PriceSort, Product, ServerResponse } from '@appTypes/index';
 import products from '@mocks/data/products.json';
 import { http, HttpResponse } from 'msw';
 
@@ -46,19 +46,38 @@ const makeServerResponse = <T>({
   };
 };
 
+const filterProducts = (category: Category | null, products: Product[]) => {
+  if (!category) return products;
+
+  return products.filter((product) => product.category === category);
+};
+
+const sortProducts = (sort: PriceSort | null, products: Product[]) => {
+  if (sort === 'price,asc' || !sort) {
+    return products.sort((prev, cur) => prev.price - cur.price);
+  }
+  return products.sort((prev, cur) => cur.price - prev.price);
+};
+
 const productsHandler = [
   http.get(END_POINTS.products, async ({ request }) => {
     const url = new URL(request.url);
     const page = Number(url.searchParams.get('page'));
     const size = Number(url.searchParams.get('size'));
-    // const category = url.searchParams.get('category');
+    const category = url.searchParams.get('category') as Category | null;
+    const sort = url.searchParams.get('sort') as PriceSort | null;
+
+    const filteredProducts = filterProducts(category, products as Product[]);
+    const sortedProducts = sortProducts(sort, filteredProducts);
+
     const limit = page === 0 ? 20 : 4;
     const start = page === 0 ? 0 : (page - 1) * 4 + 20;
     const end = start + limit;
+
     const response = makeServerResponse<Product[]>({
       page: Number(page),
       size: Number(size),
-      content: products.slice(start, end) as Product[],
+      content: sortedProducts.slice(start, end),
     });
 
     return HttpResponse.json(response);
