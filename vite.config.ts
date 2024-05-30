2f219693f6cf42ac958c47dc9ddb893b7ab25a1c
rbgksqkr <rbgks1937@naver.com>
2024-05-30 18:12:03 +0900

    chore: base url 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-shopping-products/',
   plugins: [react({ jsxImportSource: '@emotion/react' })],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
