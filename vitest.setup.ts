b050c3cd07963733af02d02bfab5a9fc579e0098
simhorim <hori5020@gmail.com>
2024-05-30 17:36:00 +0900

    chore: 환경 구현

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,12 @@
+import { vi } from 'vitest';
 import { server } from './src/mocks/server';
 
 beforeAll(() => {
+  const intersectionObserverMock = () => ({
+    observe: () => null,
+  });
+  window.IntersectionObserver = vi.fn().mockImplementation(intersectionObserverMock);
+
   server.listen({ onUnhandledRequest: 'error' });
 });
 
