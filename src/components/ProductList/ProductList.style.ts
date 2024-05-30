491e5759e1fe72af9f16cc3ea21a2d44e10d69dd
cys4585 <dev.youngsu@gmail.com>
2024-05-30 09:00:10 +0900

    design: 아이템이 하나일 때 그리드 정렬 되도록 수정
    Co-Authored-By: Soyeon Choe <77609591+soi-ha@users.noreply.github.com>

--- src/components/ProductList/ProductList.style.ts
+++ src/components/ProductList/ProductList.style.ts
@@ -4,5 +4,5 @@ export const ProductListStyle = styled.section`
   display: grid;
   grid-template-columns: 0fr 0fr;
   gap: 16px 20px;
-  justify-content: center;
+  justify-content: space-between;
 `;
