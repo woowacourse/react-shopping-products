5561256efcd3c3b6b4d84bbc7c985fb54ba23f2e
jayming66 <mjw8941@gmail.com>
2024-05-30 15:57:03 +0900

    refactor(ProductItem.tsx): ProdictItem 컴포넌트 UI 요구사항 변경 반영
    Co-Authored-By: Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>

--- src/pages/ProductListPage/components/ProductItem.tsx
+++ src/pages/ProductListPage/components/ProductItem.tsx
@@ -1,6 +1,6 @@
+import { HtmlHTMLAttributes } from 'react';
 import ProductSelectButton from './ProductSelectButton';
 import styles from '../ProductListPage.module.css';
-import { HtmlHTMLAttributes } from 'react';
 
 type productType = {
   id: number;
@@ -22,7 +22,6 @@ const ProductItem = ({ item, isSelected, onSelect }: Props) => {
       <img src={item.imageUrl} width={182} height={112} style={{ borderRadius: '8px 8px 0 0' }} />
       <div className={styles.productItemInfoContainer}>
         <span className={styles.productItemName}>{item.name}</span>
-        <span className={styles.productItemLabel}>상품 설명 텍스트</span>
         <span className={styles.productItemLabel}>{item.price.toLocaleString('KR-ko')}원</span>
       </div>
       <ProductSelectButton isSelected={isSelected} onClick={onSelect}></ProductSelectButton>
