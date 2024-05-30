fbc19b6a2caec0f7668dfaa66de62c0dc8e04036
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-30 16:46:10 +0900

    test: 상품 장바구니 추가/제거 테스트 관련 핸들러 및 데이터 추가
    Co-authored-by: 해리(최현웅) <hwinkr@users.noreply.github.com>

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from 'msw';
-import { PRODUCTS_ENDPOINT } from '../constants/apis';
+import { ENDPOINT } from '../constants/apis';
+import { initialCartItems, newCartItem } from './cartItems/initialCartItems';
 
 import {
   ProductsUnfilteredInitial,
@@ -12,31 +13,64 @@ import {
   ProductsFilteredBooksSortedDescLast,
 } from './products';
 
+interface CartItemRequestBody {
+  productId: number;
+  quantity: number;
+}
+
 export const handlers = [
-  http.get(PRODUCTS_ENDPOINT, ({ request }) => {
+  http.get(ENDPOINT.PRODUCT, ({ request }) => {
     const url = new URL(request.url);
     const page = Number(url.searchParams.get('page') || '0');
-    const category = url.searchParams.get('category') || 'all';
-    const sort = url.searchParams.get('sort') || 'asc';
+    const category = url.searchParams.get('category');
+    const sort = url.searchParams.get('sort') || 'price,asc';
 
-    if (category === 'all' && sort === 'asc') {
+    if (!category && sort === 'price,asc') {
       if (page === 0) return HttpResponse.json(ProductsUnfilteredInitial);
       return HttpResponse.json(ProductsUnfilteredLast);
     }
 
-    if (category === 'all' && sort === 'desc') {
+    if (!category && sort === 'price,desc') {
       if (page === 0) return HttpResponse.json(ProductsUnfilteredSortedDescInitial);
       return HttpResponse.json(ProductsUnfilteredSortedDescLast);
     }
 
-    if (category === 'books' && sort === 'asc') {
+    if (category === 'books' && sort === 'price,asc') {
       if (page === 0) return HttpResponse.json(ProductsFilteredBooksInitial);
       return HttpResponse.json(ProductsFilteredBooksLast);
     }
 
-    if (category === 'books' && sort === 'desc') {
+    if (category === 'books' && sort === 'price,desc') {
       if (page === 0) return HttpResponse.json(ProductsFilteredBooksSortedDescInitial);
       return HttpResponse.json(ProductsFilteredBooksSortedDescLast);
     }
+  }),
+
+  http.get(ENDPOINT.CART_ITEMS, () => {
+    return HttpResponse.json(initialCartItems);
+  }),
+
+  http.post(ENDPOINT.CART_ITEMS, async ({ request }) => {
+    const { productId } = (await request.json()) as CartItemRequestBody;
+
+    const targetCartItem = newCartItem.find((item) => item.product.id === productId);
+
+    if (targetCartItem) {
+      initialCartItems.content.push(targetCartItem);
+    }
+
+    return HttpResponse.json(null, { status: 201 });
+  }),
+
+  http.delete(`${ENDPOINT.CART_ITEMS}/*`, ({ request }) => {
+    const targetId = request.url.split('/').pop();
+
+    if (targetId) {
+      initialCartItems.content = initialCartItems.content.filter(
+        (item) => item.product.id !== Number(targetId),
+      );
+    }
+
+    return HttpResponse.json(null, { status: 204 });
   }),
 ];
