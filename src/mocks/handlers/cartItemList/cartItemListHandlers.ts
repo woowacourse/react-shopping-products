b42bac0d7fd4086ff9f290710093f16f8aa6d79d
greetings1012 <ahnqwerty@gmail.com>
2024-06-01 02:28:42 +0900

    refactor: 필요없는 const 선언 삭제
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/mocks/handlers/cartItemList/cartItemListHandlers.ts
+++ src/mocks/handlers/cartItemList/cartItemListHandlers.ts
@@ -5,11 +5,9 @@ import productListData from "../productList/defaultData.json";
 import cartItemListData from "./defaultData.json";
 import { Product } from "../../../interfaces/Product";
 
-const mockCartItemList = cartItemListData;
-
 export const cartItemListHandlers = [
   http.get(CART_ITEMS_ENDPOINT, ({}) => {
-    return HttpResponse.json({ content: mockCartItemList }, { status: 200 });
+    return HttpResponse.json({ content: cartItemListData }, { status: 200 });
   }),
 
   http.post(`${CART_ITEMS_ENDPOINT}/cart-items`, async ({ request }) => {
@@ -20,24 +18,24 @@ export const cartItemListHandlers = [
     );
 
     const cartItemId =
-      Math.max(0, ...mockCartItemList.map((cartItem) => cartItem.id)) + 1;
+      Math.max(0, ...cartItemListData.map((cartItem) => cartItem.id)) + 1;
     console.log(product);
     if (product) {
-      mockCartItemList.push({ id: cartItemId, product, quantity: 1 });
+      cartItemListData.push({ id: cartItemId, product, quantity: 1 });
     }
 
-    return HttpResponse.json({ status: 201 });
+    return HttpResponse.json({ content: cartItemListData, status: 201 });
   }),
 
   http.delete(CART_ITEMS_ENDPOINT, ({ params }) => {
     const { id } = params;
-    const index = mockCartItemList.findIndex(
+    const index = cartItemListData.findIndex(
       (item) => item.id === parseInt(id as string, 10)
     );
     if (index === -1) {
       return HttpResponse.json("장바구니에 없는 상품입니다.", { status: 400 });
     }
-    mockCartItemList.splice(index, 1);
-    return HttpResponse.json({ status: 204 });
+    cartItemListData.splice(index, 1);
+    return HttpResponse.json({ content: cartItemListData, status: 204 });
   }),
 ];
