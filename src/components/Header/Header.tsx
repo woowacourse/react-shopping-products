40570761fda9718632126ddb3d123eda8ddefb18
cys4585 <dev.youngsu@gmail.com>
2024-05-31 18:44:42 +0900

    refactor: Header 컴포넌트 개선 (불필요한 상태 제거)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,17 @@
 import Logo from "../../assets/logo.svg";
 import Cart from "../../assets/cart.svg";
-import { HeaderStyle, CartCount, LogoImg, CartImg } from "./Header.style";
-import { useContext, useEffect, useState } from "react";
-import { CartItemsContext } from "../../context/CartItemsContext";
+import * as H from "./Header.style";
+import { useCartItemsContext } from "../../hooks/useCartItemsContext";
 
 export default function Header() {
-  const [quantityInCart, setQuantityInCart] = useState(0);
-  const { cartItems } = useContext(CartItemsContext);
-
-  useEffect(() => {
-    setQuantityInCart(cartItems.length);
-  }, [cartItems]);
+  const { cartItems } = useCartItemsContext();
+  const quantityInCart = cartItems.length;
 
   return (
-    <HeaderStyle>
-      <LogoImg src={Logo} alt="로고" />
-      <CartImg src={Cart} alt="장바구니" />
-      {quantityInCart && <CartCount>{quantityInCart}</CartCount>}
-    </HeaderStyle>
+    <H.HeaderStyle>
+      <H.LogoImg src={Logo} alt="로고" />
+      <H.CartImg src={Cart} alt="장바구니" />
+      {quantityInCart && <H.CartCount>{quantityInCart}</H.CartCount>}
+    </H.HeaderStyle>
   );
 }
