43e94d08070ca81b45874b516c2fba7f75204a51
Lee sang Yeop <mma7710@naver.com>
2024-06-01 21:21:30 +0900

    test(useHandleCartItem): 카트 아이템 추가하는 테스트

--- src/hooks/useHandleCartItem.test.tsx
+++ src/hooks/useHandleCartItem.test.tsx
@@ -3,6 +3,22 @@ import CartItemProvider from "@/provider/cartItemProvider";
 import { renderHook, act, waitFor } from "@testing-library/react";
 
 describe("장바구니 관련 테스트", () => {
+  it("장바구니에 있는 아이템 담기 버튼을 누르면 장바구니에서 추가된다.", async () => {
+    const { result } = renderHook(() => useHandleCartItem(), {
+      wrapper: ({ children }) => <CartItemProvider>{children}</CartItemProvider>,
+    });
+
+    const cartId = 777;
+
+    expect(result.current.isInCart(cartId)).toBe(false);
+
+    await act(() => {
+      result.current.onClickCartItem(cartId);
+    });
+
+    expect(result.current.isInCart(cartId)).toBe(true);
+  });
+
   it("장바구니에 있는 아이템 빼기 버튼을 누르면 장바구니에서 제거된다.", async () => {
     const { result } = renderHook(() => useHandleCartItem(), {
       wrapper: ({ children }) => <CartItemProvider>{children}</CartItemProvider>,
@@ -16,11 +32,9 @@ describe("장바구니 관련 테스트", () => {
     });
 
     act(() => {
-      result.current.onClickCartItem(3168);
+      result.current.onClickCartItem(cartId);
     });
 
-    await waitFor(() => {
-      expect(result.current.isInCart(cartId)).toEqual(false);
-    });
+    expect(result.current.isInCart(cartId)).toBe(false);
   });
 });
--- src/mocks/handler/cartItemsHandler.ts
+++ src/mocks/handler/cartItemsHandler.ts
@@ -7,7 +7,23 @@ export const cartItemsHandler = [
   http.get(SERVER_URL.apiUrl + END_POINT.cartItems, () => {
     return HttpResponse.json(cartItems, { status: 200 });
   }),
-  http.post(SERVER_URL.apiUrl + END_POINT.cartItems, () => {
+  http.post(SERVER_URL.apiUrl + END_POINT.cartItems, async ({ request }) => {
+    const res = (await request.json()) as { productId: number; quantity: number };
+
+    const tmpCartItem = {
+      id: Math.random() * 10000,
+      quantity: 1,
+      product: {
+        id: res.productId,
+        name: "테스트 아이템",
+        price: 999,
+        imageUrl: "mockImg",
+        category: "fashion",
+      },
+    };
+
+    if (cartItems) cartItems.content.push(tmpCartItem);
+
     return HttpResponse.json(null, { status: 201 });
   }),
   http.delete(SERVER_URL.apiUrl + END_POINT.cartItems + "/:id", () => {
