88f7733b6797e31bcbd29df4f49a610d846379a1
이태훈 <rhymint@gmail.com>
2024-06-02 08:39:20 +0900

    storybook: 의미없는 description 데이터들 제거
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- src/components/common/Button/Button.stories.tsx
+++ src/components/common/Button/Button.stories.tsx
@@ -8,57 +8,57 @@ const meta = {
   argTypes: {
     size: {
       description:
-        '(optional) Specifies the size of the button. Choices are "small", "medium" (default), or "large", affecting the button’s padding and font size.',
+        '',
       control: { type: 'radio' },
       options: ['s', 'm', 'l', 'fit'],
     },
     width: {
       description:
-        '(optional) Determines the width setting for the button\'s CSS. "fixed" maintains a constant width, "fit" adjusts to the text size, and "full" expands to the full width of its container.',
+        '',
       control: { type: 'radio' },
       options: ['fit', 'full', 'custom'],
     },
     customWidth: {
       if: { arg: 'width', eq: 'custom' },
       description:
-        '(optional) Determines the width setting for the button\'s CSS. "fixed" maintains a constant width, "fit" adjusts to the text size, and "full" expands to the full width of its container.',
+        '',
       control: { type: 'number' },
     },
     radius: {
       description:
-        '(optional) Determines the width setting for the button\'s CSS. "fixed" maintains a constant width, "fit" adjusts to the text size, and "full" expands to the full width of its container.',
+        '',
       control: { type: 'radio' },
       options: ['s', 'm', 'l', 'custom'],
     },
     customRadius: {
       if: { arg: 'radius', eq: 'custom' },
       description:
-        '(optional) Determines the width setting for the button\'s CSS. "fixed" maintains a constant width, "fit" adjusts to the text size, and "full" expands to the full width of its container.',
+        '',
       control: { type: 'number' },
     },
     color: {
       description:
-        '(optional) Determines the width setting for the button\'s CSS. "fixed" maintains a constant width, "fit" adjusts to the text size, and "full" expands to the full width of its container.',
+        '',
       control: { type: 'radio' },
       options: ['default', 'primary'],
     },
     square: {
       description:
-        '(optional) Determines the width setting for the button\'s CSS. "fixed" maintains a constant width, "fit" adjusts to the text size, and "full" expands to the full width of its container.',
+        '',
       control: { type: 'boolean' },
     },
     isDisabled: {
       description:
-        '(required) The function that is called when the button is clicked. This handler is triggered on user interaction with the button.',
+        '',
       control: { type: 'boolean' },
     },
     onClick: {
       description:
-        '(required) The function that is called when the button is clicked. This handler is triggered on user interaction with the button.',
+        '',
     },
     children: {
       description:
-        '(required) The function that is called when the button is clicked. This handler is triggered on user interaction with the button.',
+        '',
       control: { type: 'text' },
     },
   },
