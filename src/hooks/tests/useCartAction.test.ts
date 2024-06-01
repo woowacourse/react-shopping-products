1a7843ff414ff03a965c8d529963dd94f50f81ae
badahertz52 <badahertz52@gmail.com>
2024-06-01 17:52:19 +0900

    test: useCartAction 훅 변경에 따른 테스트 코드 수정

--- src/hooks/tests/useCartAction.test.ts
+++ src/hooks/tests/useCartAction.test.ts
@@ -1,57 +1,37 @@
+import { useCartAction, useCartItemIds } from '@hooks/index';
 import { renderHook, waitFor } from '@testing-library/react';
-
-import useCartAction from './../useCartAction';
 
 describe('상품 장바구니 담기/빼기 테스트', () => {
   const PRODUCT_ID = 113;
 
+  const { result } = renderHook(() => {
+    const { refreshCartItemIds } = useCartItemIds();
+    const { addCartItem, deleteCarItem, error } = useCartAction({ refreshCartItemIds });
+
+    return { addCartItem, deleteCarItem, error };
+  });
+
   describe('장바구니 담기 테스트', () => {
     it('장바구니에 담으면, 장바구니 목록에 상품이 추가된다.', async () => {
-      const { result } = renderHook(() => {
-        const { cartItems, handleCartAction } = useCartAction();
-
-        return { cartItems, handleCartAction };
-      });
-      const { cartItems, handleCartAction } = result.current;
-
-      expect(cartItems.length).toBe(0);
+      const { addCartItem } = result.current;
 
       await waitFor(async () => {
-        await handleCartAction({
-          isInCart: false,
-          productId: PRODUCT_ID,
-          cartItem: undefined,
-        });
-
-        expect(result.current.cartItems.length).toBe(1);
+        await addCartItem(PRODUCT_ID);
       });
+
+      expect(result.current.error).toBeFalsy();
     });
   });
 
   describe('장바구니 빼기 테스트', () => {
     it('장바구니에서 빼면, 장바구니 목록에 상품이 삭제된다.', async () => {
-      const { result } = renderHook(() => {
-        const { cartItems, handleCartAction, getCartItemList } = useCartAction();
-
-        return { cartItems, getCartItemList, handleCartAction };
-      });
-      const { handleCartAction, getCartItemList } = result.current;
+      const { deleteCarItem } = result.current;
 
       await waitFor(async () => {
-        await getCartItemList();
+        await deleteCarItem(PRODUCT_ID);
       });
 
-      expect(result.current.cartItems.length).toBe(1);
-
-      await waitFor(async () => {
-        await handleCartAction({
-          isInCart: false,
-          productId: PRODUCT_ID,
-          cartItem: undefined,
-        });
-        // TODO: 같은 get요청에 대해 테스트 케이스에 따라 다른 mock data 넣는 법....
-        //expect(result.current.cartItems.length).toBe(0);
-      });
+      expect(result.current.error).toBeFalsy();
     });
   });
 });
