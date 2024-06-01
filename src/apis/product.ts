e5608e32f622666a3960031e502cf5155fe7633d
Lee sang Yeop <mma7710@naver.com>
2024-06-01 19:21:42 +0900

    refactor(product): 정의된 상수 및 함수 활용하여 코드를 직관적으로 리팩토링

--- src/apis/product.ts
+++ src/apis/product.ts
@@ -1,9 +1,10 @@
 import { END_POINT } from "@/config/endPoint";
-import SERVER_URL from "@/config/serverUrl";
 import { ERROR_MESSAGES } from "@/constants/messages";
 import { ResponseProduct } from "@/types/products";
 import { Category, SORT, Sort } from "@/constants/selectOption";
 import { CATEGORY } from "@/constants/selectOption";
+import urlFormatter from "@/utils/urlFormatter";
+
 interface getProductsProps {
   category: Category;
   page: number;
@@ -18,16 +19,17 @@ export const getProducts = async ({ category, page, size, sort }: getProductsPro
   const params = {
     page: page.toString(),
     size: size.toString(),
+    sort: convertedSort,
   };
 
   const searchParams = new URLSearchParams(params);
-  searchParams.append("sort", `price,id,${convertedSort}`);
 
   if (category !== "전체") {
     searchParams.append("category", convertedCategory);
   }
 
-  const response = await fetch(SERVER_URL.apiUrl + END_POINT.products + "?" + searchParams.toString());
+  const formattedURL = urlFormatter(END_POINT.products, searchParams.toString());
+  const response = await fetch(formattedURL);
 
   if (!response.ok) {
     throw new Error(ERROR_MESSAGES.failGetProducts);
