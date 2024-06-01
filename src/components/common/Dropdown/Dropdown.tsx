a58b0e11ebb6fcb02b937d0dd1c082dd3a7ac51a
ss0526100 <ss0526100@gmail.com>
2024-06-02 04:03:57 +0900

    feat(DropDown): context 삭제

--- src/components/common/Dropdown/Dropdown.tsx
+++ src/components/common/Dropdown/Dropdown.tsx
@@ -1,81 +1,41 @@
 import * as Styled from './Dropdown.styled';
 
-import { DownArrowSvg, UpArrowSvg } from '@assets/svg';
-import {
-  DropdownProvider,
-  useDropdownContext,
-} from './provider/DropdownProvider';
+export interface DropdownProps<T extends string> {
+  isOpen: boolean;
+  nowSelectedOption: T;
+  options: T[];
+  setOption: (option: T) => void;
+  setIsOpen: (boolean: boolean) => void;
+}
 
-type OptionType = React.FC<
-  React.PropsWithChildren<{
-    isSelected: boolean;
-    onSelectOption: () => void;
-    onHoverOption?: () => void;
-  }>
->;
-type MenuType = React.FC<React.PropsWithChildren>;
-type TriggerType = React.FC<
-  React.PropsWithChildren<{ placeholder: string; value?: string }>
->;
-
-const Dropdown: React.FC<
-  React.PropsWithChildren<{ isOpen: boolean; onToggleDropdown: () => void }>
-> & {
-  Trigger: TriggerType;
-  Menu: MenuType;
-  Option: OptionType;
-} = ({ isOpen, onToggleDropdown, children }) => {
+function Dropdown<T extends string>({
+  isOpen,
+  nowSelectedOption,
+  options,
+  setOption,
+  setIsOpen,
+}: DropdownProps<T>) {
   return (
-    <DropdownProvider value={{ isOpen, onToggleDropdown }}>
-      <Styled.DropdownContainer>{children}</Styled.DropdownContainer>
-    </DropdownProvider>
+    <Styled.DropdownContainer onClick={() => setIsOpen(!isOpen)}>
+      <Styled.Trigger isOpen={isOpen}>
+        {nowSelectedOption}
+        <Styled.Arrow isOpen={isOpen} />
+      </Styled.Trigger>
+      {isOpen && (
+        <Styled.OptionContainer isOpen={isOpen}>
+          {options.map(option => (
+            <Styled.Option
+              key={option}
+              isSelected={nowSelectedOption === option}
+              onClick={() => setOption(option)}
+            >
+              {option}
+            </Styled.Option>
+          ))}
+        </Styled.OptionContainer>
+      )}
+    </Styled.DropdownContainer>
   );
-};
-
-const Trigger: TriggerType = ({ placeholder, value }) => {
-  const { isOpen, onToggleDropdown } = useDropdownContext()!;
-
-  return (
-    <Styled.DropdownTriggerContainer
-      $isOpen={isOpen}
-      $value={value}
-      onClick={onToggleDropdown}
-    >
-      <span>{value || placeholder}</span>
-      {isOpen ? <DownArrowSvg /> : <UpArrowSvg />}
-    </Styled.DropdownTriggerContainer>
-  );
-};
-
-const Menu: MenuType = ({ children }) => {
-  const { isOpen } = useDropdownContext()!;
-
-  return isOpen ? (
-    <Styled.DropdownMenuContainer $isOpen={isOpen}>
-      {children}
-    </Styled.DropdownMenuContainer>
-  ) : null;
-};
-
-const Option: OptionType = ({
-  isSelected,
-  onSelectOption,
-  onHoverOption,
-  children,
-}) => {
-  return (
-    <Styled.DropdownOptionContainer
-      $isSelected={isSelected}
-      onMouseEnter={onHoverOption}
-      onClick={onSelectOption}
-    >
-      {children}
-    </Styled.DropdownOptionContainer>
-  );
-};
-
-Dropdown.Trigger = Trigger;
-Dropdown.Menu = Menu;
-Dropdown.Option = Option;
+}
 
 export default Dropdown;
