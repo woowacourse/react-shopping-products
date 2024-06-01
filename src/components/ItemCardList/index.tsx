15f0e24e71fe9ad62a33ca036a4a5a057364f953
Lee sang Yeop <mma7710@naver.com>
2024-06-01 19:46:29 +0900

    feat(ItemCardList): 상품이 없으면 해달이 보이도록 추가

--- src/components/ItemCardList/index.tsx
+++ src/components/ItemCardList/index.tsx
@@ -2,11 +2,19 @@ import ItemCard from "@/components/ItemCard";
 import styled from "styled-components";
 import { flexCenter } from "@/styles/common";
 import { Product } from "@/types/products";
+import SeaOtterVideo from "@/components/SeaOtterVideo";
 
 const ItemCardList = ({ products }: { products: Product[] }) => {
   return (
     <ItemCardWrapper>
-      {products && products.map((product) => <ItemCard key={Math.random() * 10000} product={product} />)}
+      {products.length ? (
+        products.map((product) => <ItemCard key={Math.random() * 10000} product={product} />)
+      ) : (
+        <>
+          <SeaOtterVideo />
+          <h2>🦦 눈 씻고 찾아봐도 상품이 없어요.. ㅠㅠ</h2>
+        </>
+      )}
     </ItemCardWrapper>
   );
 };
