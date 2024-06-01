2faae6028e3c66d0cf55872d9fff5099970f4b4a
badahertz52 <badahertz52@gmail.com>
2024-06-01 20:02:11 +0900

    test :useFilteredProducts, useStackProducts 리팩토링에 따른 테스트 코드 수정

--- src/hooks/tests/useFilteredProducts.test.ts
+++ src/hooks/tests/useFilteredProducts.test.ts
@@ -1,5 +1,5 @@
 import { fetchProduct } from '@apis/index';
-import { Category } from '@appTypes/index';
+import { Category, Filtering } from '@appTypes/index';
 import { CATEGORY_OPTIONS } from '@constants/index';
 import { renderHook, waitFor } from '@testing-library/react';
 
@@ -10,19 +10,23 @@ describe('상품 목록 필터링 테스트', () => {
   describe('카테고리 필터링 테스트', () => {
     it('전체 선택 시, 모든 상품 목록을 보여준다', async () => {
       const CATEGORY_LENGTH = CATEGORY_OPTIONS.filter((options) => !!options.value).length;
+      const FILTERING: Filtering = { category: '', sort: 'price,asc' };
 
       const { result } = renderHook(() => {
         const { fetch } = useFetch<typeof fetchProduct>(fetchProduct);
         const { getFilteredProducts } = useFilteredProducts({
           fetch,
-          filtering: { category: '', sort: 'price,asc' },
         });
         return { getFilteredProducts };
       });
 
       await waitFor(async () => {
-        const response = await result.current.getFilteredProducts();
-        const categorySet = new Set(response?.newProducts.map((product) => product.category));
+        const response = await result.current.getFilteredProducts(FILTERING);
+
+        expect(response).toBeDefined();
+        if (!response) return;
+
+        const categorySet = new Set(response.newProducts.map((product) => product.category));
 
         expect(categorySet.size).toBe(CATEGORY_LENGTH);
       });
@@ -31,18 +35,23 @@ describe('상품 목록 필터링 테스트', () => {
     it.each(CATEGORY_OPTIONS.slice(1))(
       '카테고리 선택 시, 해당 카테고리 상품만 보여준다. (선택한 카테고리 :%s)',
       async ({ value }) => {
+        const FILTERING: Filtering = { category: value as Category, sort: 'price,asc' };
+
         const { result } = renderHook(() => {
           const { fetch } = useFetch<typeof fetchProduct>(fetchProduct);
           const { getFilteredProducts } = useFilteredProducts({
             fetch,
-            filtering: { category: value as Category, sort: 'price,asc' },
           });
           return { getFilteredProducts };
         });
 
         await waitFor(async () => {
-          const response = await result.current.getFilteredProducts();
-          const isAllFashion = response?.newProducts.every((product) => product.category === value);
+          const response = await result.current.getFilteredProducts(FILTERING);
+
+          expect(response).toBeDefined();
+          if (!response) return;
+
+          const isAllFashion = response.newProducts.every((product) => product.category === value);
           expect(isAllFashion).toBeTruthy();
         });
       },
@@ -51,36 +60,46 @@ describe('상품 목록 필터링 테스트', () => {
 
   describe('가격 정렬 테스트', () => {
     it('오름차순 선택 시, 상품 각각의 오름차순으로 목록을 보여준다', async () => {
+      const FILTERING: Filtering = { category: '', sort: 'price,asc' };
+
       const { result } = renderHook(() => {
         const { fetch } = useFetch<typeof fetchProduct>(fetchProduct);
         const { getFilteredProducts } = useFilteredProducts({
           fetch,
-          filtering: { category: '', sort: 'price,asc' },
         });
         return { getFilteredProducts };
       });
 
       await waitFor(async () => {
-        const response = await result.current.getFilteredProducts();
+        const response = await result.current.getFilteredProducts(FILTERING);
+
+        expect(response).toBeDefined();
         if (!response) return;
+
         const [first, second] = response.newProducts;
+
         expect(first.price <= second.price).toBeTruthy();
       });
     });
     it('내림차순 선택 시, 상품 각겨의 내림차순으로 목록을 보여준다', async () => {
+      const FILTERING: Filtering = { category: '', sort: 'price,desc' };
+
       const { result } = renderHook(() => {
         const { fetch } = useFetch<typeof fetchProduct>(fetchProduct);
         const { getFilteredProducts } = useFilteredProducts({
           fetch,
-          filtering: { category: '', sort: 'price,desc' },
         });
         return { getFilteredProducts };
       });
 
       await waitFor(async () => {
-        const response = await result.current.getFilteredProducts();
+        const response = await result.current.getFilteredProducts(FILTERING);
+
+        expect(response).toBeDefined();
         if (!response) return;
+
         const [first, second] = response.newProducts;
+
         expect(first.price >= second.price).toBeTruthy();
       });
     });
