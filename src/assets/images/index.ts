de250876c45658d6431cb440bdfa2be085c2a9fc
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-02 15:52:51 +0900

    chore: 에러 이미지 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,3 +4,4 @@ export { default as ADD_TO_CART } from './addToCart.svg';
 export { default as REMOVE_TO_CART } from './removeToCart.svg';
 export { default as UP_ARROW } from './upArrow.svg';
 export { default as DOWN_ARROW } from './downArrow.svg';
+export { default as ERROR } from './error.svg';
