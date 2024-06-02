a42b07e1677664dd0f0f20d5b09973eaa5996e56
pakxe <pigkill40@naver.com>
2024-06-02 23:39:06 +0900

    feat: 얼리리턴하는 구조로 수정

--- src/hooks/useObserver.tsx
+++ src/hooks/useObserver.tsx
@@ -16,23 +16,22 @@ export const useObserver = ({
   threshold = 1.0,
 }: UseObserverParams): void => {
   useEffect(() => {
-    let observer: IntersectionObserver | undefined;
+    if (!target.current) {
+      return;
+    }
 
-    // 넘어오는 element가 있어야 observer를 생성할 수 있도록 한다.
+    let observer: IntersectionObserver;
+
     if (target && target.current) {
-      // callback의 인자로 들어오는 entry는 기본적으로 순환자이기 때문에
-      // 복잡한 로직을 필요로 할때가 많다.
-      // callback을 선언하는 곳에서 로직을 짜서 통째로 넘기도록 하겠다.
       observer = new IntersectionObserver(onIntersect, {
         root,
         rootMargin,
         threshold,
       });
-      // 실제 Element가 들어있는 current 관측을 시작한다.
+
       observer.observe(target.current);
     }
 
-    // observer를 사용하는 컴포넌트가 해제되면 observer 역시 꺼 주자.
     return () => observer && observer.disconnect();
-  }, [target, rootMargin, threshold]);
+  }, [target, rootMargin, threshold, onIntersect, root]);
 };
