8d2fe3e182df55e0721f0f62e5be477e73241bb1
rbgksqkr <rbgks1937@naver.com>
2024-06-02 16:07:50 +0900

    fix: 불필요한 lint 패키지 제거 및 lint 설정 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,65 +2,62 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "plugin:prettier/recommended",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:import/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'prettier',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 
-    "import/order": [
-      "error",
+    'import/order': [
+      'error',
       {
-        "groups": ['builtin', 'external', ['parent', 'sibling'], 'index'],
-        "pathGroups": [
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        pathGroups: [
           {
-            "pattern": "react*",
-            "group": "builtin",
-            "position": "before"
+            pattern: 'react*',
+            group: 'builtin',
+            position: 'before',
           },
           {
-            "pattern": "@/pages/*",
-            "group": "internal",
-            "position": "after"
+            pattern: '@/pages/*',
+            group: 'internal',
+            position: 'after',
           },
           {
-            "pattern": "@/components/*",
-            "group": "internal",
-            "position": "after"
+            pattern: '@/components/*',
+            group: 'internal',
+            position: 'after',
           },
           {
-            "pattern": "@/hooks/*",
-            "group": "internal",
-            "position": "after"
+            pattern: '@/hooks/*',
+            group: 'internal',
+            position: 'after',
           },
           {
-            "pattern": "src/**",
-            "group": "internal"
+            pattern: 'src/**',
+            group: 'internal',
           },
         ],
-        "alphabetize": {
-          "order": "asc",
-          "caseInsensitive": true
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
         },
-        "newlines-between": "always"
-      }
+        'newlines-between': 'always',
+      },
     ],
   },
   settings: {
-    "import/resolver": {
-      "typescript": {},
-      "node": {
-        "extensions": [".ts", ".tsx", ".js", ".jsx"]
-      }
-    }
-  }
+    'import/resolver': {
+      typescript: {},
+      node: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+    },
+  },
 };
