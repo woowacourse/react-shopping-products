c33d83415e23b50c302bc75859de832e17bcee2a
김진호 <rlawlsgh1227@gmail.com>
2024-06-02 17:01:32 +0900

    feat: 초기에 카트 아이템 로드할 때도 에러 화면 띄워지도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import '@styles/App.css';
 import '@styles/reset.css';
 import '@styles/global.css';
-import { PageRequest } from '@components/Fallbacks';
-import { Header, Layout } from '@components/index';
+import { CartActionError, PageRequest } from '@components/Fallbacks';
+import { Header, Layout, ToastModal } from '@components/index';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ProductListPage } from './pages';
 import useLoadCartItems from './hooks/useLoadCartItems';
 
 function App() {
-  const { cartItems, refetch } = useLoadCartItems();
+  const { cartItems, refetch, error } = useLoadCartItems();
+  const isError = error !== '';
 
   return (
     <>
@@ -18,6 +19,11 @@ function App() {
           <ProductListPage cartItems={cartItems} refetch={refetch} />
         </ErrorBoundary>
       </Layout>
+      {isError && (
+        <ToastModal isError={isError} position={{ top: 40 }}>
+          <CartActionError />
+        </ToastModal>
+      )}
     </>
   );
 }
