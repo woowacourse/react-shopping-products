51cf545351d62273a71d69bd056d95cbc4232c66
Soyeon Choe <soy2302ten@gmail.com>
2024-06-02 17:54:34 +0900

    chore: prettier 추가 및 적용

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly VITE_API_URL: string;
-	readonly VITE_USER_ID: string;
-	readonly VITE_USER_PASSWORD: string;
+  readonly VITE_API_URL: string;
+  readonly VITE_USER_ID: string;
+  readonly VITE_USER_PASSWORD: string;
 }
 
 interface ImportMeta {
-	readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
