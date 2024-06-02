cd8e3c9306191e2c67e0af9a19ada50e79b671a0
Hyelim Choi <HyelimChoi01@gmail.com>
2024-06-02 16:36:59 +0900

    chore: 불필요한 임포트 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { CartProvider } from '@/contexts/CartContext';
-import { ErrorProvider } from './contexts/ErrorContext';
 import { Global } from '@emotion/react';
-import Products from '@/pages/Products';
 import ProductsProvider from './pages/ProductsProvider';
 import baseStyle from '@/style/base.style';
 
