3a04b11f0e906d9dd422f4e7beac4690d91b65dc
rbgksqkr <rbgks1937@naver.com>
2024-06-02 16:08:04 +0900

    fix: 개발환경에서 msw 적용 안되는 문제 해결

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-shopping-products/',
+  base: process.env.NODE_ENV === 'production' ? '/react-shopping-products/' : '/',
   plugins: [react({ jsxImportSource: '@emotion/react' })],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
