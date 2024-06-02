61d485b8fb50c06e1ac3bf69b28cf3154837ac9d
이태훈 <rhymint@gmail.com>
2024-06-02 15:34:48 +0900

    fix: onClick prop을 넘겨주는 방식 변경

--- src/components/ToggleCartItemButton/ToggleCartItemButton.tsx
+++ src/components/ToggleCartItemButton/ToggleCartItemButton.tsx
@@ -1,7 +1,7 @@
-import { AddToCartIcon, RemoveFromCartIcon } from '../../assets';
-import Button from '../common/Button/Button';
+import { AddToCartIcon, RemoveFromCartIcon } from "../../assets";
+import Button from "../common/Button/Button";
 
-import * as S from './ToggleCartItemButton.style';
+import * as S from "./ToggleCartItemButton.style";
 
 interface ToggleCartItemButtonProps {
   isInCart: boolean;
@@ -17,14 +17,12 @@ function ToggleCartItemButton({
       width={60}
       size="s"
       radius="s"
-      color={isInCart ? 'secondary' : 'primary'}
-      onClick={() => {
-        onClick();
-      }}
+      color={isInCart ? "secondary" : "primary"}
+      onClick={onClick}
     >
       <S.ButtonContent>
         <S.Icon src={isInCart ? RemoveFromCartIcon : AddToCartIcon} />
-        <S.Content>{isInCart ? '빼기' : '담기'}</S.Content>
+        <S.Content>{isInCart ? "빼기" : "담기"}</S.Content>
       </S.ButtonContent>
     </Button>
   );
