4f9b8016115b8523915e03130584acc9da19cb10
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-02 15:55:27 +0900

    feat(ToastProvider): ToastContext 구현 및 로직 연결

--- src/layouts/GlobalLayout/index.tsx
+++ src/layouts/GlobalLayout/index.tsx
@@ -1,13 +1,16 @@
 import { CartItemProvider } from '../../context/CartItemProvider';
+import { ToastProvider } from '../../context/ToastProvider';
 import * as S from './style';
 
 import { PropsWithChildren } from 'react';
 
 const GlobalLayout = ({ children }: PropsWithChildren) => {
   return (
-    <CartItemProvider>
-      <S.Container>{children}</S.Container>
-    </CartItemProvider>
+    <ToastProvider>
+      <CartItemProvider>
+        <S.Container>{children}</S.Container>
+      </CartItemProvider>
+    </ToastProvider>
   );
 };
 
