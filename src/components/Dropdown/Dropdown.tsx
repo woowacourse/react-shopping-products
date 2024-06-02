60a15198defb528370bcc9e718a8e76234d30dd7
jinsil <jinsil378900@gmail.com>
2024-06-02 13:36:22 +0900

    refactor: dropdown semantic로 변경

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -3,7 +3,6 @@ import UpIcon from '../../assets/UpIcon.svg';
 import DownIcon from '../../assets/DownIcon.svg';
 import * as S from '../Dropdown/Dropdown.style';
 import { Option } from '../../types/Option.type';
-import { findByName } from '../../utils/option';
 
 interface DropdownProps {
   options: Option[];
@@ -15,41 +14,32 @@ interface DropdownProps {
 const Dropdown = ({ options, selectedOption, placeholder, updateOption }: DropdownProps) => {
   const [isOptionListOpen, setIsOptionListOpen] = useState(false);
 
-  const toggleOptionList = () => setIsOptionListOpen((prev) => !prev);
-
-  const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newOption = findByName(e.target.value, options);
+  const handleOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = e.target.value;
+    const newOption = options.find((option) => option.name === selectedValue);
 
     if (newOption) {
       updateOption(newOption);
     }
-
-    setIsOptionListOpen(false);
   };
 
+  const toggleOptionList = () => setIsOptionListOpen((prev) => !prev);
+
   return (
-    <S.Layout>
-      <S.Container $isDefault={!selectedOption} onClick={toggleOptionList}>
-        <p>{selectedOption.name || placeholder}</p>
-        <img src={isOptionListOpen ? UpIcon : DownIcon} alt={isOptionListOpen ? '옵션 닫기' : '옵션 열기'} />
-      </S.Container>
-      {isOptionListOpen && (
-        <S.OptionList>
+    <S.SelectContainer>
+      <S.SelectWrapper onClick={toggleOptionList}>
+        <S.Select value={selectedOption.name} onChange={handleOptionChange} $isDefault={!selectedOption}>
+          {placeholder && <option value="">{placeholder}</option>}
           {options.map((option) => (
-            <S.OptionItem key={option.key}>
-              <input
-                type="radio"
-                id={option.key}
-                value={option.name}
-                checked={option === selectedOption}
-                onChange={handleOptionChange}
-              ></input>
-              <S.OptionLabel htmlFor={option.key}>{option.name}</S.OptionLabel>
-            </S.OptionItem>
+            <option key={option.key} value={option.name}>
+              {option.name}
+            </option>
           ))}
-        </S.OptionList>
-      )}
-    </S.Layout>
+        </S.Select>
+
+        <S.ArrowIcon src={isOptionListOpen ? UpIcon : DownIcon} alt={isOptionListOpen ? '옵션 닫기' : '옵션 열기'} />
+      </S.SelectWrapper>
+    </S.SelectContainer>
   );
 };
 
