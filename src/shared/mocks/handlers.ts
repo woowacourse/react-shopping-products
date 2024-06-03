cca58fd1e0e2e36366e8111f2bbb8afb09c4f84d
novice0840 <magnolia0840@gmail.com>
2024-06-03 12:01:46 +0900

    refactor: 상수화

--- src/shared/mocks/handlers.ts
+++ src/shared/mocks/handlers.ts
@@ -1,11 +1,11 @@
-import { http, HttpResponse } from 'msw';
+import { http, HttpResponse } from "msw";
 
-import { Product, SORT_ORDERS, SortOrder } from '@/entities/product';
-import { ALL } from '@/features/product';
+import { Product, SORT_ORDERS, SortOrder } from "@/entities/product";
+import { ALL } from "@/features/product";
 
-import { PRODUCTS_ENDPOINT } from '../api/endpoints';
+import { PRODUCTS_ENDPOINT } from "../api/endpoints";
 
-import products from './products.json';
+import products from "./products.json";
 
 const sortByPrice = (products: Product[], sort: SortOrder) => {
   if (sort === SORT_ORDERS[0]) {
@@ -21,18 +21,23 @@ export const handlers = [
   http.get(PRODUCTS_ENDPOINT, ({ request }) => {
     const url = new URL(request.url);
 
-    const category = url.searchParams.get('category') || ALL;
-    const page = Number(url.searchParams.get('page') || '1');
-    const sort = (url.searchParams.get('sort') || 'ascByPrice') as SortOrder;
+    const category = url.searchParams.get("category") || ALL;
+    const page = Number(url.searchParams.get("page") || "1");
+    const sort = (url.searchParams.get("sort") || "ascByPrice") as SortOrder;
 
-    const size = page === 0 ? 20 : 4;
-    const start = page === 0 ? 0 : (page - 2) * 4 + 20;
+    const firstPageSize = 20;
+    const nextPageSize = 4;
+
+    const size = page === 0 ? firstPageSize : nextPageSize;
+    const start = page === 0 ? 0 : (page - 2) * nextPageSize + firstPageSize;
     const end = start + size;
     let paginatedProducts;
     if (category === ALL) {
       paginatedProducts = products.slice(start, end);
     } else {
-      paginatedProducts = products.filter((product) => product.category === category).slice(start, end);
+      paginatedProducts = products
+        .filter((product) => product.category === category)
+        .slice(start, end);
     }
 
     paginatedProducts = sortByPrice(paginatedProducts as Product[], sort);
