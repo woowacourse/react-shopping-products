68e37017c098cba5dde41c105085f9953298c378
simhorim <hori5020@gmail.com>
2024-06-03 15:02:28 +0900

    fix(products): 이름순이 먼저 반영이 되어 가격순이 정확하게 반영되지 않는 오류 해결

--- src/api/products.ts
+++ src/api/products.ts
@@ -13,8 +13,8 @@ export async function fetchProducts(
   if (category !== 'all') params.append('category', category);
   params.append('page', String(page));
   params.append('size', String(size));
-  params.append('sort', `name,asc`);
   params.append('sort', `price,${priceOrder}`);
+  params.append('sort', 'name,asc');
 
   const response = await fetch(`${PRODUCTS_ENDPOINT}?${params.toString()}`);
 
