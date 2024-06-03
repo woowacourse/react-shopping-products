dd5c50989ddb647255402c75abe547a2a377351b
bmj0053 <bmj05378@gmail.com>
2024-06-03 23:43:36 +0900

    fix: productSorter 불변성 추가
    Co-authored-by: skiende74 <skiende74@gmail.com>

--- src/utils/productSorter.ts
+++ src/utils/productSorter.ts
@@ -2,7 +2,8 @@ import { Product, ProductResponse } from '../types/fetch';
 import { Order } from '../types/sort';
 
 const productSorter = (sortings: string[], productCopy: ProductResponse) => {
-  const products = [...productCopy.content];
+  const products = productCopy.content.slice();
+
   sortings.forEach((sorting) => {
     const [name, order] = sorting.split(',') as [keyof Product, Order];
     if (name === 'name' || name === 'imageUrl' || name === 'category') {
