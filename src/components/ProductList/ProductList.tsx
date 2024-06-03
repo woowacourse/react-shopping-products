9512040870422d1d4644d3dd2d5370c23a4492b7
pakxe <pigkill40@naver.com>
2024-06-03 17:10:49 +0900

    feat: 리스트에 key 추가

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,4 +1,4 @@
-import { Product } from '@/types';
+import { Product } from '@/types/product.type';
 import ProductItem from '../ProductItem/ProductItem';
 import styles from './ProductList.module.css';
 
@@ -10,7 +10,7 @@ export default function ProductList({ productList }: { productList: Product[] })
       ) : (
         <>
           {productList.map((product) => (
-            <ProductItem {...product} />
+            <ProductItem {...product} key={product.id} />
           ))}
         </>
       )}
