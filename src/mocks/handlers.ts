a4d52b4bf1f025afe2db9d4f996361c7ff3a61f7
GimGayeon <kky1373@naver.com>
2024-06-04 00:41:40 +0900

    feat : msw handler 에서 데이터 사이즈 요청 url param 으로 결정하도록 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,14 +3,16 @@ import { http, HttpResponse } from 'msw';
 import productList from './products.json';
 
 import { END_POINT } from '@/api/endpoints';
+import { PRODUCT_DATA_SIZE } from '@/constants/productData';
 
 export const handlers = [
   http.get(END_POINT.products, ({ request }) => {
     const url = new URL(request.url);
 
     const page = Number(url.searchParams.get('page')) || 0;
-    const limit = page === 0 ? 20 : 4;
-    const start = page === 0 ? 0 : (page - 5) * 4 + 20;
+    const limit = Number(url.searchParams.get('size'));
+    const start =
+      page === 0 ? 0 : (page - 5) * PRODUCT_DATA_SIZE.nextPage + PRODUCT_DATA_SIZE.firstPage;
     const end = start + limit;
     const content = productList.content.slice(start, end);
 
