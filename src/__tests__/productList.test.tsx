114475fad58bc4897792498c205fa005c83346a5
rbgksqkr <rbgks1937@naver.com>
2024-06-03 17:56:48 +0900

    fix: 테스트 코드 provider 추가

--- src/__tests__/productList.test.tsx
+++ src/__tests__/productList.test.tsx
@@ -1,11 +1,19 @@
 import { renderHook, waitFor } from '@testing-library/react';
-import { act } from 'react';
+import { PropsWithChildren, act } from 'react';
+
+import { ToastContext } from '@/context/toast';
 
 import useProductList from '@/hooks/useProductList';
 
+const wrapper = ({ children }: PropsWithChildren) => (
+  <ToastContext.Provider value={{ isShow: true, error: () => {} }}>
+    {children}
+  </ToastContext.Provider>
+);
+
 describe('productList', () => {
   it('첫 페이지에서는 20개의 상품 목록을 불러온다.', async () => {
-    const { result } = renderHook(() => useProductList());
+    const { result } = renderHook(() => useProductList(), { wrapper });
 
     await waitFor(() => {
       expect(result.current.productList).toHaveLength(20);
@@ -14,7 +22,7 @@ describe('productList', () => {
   });
 
   it('다음 페이지에서는 4개의 상품 목록을 추가로 불러온다.', async () => {
-    const { result } = renderHook(() => useProductList());
+    const { result } = renderHook(() => useProductList(), { wrapper });
 
     await waitFor(() => {
       expect(result.current.productList).toHaveLength(20);
@@ -31,7 +39,7 @@ describe('productList', () => {
     });
   });
   it('마지막 페이지 일 때 상품 목록을 불러오지 않는다.', async () => {
-    const { result } = renderHook(() => useProductList());
+    const { result } = renderHook(() => useProductList(), { wrapper });
 
     await waitFor(() => {
       expect(result.current.productList.length).toBe(20);
@@ -62,7 +70,7 @@ describe('productList', () => {
   });
 
   it('상품 목록 데이터를 불러오기 전 로딩 화면이 표시된다.', async () => {
-    const { result } = renderHook(() => useProductList());
+    const { result } = renderHook(() => useProductList(), { wrapper });
 
     expect(result.current.isLoading).toBeTruthy();
 
