82f00a47a430a4e8e3ad8300e45c5cf175ef2bac
simhorim <hori5020@gmail.com>
2024-06-03 21:41:09 +0900

    refactor(ProductsContent): ProductItem 컴포넌트 합성 및 useProducts 전역상태 호출

--- src/components/ProductsContent/index.tsx
+++ src/components/ProductsContent/index.tsx
@@ -1,9 +1,26 @@
 import * as S from './style';
 
-import { PropsWithChildren } from 'react';
+import { useContext, useRef } from 'react';
 
-const ProductsContent = ({ children }: PropsWithChildren) => {
-  return <S.ProductsContent>{children}</S.ProductsContent>;
+import useIntersectionObserver from '../../hooks/useIntersectionObserver';
+import ProductItem from '../ProductItem';
+import { UseProductsContext } from '../ShoppingProductsPage';
+
+const ProductsContent = () => {
+  const { products, fetchNextPage, productsLoading } = useContext(UseProductsContext);
+
+  const observerRef = useRef<HTMLDivElement | null>(null);
+
+  useIntersectionObserver(productsLoading, observerRef, fetchNextPage, { threshold: 0.8 });
+
+  return (
+    <S.ProductsContent>
+      {products.map((product) => (
+        <ProductItem key={product.id} {...product} />
+      ))}
+      <div ref={observerRef} id="observer" style={{ height: '10px' }} />
+    </S.ProductsContent>
+  );
 };
 
 export default ProductsContent;
