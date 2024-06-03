a17514744b7119aabec4fa7588f2cf294476d3e9
simhorim <hori5020@gmail.com>
2024-06-03 21:40:23 +0900

    refactor(FilterContainer): dropdown 컴포넌트 합성 및 useProducts 전역상태 호출

--- src/components/FilterContainer/index.tsx
+++ src/components/FilterContainer/index.tsx
@@ -1,9 +1,37 @@
 import * as S from './style';
 
-import { PropsWithChildren } from 'react';
+import { useContext } from 'react';
 
-const FilterContainer = ({ children }: PropsWithChildren) => {
-  return <S.FilterContainer>{children}</S.FilterContainer>;
+import Dropdown from '../common/Dropdown';
+import { UseProductsContext } from '../ShoppingProductsPage';
+
+import { CATEGORY, PRICE_SORT } from '../../constants/filter';
+
+const FilterContainer = () => {
+  const { category, priceOrder, handleCategoryChange, handlePriceOrderChange } =
+    useContext(UseProductsContext);
+
+  const selectedCategoryOption = Object.entries(CATEGORY).find(
+    ([, value]) => value === category,
+  )![0];
+  const selectedPriceOrderOption = Object.entries(PRICE_SORT).find(
+    ([, value]) => value === priceOrder,
+  )![0];
+
+  return (
+    <S.FilterContainer>
+      <Dropdown
+        optionArray={Object.keys(CATEGORY)}
+        selectedOption={selectedCategoryOption}
+        optionChange={(option) => handleCategoryChange(CATEGORY[option])}
+      />
+      <Dropdown
+        optionArray={Object.keys(PRICE_SORT)}
+        selectedOption={selectedPriceOrderOption}
+        optionChange={(option) => handlePriceOrderChange(PRICE_SORT[option])}
+      />
+    </S.FilterContainer>
+  );
 };
 
 export default FilterContainer;
