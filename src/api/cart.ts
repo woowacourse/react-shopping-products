fde90d553bb330554a52ac90b36cdca85901b77b
chysis <chysiss@naver.com>
2024-06-03 16:06:36 +0900

    refactor: 장바구니 아이템을 fetch하는 함수의 반환형 명시

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -1,8 +1,9 @@
+import { CartItem } from '../types';
 import { generateQueryParams } from '../utils/generateQueryParams';
 import { HEADERS } from './common';
 import { CART_ITEMS_ENDPOINT } from './endpoints';
 
-export const fetchCartItems = async () => {
+export const fetchCartItems = async (): Promise<CartItem[]> => {
   const params = generateQueryParams({ size: 100 });
   const response = await fetch(`${CART_ITEMS_ENDPOINT}?${params}`, {
     method: 'GET',
