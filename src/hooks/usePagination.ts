192ada3010266f6dd03114dcccfe64f500bad210
anttiey <hanuuny@gmail.com>
2024-06-03 13:14:44 +0900

    comment(usePagination): moveNextPage 함수 주석 추가

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -12,6 +12,11 @@ const usePagination = (): UsePaginationResult => {
 
   const resetPage = () => setPage(0);
 
+  /**
+   * page 번호가 0일 때 초기 데이터 패칭이므로 20개의 데이터를 가져온다.
+   * 이때 실질적으로 각 page 마다 4개의 데이터를 가지고 있어 1 ~ 4 page 의 데이터는 초기에 가져온 데이터와 중복된다.
+   * 따라서 page 번호가 0일 때 다음 번호를 5로 세팅하여 데이터 중복을 피했습니다.
+   */
   const moveNextPage = (loading: boolean, isLast: boolean) => {
     if (!loading && !isLast) {
       setPage((prevPage) => (prevPage === 0 ? PAGE_INTERVAL.INITIAL : prevPage + PAGE_INTERVAL.DEFAULT));
