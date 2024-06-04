a0b4c915cec27dad72391c4388f61a9876b805f9
이태훈 <rhymint@gmail.com>
2024-06-04 20:55:46 +0900

    feaat: useAddCartItem, useDeleteCartItem hook 구현

---
+++ src/hooks/useAddCartItem.ts
@@ -0,0 +1,14 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { addCartItem } from '../apis/carItems';
+import { QUERY_KEYS } from '../apis/config';
+
+export default function useAddCartItem() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: addCartItem,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.CART_ITMES] });
+    },
+  });
+}
---
+++ src/hooks/useDeleteCartItem.ts
@@ -0,0 +1,14 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deleteCartItem } from '../apis/carItems';
+import { QUERY_KEYS } from '../apis/config';
+
+export default function useDeleteCartItem() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteCartItem,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.CART_ITMES] });
+    },
+  });
+}
