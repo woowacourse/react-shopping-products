cf7dbd395ee11d691a403177d0dfbbf44fc94faa
useon <yoosk4986@naver.com>
2024-06-04 20:54:53 +0900

    refactor: useEffect훅에서 별도의 함수로 분리

--- src/hooks/useProductFetch.ts
+++ src/hooks/useProductFetch.ts
@@ -22,29 +22,30 @@ export function useProductFetch({ selectBarCondition }: Props) {
     setHasMore(true);
   }, [selectBarCondition.category, selectBarCondition.sort]);
 
-  useEffect(() => {
-    const setFetchedProducts = async () => {
-      try {
-        setIsLoading(true);
-        const { size, queryPage } = calculatePageParams(page);
-        const newProducts = await fetchProducts({
-          page: queryPage,
-          size,
-          category: selectBarCondition.category,
-          sort: formattedKey(selectBarCondition.sort),
-        });
-        setProducts((prev) => [...prev, ...newProducts]);
-        setHasMore(newProducts.length > 0);
-      } catch (error) {
-        if (error instanceof Error) {
-          console.error(error.message);
-          showToast('상품 목록을 불러오는데 실패했습니다. 잠시 후 다시 시도해주세요.');
-        }
-      } finally {
-        setIsLoading(false);
+  const fetchMoreProducts = async () => {
+    try {
+      setIsLoading(true);
+      const { size, queryPage } = calculatePageParams(page);
+      const newProducts = await fetchProducts({
+        page: queryPage,
+        size,
+        category: selectBarCondition.category,
+        sort: formattedKey(selectBarCondition.sort),
+      });
+      setProducts((prev) => [...prev, ...newProducts]);
+      setHasMore(newProducts.length > 0);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+        showToast('상품 목록을 불러오는데 실패했습니다. 잠시 후 다시 시도해주세요.');
       }
-    };
-    setFetchedProducts();
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMoreProducts();
   }, [page, selectBarCondition.category, selectBarCondition.sort]);
 
   return { products, setPage, hasMore, isLoading };
