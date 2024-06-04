c3f90f639075fc38f8ae5ec9ec1857bf4f9d4d19
이태훈 <rhymint@gmail.com>
2024-06-04 22:31:10 +0900

    fix: AddCartItemButton prop 수정

--- src/components/AddCartItemButton/AddCartItemButton.tsx
+++ src/components/AddCartItemButton/AddCartItemButton.tsx
@@ -1,4 +1,4 @@
-import { AddToCartIcon } from '../../assets';
+import { AddToCartIcon } from '../../assets/';
 import Button from '../common/Button/Button';
 
 import * as S from './AddCartItemButton.style';
@@ -9,7 +9,7 @@ interface AddCartItemButtonProps {
 
 function AddCartItemButton({ onClick }: AddCartItemButtonProps) {
   return (
-    <Button width={60} size="s" radius="s" onClick={onClick}>
+    <Button width={60} size="s" radius="s" color="primary" onClick={onClick}>
       <S.ButtonContent>
         <AddToCartIcon />
         <S.Content>{'담기'}</S.Content>
