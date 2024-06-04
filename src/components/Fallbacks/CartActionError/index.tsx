db3b9036bd3524fd831227ba3463b4b7e6c93ff3
김진호 <rlawlsgh1227@gmail.com>
2024-06-04 14:27:12 +0900

    fix: fallback 한 줄로 보이도록 css 수정

--- src/components/Fallbacks/CartActionError/index.tsx
+++ src/components/Fallbacks/CartActionError/index.tsx
@@ -1,7 +1,7 @@
 import style from './style.module.css';
 
 function CartActionError() {
-  const errorMessage = `오류가 발생했습니다. ${window.innerWidth <= 500 ? '\n' : ''} 잠시 후 다시 시도해 주세요.`;
+  const errorMessage = `오류가 발생했습니다. 잠시 후 다시 시도해 주세요.`;
   return <div className={style.cartAction}>{errorMessage}</div>;
 }
 
