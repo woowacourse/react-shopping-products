aa8d2ba0df3c1de93ad54fbce86d03c27194e96c
badahertz52 <badahertz52@gmail.com>
2024-06-04 19:24:55 +0900

    feat: 장바구니 목록 배지 아이콘 클릭 시, 장바구니 목록 모달 여는 기능 추가

--- src/components/Header/CartBadgeButton/index.tsx
+++ src/components/Header/CartBadgeButton/index.tsx
@@ -1,26 +1,37 @@
 import { CartIcon } from '@assets/index';
+import CartListModal from '@components/CartListModal';
+import { useState } from 'react';
 
 import style from './style.module.css';
 
 const BADGE_MAX_QUANTITY = 50;
 
 interface CartActionButtonProps {
+  // TODO : 삭제 (장바구니 받아오는 것으로, CartListModal에 props로 전달)
   cartItemsLength: number;
 }
 
-function CartBadgeButton({ cartItemsLength }: CartActionButtonProps) {
+const CartBadgeButton = ({ cartItemsLength }: CartActionButtonProps) => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
   const quantityClassName = `${style.quantity} ${cartItemsLength > BADGE_MAX_QUANTITY ? style.over : ''}`;
 
+  const handleClickButton = () => {
+    setIsOpenModal(true);
+  };
+
   return (
-    <button type="button" className={style.button}>
-      <img src={CartIcon} alt="장바구니 아이콘(장바구니 페이지 이동)" />
-      {!!cartItemsLength && (
-        <div className={quantityClassName}>
-          {cartItemsLength > BADGE_MAX_QUANTITY ? BADGE_MAX_QUANTITY : cartItemsLength}
-        </div>
-      )}
-    </button>
+    <>
+      <button type="button" className={style.button} onClick={handleClickButton}>
+        <img src={CartIcon} alt="장바구니 아이콘(장바구니 목록 열기)" />
+        {!!cartItemsLength && (
+          <div className={quantityClassName}>
+            {cartItemsLength > BADGE_MAX_QUANTITY ? BADGE_MAX_QUANTITY : cartItemsLength}
+          </div>
+        )}
+      </button>
+      <CartListModal isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal} />
+    </>
   );
-}
+};
 
 export default CartBadgeButton;
