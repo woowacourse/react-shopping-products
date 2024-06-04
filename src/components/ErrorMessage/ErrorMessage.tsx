08327f708b4841a590e362e33564b663d3539003
skiende74 <skiende74@gmail.com>
2024-06-04 16:36:46 +0900

    refactor(ErrorMessage): 메시지를 프롭으로 받게 수정
    Co-authored-by: skiende74 <skiende74@gmail.com>

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,7 +1,11 @@
-import * as S from './ErrorMessage.styled'
-
-function ErrorMessage() {
-  return <S.ErrorMessageContainer>오류가 발생했습니다. 잠시 후 다시 시도해 주세요.</S.ErrorMessageContainer>;
+import * as S from './ErrorMessage.styled';
+interface ErrorMessageProps {
+  message: string;
+}
+function ErrorMessage({
+  message = '오류가 발생했습니다. 잠시 후 다시 시도해 주세요.',
+}: ErrorMessageProps) {
+  return <S.ErrorMessageContainer>{message}</S.ErrorMessageContainer>;
 }
 
 export default ErrorMessage;
