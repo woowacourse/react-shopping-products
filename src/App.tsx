e596c396fc3131a51c6bc5ec1a170f2eb23ca3e6
river <130737187+0jenn0@users.noreply.github.com>
2024-06-04 21:12:14 +0900

    feat: QueryClientProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,19 @@ import ProductPage from './page/ProductPage';
 import { ToastContextProvider } from './contexts/ToastContextProvider';
 import { CartProvider } from '@/contexts/CartListContext';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <ToastContextProvider>
-      <CartProvider>
-        <ProductPage />
-      </CartProvider>
-    </ToastContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ToastContextProvider>
+        <CartProvider>
+          <ProductPage />
+        </CartProvider>
+      </ToastContextProvider>
+    </QueryClientProvider>
   );
 }
 
