a89d5dd5877d6252a60aaefde1fc97319f517428
이태훈 <rhymint@gmail.com>
2024-06-04 16:50:26 +0900

    fix: cartItems api 메소드 이름 변경

--- src/apis/carItems.ts
+++ src/apis/carItems.ts
@@ -1,7 +1,7 @@
 import { END_POINTS } from './config';
 import response from './response';
 
-export async function requestFetchCartItemList() {
+export async function fetchCartItemList() {
   const data = await response({
     url: `${END_POINTS.CART_ITMES}?size=2000`,
     method: 'GET',
@@ -9,7 +9,7 @@ export async function requestFetchCartItemList() {
   return data;
 }
 
-export async function requestAddCartItem(productId: number, quantity: number) {
+export async function addCartItem(productId: number, quantity: number) {
   await response({
     url: `${END_POINTS.CART_ITMES}`,
     method: 'POST',
@@ -17,7 +17,7 @@ export async function requestAddCartItem(productId: number, quantity: number) {
   });
 }
 
-export async function requestDeleteCartItem(cartItemId: number | undefined) {
+export async function deleteCartItem(cartItemId: number | undefined) {
   await response({
     url: `${END_POINTS.CART_ITMES}/${cartItemId}`,
     method: 'DELETE',
