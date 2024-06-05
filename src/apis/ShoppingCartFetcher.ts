7c6edd415a5fe2297caeeea8a5077a125ea8a6b8
ss0526100 <ss0526100@gmail.com>
2024-06-06 00:45:30 +0900

    feat(ShoppingCartFetcher):updateCartItemQuantity 구현

--- src/apis/ShoppingCartFetcher.ts
+++ src/apis/ShoppingCartFetcher.ts
@@ -5,21 +5,38 @@ export default class ShoppingCartFetcher {
   static async getCartItems(): Promise<CartItem[]> {
     const response = await APIClient.get('cart-items');
 
-    APIClient.validateResponse(response, '장바구니 목록을 불러오는데 실패했습니다.');
+    APIClient.validateResponse(
+      response,
+      '장바구니 목록을 불러오는데 실패했습니다.'
+    );
 
     const data = await response.json();
 
     return data.content;
   }
 
+  static async updateCartItemQuantity(cartItemId: number, quantity: number) {
+    const response = await APIClient.patch(`cart-items/${cartItemId}`, {
+      quantity,
+    });
+
+    APIClient.validateResponse(response, '수량을 변경하지 못했습니다.');
+  }
+
   static async deleteCartItem(id: number) {
     const response = await APIClient.delete(`cart-items/${id}`);
 
-    APIClient.validateResponse(response, '장바구니 아이템을 삭제하지 못했습니다.');
+    APIClient.validateResponse(
+      response,
+      '장바구니 아이템을 삭제하지 못했습니다.'
+    );
   }
 
   static async addProduct(id: number) {
-    const response = await APIClient.post(`cart-items`, { productId: id, quantity: 1 });
+    const response = await APIClient.post(`cart-items`, {
+      productId: id,
+      quantity: 1,
+    });
 
     APIClient.validateResponse(response, '장바구니에 물건을 담지 못했습니다.');
   }
