08a7a93abe4e5804ba14425e4e615c125355c8a5
ss0526100 <ss0526100@gmail.com>
2024-06-06 04:52:03 +0900

    feat(App.tsx): QueryClient 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { Global } from '@emotion/react';
 import ProductPage from '@pages/ProductPage/ProductPage';
 import { resetCSS } from '@styles/resetCSS';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Global styles={resetCSS} />
       <ProductPage />
-    </>
+    </QueryClientProvider>
   );
 }
 
