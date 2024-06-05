73a9d0e084a68509d9898eb8a2f03e262bd9c3c1
jayming66 <mjw8941@gmail.com>
2024-06-05 16:29:24 +0900

    feat(useProducts.test.ts): 테스트 추가 - '다음 페이지 상품 4개를 불러온다'

--- src/hooks/useProducts.test.ts
+++ src/hooks/useProducts.test.ts
@@ -1,20 +1,44 @@
 import { ToastProvider } from './../context/ToastProvider';
-import { renderHook, waitFor } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 import { useProductFetch } from './useProductFetch';
 
 describe('useProducts', () => {
   describe('상품 목록 조회', () => {
+    const initialSelectBarCondition = {
+      category: 'all',
+      sort: 'priceAsc',
+    };
     it('초기 렌더링 시, 전체 목록 중 20개의 상품 목록을 가져온다.', async () => {
-      const selectBarCondition = {
-        category: 'all',
-        sort: 'priceAsc',
-      };
-      const { result } = renderHook(() => useProductFetch({ selectBarCondition }), {
-        wrapper: ToastProvider,
-      });
+      const { result } = renderHook(
+        () => useProductFetch({ selectBarCondition: initialSelectBarCondition }),
+        {
+          wrapper: ToastProvider,
+        },
+      );
 
       await waitFor(() => {
         expect(result.current.products).toHaveLength(20);
+      });
+    });
+
+    it('다음 페이지 상품 4개를 불러온다.', async () => {
+      const { result } = renderHook(
+        () => useProductFetch({ selectBarCondition: initialSelectBarCondition }),
+        {
+          wrapper: ToastProvider,
+        },
+      );
+
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(20);
+      });
+
+      act(() => {
+        result.current.increaseNextPage();
+      });
+
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(24);
       });
     });
   });
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,10 +1,22 @@
 import { http, HttpResponse } from 'msw';
 import products from './products.json';
+import { AFTER_FETCH_SIZE, FIRST_FETCH_PAGE, FIRST_FETCH_SIZE } from '../constant/products';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const handlers = [
-  http.get(`${API_URL}/products`, () => {
-    return HttpResponse.json(products);
+  http.get(`${API_URL}/products`, ({ request }) => {
+    const url = new URL(request.url);
+    const page = Number(url.searchParams.get('page') || '0');
+    const size = page === FIRST_FETCH_PAGE ? FIRST_FETCH_SIZE : AFTER_FETCH_SIZE;
+
+    const startIndex =
+      page === FIRST_FETCH_PAGE ? page : AFTER_FETCH_SIZE * (page - 5) + FIRST_FETCH_SIZE;
+    const endIndex = startIndex + size;
+
+    const formattedProducts = products.content.slice(startIndex, endIndex);
+    const last = !products.content[endIndex + 1];
+
+    return HttpResponse.json({ last, content: [...formattedProducts] });
   }),
 ];
