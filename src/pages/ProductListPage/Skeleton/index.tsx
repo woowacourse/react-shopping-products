c66c566739597a05600f9dafee692f41d5c49da4
badahertz52 <badahertz52@gmail.com>
2024-06-05 15:55:26 +0900

    fix: 확장자 오류 수정

--- src/pages/ProductListPage/Skeleton/index.tsx
+++ src/pages/ProductListPage/Skeleton/index.tsx
@@ -1,2 +1,3 @@
+export { default as ProductListPageSkeleton } from './ProductListPageSkeleton';
 export { default as ProductCardSkeleton } from './ProductCardSkeleton';
 export { default as ProductListSkeleton } from './ProductListSkeleton';
