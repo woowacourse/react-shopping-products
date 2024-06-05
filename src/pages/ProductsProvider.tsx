8e363663519e7964635261c69c4b7dbb118e8909
Hyelim Choi <HyelimChoi01@gmail.com>
2024-06-05 17:12:29 +0900

    chore: cartList provider 삭제

--- src/pages/ProductsProvider.tsx
+++ src/pages/ProductsProvider.tsx
@@ -1,4 +1,3 @@
-import { CartProvider } from '@/contexts/CartContext';
 import { ErrorProvider } from '@/contexts/ErrorContext';
 import Products from './Products';
 import { Suspense } from 'react';
@@ -6,11 +5,9 @@ import { Suspense } from 'react';
 const ProductsProvider = () => {
   return (
     <ErrorProvider>
-      <CartProvider>
-        <Suspense>
-          <Products />
-        </Suspense>
-      </CartProvider>
+      <Suspense>
+        <Products />
+      </Suspense>
     </ErrorProvider>
   );
 };
