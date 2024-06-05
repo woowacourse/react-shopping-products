e7350c6fd6046edffd0c009826c03e9ec09f4fd7
river <130737187+0jenn0@users.noreply.github.com>
2024-06-05 14:06:04 +0900

    refactor: asset -> assets로 폴더명 수정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,5 @@ export { default as LogoIcon } from './LogoIcon.svg';
 export { default as ShoppingCart } from './ShoppingCart.svg';
 export { default as AddCart } from './AddCart.svg';
 export { default as MinusCart } from './MinusCart.svg';
+export { default as MinusIcon } from './MinusIcon.svg';
+export { default as PlusIcon } from './PlusIcon.svg';
