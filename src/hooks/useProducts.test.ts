0f51f33452a90250b87812ad57c238f182b480f2
useon <yoosk4986@naver.com>
2024-06-05 16:35:18 +0900

    refactor: 상품 목록 조회 테스트 코드 수정

--- src/hooks/useProducts.test.ts
+++ src/hooks/useProducts.test.ts
@@ -1,13 +1,21 @@
 import { renderHook, waitFor } from '@testing-library/react';
 import useProducts from './useProducts';
+import { ToastProvider } from '../context/ToastProvider';
 
 describe('useProducts', () => {
   describe('상품 목록 조회', () => {
-    it('초기 렌더링 시, 전체 목록 중 20개의 상품 목록을 가져온다.', async () => {
-      const { result } = renderHook(() => useProducts());
+    it('초기 상품 목록을 가져온다', async () => {
+      const { result } = renderHook(() => useProducts(), { wrapper: ToastProvider });
+
       await waitFor(() => {
-        expect(result.current.products).toHaveLength(20);
+        expect(result.current.products.length).toBeGreaterThan(0);
       });
+    });
+
+    it('초기 상품 목록을 가져올 때 로딩 상태를 보여준다', () => {
+      const { result } = renderHook(() => useProducts(), { wrapper: ToastProvider });
+
+      expect(result.current.isLoading).toBe(true);
     });
   });
 });
