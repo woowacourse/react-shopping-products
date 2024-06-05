574098251abcb382e2a2ec5646ff4b227d2d1701
pakxe <pigkill40@naver.com>
2024-06-05 17:12:19 +0900

    feat: 상품 목록을 infiniteQuery를 사용하도록 수정

--- src/hooks/useProductList.test.tsx
+++ src/hooks/useProductList.test.tsx
@@ -1,130 +1,71 @@
 import useProductList, { PAGE } from './useProductList';
 import { waitFor, renderHook, act } from '@testing-library/react';
-import { ChangeEvent } from 'react';
-import ToastProvider from './useToast';
-import { SortValue } from '@/constants/filter';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+
+let queryClient: any;
+let wrapper: any;
+
+beforeEach(() => {
+  queryClient = new QueryClient();
+  wrapper = ({ children }: { children: ReactNode }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+});
 
 describe('useProductList', () => {
-  const wrapper = ({ children }: { children: React.ReactNode }) => (
-    <ToastProvider>{children}</ToastProvider>
-  );
-  describe('상품 목록 조회', () => {
-    it('상품 목록을 조회한다.', async () => {
-      const { result } = renderHook(() => useProductList(), { wrapper });
-
-      act(() => result.current.fetchNextPage());
-
-      await waitFor(() => {
-        expect(result.current.productList).toHaveLength(PAGE.START_SIZE);
-      });
-    });
-
-    it('상품 목록 조회 중엔 로딩 상태가 된다.', () => {
-      const { result } = renderHook(() => useProductList(), { wrapper });
-
-      act(() => result.current.fetchNextPage());
-
-      expect(result.current.loading).toBe(true);
-    });
-
-    it('상품 목록 조회가 끝나면 로딩 상태도 끝난다.', async () => {
-      const { result } = renderHook(() => useProductList(), { wrapper });
-
-      act(() => result.current.fetchNextPage());
-
-      await waitFor(() => {
-        expect(result.current.loading).toBe(false);
-      });
-    });
-  });
-
   describe('페이지네이션', () => {
     it(`첫 페이지는 ${PAGE.START_SIZE}개의 상품 목록을 불러온다.`, async () => {
       const { result } = renderHook(() => useProductList(), { wrapper });
 
-      act(() => result.current.fetchNextPage());
+      await act(() => result.current.fetchNextPage());
 
       await waitFor(() => {
-        expect(result.current.productList).toHaveLength(PAGE.START_SIZE);
+        expect(result.current.data).toHaveLength(PAGE.START_SIZE);
       });
     });
 
     it(`이후 페이지는 ${PAGE.SIZE}개의 상품 목록을 불러온다.`, async () => {
       const { result } = renderHook(() => useProductList(), { wrapper });
 
-      act(() => result.current.fetchNextPage());
-
-      await waitFor(() => {
-        expect(result.current.productList).toHaveLength(PAGE.START_SIZE);
-        expect(result.current.page).toBe(PAGE.START);
+      await act(() => {
+        result.current.fetchNextPage();
       });
 
-      act(() => result.current.fetchNextPage());
+      await waitFor(() => {
+        expect(result.current.data).toHaveLength(PAGE.START_SIZE);
+      });
+
+      await act(() => result.current.fetchNextPage());
 
       await waitFor(() => {
-        expect(result.current.productList).toHaveLength(PAGE.START_SIZE + PAGE.SIZE);
-        expect(result.current.page).toBe(PAGE.START + 1);
+        expect(result.current.data).toHaveLength(PAGE.START_SIZE + PAGE.SIZE);
       });
     });
   });
 
-  describe('필터 변경 발생', () => {
-    it(`정렬 기준이 바뀌면 ${PAGE.START}페이지부터 다시 받아온다.`, async () => {
+  describe('상품 목록 조회', () => {
+    it('상품 목록을 조회한다.', async () => {
       const { result } = renderHook(() => useProductList(), { wrapper });
-      const sortValue: SortValue = 'price,desc';
-      act(() => {
-        result.current.fetchNextPage();
-        result.current.fetchNextPage(); // 연속 페치가 될 지 확신 없음
-      });
-
-      await waitFor(() => {
-        expect(result.current.page).toBe(PAGE.START + 1);
-      });
 
       act(() => {
-        const event = {
-          target: {
-            value: sortValue,
-          },
-        } as ChangeEvent<HTMLSelectElement>;
-
-        result.current.handleSortType(event.target.value as SortValue);
+        result.current.fetchNextPage(); // 대괄호로 묶어주지 않으면 오류발생
       });
 
+      await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
       await waitFor(() => {
-        expect(result.current.page).toBe(PAGE.START);
+        expect(result.current.data).toHaveLength(PAGE.START_SIZE);
       });
     });
 
-    it(`카테고리가 바뀌면 ${PAGE.START}페이지부터 다시 받아온다.`, async () => {
+    it('상품 목록 조회가 끝나면 로딩 상태도 끝난다.', async () => {
       const { result } = renderHook(() => useProductList(), { wrapper });
 
-      const targetCategory = 'electronics';
-
-      act(() => {
-        result.current.fetchNextPage();
-        result.current.fetchNextPage();
-      });
+      await act(() => result.current.fetchNextPage());
 
       await waitFor(() => {
-        expect(result.current.page).toBe(PAGE.START + 1);
-      });
-
-      act(() => {
-        const event = {
-          target: {
-            value: 'electronics',
-          },
-        } as ChangeEvent<HTMLSelectElement>;
-
-        result.current.handleCategory(event.target.value);
-      });
-
-      await waitFor(() => {
-        expect(result.current.page).toBe(PAGE.START);
-        expect(
-          result.current.productList.every(({ category }) => category === targetCategory),
-        ).toBe(true);
+        expect(result.current.isLoading).toBe(false);
       });
     });
   });
--- src/hooks/useProductList.tsx
+++ src/hooks/useProductList.tsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from 'react';
-import { CATEGORY_OPTION_LIST, FILTER_OPTION_LIST, SortValue } from '@/constants/filter';
 import { requestProductList } from '@/apis/request/product';
-import { useToast } from './useToast';
-import { Product } from '@/types/product.type';
+import { CATEGORY_OPTION_LIST, FILTER_OPTION_LIST, SortValue } from '@/constants/filter';
+import { QUERY_KEYS } from '@/constants/queryKeys';
 import { Category } from '@/types/filter.type';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export const PAGE = {
   START: 0,
@@ -12,101 +12,60 @@ export const PAGE = {
 };
 
 const ERROR_MESSAGE = {
+  GET_PRODUCTS: '오류가 발생했습니다. 잠시 후 다시 시도해 주세요.',
   INVALID_SORT_TYPE: '올바른 정렬 기준이 아닙니다.',
   INVALID_CATEGORY: '올바른 카테고리가 아닙니다.',
 };
 
 const useProductList = () => {
-  const [productList, setProductList] = useState<Product[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [page, setPage] = useState(PAGE.START - 1);
-  const [totalPage, setTotalPage] = useState<number | null>(null);
-
   const [sortType, setSortType] = useState<SortValue>('price,asc');
   const [category, setCategory] = useState<Category>('all');
-
-  const { showToast } = useToast();
-  const [error, setError] = useState<Error | null>(null);
-
-  useEffect(() => {
-    if (page === -1) return;
-
-    const getProducts = async () => {
-      try {
-        setLoading(true);
-        const size = page === PAGE.START ? PAGE.START_SIZE : PAGE.SIZE;
-        const { paginatedProducts, totalPages: curTotalPage } = await requestProductList({
-          page,
-          category,
-          sortType,
-          size,
-        });
-
-        if (curTotalPage !== totalPage) setTotalPage(curTotalPage);
-
-        setProductList((prevProducts) => [...prevProducts, ...paginatedProducts]);
-      } catch (error) {
-        setError(new Error('오류가 발생했습니다. 잠시 후 다시 시도해 주세요.'));
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getProducts();
-  }, [page, sortType, category]);
 
   const handleSortType = (sortValue: string) => {
     const sortType = FILTER_OPTION_LIST.find((sortOption) => sortOption.value === sortValue);
 
     if (!sortType) {
-      setError(new Error(ERROR_MESSAGE.INVALID_SORT_TYPE));
+      // setError(new Error(ERROR_MESSAGE.INVALID_SORT_TYPE));
+      alert(ERROR_MESSAGE.INVALID_SORT_TYPE);
       return;
     }
 
     setSortType(sortType.value);
-    resetPage();
   };
 
   const handleCategory = (value: string) => {
     const category = CATEGORY_OPTION_LIST.find((categoryItem) => categoryItem.value === value);
 
     if (!category) {
-      setError(new Error(ERROR_MESSAGE.INVALID_CATEGORY));
+      // setError(new Error(ERROR_MESSAGE.INVALID_CATEGORY));
+      alert(ERROR_MESSAGE.INVALID_CATEGORY);
       return;
     }
 
     setCategory(category.value);
-    resetPage();
   };
 
-  const resetPage = () => {
-    setPage(PAGE.START);
-    setProductList([]);
-  };
+  // const { showToast } = useToast();
 
-  const fetchNextPage = () => {
-    if (loading) return;
-    if (totalPage === page) return;
-
-    setPage((prevPage) => prevPage + 1);
-  };
-
-  useEffect(() => {
-    if (error) {
-      showToast({ message: error.message, type: 'alert' });
-      setError(null);
-    }
-  }, [error]);
+  const queryInfo = useInfiniteQuery({
+    queryKey: [QUERY_KEYS.PRODUCT, category, sortType],
+    initialPageParam: 0,
+    queryFn: ({ pageParam }) =>
+      requestProductList({
+        page: pageParam,
+        size: pageParam === 0 ? PAGE.START_SIZE : PAGE.SIZE,
+        category,
+        sortType,
+      }),
+    select: (data) => (data.pages ?? []).flatMap(({ content }) => content),
+    getNextPageParam: (lastPage) => (lastPage.hasNextPage ? lastPage.nextCursor : null),
+    staleTime: 6 * 10 * 60 * 1000,
+  });
 
   return {
+    ...queryInfo,
     handleCategory,
     handleSortType,
-    sortType,
-    category,
-    fetchNextPage,
-    productList,
-    loading,
-    page,
   };
 };
 
