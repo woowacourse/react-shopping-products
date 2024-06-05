05ea2058eb817a113fd8f57c57b99cd336489b77
pakxe <pigkill40@naver.com>
2024-06-05 17:09:26 +0900

    feat: 서버에서 준 데이터 타입 정의

---
+++ src/apis/responseTypes.ts
@@ -0,0 +1,60 @@
+import { CartItem } from '@/types/cartItem.type';
+import { Product } from '@/types/product.type';
+
+export type ResponseProductList = {
+  content: Product[];
+  pageable: {
+    sort: {
+      sorted: boolean;
+      unsorted: boolean;
+      empty: boolean;
+    };
+    pageNumber: number;
+    pageSize: number;
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  sort: {
+    sorted: boolean;
+    unsorted: boolean;
+    empty: boolean;
+  };
+  first: boolean;
+  number: number;
+  numberOfElements: number;
+  size: number;
+  empty: boolean;
+};
+
+export type ResponseCartItemList = {
+  content: CartItem[];
+  pageable: {
+    sort: {
+      sorted: boolean;
+      unsorted: boolean;
+      empty: boolean;
+    };
+    pageNumber: number;
+    pageSize: number;
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  sort: {
+    sorted: boolean;
+    unsorted: boolean;
+    empty: boolean;
+  };
+  first: boolean;
+  number: number;
+  numberOfElements: number;
+  size: number;
+  empty: boolean;
+};
