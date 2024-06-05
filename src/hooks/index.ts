db4b8ce4a30fd3d6832419dcd53defa8c489ad78
badahertz52 <badahertz52@gmail.com>
2024-06-05 15:48:23 +0900

    refactor: 훅 export 정리

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,6 @@
-export { default as useCartItemIds } from './cart/useCartItemIds';
-export { default as useCartAction } from './cart/useCartAction';
-export { default as useStackProducts } from './productList/useStackProducts';
-export { default as useFilteredProducts } from './productList/useFilteredProducts';
 export { default as useFetch } from './useFetch';
 export { default as useIntersectionObserver } from './useIntersectionObserver';
 export { default as useTargetContext } from './useTargetContext';
+export * from './productList';
+export * from './modal';
+export * from './cart';
---
+++ src/hooks/productList/index.ts
@@ -0,0 +1,2 @@
+export { default as useStackProducts } from './useStackProducts';
+export { default as useFilteredProducts } from './useFilteredProducts';
