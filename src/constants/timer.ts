d83f2a79090545e86cdcf06c1e1cd9c53e2c76c7
Lee sang Yeop <mma7710@naver.com>
2024-06-05 15:49:40 +0900

    refactor(timer): 시간 단위를 상수로 정의

---
+++ src/constants/timer.ts
@@ -0,0 +1,7 @@
+const TIMER = {
+  second: 1000,
+  minute: 1000 * 60,
+  hour: 1000 * 60 * 24,
+};
+
+export default TIMER;
--- src/hooks/useFetchProductItems.ts
+++ src/hooks/useFetchProductItems.ts
@@ -1,13 +1,15 @@
 import { getProducts } from "@/apis/product";
 import QUERY_KEY from "@/constants/queryKey";
 import { Category, Sort } from "@/constants/selectOption";
+import TIMER from "@/constants/timer";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
 const useFetchProductItems = (category: Category, sort: Sort) => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEY.getProducts, category, sort],
     queryFn: ({ pageParam = 0 }) => getProducts({ category, page: pageParam, sort }),
-    staleTime: 1000 * 60 * 5,
+    staleTime: TIMER.minute * 10,
+    gcTime: TIMER.minute * 10,
     initialPageParam: 0,
     getNextPageParam: (lastPage, pages) => {
       if (lastPage.last) return undefined;
