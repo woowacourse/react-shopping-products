3a732e4d235b503021fa1c558c6a85b83db27b49
ImxYJL <allensain14@gmail.com>
2024-06-05 11:55:18 +0900

    chore: react-query 사용을 위한 환경 설정
    + 전역 상태(CartItemsProvider) 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import ProductList from "./components/ProductList";
-import { CartItemsProvider } from "./store/cartItems";
 
 function App() {
   return (
     <>
-      <CartItemsProvider>
-        <ProductList />
-      </CartItemsProvider>
+      <ProductList />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { GlobalLayout, GlobalStyle } from "./styles/GlobalStyle.tsx";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
-    <GlobalStyle />
-    <GlobalLayout>
-      <App />
-    </GlobalLayout>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
+      <GlobalStyle />
+      <GlobalLayout>
+        <App />
+      </GlobalLayout>
+    </QueryClientProvider>
   </>
 );
