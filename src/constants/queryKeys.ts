579e5c22cd7972935cf81461c813c86d7ea278c3
river <130737187+0jenn0@users.noreply.github.com>
2024-06-05 14:18:24 +0900

    refactor: 장바구니 상품의 cartId 가져오기 react query로 리팩토링

---
+++ src/constants/queryKeys.ts
@@ -0,0 +1,6 @@
+const QUERY_KEYS = {
+  PRODUCTS: 'products',
+  CART: 'cart',
+};
+
+export default QUERY_KEYS;
--- src/hooks/useCartItem.ts
+++ src/hooks/useCartItem.ts
@@ -2,28 +2,26 @@ import { useEffect } from 'react';
 
 import { useToast } from './useToast';
 import { getCartList } from '@/api/cartItem';
+import { useQuery } from '@tanstack/react-query';
+import QUERY_KEYS from '@/constants/queryKeys';
 
 const useCartItems = () => {
-  const [cartItemIds, setCartItem] = useState<number[] | null>(null);
   const { createToast } = useToast();
+  const { data: cartItemIds, isError } = useQuery({
+    queryKey: [QUERY_KEYS.CART],
+    queryFn: async () => {
+      const cartItems = await getCartList();
+      return cartItems.map((cartItem) => cartItem.product.id);
+    },
+    initialData: [],
+  });
 
   useEffect(() => {
-    const fetchCartItems = async () => {
-      try {
-        const cartItems = await getCartList();
-
-        const newCartItemIds = cartItems.map((cartItem) => cartItem.product.id);
-        setCartItem(newCartItemIds);
-      } catch (error) {
-        if (error instanceof Error) {
-          createToast(
-            '⛔️ 장바구니 상품을 가져오는데 실패했습니다. 새로고침해주세요',
-          );
-        }
-      }
-    };
-    fetchCartItems();
-  }, [createToast]);
+    isError &&
+      createToast(
+        '장바구니에 담긴 상품들을 불러오는데 실패했습니다. 새로고침해주세요',
+      );
+  }, [isError]);
 
   const isInCart = (productId: number) => {
     return cartItemIds !== null && cartItemIds.includes(productId);
