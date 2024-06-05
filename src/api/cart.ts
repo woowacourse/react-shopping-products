a5bc9d7af6e990d9b588cb8e322c6d783566e340
soosoo22 <lllllh00@naver.com>
2024-06-06 06:30:06 +0900

    refactor: 장바구니 상품 조회 api 호출 및 불필요한 api 호출 제거

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -13,6 +13,20 @@ const createHeaders = () => {
   };
 };
 
+export async function getCartItems(): Promise<Cart[]> {
+  const response = await fetch(`${API_URL}/cart-items`, {
+    method: "GET",
+    headers: createHeaders(),
+  });
+
+  if (!response.ok) {
+    throw new Error("장바구니를 불러오는데 실패했습니다.");
+  }
+
+  const data = await response.json();
+  return data.content;
+}
+
 export async function addCartItem(productId: number): Promise<void> {
   const response = await fetch(`${API_URL}/cart-items`, {
     method: "POST",
@@ -25,27 +39,14 @@ export async function addCartItem(productId: number): Promise<void> {
   }
 }
 
-export async function removeCartItem(cartItemId: number): Promise<void> {
-  const response = await fetch(`${API_URL}/cart-items/${cartItemId}`, {
+export async function removeCartItem(productId: number): Promise<void> {
+  const response = await fetch(`${API_URL}/cart-items/${productId}`, {
     method: "DELETE",
     headers: createHeaders(),
   });
 
   if (!response.ok) {
+    console.log("실패");
     throw new Error(CART_ERROR_MESSAGES.REMOVE_ITEM_FAILURE);
   }
-}
-
-export async function getCartCounts(): Promise<number> {
-  const response = await fetch(`${API_URL}/cart-items/counts`, {
-    method: "GET",
-    headers: createHeaders(),
-  });
-
-  if (!response.ok) {
-    throw new Error(CART_ERROR_MESSAGES.GET_COUNTS_FAILURE);
-  }
-
-  const data = await response.json();
-  return data.quantity;
 }
