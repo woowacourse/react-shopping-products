e2c6fc8420d786aabbc723d75ba65d2a94e2b72f
jaeml06 <jaeml0630@gmail.com>
2024-06-05 16:50:22 +0900

    refactor : useCartItems로직 useQuery을 이용하여 구현

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -1,3 +1,4 @@
 export const QUERY_KEYS = {
   PRODUCTS: 'products',
+  CART_ITEM: 'cartItem',
 };
--- src/hooks/useCartItem.ts
+++ src/hooks/useCartItem.ts
@@ -1,39 +1,36 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { useToast } from './useToast';
 import { fetchItems } from '../api';
+import { useQuery } from '@tanstack/react-query';
+import { QUERY_KEYS } from '../constants/queryKeys';
 
 const useCartItems = () => {
-  const [cartItemIds, setCartItem] = useState<number[] | null>(null);
-  const [loading, setLoading] = useState(true);
   const { createToast } = useToast();
 
+  const { data, isLoading, error } = useQuery({
+    queryKey: [QUERY_KEYS.CART_ITEM],
+    queryFn: fetchItems,
+    initialData: [],
+    select: (data) => {
+      return data.map((cartItem) => cartItem.product.id) || [];
+    },
+  });
   useEffect(() => {
-    const fetchCartItems = async () => {
-      setLoading(true);
-      try {
-        const cartItems = await fetchItems();
-
-        const newCartItemIds = cartItems.map((cartItem) => cartItem.product.id);
-        setCartItem(newCartItemIds);
-      } catch (error) {
-        if (error instanceof Error) {
-          createToast(
-            '⛔️ 장바구니 상품을 가져오는데 실패했습니다. 새로고침해주세요',
-          );
-        }
-      } finally {
-        setLoading(false);
+    if (error) {
+      if (error instanceof Error) {
+        createToast(
+          '⛔️ 장바구니 상품을 가져오는데 실패했습니다. 새로고침해주세요',
+        );
       }
-    };
-    fetchCartItems();
-  }, [createToast]);
+    }
+  }, [error, createToast]);
 
-  const isInCart = (productId: number) => {
-    return cartItemIds !== null && cartItemIds.includes(productId);
+  const isInCart = (productId: number): boolean => {
+    return data ? data.includes(productId) : false;
   };
 
-  return { cartItemIds, isInCart, loading };
+  return { cartItemIds: data, isInCart, isLoading };
 };
 
 export default useCartItems;
