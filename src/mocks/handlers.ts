bf6c264c676378cc7454da4f570a35c2f49ac747
skiende74 <skiende74@gmail.com>
2024-06-05 17:15:37 +0900

    refactor(handlers): 변수 추출 리팩터링

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -23,68 +23,63 @@ const sortProductHandler = (
     : productCopy;
 };
 
-export const handlers = [
-  http.get(ENDPOINTS_PRODUCTS, ({ request }) => {
-    const url = new URL(request.url);
-    const page = Number(url.searchParams.get('page'));
-    const size = Number(url.searchParams.get('size'));
-    const sortings = url.searchParams.getAll('sort');
-    const category = url.searchParams.get('category');
+const getProductsHandler = http.get(ENDPOINTS_PRODUCTS, ({ request }) => {
+  const url = new URL(request.url);
+  const page = Number(url.searchParams.get('page'));
+  const size = Number(url.searchParams.get('size'));
+  const sortings = url.searchParams.getAll('sort');
+  const category = url.searchParams.get('category');
 
-    const productCopy = Object.assign({}, mockProductsResponse);
-    productCopy.content = filterProductHandler(productCopy, category);
+  const productCopy = Object.assign({}, mockProductsResponse);
+  productCopy.content = filterProductHandler(productCopy, category);
 
-    const productSorted = sortProductHandler(sortings, productCopy);
+  const productSorted = sortProductHandler(sortings, productCopy);
 
-    const start = page * size;
-    const end = (page + 1) * size;
-    const productSliced = Object.assign({}, productSorted);
-    productSliced.content = productSorted.content.slice(start, end);
-    productSliced.last =
-      productSliced.content.at(-1)!.id === productSorted.content.at(-1)!.id;
+  const start = page * size;
+  const end = (page + 1) * size;
+  const productSliced = Object.assign({}, productSorted);
+  productSliced.content = productSorted.content.slice(start, end);
+  productSliced.last =
+    productSliced.content.at(-1)!.id === productSorted.content.at(-1)!.id;
 
-    return HttpResponse.json(productSliced);
-  }),
+  return HttpResponse.json(productSliced);
+});
 
-  // 카트에 상품 담기
-  // 상품 담는 api 호출
-  // -> cart-items (GET) 호출 대신 cart.json에 mock data 추가
-  // -> mock data에서 id값이 같은 것 찾기
-  http.post(
-    `${ENDPOINTS_CART}`,
-    async ({
-      request,
-    }: {
-      request: StrictRequest<PostCartItemRequestBody>;
-    }) => {
-      const product = await request.json();
-      const id = product.productId;
-      const findProduct = mockProductsResponse.content.find(
-        (product) => product.id === id,
-      );
-
-      if (findProduct) {
-        const data = {
-          id: Math.random() * 1000,
-          quantity: 1,
-          product: findProduct,
-        };
-        mockCartResponse.content = [...mockCartResponse.content, data];
-      }
-      return HttpResponse.json(mockCartResponse);
-    },
-  ),
-  http.get(`${ENDPOINTS_CART}`, async () => {
-    return HttpResponse.json(mockCartResponse);
-  }),
-
-  // 카트에서 상품 제거
-  http.delete(`${ENDPOINTS_CART}/:id`, ({ params }) => {
-    const id = Number(params.id);
-    const newMockCart = mockCartResponse.content.filter(
-      (cart) => cart.id !== id,
+const postProductToCartHandler = http.post(
+  `${ENDPOINTS_CART}`,
+  async ({ request }: { request: StrictRequest<PostCartItemRequestBody> }) => {
+    const product = await request.json();
+    const id = product.productId;
+    const findProduct = mockProductsResponse.content.find(
+      (product) => product.id === id,
     );
-    mockCartResponse.content = newMockCart;
-    return HttpResponse.json(null, { status: 201 });
-  }),
+
+    if (findProduct) {
+      const data = {
+        id: Math.random() * 1000,
+        quantity: 1,
+        product: findProduct,
+      };
+      mockCartResponse.content = [...mockCartResponse.content, data];
+    }
+    return HttpResponse.json(mockCartResponse);
+  },
+);
+
+const getCartHandler = http.get(`${ENDPOINTS_CART}`, async () => {
+  return HttpResponse.json(mockCartResponse);
+});
+
+const deleteCartHandler = http.delete(`${ENDPOINTS_CART}/:id`, ({ params }) => {
+  const id = Number(params.id);
+  const newMockCart = mockCartResponse.content.filter((cart) => cart.id !== id);
+  mockCartResponse.content = newMockCart;
+  return HttpResponse.json(null, { status: 201 });
+});
+
+export const handlers = [
+  getProductsHandler,
+  getCartHandler,
+  postProductToCartHandler,
+  deleteCartHandler,
 ];
