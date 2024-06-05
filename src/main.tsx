41497871ebb20f84cc35a15851157b32e38fb6ef
손진영 <sm099@naver.com>
2024-06-05 14:48:44 +0900

    chore: tanstack-query 초기 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,25 @@ import * as Styled from './main.styled';
 import App from './App.tsx';
 import ReactDOM from 'react-dom/client';
 import ToastProvider from '@components/common/Toast/provider/ToastProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Styled.MainContainer>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
-  </Styled.MainContainer>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={true} />
+    <Styled.MainContainer>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </Styled.MainContainer>
+  </QueryClientProvider>
 );
