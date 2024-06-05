8c83df169df65e91d7bbab898fab6370c5d27d77
river <130737187+0jenn0@users.noreply.github.com>
2024-06-05 14:21:15 +0900

    refactor: 장바구니 상품 제거 react query로 리팩토링

--- src/hooks/useCartItemHandler.ts
+++ src/hooks/useCartItemHandler.ts
@@ -24,6 +24,7 @@ const useCartItemHandler = ({ productId, initIsInCart }: CartButtonProps) => {
     queryKey: [QUERY_KEYS.CART],
     queryFn: getCartList,
   });
+
   const [isInCart, setIsInCart] = useState(initIsInCart);
 
   const addToCartMutation = useMutation({
@@ -38,34 +39,33 @@ const useCartItemHandler = ({ productId, initIsInCart }: CartButtonProps) => {
       createToast('⛔️ 상품을 담는데 실패했습니다. 다시 시도해 주세요.');
     },
   });
+
+  const removeFromCartMutation = useMutation({
+    mutationFn: async (cartItemId: number) => deleteCartItem(cartItemId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.CART] });
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.PRODUCTS] });
+      updateCartListContext();
+      setIsInCart(false);
+    },
+    onError: () => {
+      createToast('⛔️ 상품을 제거하는데 실패했습니다. 다시 시도해 주세요.');
+    },
+  });
+
   const handleAddCartItem = () => {
     addToCartMutation.mutate(productId);
   };
+
+  const handleRemoveCartItem = () => {
+    const cartItemId = cartList?.find(
+      (cartItem) => cartItem.product.id === productId,
+    )?.id;
+    if (cartItemId) {
+      removeFromCartMutation.mutate(cartItemId);
     }
   };
 
-  const handleRemoveCartItem = async () => {
-    try {
-      setError(false);
-      setLoading(true);
-      setCartListQuantity((prev) => Math.max(0, prev - 1));
-      setIsInCart(false);
-      // const cartList = await fetchCartList();
-      const cartItemId = cartList.find(
-        (cartItem) => cartItem.product.id === productId,
-      )?.id;
-      if (cartItemId) {
-        await deleteCartItem(cartItemId);
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        createToast('⛔️ 상품을 제거하는데 실패했습니다. 다시 시도해 주세요.');
-        setCartListQuantity((prev) => prev + 1);
-        setIsInCart(true);
-        setError(true);
-      }
-    } finally {
-      setLoading(false);
     }
   };
 
