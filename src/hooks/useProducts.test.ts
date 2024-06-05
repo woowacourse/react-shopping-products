cf48fa0715662d51a38c03e7da6acb814670eab7
jayming66 <mjw8941@gmail.com>
2024-06-05 17:40:23 +0900

    feat(useProducts.test.ts): 모든 페이지의 상품을 불러오면 더 이상 요청하지 않는다.

--- src/hooks/useProducts.test.ts
+++ src/hooks/useProducts.test.ts
@@ -1,6 +1,7 @@
 import { ToastProvider } from './../context/ToastProvider';
 import { act, renderHook, waitFor } from '@testing-library/react';
 import { useProductFetch } from './useProductFetch';
+import { productCategories } from '../constant/products';
 
 describe('useProducts', () => {
   describe('상품 목록 조회', () => {
@@ -39,6 +40,41 @@ describe('useProducts', () => {
 
       await waitFor(() => {
         expect(result.current.products).toHaveLength(24);
+      });
+    });
+
+    it('모든 페이지의 상품을 불러오면 더 이상 요청하지 않는다.', async () => {
+      const { result } = renderHook(
+        () => useProductFetch({ selectBarCondition: initialSelectBarCondition }),
+        {
+          wrapper: ToastProvider,
+        },
+      );
+
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(20);
+      });
+
+      for (let i = 1; i < 21; i++) {
+        await waitFor(() => {
+          act(() => {
+            result.current.increaseNextPage();
+          });
+        });
+
+        const expectedLength = 20 + i * 4;
+
+        await waitFor(() => {
+          expect(result.current.products).toHaveLength(expectedLength);
+        });
+      }
+
+      await act(async () => {
+        result.current.increaseNextPage();
+      });
+
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(100);
       });
     });
   });
