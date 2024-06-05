1a1205214a9d7f3af6962693880b9205985d5847
rbgksqkr <rbgks1937@naver.com>
2024-06-05 11:16:24 +0900

    chore: tanstack-query 설치 및 provider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import ToastProvider from './components/common/toast/ToastProvider';
 import ProductListPage from './pages/productListPage/ProductListPage';
+import QueryProvider from './QueryProvider';
 
 function App() {
   return (
     <ToastProvider>
-      <ProductListPage />
+      <QueryProvider>
+        <ProductListPage />
+      </QueryProvider>
     </ToastProvider>
   );
 }
---
+++ src/QueryProvider.tsx
@@ -0,0 +1,10 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { PropsWithChildren } from 'react';
+
+const QueryProvider = ({ children }: PropsWithChildren) => {
+  const queryClient = new QueryClient();
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
+
+export default QueryProvider;
