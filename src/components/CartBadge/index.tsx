fc3f4eb3fdbab7d994cfe21564d246e3ff3444cb
Lee sang Yeop <mma7710@naver.com>
2024-06-06 17:11:29 +0900

    feat(CartBadge): 헤더의 장바구니 아이콘을 누르면 모달이 열리도록 추가

--- src/components/CartBadge/index.tsx
+++ src/components/CartBadge/index.tsx
@@ -1,12 +1,16 @@
 import { getCartItems } from "@/apis/cartItem";
 import Icon from "@/components/_common/Icon";
 import * as S from "@/components/CartBadge/style";
+import ShoppingBasket from "@/components/ShoppingBasket";
 import { END_POINT } from "@/config/endPoint";
 import QUERY_KEY from "@/constants/queryKey";
 import TIMER from "@/constants/timer";
 import { useQuery } from "@tanstack/react-query";
+import { MouseEventHandler, useState } from "react";
+import { CustomModal } from "woowacourse-todari-components";
 
 const CartBadge = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { data: cartItems } = useQuery({
     queryKey: [QUERY_KEY.getCartItems],
     queryFn: getCartItems,
@@ -14,13 +18,29 @@ const CartBadge = () => {
     staleTime: TIMER.hour,
   });
 
+  const openItemModal: MouseEventHandler = () => {
+    setIsModalOpen(true);
+  };
+
   return (
-    <S.Container onClick={() => (window.location.href = END_POINT.cartItemPage)}>
-      <Icon kind="cart" />
-      <S.Badge>
-        <S.BadgeNumber>{!cartItems || cartItems.length === 0 ? 0 : cartItems.length}</S.BadgeNumber>
-      </S.Badge>
-    </S.Container>
+    <>
+      <CustomModal
+        isOpened={isModalOpen}
+        modalPosition="bottom"
+        onClose={() => setIsModalOpen(false)}
+        title="장바구니"
+        primaryButton={{ text: "다음", onClick: () => (window.location.href = END_POINT.cartItemPage) }}
+        secondaryButton={{ text: "닫기", onClick: () => setIsModalOpen(false) }}
+      >
+        <ShoppingBasket />
+      </CustomModal>
+      <S.Container onClick={openItemModal}>
+        <Icon kind="cart" />
+        <S.Badge>
+          <S.BadgeNumber>{!cartItems || cartItems.length === 0 ? 0 : cartItems.length}</S.BadgeNumber>
+        </S.Badge>
+      </S.Container>
+    </>
   );
 };
 
