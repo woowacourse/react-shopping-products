3229301019b742f8628b961f011000a3b2e1155c
Kim Da Eun <midekuna@gmail.com>
2024-06-06 22:55:32 +0900

    chore: tanstack query 설치 및 기본 설정

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,21 @@ import { Global, ThemeProvider } from '@emotion/react';
 import theme from './styles/theme.ts';
 import globalStyles from './styles/globalStyles.ts';
 import AppRouter from './router/AppRouter.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 0 },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Global styles={globalStyles()} />
-      <AppRouter />
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
