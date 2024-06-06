ef39f05366db9a1187a5f7e0740755d894352b58
Seongjin Hong <hongseongjin.to@gmail.com>
2024-06-06 15:44:56 +0900

    chore: 미션 진행에 필요한 react-query 의존성 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import GlobalStyle from "./styles/reset";
 import ProductPage from "./pages/ProductPage";
 
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <ProductPage />
-    </>
+    </QueryClientProvider>
   );
 }
 
