0018aa527eaab6ed03b69ce8f252bed3eb7ea72a
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-06 21:24:43 +0900

    feat: QueryClient 연결

--- src/hooks/useFetchProductsTest/wrapper.tsx
+++ src/hooks/useFetchProductsTest/wrapper.tsx
@@ -3,16 +3,21 @@ import { ReactNode } from 'react';
 import GlobalLayout from '../../layouts/GlobalLayout';
 import theme from '../../theme';
 import { globalStyles } from '../../globalStyle';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 interface WrapperProps {
   children: ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 const wrapper = ({ children }: WrapperProps) => (
-  <ThemeProvider theme={theme}>
-    <Global styles={globalStyles} />
-    <GlobalLayout>{children}</GlobalLayout>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
+      <GlobalLayout>{children}</GlobalLayout>
+    </ThemeProvider>
+  </QueryClientProvider>
 );
 
 export default wrapper;
--- src/layouts/GlobalLayout/index.tsx
+++ src/layouts/GlobalLayout/index.tsx
@@ -1,16 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { CartItemProvider } from '../../context/CartItemProvider';
 import { ToastProvider } from '../../context/ToastProvider';
 import * as S from './style';
 
 import { PropsWithChildren } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const GlobalLayout = ({ children }: PropsWithChildren) => {
+  const queryClient = new QueryClient();
+
   return (
-    <ToastProvider>
-      <CartItemProvider>
-        <S.Container>{children}</S.Container>
-      </CartItemProvider>
-    </ToastProvider>
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider>
+        <CartItemProvider>
+          <S.Container>{children}</S.Container>
+        </CartItemProvider>
+      </ToastProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
