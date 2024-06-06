3abddcf3eab6be92390d590510a7bbba54344237
skiende74 <skiende74@gmail.com>
2024-06-07 00:25:49 +0900

    fix(CartContext): addProductToCart 프롭을 async함수로 수정

--- src/CartContext.ts
+++ src/CartContext.ts
@@ -4,7 +4,7 @@ import useFetchAddCart from './hooks/useFetchAddCart';
 export const CartContext = createContext<ReturnType<typeof useFetchAddCart>>({
   productIdSetInCart: new Set(),
   setProductIdSetInCart: () => {},
-  addProductToCart: () => {},
+  addProductToCart: async () => {},
   patchToRemoveCart: async () => {},
   fetchCart: async () => [],
 });
