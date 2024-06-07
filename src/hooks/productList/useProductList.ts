88f604c6ae875c83ab80efd2cee97686c3049a9d
badahertz52 <badahertz52@gmail.com>
2024-06-07 12:21:21 +0900

    refactor: useProductList 리팩토링

--- src/hooks/productList/useProductList.ts
+++ src/hooks/productList/useProductList.ts
@@ -1,32 +1,29 @@
-import { fetchProductList,FetchProductListReturnType } from '@src/apis';
+import { fetchProductList, FetchProductListReturnType } from '@src/apis';
 import { Filtering, Product } from '@src/appTypes';
-import { PRODUCT_LIST_PAGE, QUERY_KEY } from '@src/constants'
-import {  QueryFunction, QueryKey, useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
+import { PRODUCT_LIST_PAGE, QUERY_KEY } from '@src/constants';
+import { QueryFunction, QueryKey, useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
 import { useEffect } from 'react';
 
-
-
 const useProductList = (filtering: Filtering) => {
-  const queryClient =useQueryClient();
-  const queryKey =[QUERY_KEY.productList, filtering]
-  const queryFn :QueryFunction<FetchProductListReturnType, QueryKey, unknown>  =async ({ pageParam = 0 }) => fetchProductList({ filtering, page: pageParam as number });
-  const {data,fetchNextPage ,error ,refetch ,status} = useInfiniteQuery<FetchProductListReturnType, Error>({
-    queryKey: queryKey,
-    queryFn: queryFn ,
-    initialPageParam:PRODUCT_LIST_PAGE.first,
-    getNextPageParam: (params : FetchProductListReturnType|
-      undefined
-    ) => {
+  const queryClient = useQueryClient();
 
-      if(!params) return;
-      const {isLast, currentPage} = params;
+  const queryKey = [QUERY_KEY.productList, filtering];
+  const queryFn: QueryFunction<FetchProductListReturnType, QueryKey, unknown> = async ({ pageParam = 0 }) =>
+    fetchProductList({ filtering, page: pageParam as number });
+
+  const { data, fetchNextPage, error, refetch, status } = useInfiniteQuery<FetchProductListReturnType, Error>({
+    queryKey: queryKey,
+    queryFn: queryFn,
+    initialPageParam: PRODUCT_LIST_PAGE.first,
+    getNextPageParam: (params: FetchProductListReturnType | undefined) => {
+      if (!params) return;
+      const { isLast, currentPage } = params;
 
       if (isLast) return;
 
       return currentPage === PRODUCT_LIST_PAGE.first ? PRODUCT_LIST_PAGE.second : currentPage + 1;
-    }
+    },
   });
-
 
   const handleReset = () => {
     queryClient.setQueryData([QUERY_KEY.productList, filtering], () => ({
@@ -36,24 +33,21 @@ const useProductList = (filtering: Filtering) => {
     refetch();
   };
 
-  const makeProducts =()=>{
-    if(!data?.pages[0]) return;
+  const makeProducts = () => {
+    if (!data?.pages[0]) return;
 
-    return data.pages.reduce((acc,cur) => acc.concat(cur.products) ,[] as Product[])
-  }
+    return data.pages.reduce((acc, cur) => acc.concat(cur.products), [] as Product[]);
+  };
 
- useEffect(()=>{
-  handleReset()
- },[filtering])
-
+  useEffect(() => {
+    handleReset();
+  }, [filtering]);
 
   return {
     products: makeProducts(),
     fetchNextPage,
     status,
-    error
-  }
+    error,
+  };
 };
-export default useProductList
-
-
+export default useProductList;
