69e11c59a0e413787d553cc5b27bca406e328953
rbgksqkr <rbgks1937@naver.com>
2024-06-08 02:32:04 +0900

    feat: 개수가 1개에서 0개가 될 경우 장바구니 제품 삭제

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,12 +1,20 @@
+import useDeleteCartItemQuery from './useDeleteCartItemQuery';
 import useUpdateItemQuantityQuery from './useUpdateItemQuantityQuery';
 
 import { CartItemInfo } from '@/types/cartItem';
 
 const useCounter = (item: CartItemInfo) => {
   const { mutate: updateItemQuantity } = useUpdateItemQuantityQuery();
+  const { mutate: deleteCartItem } = useDeleteCartItemQuery();
 
   const handleDecrementQuantity = async () => {
-    const newQuantity = Math.max(item.quantity - 1, 1);
+    const newQuantity = item.quantity - 1;
+
+    if (newQuantity <= 0) {
+      deleteCartItem(item.id);
+      return;
+    }
+
     updateItemQuantity({ cartId: item.id, quantity: newQuantity });
   };
 
