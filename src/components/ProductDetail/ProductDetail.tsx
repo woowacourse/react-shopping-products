13cc6e71e13f37c2f2dddc4f1693c7b312f50fe6
bmj0053 <bmj05378@gmail.com>
2024-06-08 01:11:42 +0900

    design: 모달 컨텐츠 디자인 구현

--- src/components/ProductDetail/ProductDetail.tsx
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -2,6 +2,8 @@ import { useContext } from 'react';
 import { Carts } from '../../types/fetch';
 import { CartContext } from '../../context/CartContext';
 
+import * as S from './ProductDetail.styled';
+
 interface ProductDetailProps {
   cartItems: Carts[];
 }
@@ -17,10 +19,34 @@ function ProductDetail({ cartItems }: ProductDetailProps) {
     // isDeletePending,
     // isDeleteError,
   } = cartContext;
+
+  const handleDecreasedQuantity = () => {};
+
+  const handleIncreasedQuantity = () => {};
+
   return (
     <div>
       {cartItems.map((item) => (
-        <>123 </>
+        <S.CardContainer>
+          <S.Button onClick={() => console.log('삭제기능')}>삭제</S.Button>
+
+          <S.CardContent>
+            <S.ItemImg src={item.product.imageUrl} alt={item.product.name} />
+            <S.CardDetail>
+              <S.CardInfo>
+                <S.ItemName>{item.product.name}</S.ItemName>
+                <S.ItemPrice>
+                  {item.product.price.toLocaleString()}원
+                </S.ItemPrice>
+              </S.CardInfo>
+              <S.CardQuantityButton>
+                <S.Button onClick={handleDecreasedQuantity}>-</S.Button>
+                <p>{item.quantity}</p>
+                <S.Button onClick={handleIncreasedQuantity}>+</S.Button>
+              </S.CardQuantityButton>
+            </S.CardDetail>
+          </S.CardContent>
+        </S.CardContainer>
       ))}
     </div>
   );
