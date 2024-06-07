7529074df16b86681235a2346c20606724587002
손진영 <sm099@naver.com>
2024-06-07 18:29:24 +0900

    style(ItemDescription): import path 수정

--- src/components/common/Item/ItemDescription/ItemDescription.tsx
+++ src/components/common/Item/ItemDescription/ItemDescription.tsx
@@ -1,9 +1,11 @@
-import { Product } from '@appTypes/shoppingCart';
 import { formatKoreanCurrency } from '@utils/currency';
 
 import * as Styled from './ItemDescription.styled';
+import { Product } from '@appTypes/product';
 
-export type ItemDescriptionType = React.FC<React.PropsWithChildren<Pick<Product, 'name' | 'price'>>>;
+export type ItemDescriptionType = React.FC<
+  React.PropsWithChildren<Pick<Product, 'name' | 'price'>>
+>;
 
 const ItemDescription: ItemDescriptionType = ({ children, name, price }) => {
   return (
