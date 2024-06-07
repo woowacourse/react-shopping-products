ba4a33ce9992814fcb0e2af7d6ee9984cb1f7e48
badahertz52 <badahertz52@gmail.com>
2024-06-07 12:14:41 +0900

    test: useProdutList를 사용한 카테고리 필터링 테스트 생성

--- src/hooks/productList/tests/useProductList.test.ts
+++ src/hooks/productList/tests/useProductList.test.ts
@@ -1,4 +1,5 @@
-import { Filtering } from '@appTypes/index';
+import { Category, Filtering } from '@appTypes/index';
+import { CATEGORY_OPTIONS } from '@constants/index';
 import queryClientWrapper from '@src/testSetup/queryClientWrapper';
 import { act, renderHook, waitFor } from '@testing-library/react';
 
@@ -50,5 +51,89 @@ describe('상품 목록 테스트', () => {
     });
   });
 
-  describe('필터링 테스트', () => {});
+  describe('필터링 테스트', () => {
+    describe('카테고리 필터링 테스트', () => {
+      it('전체 선택 시, 모든 상품 목록을 보여준다', async () => {
+        const CATEGORY_LENGTH = CATEGORY_OPTIONS.filter((options) => !!options.value).length;
+        const FILTERING: Filtering = { category: '', sort: 'price,asc' };
+
+        const { result } = renderHook(() => useProductList(FILTERING), { wrapper: queryClientWrapper });
+
+        await waitFor(() => expect(result.current.status).toBe('success'));
+
+        await waitFor(() => {
+          expect(result.current.products).toBeDefined();
+        });
+
+        const categorySet = new Set(result.current.products?.map((product) => product.category));
+
+        expect(categorySet.size).toBe(CATEGORY_LENGTH);
+      });
+
+      it.each(CATEGORY_OPTIONS.slice(1))(
+        '카테고리 선택 시, 해당 카테고리 상품만 보여준다. (선택한 카테고리 :%s)',
+        async ({ value }) => {
+          const FILTERING: Filtering = { category: value as Category, sort: 'price,asc' };
+
+          const { result } = renderHook(() => useProductList(FILTERING), { wrapper: queryClientWrapper });
+
+          await waitFor(() => expect(result.current.status).toBe('success'));
+
+          await waitFor(() => {
+            expect(result.current.products).toBeDefined();
+          });
+
+          const isAllFashion = result.current.products?.every((product) => product.category === value);
+          expect(isAllFashion).toBeTruthy();
+        },
+      );
+    });
+
+    describe('가격 정렬 테스트', () => {
+      it('오름차순 선택 시, 상품 각각의 오름차순으로 목록을 보여준다', async () => {
+        const FILTERING: Filtering = { category: '', sort: 'price,asc' };
+
+        const { result } = renderHook(() => {
+          const { fetch } = useFetch<typeof fetchProduct>(fetchProduct);
+          const { getFilteredProducts } = useFilteredProducts({
+            fetch,
+          });
+          return { getFilteredProducts };
+        });
+
+        await waitFor(async () => {
+          const response = await result.current.getFilteredProducts(FILTERING);
+
+          expect(response).toBeDefined();
+          if (!response) return;
+
+          const [first, second] = response.newProducts;
+
+          expect(first.price <= second.price).toBeTruthy();
+        });
+      });
+      it('내림차순 선택 시, 상품 각겨의 내림차순으로 목록을 보여준다', async () => {
+        const FILTERING: Filtering = { category: '', sort: 'price,desc' };
+
+        const { result } = renderHook(() => {
+          const { fetch } = useFetch<typeof fetchProduct>(fetchProduct);
+          const { getFilteredProducts } = useFilteredProducts({
+            fetch,
+          });
+          return { getFilteredProducts };
+        });
+
+        await waitFor(async () => {
+          const response = await result.current.getFilteredProducts(FILTERING);
+
+          expect(response).toBeDefined();
+          if (!response) return;
+
+          const [first, second] = response.newProducts;
+
+          expect(first.price >= second.price).toBeTruthy();
+        });
+      });
+    });
+  });
 });
