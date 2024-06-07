60d96b63eb7aa524ff01751e113011364670bc26
bmj0053 <bmj05378@gmail.com>
2024-06-07 16:09:32 +0900

    test: useFetchAddCart 중 useFetchCartItems 관련 테스트 추가
    Co-authored-by: skiende74 <skiende74@gmail.com>

--- src/hooks/useFetchAddCart/useFetchAddCart.test.tsx
+++ src/hooks/useFetchAddCart/useFetchAddCart.test.tsx
@@ -1,6 +1,8 @@
 import { act, renderHook, waitFor } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import useFetchAddCart from './useFetchAddCart';
+import useFetchCartItems from '../useFetchCartItems/useFetchCartItems';
+import { Carts } from '../../types/fetch';
 
 const queryClient = new QueryClient();
 
@@ -32,29 +34,28 @@ describe('useFetchAddCart', async () => {
   //   expect(result.current.cartIdSet.has(PRODUCT_ID)).toBe(false);
   // });
 
-  // TODO: 장바구니 fetch 함수 구현 후 작성하기
-  // it('장바구니에 담겨있지 않은 제품을 담으면 장바구니에 담긴 제품 종류 개수가 증가되어야 한다.', async () => {
-  //   const { result } = renderHook(
-  //     () => {
-  //       const fetchProduct = useFetchProducts();
-  //       const fetchAddCart = useFetchAddCart();
-  //       return { fetchProduct, fetchAddCart };
-  //     },
-  //     { wrapper },
-  //   );
+  it('장바구니에 담겨있지 않은 제품을 담으면 장바구니에 담긴 제품 종류 개수가 증가되어야 한다.', async () => {
+    const { result } = renderHook(
+      () => {
+        const fetchCartItems = useFetchCartItems();
+        const fetchAddCart = useFetchAddCart();
+        return { fetchCartItems, fetchAddCart };
+      },
+      { wrapper },
+    );
 
-  //   expect(result.current.fetchProduct.data).toHaveLength(0);
+    expect(result.current.fetchCartItems.data).toHaveLength(0);
 
-  //   act(() => {
-  //     result.current.fetchAddCart.addCartItem(PRODUCT_ID);
-  //   });
+    act(() => {
+      result.current.fetchAddCart.addCartItem(PRODUCT_ID);
+    });
 
-  //   await waitFor(() =>
-  // // 장바구니 데이터 fetch로 바꾸기
-  //     expect(result.current.fetchCart.data).toHaveLength(1),
-  //   );
-  // });
+    await waitFor(() =>
+      expect(result.current.fetchCartItems.data).toHaveLength(1),
+    );
+  });
 
+  // TODO: 장바구니 삭제 기능 추가 후 구현하기
   // it('장바구니에 담겨있는 제품을 삭제하면 장바구니에 담긴 제품 종류 개수가 감소되어야 한다.', async () => {
   //   const { result } = renderHook(() => useFetchAddCart());
 
@@ -71,45 +72,68 @@ describe('useFetchAddCart', async () => {
   //   expect(result.current.cartIdSet.size).toBe(0);
   // });
 
-  // it('장바구니에 제품을 추가하고 장바구니를 다시 불러왔을 때, 해당 제품이 포함되어 있어야한다.', async () => {
-  //   const PRODUCT_ID = 3;
-  //   const { result } = renderHook(() => useFetchAddCart());
+  it('장바구니에 제품을 추가하고 장바구니를 다시 불러왔을 때, 해당 제품이 포함되어 있어야한다.', async () => {
+    const { result } = renderHook(
+      () => {
+        const fetchCartItems = useFetchCartItems();
+        const fetchAddCart = useFetchAddCart();
+        return { fetchCartItems, fetchAddCart };
+      },
+      { wrapper },
+    );
+    await waitFor(() => {
+      result.current.fetchAddCart.addCartItem(PRODUCT_ID);
+    });
 
-  //   await waitFor(() => {
-  //     result.current.postToAddCart(PRODUCT_ID);
-  //   });
+    await waitFor(() => {
+      result.current.fetchCartItems.refetch();
+    });
 
-  //   let cartItems: Carts[] = [];
-  //   await waitFor(async () => {
-  //     cartItems = await result.current.fetchCart();
-  //   });
+    let cartItems: Carts[] = [];
+    await waitFor(() => {
+      cartItems = result.current.fetchCartItems.data;
+    });
 
-  //   await waitFor(() => {
-  //     expect(cartItems.some((item) => item.product.id === PRODUCT_ID)).toBe(
-  //       true,
-  //     );
-  //   });
-  // });
+    await waitFor(() => {
+      expect(cartItems.some((item) => item.product.id === PRODUCT_ID)).toBe(
+        true,
+      );
+    });
+  });
 
-  // it('장바구니에 제품을 두 개 추가하고 장바구니를 다시 불러왔을 때, 해당 제품들이 포함되어 있어야한다.', async () => {
-  //   const PRODUCT_ID_TWO = 2;
-  //   const PRODUCT_ID_THREE = 3;
-  //   const { result } = renderHook(() => useFetchAddCart());
+  it('장바구니에 제품을 두 개 추가하고 장바구니를 다시 불러왔을 때, 해당 제품들이 포함되어 있어야한다.', async () => {
+    const PRODUCT_ID_TWO = 2;
+    const PRODUCT_ID_THREE = 3;
 
-  //   await act(async () => {
-  //     await result.current.postToAddCart(PRODUCT_ID_TWO);
-  //     await result.current.postToAddCart(PRODUCT_ID_THREE);
-  //   });
+    const { result } = renderHook(
+      () => {
+        const fetchCartItems = useFetchCartItems();
+        const fetchAddCart = useFetchAddCart();
+        return { fetchCartItems, fetchAddCart };
+      },
+      { wrapper },
+    );
+    await waitFor(() => {
+      result.current.fetchAddCart.addCartItem(PRODUCT_ID_TWO);
+      result.current.fetchAddCart.addCartItem(PRODUCT_ID_THREE);
+    });
 
-  //   await waitFor(async () => {
-  //     const cartItems = await result.current.fetchCart();
+    await waitFor(() => {
+      result.current.fetchCartItems.refetch();
+    });
 
-  //     expect(cartItems.some((item) => item.product.id === PRODUCT_ID_TWO)).toBe(
-  //       true,
-  //     );
-  //     expect(
-  //       cartItems.some((item) => item.product.id === PRODUCT_ID_THREE),
-  //     ).toBe(true);
-  //   });
-  // });
+    let cartItems: Carts[] = [];
+    await waitFor(() => {
+      cartItems = result.current.fetchCartItems.data;
+    });
+
+    await waitFor(() => {
+      expect(cartItems.some((item) => item.product.id === PRODUCT_ID_TWO)).toBe(
+        true,
+      );
+      expect(
+        cartItems.some((item) => item.product.id === PRODUCT_ID_THREE),
+      ).toBe(true);
+    });
+  });
 });
