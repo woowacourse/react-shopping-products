2be11c59d79771216076a38ef074a0b2cd9f0b44
이태훈 <rhymint@gmail.com>
2024-06-07 16:29:22 +0900

    fix: Header에 CartItemModal 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,26 @@
 import { MainLogo, ShoppingCartIcon } from '../../assets';
 
 import * as S from './Header.style';
+import { useState } from 'react';
+import CartItemModal from '../CartItemModal/CartItemModal';
 
 function Header() {
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  const handleModalOpen = () => {
+    setIsModalOpened(true);
+  };
+  const handleModalClose = () => {
+    setIsModalOpened(false);
+  };
+
   const quantity = 0;
 
   return (
     <S.HeaderBackground>
+      <CartItemModal isOpened={isModalOpened} onClose={handleModalClose} />
       <S.HeaderWrapper>
         <MainLogo />
-        <S.ShoppingCartButton>
+        <S.ShoppingCartButton onClick={handleModalOpen}>
           <ShoppingCartIcon />
           {(quantity ?? 0) > 0 && (
             <S.ShoppingCartQuantityContainer>
