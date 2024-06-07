731d757265704c87c2fd8a3d6299580411e6b141
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-07 20:30:47 +0900

    refactor: useCartItem 훅에서 장바구니 관리 로직 useHandlerCartItems 훅으로 분리

--- src/hooks/useCartItems/index.ts
+++ src/hooks/useCartItems/index.ts
@@ -1,55 +1,12 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEYS } from '../../constants/queryKeys';
-import {
-  addCartItem,
-  deleteCartItem,
-  fetchCartItems,
-  patchCartItemQuantity,
-} from '../../api/cartItems';
-import { useContext } from 'react';
-import { ToastContext } from '../../context/ToastProvider';
+import { fetchCartItems } from '../../api/cartItems';
 
 const useCartItems = () => {
-  const { showToast } = useContext(ToastContext);
-  const queryClient = useQueryClient();
-
-  const cartItems = useQuery({
+  return useQuery({
     queryKey: [QUERY_KEYS.cartItem],
     queryFn: fetchCartItems,
   });
-
-  const addCart = useMutation({
-    mutationFn: (productId: number) => addCartItem(productId),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.cartItem] });
-    },
-    onError: (error) => {
-      showToast(error.message);
-    },
-  });
-
-  const deleteCart = useMutation({
-    mutationFn: (cartId: number) => deleteCartItem(cartId),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.cartItem] });
-    },
-    onError: (error) => {
-      showToast(error.message);
-    },
-  });
-
-  const updateCartItemQuantity = useMutation({
-    mutationFn: ({ cartId, quantity }: { cartId: number; quantity: number }) =>
-      patchCartItemQuantity(cartId, quantity),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.cartItem] });
-    },
-    onError: (error) => {
-      showToast(error.message);
-    },
-  });
-
-  return { cartItems, addCart, deleteCart, updateCartItemQuantity };
 };
 
 export default useCartItems;
---
+++ src/hooks/useHandlerCartItems.ts
@@ -0,0 +1,45 @@
+import { useContext } from 'react';
+import { ToastContext } from '../context/ToastProvider';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { addCartItem, deleteCartItem, patchCartItemQuantity } from '../api/cartItems';
+import { QUERY_KEYS } from '../constants/queryKeys';
+
+const useHandlerCartItems = () => {
+  const { showToast } = useContext(ToastContext);
+  const queryClient = useQueryClient();
+
+  const addCart = useMutation({
+    mutationFn: (productId: number) => addCartItem(productId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.cartItem] });
+    },
+    onError: (error) => {
+      showToast(error.message);
+    },
+  });
+
+  const deleteCart = useMutation({
+    mutationFn: (cartId: number) => deleteCartItem(cartId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.cartItem] });
+    },
+    onError: (error) => {
+      showToast(error.message);
+    },
+  });
+
+  const updateCartItemQuantity = useMutation({
+    mutationFn: ({ cartId, quantity }: { cartId: number; quantity: number }) =>
+      patchCartItemQuantity(cartId, quantity),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.cartItem] });
+    },
+    onError: (error) => {
+      showToast(error.message);
+    },
+  });
+
+  return { addCart, deleteCart, updateCartItemQuantity };
+};
+
+export default useHandlerCartItems;
