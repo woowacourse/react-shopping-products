272ed35fc7f93747e6f1cb20db11a435a1410c55
Hyelim Choi <HyelimChoi01@gmail.com>
2024-06-09 01:37:20 +0900

    test: get list 테스트 value 체크 -> property 체크로 변경

--- src/hooks/__test__/useGetCartListQuery.test.tsx
+++ src/hooks/__test__/useGetCartListQuery.test.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { renderHook, waitFor } from '@testing-library/react';
 
-import { CartItem } from '@/types/cartItem.type';
 import useGetCartListQuery from '../cart/useGetCartListQuery';
 
 const queryClient = new QueryClient();
@@ -16,19 +15,13 @@ describe('useGetCartListQuery', () => {
 
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
 
-    expect(result.current.data?.slice(0, 1)).toEqual<CartItem[]>([
-      {
-        id: 7076,
-        quantity: 1,
-        product: {
-          id: 109,
-          name: '과학소설',
-          price: 7000,
-          imageUrl:
-            'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?q=80&w=2730&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          category: 'books',
-        },
-      },
-    ]);
+    expect(result.current.data?.[0]).toHaveProperty('id');
+    expect(result.current.data?.[0]).toHaveProperty('quantity');
+    expect(result.current.data?.[0]).toHaveProperty('product');
+    expect(result.current.data?.[0].product).toHaveProperty('id');
+    expect(result.current.data?.[0].product).toHaveProperty('name');
+    expect(result.current.data?.[0].product).toHaveProperty('price');
+    expect(result.current.data?.[0].product).toHaveProperty('imageUrl');
+    expect(result.current.data?.[0].product).toHaveProperty('category');
   });
 });
