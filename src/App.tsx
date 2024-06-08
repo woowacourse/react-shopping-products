a8bd88ee2e39948cbcc0ba79854c34d8b9eae725
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-06-08 16:04:34 +0900

    remove: CartContext 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { CartProvider } from './contexts/CartContext';
 import Products from '@/pages/Products';
 
 import baseStyle from '@/styles/base.style';
 import { Global } from '@emotion/react';
+
 function App() {
   return (
     <>
       <Global styles={baseStyle} />
-      <CartProvider>
-        <Products />
-      </CartProvider>
+      <Products />
     </>
   );
 }
