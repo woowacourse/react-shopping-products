c5dfbd323fdfc96e151a0ca564988a25c72f27bc
hwinkr <dnddl8280@naver.com>
2024-06-08 14:35:55 +0900

    feat(Stepper): 상품 수량을 조절하는 Stepper 컴포넌트 구현

---
+++ src/components/Stepper/Stepper.style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StepperContainer = styled.div`
+  width: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const StepperButton = styled.button`
+  height: 24px;
+  width: 24px;
+`;
+
+export const StepperValue = styled.span`
+  font-size: 12px;
+  line-height: 15px;
+`;
