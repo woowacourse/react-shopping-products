02f66e73702f0d281481fda1e2f6f19c8c355851
Soyeon Choe <soy2302ten@gmail.com>
2024-06-08 17:39:41 +0900

    chore: StrictMode 주석 제거

--- src/main.tsx
+++ src/main.tsx
@@ -7,23 +7,26 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./theme.ts";
 
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 2000,
+      refetchOnWindowFocus: false,
     },
   },
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <ThemeProvider theme={theme}>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen />
-      <GlobalStyle />
-      <App />
-    </QueryClientProvider>
-  </ThemeProvider>,
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen />
+        <GlobalStyle />
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
+    ,
+  </React.StrictMode>,
 );
