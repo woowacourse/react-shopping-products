d51ab7fb37b5c570ca45f4728b223da1bbf401b4
river <130737187+0jenn0@users.noreply.github.com>
2024-06-08 20:28:46 +0900

    feat: index.ts에 모아져있던 상수 분리 및 react query key 상수 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,26 +1,3 @@
-const INITIAL_DATA_LOAD_COUNT = 20;
-const SUBSEQUENT_DATA_LOAD_COUNT = 4;
-const MAX_CART_ITEMS_COUNTS = 100;
-
-const CATEGORY: Record<string, string> = {
-  all: '전체',
-  fashion: '의류',
-  beverage: '음료',
-  electronics: '전자기기',
-  kitchen: '주방용품',
-  fitness: '피트니스',
-  books: '도서',
-} as const;
-
-const SORT: Record<string, string> = {
-  asce: '낮은 가격순',
-  desc: '높은 가격순',
-} as const;
-
-export {
-  INITIAL_DATA_LOAD_COUNT,
-  SUBSEQUENT_DATA_LOAD_COUNT,
-  CATEGORY,
-  SORT,
-  MAX_CART_ITEMS_COUNTS,
-};
+export * from './categoryAndSortConstants';
+export * from './pagination';
+export * from './queryKeys';
--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -1,6 +1,11 @@
 const QUERY_KEYS = {
   PRODUCTS: 'products',
-  CART: 'cart',
+  CART: {
+    BASE: 'cart',
+    TOTAL_QUANTITY: 'total_quantity',
+    FETCH: 'fetch',
+    TOTAL_PRICE: 'total_price',
+  },
 };
 
 export default QUERY_KEYS;
