4f4cb9c81911c0801639ec5fa5b8a38b299b642a
pakxe <pigkill40@naver.com>
2024-06-09 03:31:22 +0900

    feat: 장바구니에 담은 상품이 없으면 모달을 띄우지 않도록 구현

--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -1,17 +1,35 @@
 import CartIconImage from '@/assets/Cart.svg';
 import ImageBox from '@/components/common/ImageBox/ImageBox';
 import styles from './CartIcon.module.css';
-import { useCartItemListContext } from '@/hooks/useCartItemList';
+import useCartItemList from '@/hooks/cartItem/useCartItemList';
+import { useState } from 'react';
+import CartListModal from '../CartListModal/CartListModal';
+import { useToast } from '@/hooks/useToast';
 
 export default function CartIcon() {
-  const { cartItemList } = useCartItemListContext();
+  const { data, getTotalQuantity } = useCartItemList();
+  const [isOpen, setIsOpen] = useState(false);
+  const { showToast } = useToast();
+
+  const handleModal = () => {
+    if (data?.length === 0) {
+      showToast(`장바구니에 담긴 상품이 없습니다. 구매할 상품을 담아주세요.`);
+      return;
+    }
+
+    setIsOpen((prev) => !prev);
+  };
 
   return (
-    <div className={styles.container}>
-      <ImageBox className={styles.cartIcon} src={CartIconImage} width={32} height={32} />
-      <div className={styles.amountContainer}>
-        <span className={styles.amount}>{cartItemList.length}</span>
+    <>
+      <div className={styles.container} onClick={handleModal}>
+        <ImageBox className={styles.cartIcon} src={CartIconImage} width={32} height={32} />
+        <div className={styles.amountContainer}>
+          <span className={styles.amount}>{getTotalQuantity()}</span>
+        </div>
       </div>
-    </div>
+
+      <CartListModal isOpen={isOpen} close={handleModal} />
+    </>
   );
 }
