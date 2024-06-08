634a165c1f0d9c240c2bf5029176d71f6b791b16
river <130737187+0jenn0@users.noreply.github.com>
2024-06-08 21:41:30 +0900

    test: test utils 폴더에 테스트에 사용되는 wrapper들 추가

--- __test__/utils/test-utils.tsx
+++ __test__/utils/test-utils.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { ToastContextProvider } from '../../src/contexts/ToastContextProvider';
-import { CartProvider } from '../../src/contexts/ShoppingCartCountContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export const wrapper = ({ children }) => (
+export const testQueryClient = new QueryClient();
+
+export const queryClientWrapper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => (
+  <QueryClientProvider client={testQueryClient}>{children}</QueryClientProvider>
+);
+
+export const combinedContextWrapper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => (
   <ToastContextProvider>
-    <CartProvider>{children}</CartProvider>
+    <QueryClientProvider client={testQueryClient}>
+      {children}
+    </QueryClientProvider>
   </ToastContextProvider>
 );
