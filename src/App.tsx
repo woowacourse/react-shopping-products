f629612d750c559020134a240d6d286559778b5b
river <130737187+0jenn0@users.noreply.github.com>
2024-06-08 21:31:23 +0900

    feat: App에서 사용하지 않는 provider 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import './index.css';
 
 import ProductPage from './page/ProductPage';
 import { ToastContextProvider } from './contexts/ToastContextProvider';
-import { CartProvider } from '@/contexts/CartListContext';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
@@ -13,9 +11,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ToastContextProvider>
-        <CartProvider>
-          <ProductPage />
-        </CartProvider>
+        <ProductPage />
       </ToastContextProvider>
     </QueryClientProvider>
   );
