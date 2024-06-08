964b974f8b5bfd6dc24681d5fdbfbdfc66b9bb60
pakxe <pigkill40@naver.com>
2024-06-09 05:03:56 +0900

    style: 컬러를 위한 테마 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import ProductListPageContainer from './pages/ProductListPageContainer';
 import ToastProvider from './hooks/useToast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Layout from './components/Layout/Layout';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 function App() {
   const queryClient = new QueryClient();
@@ -10,13 +13,13 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <div style={{ width: '472px' }}>
+      <ThemeProvider theme={theme}>
+        <Layout>
           <ToastProvider>
             <ProductListPageContainer />
           </ToastProvider>
-        </div>
-      </div>
+        </Layout>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+// const screenSize = {
+//   small: `screen and (max-width: '600px')`,
+//   base: `screen and (max-width: '768px')`,
+//   large: `screen and (max-width: '1024px')`,
+// };
+
+const color = {
+  lightGray: '#e0e0e0',
+};
+
+const theme = {
+  // screenSize,
+  color,
+};
+
+export default theme;
