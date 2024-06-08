63c262ce24dc5e8b0563d0e6c3bfd3eeba7d3128
river <130737187+0jenn0@users.noreply.github.com>
2024-06-08 21:25:20 +0900

    refactor: 담기/빼기 버튼 컴포넌트 기능 구현 로직 수정(props -> hook 사용)
    - 외부에서 장바구니 상품 수량변경,삭제 함수를 props로 받던 부분을 컴포넌트 내부에서 hooks 사용하는것으로 수정
    - Button 컴포넌트에 주는 props 수정하여 UI 수정
    - 버튼 속 텍스트,아이콘을 정렬하기 위한 styled div 추가

--- src/components/CartButton/CartButton.tsx
+++ src/components/CartButton/CartButton.tsx
@@ -1,41 +1,57 @@
-import Button from '@/components/common/Button/Button';
 import { AddCart, MinusCart } from '@/assets/index';
-import { ButtonImg } from './CartButton.style';
-import useCartItem from '@/hooks/useCartItemHandler';
-import QuantityPicker from '@/components/QuantityPicker/QuantityPicker';
+import { ButtonImg, CartButtonContentWrapper } from './CartButton.style';
+import { QuantityPicker, Button } from '@/components/index';
+import {
+  useCartItemStatus,
+  usePostCartItem,
+  useFetchCartItems,
+  useDeleteCartItem,
+} from '@/hooks/index';
 
 interface CartButtonProps {
   productId: number;
-  initIsInCart: boolean;
 }
 
-const CartButton: React.FC<CartButtonProps> = ({ productId, initIsInCart }) => {
-  const {
-    isInCart,
-    handleAddCartItem,
-    handlePatchCartItem,
-    handleRemoveCartItem,
-  } = useCartItem({
-    productId,
-    initIsInCart,
-  });
+const CartButton: React.FC<CartButtonProps> = ({ productId }) => {
+  const { isInCart } = useCartItemStatus(productId);
+  const { cartItemList } = useFetchCartItems();
+  const { addToCartMutation } = usePostCartItem();
+  const { delelteCartMutation } = useDeleteCartItem();
+  const { quantity } = useCartItemStatus(productId);
+  const cartItem = cartItemList.find(
+    (cartItem) => cartItem.product.id === productId,
+  );
 
-  if (isInCart) {
+  const handleRemoveCartItem = () => {
+    if (cartItem) {
+      delelteCartMutation.mutate(cartItem.id);
+    }
+  };
+
+  const handleAddCartItem = () => {
+    addToCartMutation.mutate(productId);
+  };
+
+  if (cartItem && quantity > 0) {
     return (
       <QuantityPicker
-        handlePatchCartItem={handlePatchCartItem}
-        handleRemoveCartItem={handleRemoveCartItem}
+        cartItemId={cartItem.id}
+        productId={productId}
+        isAutoDelete
       />
     );
   }
 
   return (
     <Button
-      isGray={isInCart}
+      color={!isInCart ? '#fff' : '#000'}
+      backgroundColor={isInCart ? '#eaeaea' : '#000'}
       onClick={isInCart ? handleRemoveCartItem : handleAddCartItem}
     >
-      <ButtonImg src={isInCart ? MinusCart : AddCart} />
-      <span>{isInCart ? '빼기' : '담기'}</span>
+      <CartButtonContentWrapper>
+        <ButtonImg src={isInCart ? MinusCart : AddCart} />
+        <span>{isInCart ? '빼기' : '담기'}</span>
+      </CartButtonContentWrapper>
     </Button>
   );
 };
