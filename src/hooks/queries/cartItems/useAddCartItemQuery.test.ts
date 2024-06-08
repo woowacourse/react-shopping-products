d80c3f1f6b363e51c7d09fc30824dd5b4097d9e6
rbgksqkr <rbgks1937@naver.com>
2024-06-09 00:56:27 +0900

    test: 장바구니 담기 및 수량 증가 쿼리 테스트 코드 작성

---
+++ src/hooks/queries/cartItems/useAddCartItemQuery.test.ts
@@ -0,0 +1,25 @@
+import { act, renderHook, waitFor } from '@testing-library/react';
+
+import useAddCartItemQuery from './useAddCartItemQuery';
+
+import MockWrapper from '@/mocks/MockWrapper';
+import { Product } from '@/types/product';
+
+describe('useAddCartItemQuery', () => {
+  it('새로운 제품을 추가할 수 있다.', async () => {
+    const { result } = renderHook(() => useAddCartItemQuery(), { wrapper: MockWrapper });
+    const product: Product = {
+      id: -1,
+      name: 'hello',
+      price: 10000000,
+      imageUrl: '',
+      category: 'kitchen',
+    };
+
+    act(() => {
+      result.current.mutate(product);
+    });
+
+    await waitFor(() => expect(result.current.variables).toEqual(product));
+  });
+});
