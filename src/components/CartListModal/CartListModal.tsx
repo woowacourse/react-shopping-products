704092d12ee1e96fa5d71118c3e2f3d5c093cf9c
pakxe <pigkill40@naver.com>
2024-06-09 03:31:13 +0900

    feat: 장바구니 모달 컴포넌트 구현

---
+++ src/components/CartListModal/CartListModal.tsx
@@ -0,0 +1,32 @@
+import useCartItemList from '@/hooks/cartItem/useCartItemList';
+import { Modal } from '@pakxe/react-simple-modal';
+import CartItem from '../CartItem/CartItem';
+import { PriceTable } from '../PriceTable/PriceTable';
+import Divider from '../common/Divider/Divider';
+
+type CartListModalProps = {
+  isOpen: boolean;
+  close: () => void;
+};
+
+const CartListModal = ({ isOpen, close }: CartListModalProps) => {
+  const { data, getTotalPrice } = useCartItemList();
+
+  return (
+    <Modal isOpen={isOpen} close={close} position="bottom" style={{ color: 'black' }}>
+      <Modal.Header>
+        <Modal.Title>장바구니</Modal.Title>
+      </Modal.Header>
+      <Modal.Body style={{ color: 'black', maxHeight: '300px', scrollbarWidth: 'none' }}>
+        {data?.map((cartItem) => <CartItem {...cartItem} />)}
+      </Modal.Body>
+      <Modal.Footer>
+        <Divider />
+        <PriceTable.Row name="총 결제 금액" price={getTotalPrice() ?? 0} />
+        <Modal.Button fullWidth text="닫기" onClick={close} />
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default CartListModal;
