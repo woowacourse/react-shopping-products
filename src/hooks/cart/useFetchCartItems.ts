2c5356f692b739c8e7664056c3e74153bb5c766a
river <130737187+0jenn0@users.noreply.github.com>
2024-06-08 21:11:19 +0900

    feat: 장바구니 목록 불러오는 hook 구현

---
+++ src/hooks/cart/useFetchCartItems.ts
@@ -0,0 +1,20 @@
+import { getCartList } from '@/api/cartItem';
+import QUERY_KEYS from '@/constants/queryKeys';
+import { CartItem } from '@/types/index';
+import { useQuery } from '@tanstack/react-query';
+
+const useFetchCartItems = () => {
+  const {
+    data: cartItemList,
+    isError,
+    isLoading,
+  } = useQuery<CartItem[]>({
+    queryKey: [QUERY_KEYS.CART.BASE, QUERY_KEYS.CART.FETCH],
+    queryFn: getCartList,
+    initialData: [],
+  });
+
+  return { cartItemList, isError, isLoading };
+};
+
+export default useFetchCartItems;
