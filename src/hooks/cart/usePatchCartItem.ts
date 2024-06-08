5ef851f0d3462fc9dc6c0649e5063d46c75980d1
river <130737187+0jenn0@users.noreply.github.com>
2024-06-08 21:10:52 +0900

    feat: 장바구니 속 상품 수 변경 hook 구현

---
+++ src/hooks/cart/usePatchCartItem.ts
@@ -0,0 +1,37 @@
+import { patchCartItem } from '@/api/cartItem';
+import QUERY_KEYS from '@/constants/queryKeys';
+import { useToast } from '@/hooks/index';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+const usePatchCartItem = () => {
+  const queryClient = useQueryClient();
+  const { createToast } = useToast();
+
+  const cartItemQuantityMutaion = useMutation({
+    mutationFn: async ({
+      cartItemId,
+      quantity,
+    }: {
+      cartItemId: number;
+      quantity: number;
+    }) => patchCartItem({ cartItemId, quantity }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.CART.BASE, QUERY_KEYS.CART.TOTAL_QUANTITY],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.CART.BASE, QUERY_KEYS.CART.TOTAL_PRICE],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.CART.BASE, QUERY_KEYS.CART.FETCH],
+      });
+    },
+    onError: () => {
+      createToast('⛔️ 상품을 제거하는데 실패했습니다. 다시 시도해 주세요.');
+    },
+  });
+
+  return { cartItemQuantityMutaion };
+};
+
+export default usePatchCartItem;
