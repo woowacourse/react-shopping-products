1ffb25f110f6a33d822723e3d029bc89dd9cbab9
rbgksqkr <rbgks1937@naver.com>
2024-06-09 02:49:04 +0900

    refactor: useQuery에서 fetch 에러 같은 경우 queryCache를 이용해 전역으로 에러 핸들링

--- src/QueryProvider.tsx
+++ src/QueryProvider.tsx
@@ -1,8 +1,16 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PropsWithChildren } from 'react';
 
+import useToast from './hooks/useToast';
+
 const QueryProvider = ({ children }: PropsWithChildren) => {
-  const queryClient = new QueryClient();
+  const toast = useToast();
+
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+      onError: (error) => toast.error(error.message),
+    }),
+  });
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
