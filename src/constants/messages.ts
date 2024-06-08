b6db386197fe88f15ff60a06b2ebae9d1c8ef49f
pakxe <pigkill40@naver.com>
2024-06-09 03:16:07 +0900

    feat: 에러 메세지 선언

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,2 +1,16 @@
 // 설계 미스일 경우
-export const SYSTEM_ERROR_MESSAGE = 'system error. 관리자에게 문의하십시오';
+export const ERROR_MESSAGE = {
+  SYSTEM_FAULT: 'system error. 관리자에게 문의하십시오',
+  OFFLINE: '네트워크 연결이 끊어졌습니다. 인터넷 연결을 확인하고 다시 시도하세요.',
+};
+
+export const REQUEST_CART_ITEMS_ERROR_MESSAGE = {
+  GET_CART_ITEMS: '장바구니 목록을 불러올 수 없습니다.',
+  ADD_CART_ITEM: '장바구니에 상품을 추가할 수 없습니다.',
+  DELETE_CART_ITEM: '장바구니에 상품을 제거할 수 없습니다.',
+  MODIFY_CART_ITEM: '장바구니 상품의 수량을 변경할 수 없습니다.',
+};
+
+export const REQUEST_PRODUCTS_ERROR_MESSAGE = {
+  GET_PRODUCT_LIST: '상품 목록을 불러올 수 없습니다.',
+};
