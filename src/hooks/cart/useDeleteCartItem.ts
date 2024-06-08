918dd9666af9821da4533b673fd187cfe1e13d29
river <130737187+0jenn0@users.noreply.github.com>
2024-06-08 21:13:29 +0900

    feat: 장바구니 삭제 hook 구현

---
+++ src/hooks/cart/useDeleteCartItem.ts
@@ -0,0 +1,29 @@
+import { deleteCartItem } from '@/api/cartItem';
+import QUERY_KEYS from '@/constants/queryKeys';
+import { useToast } from '@/hooks/index';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+const useDeleteCartItem = () => {
+  const queryClient = useQueryClient();
+  const { createToast } = useToast();
+
+  const delelteCartMutation = useMutation({
+    mutationFn: (cartItemId: number) => deleteCartItem(cartItemId),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.CART.BASE, QUERY_KEYS.CART.TOTAL_QUANTITY],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.CART.BASE, QUERY_KEYS.CART.FETCH],
+      });
+    },
+    onError: () => {
+      createToast('⛔️ 상품을 삭제하는데 실패했습니다. 다시 시도해 주세요.');
+    },
+  });
+
+  return { delelteCartMutation };
+};
+
+export default useDeleteCartItem;
