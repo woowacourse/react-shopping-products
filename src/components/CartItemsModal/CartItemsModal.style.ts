b733daad597252989502002a80b850e279c6b950
river <130737187+0jenn0@users.noreply.github.com>
2024-06-08 21:04:01 +0900

    feat: 장바구니 모달 구현

---
+++ src/components/CartItemsModal/CartItemsModal.style.ts
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const CartItemListContainer = styled.ul`
+  height: 300px;
+  overflow-y: auto;
+`;
+
+export const TotalPriceContainer = styled.div`
+  border-top: 1px solid #0000001a;
+  padding: 10px;
+  width: 100%;
+  box-size: border-box;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & > p:first-of-type {
+    font-size: 16px;
+    font-weight: 700;
+  }
+
+  & > p:nth-of-type(2) {
+    font-size: 24px;
+    font-weight: 700;
+  }
+`;
---
+++ src/components/CartItemsModal/CartItemsModal.tsx
@@ -0,0 +1,64 @@
+import { PropsWithChildren } from 'react';
+import { Modal } from 'river-modal-component';
+import { formatPriceToKoreanWon } from '@/utils/index';
+import { useFetchCartItems, useTotalPrice } from '@/hooks/index';
+import {
+  CartItemListContainer,
+  TotalPriceContainer,
+} from './CartItemsModal.style';
+import { CartItemCard, Button, FallbackCart } from '@/components/index';
+
+const CartItemsModal: React.FC<PropsWithChildren> = () => {
+  const { cartItemList, isLoading, isError } = useFetchCartItems();
+  const { totalPrice } = useTotalPrice();
+  return (
+    <Modal.Content
+      modalPosition="bottom"
+      closeButtonPosition="bottom"
+      size="small"
+    >
+      <Modal.Header containClose={false} title="장바구니" />
+      <Modal.Body>
+        {isError && (
+          <FallbackCart message="장바구니 목록을 불러오는데 예기치 않은 오류가 생겼습니다." />
+        )}
+        {!isLoading &&
+          !isError &&
+          (cartItemList.length !== 0 ? (
+            <>
+              <CartItemListContainer>
+                {cartItemList.map((cartItem, index) => (
+                  <CartItemCard
+                    key={`${cartItem.id}${index}`}
+                    cartItem={cartItem}
+                  />
+                ))}
+              </CartItemListContainer>
+              <TotalPriceContainer>
+                <p>총 결제 가격</p>
+                <p>{formatPriceToKoreanWon(totalPrice)}</p>
+              </TotalPriceContainer>
+            </>
+          ) : (
+            <FallbackCart message="장바구니가 텅- 비었습니다." />
+          ))}
+      </Modal.Body>
+      <Modal.Footer align="center">
+        <Modal.Close>
+          <Button
+            color="#fff"
+            backgroundColor="#333"
+            hasBorderRadius
+            borderColor="#0000001A"
+            width="400px"
+            height="44px"
+          >
+            닫기
+          </Button>
+        </Modal.Close>
+      </Modal.Footer>
+    </Modal.Content>
+  );
+};
+
+export default CartItemsModal;
