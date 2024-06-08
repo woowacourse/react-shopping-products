40fa0d77059ded4017c29e6497d9080dcad1d495
Soyeon Choe <soy2302ten@gmail.com>
2024-06-08 13:55:51 +0900

    chore: react-query-devtools 적용

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,21 @@ import GlobalStyle from "./GlobalStyle.style.ts";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme.ts";
 
-const queryClient = new QueryClient();
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 2000,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <ThemeProvider theme={theme}>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
       <GlobalStyle />
       <App />
     </QueryClientProvider>
