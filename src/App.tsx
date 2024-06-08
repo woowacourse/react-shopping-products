ec472cb2ddf918fcfa7a88b98b945cae3bfc9ac9
skiende74 <skiende74@gmail.com>
2024-06-09 03:10:25 +0900

    feat(App): QueryClientProvider로 감쌈

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Product from './pages/Product';
 
+const queryClient = new QueryClient();
 function App() {
-  return <Product />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Product />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
