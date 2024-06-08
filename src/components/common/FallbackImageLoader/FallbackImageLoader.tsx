c0ea8ca80623b3d8a98d87e81f92dd9993c71303
river <130737187+0jenn0@users.noreply.github.com>
2024-06-08 21:02:37 +0900

    feat: FallbackImageLoader에서 fallback으로 받는 컴포넌트를 children 크기로 조절

--- src/components/common/FallbackImageLoader/FallbackImageLoader.tsx
+++ src/components/common/FallbackImageLoader/FallbackImageLoader.tsx
@@ -1,15 +1,26 @@
-import FallbackImage from '@/components/Fallbacks/FallbackImage/FallbackImage';
 import React, {
   PropsWithChildren,
   useState,
   useEffect,
+  useRef,
   ReactElement,
+  ReactNode,
 } from 'react';
 
+interface FallbackImageLoaderProps
+  extends React.ImgHTMLAttributes<HTMLImageElement> {
+  fallbackComponent: ReactNode;
+}
+
 const FallbackImageLoader: React.FC<
-  PropsWithChildren<React.ImgHTMLAttributes<HTMLImageElement>>
-> = ({ children, ...props }) => {
+  PropsWithChildren<FallbackImageLoaderProps>
+> = ({ fallbackComponent, children, ...props }) => {
   const [isError, setIsError] = useState<boolean>(false);
+  const [dimensions, setDimensions] = useState<{
+    width: number;
+    height: number;
+  }>({ width: 0, height: 0 });
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     setIsError(false);
@@ -18,6 +29,25 @@ const FallbackImageLoader: React.FC<
   const handleError = () => {
     setIsError(true);
   };
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const resizeObserver = new ResizeObserver((entries) => {
+        if (entries[0].contentRect) {
+          setDimensions({
+            width: entries[0].contentRect.width,
+            height: entries[0].contentRect.height,
+          });
+        }
+      });
+
+      resizeObserver.observe(containerRef.current);
+
+      return () => {
+        resizeObserver.disconnect();
+      };
+    }
+  }, [containerRef.current]);
 
   const childWithProps = React.Children.map(children, (child) => {
     if (React.isValidElement(child)) {
@@ -30,10 +60,21 @@ const FallbackImageLoader: React.FC<
   });
 
   if (isError) {
-    return <FallbackImage />;
+    return (
+      <div
+        style={{
+          width: `${dimensions.width}px`,
+          height: `${dimensions.height}px`,
+        }}
+      >
+        {React.cloneElement(fallbackComponent as ReactElement, {
+          style: { width: '100%', height: '100%' },
+        })}
+      </div>
+    );
   }
 
-  return <>{childWithProps}</>;
+  return <div ref={containerRef}>{childWithProps}</div>;
 };
 
 export default FallbackImageLoader;
