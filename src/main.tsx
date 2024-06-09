5a2afbd8d24b59093e74ed9cf57e69a9de157f76
chysis <chysiss@naver.com>
2024-06-10 01:55:55 +0900

    chore: 코드 최상단에서 queryClientProvider 정의

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,17 @@ import { Global, ThemeProvider } from '@emotion/react';
 import theme from './styles/theme.ts';
 import globalStyles from './styles/globalStyles.ts';
 import AppRouter from './router/AppRouter.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Global styles={globalStyles()} />
-      <AppRouter />
+      <QueryClientProvider client={queryClient}>
+        <Global styles={globalStyles()} />
+        <AppRouter />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
