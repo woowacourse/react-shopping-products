fe6e466bc68eb6fcae203d7ac5d914de949853e5
jinsil <jinsil378900@gmail.com>
2024-06-09 14:08:55 +0900

    test: fetch 테스트 코드 작성

---
+++ src/hooks/useCartItems/useFetchCartItems.test.tsx
@@ -0,0 +1,51 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { server } from '../../mocks/server';
+import { HttpResponse, http } from 'msw';
+
+import useFetchCartItems from './useFetchCartItems';
+import { API_ENDPOINTS } from '../../api/endpoints';
+import ToastProvider from '../../context/ToastProvider';
+
+const createWrapper =
+  () =>
+  ({ children }: { children: React.ReactNode }) =>
+    (
+      <QueryClientProvider client={new QueryClient()}>
+        <ToastProvider>{children}</ToastProvider>
+      </QueryClientProvider>
+    );
+
+describe('useFetchCartItems', () => {
+  describe('장바구니 아이템 조회', () => {
+    it('장바구니 아이템을 조회한다.', async () => {
+      const { result } = renderHook(() => useFetchCartItems(), { wrapper: createWrapper() });
+
+      await waitFor(() => {
+        expect(result.current.cartItems).toHaveLength(5);
+      });
+    });
+
+    it('로딩 중인 상태를 표시한다.', async () => {
+      const { result } = renderHook(() => useFetchCartItems(), { wrapper: createWrapper() });
+
+      await waitFor(() => {
+        expect(result.current.status).toBe('pending');
+      });
+    });
+
+    it('에러 상태를 표시한다.', async () => {
+      server.use(
+        http.get(API_ENDPOINTS.CART, () => {
+          return new HttpResponse(null, { status: 500 });
+        }),
+      );
+
+      const { result } = renderHook(() => useFetchCartItems(), { wrapper: createWrapper() });
+
+      await waitFor(() => {
+        expect(result.current.status).toBe('error');
+      });
+    });
+  });
+});
