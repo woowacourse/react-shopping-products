2b3cb1eb90ef67b816d712f35d43fbc8884e0d6c
Seongjin Hong <hongseongjin.to@gmail.com>
2024-06-09 21:56:28 +0900

    chore: 이미지 타입 에셋 추가 및 폴더링 정리

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,9 @@
-export { default as CartItemIcon } from "./cartItem.svg";
-export { default as AddToCartIcon } from "./addToCartItem.svg";
-export { default as RemoveFromCartIcon } from "./removeFromCartItem.svg";
-export { default as SelectArrowDown } from "./selectArrowDown.svg";
-export { default as SelectArrowUp } from "./selectArrowUp.svg";
+export { default as CartItemIcon } from "./images/cartItem.svg";
+export { default as AddToCartIcon } from "./images/addToCartItem.svg";
+export { default as RemoveFromCartIcon } from "./images/removeFromCartItem.svg";
+export { default as SelectArrowDown } from "./images/selectArrowDown.svg";
+export { default as SelectArrowUp } from "./images/selectArrowUp.svg";
+export { default as IncreaseIcon } from "./images/increaseIcon.svg";
+export { default as IncreaseIconDisabled } from "./images/increaseIconDisabled.svg";
+export { default as DecreaseIcon } from "./images/decreaseIcon.svg";
+export { default as DecreaseIconDisabled } from "./images/decreaseIconDisabled.svg";
