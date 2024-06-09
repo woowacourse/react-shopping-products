c5fdca027efbb8b9b8f41f70fbf3b59dd108937f
이태훈 <rhymint@gmail.com>
2024-06-09 20:47:27 +0900

    fix: api 응답에 대응하는 type 선언

--- src/types/type.ts
+++ src/types/type.ts
@@ -25,6 +25,35 @@ export interface CartItem {
 
 export type CartItemList = CartItem[];
 
+export interface CartItemListResponse {
+  content: CartItemList;
+  pageable: Pageable;
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  sort: SortPage;
+  first: boolean;
+  number: number;
+  numberOfElements: number;
+  size: number;
+  empty: boolean;
+}
+
+export interface Pageable {
+  sort: SortPage;
+  pageNumber: number;
+  pageSize: number;
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+export interface SortPage {
+  sorted: boolean;
+  unsorted: boolean;
+  empty: boolean;
+}
+
 export interface AddCartItemAction {
   type: 'ADD';
   payload: CartItem;
@@ -35,6 +64,7 @@ export interface DeleteCartItemAction {
   payload: { productId: number };
 }
 
+// TODO: 확인 후 필요없는 코드 삭제
 export interface PatchCartItemAction {
   type: 'PATCH';
   payload: { productId: number; quantity: number };
