9e2a354085cdb62759ca7c5bce0ea130b1e83396
bmj0053 <bmj05378@gmail.com>
2024-06-09 21:11:38 +0900

    fix: 모달 라이브러리 Modal 타입 추가 및 빌드 에러 해결

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,4 +3,22 @@ declare module 'woowacourse-react-modal-component' {
     isOpen: boolean;
     toggleModal: () => void;
   };
+
+  interface ModalProps {
+    isOpen: boolean;
+    toggleModal: () => void;
+    position?: 'top' | 'center' | 'bottom';
+    size?: 'small' | 'medium' | 'large';
+    children?: React.ReactNode;
+  }
+
+  interface ModalHeaderProps {
+    title: string;
+    closeOption?: 'button' | 'none';
+    handleCloseButton: () => void;
+  }
+
+  export const Modal: React.FC<ModalProps> & {
+    Header: React.FC<ModalHeaderProps>;
+  };
 }
