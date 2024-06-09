f66c4ef2ae997ad9311b5078c360149e9a208494
이태훈 <rhymint@gmail.com>
2024-06-09 22:27:03 +0900

    remove: 사용하지 않는 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { useState } from 'react';
-
 import Header from './components/Header/Header';
 import Product from './pages/Product';
 import GlobalStyles from './styles/Global.style';
-import Toast from './components/common/Toast/Toast';
-import { ErrorContext } from './store/ErrorContext';
 
 function App() {
-  const [error, setError] = useState<Error | null>(null);
-
   return (
     <>
       <GlobalStyles />
-      <ErrorContext.Provider value={{ error, setError }}>
-        <Toast />
-        <Header />
-        <Product />
-      </ErrorContext.Provider>
+      <Header />
+      <Product />
     </>
   );
 }
