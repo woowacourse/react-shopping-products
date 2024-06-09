360e9750dfb966cef670b7203b1b0d5984013d5d
river <130737187+0jenn0@users.noreply.github.com>
2024-06-09 22:48:03 +0900

    feat: theme 객체 생성 및 ThemeProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import './index.css';
 import ProductPage from './page/ProductPage';
 import { ToastContextProvider } from './contexts/ToastContextProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from '@emotion/react';
+import theme from '@/styles/theme';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ToastContextProvider>
-        <ProductPage />
-      </ToastContextProvider>
+      <ThemeProvider theme={theme}>
+        <ToastContextProvider>
+          <ProductPage />
+        </ToastContextProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
