8dff73aea095710cbb90d1da6274b232ca3bc72f
이태훈 <rhymint@gmail.com>
2024-06-09 22:27:23 +0900

    feat: ToastProvider 적용

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App.tsx';
+import { ToastProvider } from './store/ToastProvider.tsx';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
