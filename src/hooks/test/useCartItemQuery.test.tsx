e3af3979a2d7708a858d05a5460dd864a4cc263c
jayming66 <mjw8941@gmail.com>
2024-06-09 14:09:48 +0900

    feat(vite.config.ts): 경로 alias 설정

--- src/hooks/test/useCartItemQuery.test.tsx
+++ src/hooks/test/useCartItemQuery.test.tsx
@@ -1,7 +1,7 @@
 import { renderHook, waitFor } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import useCartItemQuery from '../useCartItemQuery';
-import cartItems from '../../mocks/cartItems.json';
+import cartItems from '@/mocks/cartItems.json';
 
 const queryClient = new QueryClient();
 
