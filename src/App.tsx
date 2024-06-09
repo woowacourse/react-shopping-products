b856875f9327836dde1f7cfd4dfb897fac56414a
useon <yoosk4986@naver.com>
2024-06-09 09:54:28 +0900

    chore: 리액트 쿼리 초기세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ProductListPage from './pages/ProductListPage/ProductListPage';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 10,
+      retry: 0,
+    },
+  },
+});
 const App = () => {
   return (
     <>
-      <ProductListPage />
+      <QueryClientProvider client={queryClient}>
+        <ProductListPage />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 };
