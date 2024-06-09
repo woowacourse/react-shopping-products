3c3455a69b7fa66f4b034610f6af20131b162151
river <130737187+0jenn0@users.noreply.github.com>
2024-06-09 20:21:35 +0900

    refactor: queryKey 상수를 factory 객체으로 리팩토링

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -1,8 +1,16 @@
-export const QUERY_KEYS = {
-  PRODUCTS: 'products',
-  CART: {
-    BASE: 'cart',
-    TOTAL_QUANTITY: 'total_quantity',
-    FETCH: 'fetch',
-  },
+export const productsKeys = {
+  all: ['products'] as const,
+  byCategoryAndSort: ({
+    category,
+    sort,
+  }: {
+    category: string;
+    sort: string;
+  }) => [...productsKeys.all, category, sort],
+};
+
+export const cartKeys = {
+  all: ['cart'] as const,
+  totalQuantity: () => [...cartKeys.all, 'total_quantity'] as const,
+  totalPrice: () => [...cartKeys.all, 'total_price'] as const,
 };
