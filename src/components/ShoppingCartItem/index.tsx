49817758fd535650f54fbe8f43d786ef06186fe4
simhorim <hori5020@gmail.com>
2024-06-09 21:06:52 +0900

    design(ShoppingCartItem): UI 구현

---
+++ src/components/ShoppingCartItem/index.tsx
@@ -0,0 +1,34 @@
+import * as S from './style';
+
+import Divider from '../Divider/Divider';
+import AdjustButtonContainer from '../AdjustButtonContainer';
+
+import { CartItem } from '../../types/cart';
+import RemoveButton from '../RemoveButton';
+
+interface ShoppingCartItemProps {
+  cartItem: CartItem;
+}
+
+const ShoppingCartItem = ({ cartItem }: ShoppingCartItemProps) => {
+  return (
+    <S.ShoppingCartItem>
+      <Divider />
+      <S.ItemInfoContainer>
+        <S.ItemImg src={cartItem.product.imageUrl} />
+        <S.ItemInfo>
+          <S.ItemDetailsContainer>
+            <S.ItemDetails>
+              <S.ItemName>{cartItem.product.name}</S.ItemName>
+              <S.ItemPrice>{`${cartItem.product.price.toLocaleString('ko-kr')}원`}</S.ItemPrice>
+            </S.ItemDetails>
+            <RemoveButton cartItem={cartItem} />
+          </S.ItemDetailsContainer>
+          <AdjustButtonContainer cartItem={cartItem} />
+        </S.ItemInfo>
+      </S.ItemInfoContainer>
+    </S.ShoppingCartItem>
+  );
+};
+
+export default ShoppingCartItem;
