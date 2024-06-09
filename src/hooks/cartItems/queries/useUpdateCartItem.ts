fdb819cdb860955f3b09d3ad1b286e4ec6f7d9ef
Seongjin Hong <hongseongjin.to@gmail.com>
2024-06-09 21:58:40 +0900

    refactor: useUpdateCartItem의 인자로 들어오는 mutationFn을 제네릭 타입으로 수정

--- src/hooks/cartItems/queries/useUpdateCartItem.ts
+++ src/hooks/cartItems/queries/useUpdateCartItem.ts
@@ -1,11 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { QUERY_KEYS } from "../../../constants";
 
-export default function useUpdateCartItem(mutationFn: (productId: number) => Promise<void>) {
+type UpdateCartItemFuntion<T> = (args: T) => Promise<void>;
+
+export default function useUpdateCartItem<T>(mutationFn: UpdateCartItemFuntion<T>) {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn,
+    mutationFn: (args: T) => mutationFn(args),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.CART_ITEMS] });
     },
