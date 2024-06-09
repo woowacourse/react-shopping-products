5948cb99bbe600c59671b37ab720d883dc4ab75a
00kang <kdb070970@gmail.com>
2024-06-10 01:09:41 +0900

    feat : minus, plus 이미지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,5 @@
 export { default as MinusShoppingCartIcon } from "./MinusShoppingCartIcon.svg";
 export { default as PlusShoppingCartIcon } from "./PlusShoppingCartIcon.svg";
 export { default as ShoppingCartIcon } from "./ShoppingCartIcon.svg";
+export { default as PlusIcon } from "./PlusIcon.svg";
+export { default as MinusIcon } from "./MinusIcon.svg";
