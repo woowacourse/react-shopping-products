927b7ef61ca443ab6be26714673869b1719f66bf
simhorim <hori5020@gmail.com>
2024-06-09 15:58:49 +0900

    chore(assets): 버튼 이미지 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,3 +4,6 @@ export { default as ADD_TO_CART } from './addToCart.svg';
 export { default as REMOVE_TO_CART } from './removeToCart.svg';
 export { default as UP_ARROW } from './upArrow.svg';
 export { default as DOWN_ARROW } from './downArrow.svg';
+export { default as MINUS_BUTTON } from './minusButton.svg';
+export { default as PLUS_BUTTON } from './plusButton.svg';
+export { default as REMOVE_BUTTON } from './removeButton.svg';
