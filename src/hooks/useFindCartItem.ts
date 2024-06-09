d9d5d636c365a314df3f1b60c63d9230aba0e33e
lurgi <wahoo9040@naver.com>
2024-06-09 18:08:24 +0900

    refactor: 타입 명시적으로 작성

--- src/hooks/useFindCartItem.ts
+++ src/hooks/useFindCartItem.ts
@@ -2,22 +2,23 @@ import { useQuery } from "@tanstack/react-query";
 import { CartItemType } from "../types/cartItems";
 import { getCartItems } from "../api/cartItems";
 
-interface UseFindCartItemProps {
-  cartId?: number;
-  productId?: number;
+interface UseFindCartItemByCartId {
+  cartId: number;
 }
 
-const useFindCartItem = ({ cartId, productId }: UseFindCartItemProps): CartItemType | undefined => {
+interface UseFindCartItemByProductId {
+  productId: number;
+}
+
+const useFindCartItem = (props: UseFindCartItemByCartId | UseFindCartItemByProductId): CartItemType | undefined => {
   const { data: cartItems } = useQuery<CartItemType[]>({ queryKey: ["cartItems"], queryFn: getCartItems });
 
-  const item1 = cartItems?.find((it) => it.id === cartId);
-  if (cartId && item1) {
-    return item1;
+  if ("cartId" in props) {
+    return cartItems?.find((it) => it.id === props.cartId);
   }
 
-  const item2 = cartItems?.find((it) => it.product.id === productId);
-  if (productId && item2) {
-    return item2;
+  if ("productId" in props) {
+    return cartItems?.find((it) => it.product.id === props.productId);
   }
 };
 
