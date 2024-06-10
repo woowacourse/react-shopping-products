c868bcb0ca505b44254a3b3eaf13c74a05de49a2
Seongjin Hong <hongseongjin.to@gmail.com>
2024-06-10 13:13:37 +0900

    chore: 미사용 훅 제거

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,4 @@
-export { default as useFetch } from "./useFetch";
 export { default as useIntersectionObserver } from "./useIntersectionObserver";
-export { default as usePagination } from "./usePagination";
 export { default as useToast } from "./useToast";
 
 export { default as useCartItems } from "./cartItems/useCartItems";
