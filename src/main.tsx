9560a8eead974ee3fb4a1a0133cdef54db98ede2
chysis <chysiss@naver.com>
2024-06-10 17:07:17 +0900

    chore: 최상단에 toastProvider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,19 @@ import theme from './styles/theme.ts';
 import globalStyles from './styles/globalStyles.ts';
 import AppRouter from './router/AppRouter.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ToastProvider } from '@_context/ToastProvider.tsx';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <Global styles={globalStyles()} />
-        <AppRouter />
-      </QueryClientProvider>
+      <ToastProvider>
+        <QueryClientProvider client={queryClient}>
+          <Global styles={globalStyles()} />
+          <AppRouter />
+        </QueryClientProvider>
+      </ToastProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
