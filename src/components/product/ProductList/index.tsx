960bc871cb1b165ad83ebbbcc31d5d0733b1933e
chysis <chysiss@naver.com>
2024-06-10 16:56:38 +0900

    refactor: toast를 전역에서 사용할 수 있도록 toastProvider 정의 및 에러 처리

--- src/components/product/ProductList/index.tsx
+++ src/components/product/ProductList/index.tsx
@@ -1,6 +1,7 @@
-import APIErrorFallbackModal from '@_components/common/APIErrorFallbackModal';
 import * as S from './style';
 import { LoadingSpinner } from '@_components/common/LoadingSpinner/style';
+import { useToast } from '@_hooks/useToast';
+import { useEffect } from 'react';
 
 interface ProductListProps {
   loading: boolean;
@@ -8,10 +9,22 @@ interface ProductListProps {
 }
 
 export default function ProductList({ error, loading, children }: React.PropsWithChildren<ProductListProps>) {
+  const { showToast } = useToast();
+
+  useEffect(() => {
+    switch (error && error.message[0]) {
+      case '4':
+      case '5':
+        showToast('서버 오류가 발생했습니다.');
+        break;
+
+      default:
+        break;
+    }
+  }, [error]);
+
   return (
     <S.Grid>
-      {error && <APIErrorFallbackModal errorMessage={error.message} />}
-
       {children}
 
       {loading && (
