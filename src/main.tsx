2870033f4b7c0468f2f217e999fad99334e9456d
00kang <kdb070970@gmail.com>
2024-06-10 16:44:54 +0900

    feat : tanstack query 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ErrorProvider } from "./context/errorContext.tsx";
 import "./reset.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ErrorProvider>
-      <App />
-    </ErrorProvider>
+    <QueryClientProvider client={queryClient}>
+      <ErrorProvider>
+        <App />
+      </ErrorProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
