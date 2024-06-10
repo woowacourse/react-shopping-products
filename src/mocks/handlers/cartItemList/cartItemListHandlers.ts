771bcd9a56f424e0c062bbc00c2f5d69a38296a9
이태훈 <rhymint@gmail.com>
2024-06-10 14:48:48 +0900

    fix: handler 코드 변경

--- src/mocks/handlers/cartItemList/cartItemListHandlers.ts
+++ src/mocks/handlers/cartItemList/cartItemListHandlers.ts
@@ -1,18 +1,16 @@
 import { http, HttpResponse } from 'msw';
 
-import { CART_ITEMS_ENDPOINT } from '../../../apis/config';
+import { END_POINTS } from '../../../apis/config';
 import productListData from '../productList/defaultData.json';
 
 import cartItemListData from './defaultData.json';
 
-const mockCartItemList = cartItemListData;
-
 export const cartItemListHandlers = [
-  http.get(CART_ITEMS_ENDPOINT, () =>
-    HttpResponse.json({ content: mockCartItemList }, { status: 200 }),
+  http.get(END_POINTS.CART_ITEMS, () =>
+    HttpResponse.json({ content: cartItemListData }, { status: 200 }),
   ),
 
-  http.post(CART_ITEMS_ENDPOINT, ({ params }) => {
+  http.post(END_POINTS.CART_ITEMS, ({ params }) => {
     const { id } = params;
     const newItem = productListData.find(
       (item) => item.id === Number(id as string),
@@ -20,28 +18,27 @@ export const cartItemListHandlers = [
     if (!newItem) {
       return HttpResponse.json('상품을 찾을 수 없습니다.', { status: 404 });
     }
-    if (mockCartItemList.some((item) => newItem.id === item.product.id)) {
+    if (cartItemListData.some((item) => newItem.id === item.product.id)) {
       return HttpResponse.json('이미 장바구니에 상품이 있습니다.', {
         status: 400,
       });
     }
-    mockCartItemList.push({
-      id: mockCartItemList[mockCartItemList.length - 1].id + 1,
+    cartItemListData.push({
+      id: cartItemListData[cartItemListData.length - 1].id + 1,
       quantity: 1,
       product: newItem,
     });
     return HttpResponse.json({ status: 201 });
   }),
 
-  http.delete(CART_ITEMS_ENDPOINT, ({ params }) => {
+  http.delete(END_POINTS.CART_ITEMS, ({ params }) => {
     const { id } = params;
-    const index = mockCartItemList.findIndex(
+    const index = cartItemListData.findIndex(
       (item) => item.id === parseInt(id as string, 10),
     );
     if (index === -1) {
       return HttpResponse.json('장바구니에 없는 상품입니다.', { status: 400 });
     }
-    mockCartItemList.splice(index, 1);
     return HttpResponse.json({ status: 204 });
   }),
 ];
--- src/mocks/handlers/productList/productListHandlers.ts
+++ src/mocks/handlers/productList/productListHandlers.ts
@@ -1,43 +1,36 @@
 import { http, HttpResponse } from 'msw';
 
-import { PRODUCTS_ENDPOINT } from '../../../apis/config';
+import { END_POINTS } from '../../../apis/config';
 import { PRODUCT_LIST } from '../../../constants/productList';
 import { Product } from '../../../types/type';
 
 import productListData from './defaultData.json';
 
 export const productListHandlers = [
-  http.get(PRODUCTS_ENDPOINT, ({ request }) => {
+  http.get(END_POINTS.PRODUCTS, ({ request }) => {
     const url = new URL(request.url);
     const initialData: Product[] = productListData;
     const page = Number(url.searchParams.get('page') || '0');
     const category = url.searchParams.get('category') || '';
 
-    const sort = url.searchParams.getAll('sort') || 'price,asc';
+    const sort = url.searchParams.getAll('sort') || ['price,asc', 'name,asc'];
     const isValidSortParams = !sort.some((option) => {
       const [sortType, sortProperty] = option.split(',');
       return (
         !Object.keys(initialData[0]).includes(sortType) ||
-        sortProperty !== ('asc' || 'desc')
+        !['asc', 'desc'].includes(sortProperty)
       );
     });
     if (!isValidSortParams) {
       return new HttpResponse('올바른 형식의 sort parameter가 아닙니다.', {
-        status: 500,
+        status: 400,
       });
     }
 
     const limit =
       page === 0 ? PRODUCT_LIST.initialQuantity : PRODUCT_LIST.quantityPerPage;
 
-    const pageNumber =
-      page - PRODUCT_LIST.initialQuantity / PRODUCT_LIST.quantityPerPage;
-
-    const start =
-      page === 0
-        ? 0
-        : pageNumber * PRODUCT_LIST.quantityPerPage +
-          PRODUCT_LIST.initialQuantity;
+    const start = page * limit;
     const end = start + limit;
 
     const filteredData =
@@ -46,16 +39,15 @@ export const productListHandlers = [
         : initialData.filter((item) => item.category === category);
 
     const sortedData = filteredData.sort((a, b) => {
-      let comparison = 0;
-      sort.forEach((option) => {
+      for (const option of sort) {
         const [sortType, sortProperty] = option.split(',');
         const sortKey = sortType as keyof Product;
-        const sortOrder = sortProperty === 'asc' ? -1 : 1;
+        const sortOrder = sortProperty === 'asc' ? 1 : -1;
 
-        if (a[sortKey] < b[sortKey]) comparison = 1 * sortOrder;
-        if (a[sortKey] > b[sortKey]) comparison = -1 * sortOrder;
-      });
-      return comparison;
+        if (a[sortKey] < b[sortKey]) return -1 * sortOrder;
+        if (a[sortKey] > b[sortKey]) return 1 * sortOrder;
+      }
+      return 0;
     });
 
     const paginatedData = sortedData.slice(start, end);
@@ -63,8 +55,8 @@ export const productListHandlers = [
       sortedData.length <= PRODUCT_LIST.initialQuantity
         ? 0
         : sortedData.length - 1 / PRODUCT_LIST.quantityPerPage;
-    const isLastPage = lastPage === page;
 
+    const isLastPage = lastPage === page;
     return new HttpResponse(
       JSON.stringify({ content: paginatedData, last: isLastPage }),
       {
