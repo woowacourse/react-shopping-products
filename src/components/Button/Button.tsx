bf48ac5d421b107fa4cd470fb9c65313f8d8fe5c
Soyeon Choe <soy2302ten@gmail.com>
2024-06-11 16:24:41 +0900

    refactor: 기본 button 태그가 받는 property 받도록 하기

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,9 +3,19 @@ import { ButtonStyle } from "./Button.style";
 export default function Button({
   text,
   onClick,
+  disabled,
+  type,
+  name,
 }: {
   text: string;
   onClick?: () => void;
+  disabled?: boolean;
+  type?: "submit" | "reset" | "button";
+  name?: string;
 }) {
-  return <ButtonStyle onClick={onClick}>{text}</ButtonStyle>;
+  return (
+    <ButtonStyle onClick={onClick} disabled={disabled} type={type} name={name}>
+      {text}
+    </ButtonStyle>
+  );
 }
