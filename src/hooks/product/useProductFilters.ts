a7a62898b615ee0753c721f08e876497c7b7d242
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-06-12 00:13:51 +0900

    refactor: 불필요한 조건문 제거 (useProductFilters)

--- src/hooks/product/useProductFilters.ts
+++ src/hooks/product/useProductFilters.ts
@@ -4,25 +4,11 @@ const useProductFilters = () => {
   const [sort, setSort] = useState('asc');
   const [category, setCategory] = useState('');
 
-  const handleSortChange = (newSort: string) => {
-    if (sort === newSort) {
-      return;
-    }
-    setSort(newSort);
-  };
-
-  const handleCategoryChange = (newCategory: string) => {
-    if (category === newCategory) {
-      return;
-    }
-    setCategory(newCategory);
-  };
-
   return {
     sort,
     category,
-    handleSortChange,
-    handleCategoryChange,
+    handleSortChange: setSort,
+    handleCategoryChange: setCategory,
   };
 };
 
