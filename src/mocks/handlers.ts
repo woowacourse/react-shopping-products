3a77653e21ca1a1e91920a4ad1c48736c6bcec81
ImxYJL <allensain14@gmail.com>
2024-06-11 14:59:03 +0900

    refactor: MSW 핸들러 리팩토링 - 반복되는 로직(url 제작, 응답 받기)을 함수로 추출

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,59 +3,80 @@ import { BASE_URL } from "../api/cartClient";
 import { API_URL } from "../constants/url";
 import products from "./products.json";
 import { SORT_OPTIONS } from "../constants/products";
-import { ICartItem } from "./../api/cartItems";
 import cartItems from "./cartItems.json";
 
+const createUrl = (endpoint) => `${BASE_URL}/${endpoint}`;
+
+const createResponse = async (handler, req) => {
+  const data = await handler(req);
+  return HttpResponse.json(data.response, { status: data.status });
+};
+
 export const handlers = [
-  // NOTE: add a cartItem
-  http.post(
-    BASE_URL + "/" + API_URL.cartItems,
-    async ({ request }: { request: { json: () => Promise<ICartItem> } }) => {
+  http.post(createUrl(API_URL.cartItems), (req) =>
+    createResponse(async ({ request }) => {
       const newCartItem = await request.json();
-      return HttpResponse.json(newCartItem, { status: 201 });
-    }
+      return { response: newCartItem, status: 201 };
+    }, req)
+  ),
+
+  http.delete(createUrl(API_URL.cartItems) + "/:cartItemId", (req) =>
+    createResponse(async () => {
+      return { response: {}, status: 201 };
+    }, req)
+  ),
+
+  http.patch(createUrl(API_URL.cartItems) + "/:cartItemId", (req) =>
+    createResponse(async () => {
+      return { response: {}, status: 201 };
+    }, req)
   ),
 
   // NOTE: get cartItems
-  http.get(BASE_URL + "/" + API_URL.cartItems, async () => {
-    const defaultCartItems = cartItems;
-    return HttpResponse.json(defaultCartItems, { status: 201 });
-  }),
+  http.get(createUrl(API_URL.cartItems), (req) =>
+    createResponse(async () => {
+      const defaultCartItems = cartItems;
+      return { response: defaultCartItems, status: 201 };
+    }, req)
+  ),
 
-  http.get(BASE_URL + "/" + API_URL.products, ({ request }) => {
-    const url = new URL(request.url);
+  // NOTE: get products
+  http.get(createUrl(API_URL.products), (req) =>
+    createResponse(async ({ request }) => {
+      const url = new URL(request.url);
 
-    const page = Number(url.searchParams.get("page") || "0");
-    const size = Number(url.searchParams.get("size") || "20");
-    const category = url.searchParams.get("category");
-    const sort = url.searchParams.get("sort")?.split(",");
+      const page = Number(url.searchParams.get("page") || "0");
+      const size = Number(url.searchParams.get("size") || "20");
+      const category = url.searchParams.get("category");
+      const sort = url.searchParams.get("sort")?.split(",");
 
-    const start = page * size;
-    const end = start + size;
+      const start = page * size;
+      const end = start + size;
 
-    const paginatedProducts = products.slice(start, end);
-    const filteredProducts = paginatedProducts.map((product) => ({
-      ...product,
-      category,
-    }));
+      const paginatedProducts = products.slice(start, end);
+      const filteredProducts = paginatedProducts.map((product) => ({
+        ...product,
+        category,
+      }));
 
-    const [sortKey, sortValue] = sort || [null, null];
-    const isPriceSort = sortKey === "price";
-    const sortedProducts = filteredProducts.sort((a, b) => {
-      if (!isPriceSort) {
-        return 0;
-      }
+      const [sortKey, sortValue] = sort || [null, null];
+      const isPriceSort = sortKey === "price";
+      const sortedProducts = filteredProducts.sort((a, b) => {
+        if (!isPriceSort) {
+          return 0;
+        }
 
-      if (typeof sortValue === "string" && sortValue === SORT_OPTIONS.asc) {
-        return a.price - b.price;
-      } else {
-        return b.price - a.price;
-      }
-    });
+        if (typeof sortValue === "string" && sortValue === SORT_OPTIONS.asc) {
+          return a.price - b.price;
+        } else {
+          return b.price - a.price;
+        }
+      });
 
-    return HttpResponse.json({
-      content: sortedProducts,
-      lastPage: end >= products.length,
-    });
-  }),
+      return {
+        response: { content: sortedProducts, lastPage: end >= products.length },
+        status: 201,
+      };
+    }, req)
+  ),
 ];
