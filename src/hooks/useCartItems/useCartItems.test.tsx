1321cf6b7270edd1aedef19cf1f2e60806717550
ImxYJL <allensain14@gmail.com>
2024-06-12 11:54:47 +0900

    test: useCartItems 리팩토링 - result에서 응답 결과의 property를 확인하는 방식으로 변경

--- src/hooks/useCartItems/useCartItems.test.tsx
+++ src/hooks/useCartItems/useCartItems.test.tsx
@@ -10,10 +10,15 @@ const wrapper = ({ children }: { children: React.ReactNode }) => (
 
 describe("useCartItems", () => {
   it("장바구니에 존재하는 상품을 불러올 수 있다.", async () => {
+    const cartItems = "cartItems";
+    const errorMessage = "errorMessage";
+    const isLoading = "isLoading";
     const { result } = renderHook(() => useCartItems(), { wrapper });
 
     await waitFor(() => expect(result.current.isLoading).toEqual(false));
 
-    expect(result.current.cartItems.length).toBeGreaterThan(0);
+    expect(result.current).toHaveProperty(cartItems);
+    expect(result.current).toHaveProperty(isLoading);
+    expect(result.current).toHaveProperty(errorMessage);
   });
 });
