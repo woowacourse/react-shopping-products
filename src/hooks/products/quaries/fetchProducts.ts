4ee0206f03f963e85cea4d0b77fae6e2e364c13f
Seongjin Hong <hongseongjin.to@gmail.com>
2024-06-12 17:04:48 +0900

    refactor: product 도메인에 종속된 createFetchProductsParams를 fetchProducts 내부로 이동

--- src/hooks/products/quaries/fetchProducts.ts
+++ src/hooks/products/quaries/fetchProducts.ts
@@ -1,6 +1,11 @@
 import { getProducts } from "../../../apis";
-import { CategoryQueryString, ENDPOINT, SortOptionQueryString } from "../../../constants";
-import { createFetchProductsParams, createFetchUrl } from "../../../utils";
+import {
+  CategoryQueryString,
+  ENDPOINT,
+  PRODUCTS_SIZE,
+  SortOptionQueryString,
+} from "../../../constants";
+import { createFetchUrl } from "../../../utils";
 
 interface FetchProductsProps {
   page: number;
@@ -8,8 +13,31 @@ interface FetchProductsProps {
   sortOption: SortOptionQueryString;
 }
 
+interface createFetchProductsParamsProps {
+  page: number;
+  category: string;
+  sortOption: string;
+}
+
 export default async function fetchProducts({ page, category, sortOption }: FetchProductsProps) {
   const params = createFetchProductsParams({ page, category, sortOption });
   const fetchUrl = createFetchUrl({ endpoint: ENDPOINT.PRODUCT, params });
   return await getProducts(fetchUrl.href);
+}
+
+function createFetchProductsParams({
+  page,
+  category,
+  sortOption,
+}: createFetchProductsParamsProps): Record<string, string | undefined> {
+  const sizeParam = page === 0 ? PRODUCTS_SIZE.initial : PRODUCTS_SIZE.perRequest;
+  const categoryParam = category === "all" ? undefined : category;
+  const sortParam = `price,${sortOption}`;
+
+  return {
+    page: page.toString(),
+    size: sizeParam.toString(),
+    category: categoryParam,
+    sort: sortParam,
+  };
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export { default as createFetchUrl } from "./createFetchUrl";
-export { default as createFetchProductsParams } from "./createFetchProductsParams";
 export { default as generateBasicToken } from "./generateBasicToken";
 export { default as convertToLocaleAmount } from "./convertToLocaleAmount";
 export { default as validateProductImageUrl } from "./validateProductImageUrl";
