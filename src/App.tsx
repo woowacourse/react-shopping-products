c6eb906dc5e8dc5ba6948555a9ee703d593360b3
00kang <kdb070970@gmail.com>
2024-06-12 18:04:23 +0900

    refactor : useToast와 QueryCache, mutationCache를 활용한 에러 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { ProductSection } from "./components/ProductSection/ProductSection";
-import { ToastNotification } from "./components/ToastNotification/ToastNotification";
+import ToastNotification from "./components/ToastNotification/ToastNotification";
+import { useToast } from "./hooks";
 
 const App = () => {
+  const { showToast, toastMessage } = useToast();
+
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+      onError: (error) => {
+        showToast(error.message);
+      },
+    }),
+    mutationCache: new MutationCache({
+      onError: (error) => {
+        showToast(error.message);
+      },
+    }),
+  });
+
   return (
     <>
-      <Header />
-      <ToastNotification />
-      <ProductSection />
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <ProductSection />
+        {toastMessage && <ToastNotification message={toastMessage} />}
+      </QueryClientProvider>
     </>
   );
 };
