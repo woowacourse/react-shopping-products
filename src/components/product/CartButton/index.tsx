65fdfbe1ac92c7df2fe514a0b750da5092f8411b
Kim Da Eun <midekuna@gmail.com>
2024-06-13 00:15:47 +0900

    refactor: CartButtonContext를 없애고 isPushed를 직접 계산

--- src/components/product/CartButton/index.tsx
+++ src/components/product/CartButton/index.tsx
@@ -1,9 +1,5 @@
-import { CartButtonProvider } from '../../../context/cartButton/CartButtonProvider';
-import { useCartButtonContext } from '../../../context/cartButton/useCartButtonContext';
-import { cartMutations } from '../../../hooks/queries/cart';
+import { cartMutations, cartQueries } from '../../../hooks/queries/cart';
 
-import { Suspense } from 'react';
-import { LoadingSpinner } from '../../common/LoadingSpinner/style';
 import QuantityButton from '../../common/QuantityButton';
 import { AddCartIcon } from './Icons';
 import * as S from './style';
@@ -13,42 +9,30 @@ export interface CartButtonProps {
 }
 
 export default function CartButton({ productId }: CartButtonProps) {
+  const { data: cartItems } = cartQueries.useGetCartItems();
+
+  const isPushed = cartItems.some(
+    (cartItem) => cartItem.product.id === productId
+  );
+
   return (
-    <CartButtonProvider productId={productId}>
-      <CartButton.Toggle />
-    </CartButtonProvider>
+    <S.ButtonContainer>
+      {isPushed ? (
+        <QuantityButton productId={productId} />
+      ) : (
+        <CartButton.Add productId={productId} />
+      )}
+    </S.ButtonContainer>
   );
 }
 
-CartButton.Toggle = function Toggle() {
-  const { productId, isPushed } = useCartButtonContext();
-
-  return (
-    <Suspense fallback={<LoadingSpinner />}>
-      <S.ButtonContainer>
-        {isPushed ? (
-          <QuantityButton productId={productId} />
-        ) : (
-          <CartButton.Add />
-        )}
-      </S.ButtonContainer>
-    </Suspense>
-  );
-};
-
-CartButton.Add = function Add() {
-  const { productId } = useCartButtonContext();
-
+CartButton.Add = function Add({ productId }: { productId: number }) {
   const { mutate: addCartItem } = cartMutations.useAddCartItem({
     productId,
   });
 
-  const handleClick = () => {
-    addCartItem();
-  };
-
   return (
-    <S.Button isPushed={false} onClick={handleClick}>
+    <S.Button isPushed={false} onClick={() => addCartItem}>
       <AddCartIcon />
       <p>담기</p>
     </S.Button>
