f20911dd854ca0ff89210c15e4d2e758727a75e0
jaeml06 <jaeml0630@gmail.com>
2024-06-12 15:02:34 +0900

    refactor : 불필요한 fragment 제거

--- src/components/CartItemModal/CartItemModal.tsx
+++ src/components/CartItemModal/CartItemModal.tsx
@@ -16,29 +16,27 @@ const CartItemModal = ({ setIsOpenModal }: CartItemModalProps) => {
     setIsOpenModal(false);
   };
   return (
-    <>
-      <Modal position="bottom" size="large" onDimmedClick={handleClose}>
-        <Modal.Header>
-          <Modal.Title title="장바구니" />
-          <Modal.XButton onClick={handleClose}></Modal.XButton>
-        </Modal.Header>
-        <Modal.Body>
-          <CartItemList items={cartItems} />
-          <CartTotalAmount
-            totalCartItemAmount={calculateTotalAmount(cartItems)}
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Modal.Button
-            disabled={cartItems.length === 0}
-            onClick={handleConfirm}
-            width="stretch"
-          >
-            {BUTTON_MESSAGE.CLOSE}
-          </Modal.Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <Modal position="bottom" size="large" onDimmedClick={handleClose}>
+      <Modal.Header>
+        <Modal.Title title="장바구니" />
+        <Modal.XButton onClick={handleClose}></Modal.XButton>
+      </Modal.Header>
+      <Modal.Body>
+        <CartItemList items={cartItems} />
+        <CartTotalAmount
+          totalCartItemAmount={calculateTotalAmount(cartItems)}
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Modal.Button
+          disabled={cartItems.length === 0}
+          onClick={handleConfirm}
+          width="stretch"
+        >
+          {BUTTON_MESSAGE.CLOSE}
+        </Modal.Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
