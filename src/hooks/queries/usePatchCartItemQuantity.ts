1acdced93ed4f6fb10d932ae396120b6d8c96cd8
jayming66 <mjw8941@gmail.com>
2024-06-12 16:54:56 +0900

    feat: 장바구니 수량 조절 시, 낙관적 업데이트 적용해보기

--- src/hooks/queries/usePatchCartItemQuantity.ts
+++ src/hooks/queries/usePatchCartItemQuantity.ts
@@ -1,6 +1,7 @@
 import { patchCartItemQuantity } from '@/api';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { QUERY_KEY } from '@/constant/queryKey';
+import { CartItemType } from '@/types';
 
 interface Props {
   onError: () => void;
@@ -11,12 +12,35 @@ const usePatchCartItemQuantity = ({ onError }: Props) => {
 
   const { mutate, isSuccess, isError } = useMutation({
     mutationFn: patchCartItemQuantity,
-    onSuccess: () => {
+    onSettled: () => {
       client.invalidateQueries({
         queryKey: [QUERY_KEY.CART_ITEMS],
       });
     },
-    onError,
+    onMutate: async ({ cartItemId, newQuantity }) => {
+      await client.cancelQueries({ queryKey: [QUERY_KEY.CART_ITEMS] });
+
+      const prevData = client.getQueryData<CartItemType[]>([QUERY_KEY.CART_ITEMS]);
+
+      client.setQueryData([QUERY_KEY.CART_ITEMS], (oldData: CartItemType[]) => {
+        const newData = oldData.map((item) =>
+          item.id === cartItemId
+            ? {
+                ...item,
+                quantity: newQuantity,
+              }
+            : item,
+        );
+
+        return newData;
+      });
+
+      return { prevData };
+    },
+    onError: (_, __, context) => {
+      client.setQueryData([QUERY_KEY.CART_ITEMS], context?.prevData);
+      onError();
+    },
   });
   return {
     patchCartItemQuantity: mutate,
