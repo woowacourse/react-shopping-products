e76ece62318ca943dd0194062a6859e85c3fb1a0
useon <yoosk4986@naver.com>
2024-06-13 18:17:46 +0900

    feat: 페이지네이션 테스트 코드 추가

--- src/test/useProductFetch.test.tsx
+++ src/test/useProductFetch.test.tsx
@@ -2,6 +2,7 @@ import { renderHook, waitFor } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useProductFetch } from '../hooks/useProductFetch';
 import { ToastProvider } from '../context/ToastProvider';
+import { act } from 'react';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,11 +24,29 @@ describe('useProductFetch', () => {
   });
 
   describe('상품 목록 조회', () => {
-    it('초기 상품 목록을 가져온다', async () => {
+    it('초기 상품 목록을 20개를 가져온다', async () => {
       const { result } = renderHook(() => useProductFetch(), { wrapper });
 
       await waitFor(() => {
-        expect(result.current.products.length).toBe(20);
+        expect(result.current.products).toHaveLength(20);
+      });
+    });
+  });
+
+  describe('페이지네이션', () => {
+    it('다음 페이지 상품 목록 4개를 가져온다', async () => {
+      const { result } = renderHook(() => useProductFetch(), { wrapper });
+
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(20);
+      });
+
+      act(() => {
+        result.current.fetchNextPage();
+      });
+
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(24);
       });
     });
   });
