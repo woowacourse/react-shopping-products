2fd81068c569f76953d4ced83e817cdfe0afb080
simhorim <hori5020@gmail.com>
2024-06-13 15:24:03 +0900

    feat(errorMessage): 에러 메시지 상수화

---
+++ src/constants/errorMessage.ts
@@ -0,0 +1,10 @@
+const ERROR_MESSAGE = {
+  overMaxCartItemCounts: '장바구니 최대 허용치를 초과하여',
+  addToCart: '장바구니 추가 요청중',
+  deleteFromCart: '장바구니 삭제 요청중',
+  fetchCart: '장바구니 불러오기중',
+  fetchProducts: '상품 불러오기중',
+  adjustCartItemQuantity: '상품 수량 조절중',
+};
+
+export default ERROR_MESSAGE;
