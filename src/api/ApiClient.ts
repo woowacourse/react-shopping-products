c2abb6930d21300352b3e7939776191dee82167b
skiende74 <skiende74@gmail.com>
2024-06-13 17:33:30 +0900

    feat(Interceptor): Interceptor로 좀 더 유연하게 사용가능하게 함.

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -1,4 +1,5 @@
 import { generateBasicToken } from '../utils/auth';
+import { Interceptor } from './Interceptor';
 
 const USER_ID = import.meta.env.VITE_USER_ID;
 const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD;
@@ -6,6 +7,10 @@ export const token = generateBasicToken(USER_ID, USER_PASSWORD);
 
 export class ApiClient {
   private headers: HeadersInit = {};
+  interceptor: Interceptor = new Interceptor(
+    (request) => request,
+    (error) => error,
+  );
 
   constructor(headers?: HeadersInit) {
     if (!headers) return;
@@ -14,6 +19,10 @@ export class ApiClient {
 
   setHeaders(headers: HeadersInit) {
     this.headers = headers;
+  }
+
+  setInterceptor(interceptor: Interceptor) {
+    this.interceptor = interceptor;
   }
 
   async get<R, T = object>(url: string, body?: T) {
@@ -35,13 +44,23 @@ export class ApiClient {
       headers: this.headers,
       body: JSON.stringify(requestBody),
     };
-
-    const response = await fetch(url, requestInit);
+    const interceptedRequest = this.interceptor.onRequest({ ...requestInit, url });
+    const response = await fetch(interceptedRequest.url, interceptedRequest);
 
     if (!response.ok) {
       const errorText = await response.text();
-      throw new Error(`HTTP 응답 코드 에러 status: ${response.status} ${errorText}`);
+      const responseError = new ResponseError(`HTTP 응답 코드 에러 status: ${response.status} ${errorText}`, response.status);
+      const interceptedError = this.interceptor.onError(responseError);
+      throw interceptedError;
     }
-    return await response;
+    return response;
+  }
+}
+
+export class ResponseError extends Error {
+  status;
+  constructor(message: string, status: number) {
+    super(message);
+    this.status = status;
   }
 }
