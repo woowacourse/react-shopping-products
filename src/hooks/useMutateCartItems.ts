d192714138eed2ab360d0202a7821a9a3ae79c1e
chysis <chysiss@naver.com>
2024-06-13 23:15:59 +0900

    chore: 장바구니에 추가, 삭제 및 업데이트할 때 에러 처리

--- src/hooks/useMutateCartItems.ts
+++ src/hooks/useMutateCartItems.ts
@@ -8,12 +8,17 @@ import {
   updateCartItem,
 } from '@_api/cart';
 import { QUERY_KEYS } from '@_constants/queryKeys';
+import { useToast } from './useToast';
 
 export function useMutateCartItems() {
+  const { showToast } = useToast();
   const queryClient = useQueryClient();
 
   const addMutation = useMutation({
     mutationFn: (productId: number) => addCartItem({ productId }),
+    onError: () => {
+      showToast('문제가 발생해 상품을 장바구니에 담지 못했어요.');
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.cart] });
     },
@@ -21,6 +26,9 @@ export function useMutateCartItems() {
 
   const removeMutation = useMutation({
     mutationFn: (cartItemId: number) => removeCartItem({ cartItemId }),
+    onError: () => {
+      showToast('문제가 발생해 상품을 삭제하지 못했어요.');
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.cart] });
     },
@@ -28,6 +36,9 @@ export function useMutateCartItems() {
 
   const updateMutation = useMutation({
     mutationFn: ({ cartItemId, quantity }: UpdateCartItemArgs) => updateCartItem({ cartItemId, quantity }),
+    onError: () => {
+      showToast('문제가 발생해 수량을 변경하지 못했어요.');
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.cart] });
     },
