b55e56c1f5c759ab97823c983bb3cc40698cc1b9
rbgksqkr <rbgks1937@naver.com>
2024-06-13 11:06:07 +0900

    test: 사용자 관점으로 이벤트를 발생시키는 user-event 적용

--- src/__tests__/shoppingCartModal.test.tsx
+++ src/__tests__/shoppingCartModal.test.tsx
@@ -1,5 +1,6 @@
 import { UseQueryOptions } from '@tanstack/react-query';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { describe, expect, it, vi } from 'vitest';
 
 import Header from '@/components/common/header/Header';
@@ -21,48 +22,52 @@ vi.mock('@tanstack/react-query', async (importOriginal) => {
 
 describe('shoppingCartModal', () => {
   it('장바구니 버튼을 클릭하면 모달로 장바구니에 담은 목록을 확인할 수 있다.', async () => {
+    const user = userEvent.setup();
+
     render(
       <MockWrapper>
         <Header />
       </MockWrapper>,
     );
 
-    fireEvent.click(screen.getByAltText('장바구니'));
+    await user.click(screen.getByAltText('장바구니'));
+
     const shoppingCartModalHeader = screen.getByText('장바구니');
     expect(shoppingCartModalHeader).toBeInTheDocument();
   });
 
   it('장바구니 모달에서 + 버튼을 클릭하면 제품 수량을 1 증가시킬 수 있다.', async () => {
+    const user = userEvent.setup();
+
     render(
       <MockWrapper>
         <Header />
       </MockWrapper>,
     );
-
-    fireEvent.click(screen.getByAltText('장바구니'));
+    await user.click(screen.getByAltText('장바구니'));
 
     const shoppingCartModalHeader = screen.getByText('장바구니');
-
     expect(shoppingCartModalHeader).toBeInTheDocument();
 
     const incrementButton = screen.getByTestId('shopping-cart-128-plus');
-    fireEvent.click(incrementButton);
+    await user.click(incrementButton);
   });
 
   it('장바구니 모달에서 - 버튼을 클릭하면 제품 수량을 1 감소시킬 수 있다.', async () => {
+    const user = userEvent.setup();
+
     render(
       <MockWrapper>
         <Header />
       </MockWrapper>,
     );
 
-    fireEvent.click(screen.getByAltText('장바구니'));
+    await user.click(screen.getByAltText('장바구니'));
 
     const shoppingCartModalHeader = screen.getByText('장바구니');
-
     expect(shoppingCartModalHeader).toBeInTheDocument();
 
-    const incrementButton = screen.getByTestId('shopping-cart-128-minus');
-    fireEvent.click(incrementButton);
+    const decrementButton = screen.getByTestId('shopping-cart-128-minus');
+    await user.click(decrementButton);
   });
 });
