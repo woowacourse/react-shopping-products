8ffa48c3ced9bd0929b716a965490e3d15781559
useon <yoosk4986@naver.com>
2024-06-13 16:31:50 +0900

    feat: cartItems 불러올 때 에러 발생시 에러메세지를 토스트로 띄워주는 기능

--- src/hooks/useCartItems.ts
+++ src/hooks/useCartItems.ts
@@ -1,12 +1,26 @@
-import { fetchCartItem } from '../api';
 import { useQuery } from '@tanstack/react-query';
+import { fetchCartItem } from '../api';
 import { QUERY_KEYS } from '../constant/queryKeys';
+import { useToast } from './useToast';
 
 export function useCartItems() {
-  const { data, isError, isLoading } = useQuery({
+  const { showToast } = useToast();
+
+  const fetchCart = async () => {
+    try {
+      const response = await fetchCartItem(0, 100);
+      return response;
+    } catch (error) {
+      if (error instanceof Error) {
+        showToast('카트 아이템을 불러오는 데 실패했습니다');
+      }
+    }
+  };
+
+  const { data } = useQuery({
     queryKey: [QUERY_KEYS.CART],
-    queryFn: () => fetchCartItem(0, 100),
+    queryFn: fetchCart,
   });
 
-  return { cartItems: data ?? [], isError, isLoading };
+  return { cartItems: data ?? [] };
 }
