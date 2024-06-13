b129ef12a41fd0c6cfb603343573e115c9fe9cc7
chysis <chysiss@naver.com>
2024-06-13 15:07:19 +0900

    refactor: 장바구니 목록을 불러올 때 에러 종류에 따라 retry 횟수를 다르게 하도록 설정

--- src/hooks/useGetCartItems.ts
+++ src/hooks/useGetCartItems.ts
@@ -10,20 +10,37 @@ interface FetchCartItemsResponse {
   content: CartItem[];
 }
 
+interface UseGetCartItemsResult {
+  cartItems: CartItem[];
+  error: Error | null;
+  isLoading: boolean;
+}
+
 const fetchCartItems = async (): Promise<FetchCartItemsResponse> => {
   return await fetchData<FetchCartItemsResponse>(CART_ITEMS_ENDPOINT, {
     size: 100,
   });
 };
 
-export default function useGetCartItems() {
-  const { data, status } = useQuery<FetchCartItemsResponse>({
+export default function useGetCartItems(): UseGetCartItemsResult {
+  const { data, error, isLoading } = useQuery<FetchCartItemsResponse>({
     queryKey: [QUERY_KEYS.cart],
     queryFn: () => fetchCartItems(),
     networkMode: 'always',
+    retry(failureCount, error) {
+      const errorStatus = Number(error.message);
+      if (errorStatus >= 400 && errorStatus < 500) {
+        return false;
+      }
+
+      if (errorStatus >= 500 && errorStatus < 600) {
+        return failureCount < 3;
+      }
+      return false;
+    },
   });
 
   const cartItems = (data && data.content) || [];
 
-  return { cartItems, status };
+  return { cartItems, error, isLoading };
 }
