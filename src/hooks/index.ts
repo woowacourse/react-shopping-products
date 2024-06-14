bc95437079f354b93d4ba4165d1a33d8e40a2dd2
bmj0053 <bmj05378@gmail.com>
2024-06-14 10:23:12 +0900

    remove: 미사용 파일 삭제 (useFetchProductQuantity, usePagination)

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,6 +3,4 @@ export { default as useFetchCartItems } from './useFetchCartItems/useFetchCartIt
 export { default as useFetchProducts } from './useFetchProducts/useFetchProducts';
 export { default as useFetchDeleteCart } from './useFetchDeleteCart/useFetchDeleteCart';
 export { default as useFetchUpdateQuantity } from './useFetchUpdateQuantity/useFetchUpdateQuantity';
-export { default as useFetchProductQuantity } from './useFetchProductQuantity/useFetchProductQuantity';
 export { default as useIntersectionObserver } from './useIntersectionObserver';
-export { default as usePagination } from './usePagination';
