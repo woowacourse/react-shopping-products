4e9691e6c44b475f77c8e8e60322ce8b60eb2873
pakxe <pigkill40@naver.com>
2024-06-20 18:55:30 +0900

    feat: suspense query를 사용하도록 수정

--- src/apis/queries/product.ts
+++ src/apis/queries/product.ts
@@ -1,13 +1,18 @@
 import { SortValue } from '@/constants/filter';
 import { QUERY_KEYS } from '@/constants/queryKeys';
 import { Category } from '@/types/filter.type';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 import { requestProductList } from '../request/product';
-import { PAGE } from '@/hooks/product/useProductList';
 import { TIME } from '@/constants/time';
 
+export const PAGE = {
+  START: 0,
+  START_SIZE: 20,
+  SIZE: 4,
+};
+
 export const useInfiniteProductList = (category: Category, sortType: SortValue) => {
-  return useInfiniteQuery({
+  return useSuspenseInfiniteQuery({
     queryKey: [QUERY_KEYS.PRODUCT, category, sortType],
     initialPageParam: 0,
     queryFn: ({ pageParam }) =>
