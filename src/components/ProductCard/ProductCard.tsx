7f5b6de0bea557545cce9e5c1a76706ee2e62eea
H0ngJu <hongju4385@gmail.com>
2025-05-13 16:42:14 +0900

    feat: ProductCard 구현
    Co-authored-by: JinseoKim <eunoia@handong.ac.kr>

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,55 @@
+import { css } from "@emotion/css";
+import RemoveButton from "../Button/RemoveButton";
+import AddButton from "../Button/AddButton";
+import { Product } from "../../types/product.type";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+const ProductCard = ({ product }: ProductCardProps) => {
+  const { id, name, price, imageUrl, isInCart } = product;
+
+  return (
+    <div key={id} className={CardFrame}>
+      <img src={imageUrl} alt={name} className={CardImage} />
+      <div className={CardInfo}>
+        <h2>{name}</h2>
+        <p>{price.toLocaleString()}원</p>
+        <div className={ButtonArea}>
+          {isInCart ? <RemoveButton /> : <AddButton />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
+
+const CardFrame = css`
+  width: 100%;
+  height: 224px;
+  border-radius: 8px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+`;
+
+const CardImage = css`
+  width: 100%;
+  height: 112px;
+  border: none;
+  object-fit: cover;
+`;
+
+const CardInfo = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: fl;
+  padding: 16px 8px 8px 8px;
+`;
+
+const ButtonArea = css`
+  display: flex;
+  justify-content: flex-end;
+`;
---
+++ src/types/product.type.ts
@@ -0,0 +1,10 @@
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  category: string;
+  imageUrl: string;
+  isInCart: boolean;
+}
+
+export type { Product };
