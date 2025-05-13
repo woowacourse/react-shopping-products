81deeb1079d716ae67ca9601c60b535e0fe43bb2
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-05-13 16:28:06 +0900

    🎨 design: App 기본 틀 구현
    Co-authored-by: yeongipark <yeongipark@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
+import "./index.css";
+import "./reset.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <div className="container">
       <h1>React Shopping Products</h1>
-    </>
+    </div>
   );
 }
 
