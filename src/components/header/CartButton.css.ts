3ba0eb5cf9b82f1860f76fe232eec5bb2b390ea9
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-05-13 19:42:52 +0900

    ✨ feat: 장바구니 갯수 UI 구현
    Co-authored-by: yeongipark <yeongipark@users.noreply.github.com>

--- src/components/header/CartButton.css.ts
+++ src/components/header/CartButton.css.ts
@@ -5,4 +5,19 @@ export const CartStyle = css`
   height: 32px;
   border: none;
   background-color: transparent;
+  position: relative;
+`;
+
+export const PutItemCount = css`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 19px;
+  height: 19px;
+  border-radius: 50%;
+  background-color: white;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
--- src/components/header/CartButton.tsx
+++ src/components/header/CartButton.tsx
@@ -1,9 +1,10 @@
-import { CartStyle } from "./CartButton.css";
+import { CartStyle, PutItemCount } from "./CartButton.css";
 
 function CartButton() {
   return (
     <button css={CartStyle}>
       <img src="/Cart.svg" alt="장바구니 아이콘" />
+      <div css={PutItemCount}>2</div>
     </button>
   );
 }
