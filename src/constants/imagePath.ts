9419b67197f10dddb54f95af3eea6e93bcc2785c
yeji0214 <1225yj50@naver.com>
2025-05-13 17:25:44 +0900

    feat: 이미지 추가 및 경로 상수화
    Co-authored-by: Claire <eunsoa0310@gmail.com>

--- src/constants/imagePath.ts
+++ src/constants/imagePath.ts
@@ -3,4 +3,6 @@ export const IMAGE_PATH = Object.freeze({
   SHOPPING_BASKET: "./shopping-basket.svg",
   SHOPPIN_CART_REMOVE: "./shopping-cart-remove.svg",
   SHOPPIN_CART_ADD: "./shopping-cart-add.svg",
+  DEFAULT_GROCERY: "./default-grocery.svg",
+  DEFAULT_FASHION: "./default-fashion.svg",
 });
