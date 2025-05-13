29a37cdaa2eb28344611bd8543dcdee21c779438
SuHwa Lee <tnghk9611@naver.com>
2025-05-13 18:46:00 +0900

    feat: 헤더 컴포넌트 구현
    Co-authored-by: 이상희 <sang6299@naver.com>

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,37 @@
+import Button from '../Button/Button';
+import { css } from '@emotion/css';
+
+const headerContainer = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 64px;
+  background-color: #000000;
+  padding: 0px 20px;
+`;
+
+const headerTitle = css`
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: 800;
+  line-height: 16px;
+`;
+
+const cartImg = css`
+  width: 20px;
+  height: 24px;
+`;
+
+function Header() {
+  return (
+    <div className={headerContainer}>
+      <div className={headerTitle}>SHOP</div>
+      <Button>
+        <img src="/images/cart.png" alt="cart" className={cartImg} />
+      </Button>
+    </div>
+  );
+}
+
+export default Header;
