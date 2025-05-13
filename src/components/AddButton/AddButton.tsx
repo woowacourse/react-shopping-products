2191c2408eb67540e6f8cce17a6997492fd5b64c
SuHwa Lee <tnghk9611@naver.com>
2025-05-13 19:51:53 +0900

    feat: 장바구니 담기 버튼 구현
    Co-authored-by: 이상희 <sang6299@naver.com>

---
+++ src/components/AddButton/AddButton.tsx
@@ -0,0 +1,22 @@
+import { css } from '@emotion/css';
+import Button from '../common/Button/Button';
+
+const addButton = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+`;
+
+function AddButton() {
+  return (
+    <Button backgroundColor="#000000" color="#ffffff" radius="4px">
+      <div className={addButton}>
+        <img src="/images/addCart.png" alt="addCart" />
+        <span>담기</span>
+      </div>
+    </Button>
+  );
+}
+
+export default AddButton;
