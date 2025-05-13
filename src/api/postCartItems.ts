6236d1e51bd638fd456e8157da21b3530402d261
Ko Daeun <drechsu@gmail.com>
2025-05-13 17:47:40 +0900

    feat : CartItem 관련 api 호출 함수 구현

---
+++ src/api/postCartItems.ts
@@ -0,0 +1,22 @@
+import { Product } from '../App';
+
+const postCartItems = (product: Product) => {
+  const body = JSON.stringify({
+    productId: product.id,
+    quantity: 1,
+  });
+
+  fetch(
+    'http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com/cart-items',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Basic RGFldW4tMTAwOnBhc3N3b3Jk',
+      },
+      body,
+    }
+  );
+};
+
+export default postCartItems;
