9da484f628b5b0fb877c1ecce1ecda3119088c17
H0ngJu <hongju4385@gmail.com>
2025-05-13 15:58:00 +0900

    feat: ProductListToolBar 구현
    Co-authored-by: JinseoKim <eunoia@handong.ac.kr>

---
+++ src/components/ProductListToolBar/ProductListToolBar.tsx
@@ -0,0 +1,33 @@
+import { css } from "@emotion/css";
+import FilterDropDown from "./FilterDropDown";
+import SortingDropDown from "./SortingDropDown";
+
+const ProductListToolBar = () => {
+  return (
+    <section className={ToolBarSectionStyles}>
+      <h1>bpple 상품 목록</h1>
+      <div className={ProductListToolBarStyles}>
+        <FilterDropDown options={["패션잡화", "식료품"]} />
+        <SortingDropDown options={["낮은 가격순", "높은 가격순"]} />
+      </div>
+    </section>
+  );
+};
+
+export default ProductListToolBar;
+
+const ToolBarSectionStyles = css`
+  width: 100%;
+  padding: 36px 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+const ProductListToolBarStyles = css`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
