f1bb3ee127ddb39cb3a2e25cf5651bf6bd661df8
keemsebeen <sebin0580@gmail.com>
2025-05-13 16:55:46 +0900

    chore: prettier 적용
    Co-Authored-By: Youjeong Jeong <115888336+jeongyou@users.noreply.github.com>

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,19 +1,21 @@
-import { ReactNode } from "react"
-import { cn } from "../utils/core";
+import { ReactNode } from 'react';
+import { cn } from '../utils/core';
 
 type HeaderProps = {
   left: ReactNode;
   right?: ReactNode;
-}
+};
 
-export const Header = ({left, right}:HeaderProps) => {
+export const Header = ({ left, right }: HeaderProps) => {
   return (
-    <header className={cn('min-h-16 top-0 absolute flex items-center w-full p-4 bg-black', {
-      'justify-between': right,
-      'flex-start': !right,
-    })}>
+    <header
+      className={cn('absolute top-0 flex min-h-16 w-full items-center bg-black p-4', {
+        'justify-between': right,
+        'flex-start': !right,
+      })}
+    >
       {left}
       {right}
     </header>
-  )
-}
+  );
+};
