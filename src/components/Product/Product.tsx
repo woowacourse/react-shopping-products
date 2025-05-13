212ed44cab36f033fc9b120abab929f8b1c1a0ad
SuHwa Lee <tnghk9611@naver.com>
2025-05-13 20:06:10 +0900

    feat: 상품 컴포넌트 구현
    Co-authored-by: 이상희 <sang6299@naver.com>

---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,59 @@
+import { css } from '@emotion/css';
+import AddButton from '../AddButton/AddButton';
+import DeleteButton from '../DeleteButton/DeleteButton';
+
+type ProductProps = {
+  imageUrl: string;
+  title: string;
+  price: number;
+  isAdd: boolean;
+};
+
+const productContainer = css`
+  width: 100%;
+  height: 224px;
+  border-radius: 8px;
+  position: relative;
+`;
+
+const productImage = css`
+  width: 100%;
+  height: 50%;
+`;
+
+const productContent = css`
+  padding: 10px;
+`;
+
+const productTitle = css`
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 20px;
+`;
+
+const productPrice = css`
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 15px;
+`;
+
+const buttonWrapper = css`
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+`;
+
+function Product({ imageUrl, title, price, isAdd }: ProductProps) {
+  return (
+    <li className={productContainer}>
+      <img src={imageUrl} alt="product" className={productImage} />
+      <div className={productContent}>
+        <div className={productTitle}>{title}</div>
+        <div className={productPrice}>{price}원</div>
+        <div className={buttonWrapper}>{isAdd ? <DeleteButton /> : <AddButton />}</div>
+      </div>
+    </li>
+  );
+}
+
+export default Product;
