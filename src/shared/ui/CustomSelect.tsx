5880107a50302df52e341c25b2a7b89aa7f66311
minji2219 <aabb0219@naver.com>
2025-05-13 18:48:44 +0900

    refactor: CustomSelect style 통합

--- src/shared/ui/CustomSelect.tsx
+++ src/shared/ui/CustomSelect.tsx
@@ -1,4 +1,4 @@
-import * as S from './CustomSelect.styles';
+import styled from '@emotion/styled';
 
 interface CustomSelectProps {
   items: Array<{
@@ -7,14 +7,30 @@ interface CustomSelectProps {
   }>;
 }
 
-export default function CustomSelect({ items }: CustomSelectProps) {
+export default function CustomSelect({items}: CustomSelectProps) {
   return (
-    <S.CustomSelect>
+    <StyledCustomSelect>
       {items.map((item) => (
         <option key={item.value} value={item.value}>
           {item.label}
         </option>
       ))}
-    </S.CustomSelect>
+    </StyledCustomSelect>
   );
 }
+
+const StyledCustomSelect = styled.select`
+  width: 125px;
+  height: 36px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 8px;
+
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+    border-color: #007bff;
+  }
+`;
