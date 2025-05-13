a240c155f419846ddb4cfb22dc45f686f904139d
ohgus <anakin1108@naver.com>
2025-05-13 18:20:52 +0900

    ​​feat: Text UI 구현
    Co-authored-by: 장정안 <126966681+ExceptAnyone@users.noreply.github.com>

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -4,8 +4,29 @@ const theme = {
     white: '#fff',
     gray1: '#EAEAEA',
     gray2: 'rgba(0, 0, 0, 0.10)',
-    pink: '#FFC9C9',
+    pink: '#FFC9C9'
   },
+  font: {
+    title: {
+      fontSize: '2.4rem',
+      fontWeight: '700'
+    },
+    productName: {
+      fontSize: '1.4rem',
+      fontWeight: '700',
+      lineHeight: '2rem'
+    },
+    body: {
+      fontSize: '1.4rem',
+      fontWeight: '500',
+      lineHeight: '2rem'
+    },
+    description: {
+      fontSize: '1.2rem',
+      fontWeight: '500',
+      lineHeight: '1.5rem'
+    }
+  }
 };
 
 export default theme;
