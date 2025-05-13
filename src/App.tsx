b24d4e0413c1eaf5a3f93df68d5a58df0a10e085
장정안 <wjddks96@naver.com>
2025-05-13 19:42:23 +0900

    ​​feat: App 컴포넌트에 공통 컴포넌트 예시 테스트
    Co-authored-by: ohgus <123801385+ohgus@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import { GlobalStyle, theme } from './style';
+import Button from './component/@common/Button';
+import Dropdown from './component/@common/Dropdown';
+import { useState } from 'react';
+import ArrowIcon from './component/@common/ArrowIcon';
+
+export type SortOption = '높은 가격순' | '낮은 가격순';
 
 function App() {
+  const [selected, setSelected] = useState<SortOption>('낮은 가격순');
+
+  const handleSortClick = (content: SortOption) => {
+    setSelected(content);
+  };
+
+  const sortOptions: SortOption[] = ['높은 가격순', '낮은 가격순'];
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
-        <h1>React Shopping Products</h1>
+        <Button variant="default">담기</Button>
+        <Button variant="gray">담기</Button>
+        <Dropdown.Root>
+          <Dropdown.Trigger>
+            {selected}
+            <ArrowIcon />
+          </Dropdown.Trigger>
+          <Dropdown.List>
+            {sortOptions.map((option) => (
+              <Dropdown.Item
+                key={option}
+                handleClick={handleSortClick}
+                content={option}
+              />
+            ))}
+          </Dropdown.List>
+        </Dropdown.Root>
       </ThemeProvider>
     </>
   );
