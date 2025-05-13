6e2033a1d8524f243f0c0e17ce1e0d3a6b29dd85
wo-o29 <woogur29@gmail.com>
2025-05-13 15:29:35 +0900

    ​​feat: Header 컴포넌트 구현
    Co-authored-by: Deokbin Kim <162319857+Db0111@users.noreply.github.com>

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const Header = styled.header`
+  width: 100%;
+  height: 64px;
+  padding: 0px 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #000;
+`;
+
+export const Logo = styled.a`
+  color: #fff;
+  font-size: 20px;
+  font-weight: 800;
+`;
