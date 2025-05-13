ce40e1161390015db6a53b6ce67f82a50274caf6
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-13 22:04:26 +0900

    feat MobileLayout 컴포넌트 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Global, ThemeProvider } from '@emotion/react';
+import MobileLayout from './components/common/MobileLayout';
 import ProductList from './components/features/product/product-list/ProductList';
 import reset from './global/style/reset';
 import { theme } from './global/style/theme';
 
 function App() {
   return (
-    <>
+    <MobileLayout>
       <Global styles={reset} />
       <ThemeProvider theme={theme}>
         <ProductList />
       </ThemeProvider>
-    </>
+    </MobileLayout>
   );
 }
 
---
+++ src/components/common/MobileLayout.tsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+import Flex from './Flex';
+import { PropsWithChildren } from 'react';
+
+function MobileLayout({ children }: PropsWithChildren) {
+  return (
+    <Flex>
+      <Container>{children}</Container>
+    </Flex>
+  );
+}
+
+const Container = styled(Flex)`
+  width: 440px;
+  height: 100vh;
+  border: 1px solid grey;
+  overflow: hidden;
+`;
+
+export default MobileLayout;
