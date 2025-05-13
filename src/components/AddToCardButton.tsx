000e50488742d81c199d92b1548f65c70fd4ee42
Ko Daeun <drechsu@gmail.com>
2025-05-13 16:06:05 +0900

    feat : 담기/빼기 버튼 구현

---
+++ src/components/AddToCardButton.tsx
@@ -0,0 +1,5 @@
+const AddToCardButton = ({ onClick }: { onClick: () => void }) => {
+  return <button onClick={onClick}>담기</button>;
+};
+
+export default AddToCardButton;
---
+++ src/components/ProductItem/RemoveFromCartButton.tsx
@@ -0,0 +1,4 @@
+const RemoveFromCartButton = ({ onClick }: { onClick: () => void }) => {
+  return <button onClick={onClick}>빼기</button>;
+};
+export default RemoveFromCartButton;
