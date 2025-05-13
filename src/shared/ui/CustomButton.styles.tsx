1af5dfd0192f495b4d82837972627e0b14c52163
minji2219 <aabb0219@naver.com>
2025-05-13 18:53:19 +0900

    feat: CustomButton 컴포넌트 구현

---
+++ src/shared/ui/CustomButton.styles.tsx
@@ -0,0 +1,21 @@
+import {SerializedStyles} from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const CustomButton = styled.button<{css?: SerializedStyles}>`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  padding: 0 8px;
+  background-color: #000;
+  color: #fff;
+  border-radius: 4px;
+  cursor: pointer;
+  ${({css}) => css}
+`;
+
+export const ButtonIcon = styled.img`
+  width: 15px;
+  height: 15px;
+`;
+
+export const ButtonTitle = styled.span``;
---
+++ src/shared/ui/CustomButton.tsx
@@ -0,0 +1,23 @@
+import {SerializedStyles} from '@emotion/react';
+import * as S from './CustomButton.styles';
+
+interface CustomButtonProps {
+  iconUrl: string;
+  title: string;
+  css?: SerializedStyles;
+  onClick: () => void;
+}
+
+export default function CustomButton({
+  iconUrl,
+  title,
+  onClick,
+  css,
+}: CustomButtonProps) {
+  return (
+    <S.CustomButton onClick={onClick} css={css}>
+      <S.ButtonIcon src={iconUrl} />
+      <S.ButtonTitle>{title}</S.ButtonTitle>
+    </S.CustomButton>
+  );
+}
