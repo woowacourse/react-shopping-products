ae86168773759d13d9123bffdf9211791e5772dc
H0ngJu <hongju4385@gmail.com>
2025-05-13 15:19:07 +0900

    feat: 모바일 뷰 세팅
    Co-authored-by: JinseoKim <eunoia@handong.ac.kr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { css } from "@emotion/css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>React Shopping Products</h1>
-    </>
+    <main className={mainStyles}>
+      <h1>ㅎㅇ</h1>
+    </main>
   );
 }
 
 export default App;
+
+const mainStyles = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 20px;
+  height: 100vh;
+  background-color: #ffffff;
+`;
