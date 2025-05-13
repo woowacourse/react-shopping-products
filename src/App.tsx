4bfb31e1e88e846c5d7d39d06c198d1fdf457260
mun-kyeong <horse6953@naver.com>
2025-05-13 21:14:10 +0900

    feat : reset CSS 적용
    Co-authored-by: ha-kuku <hakukudh@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import ShopPage from "./page/ShopPage";
+import GlobalStyle from "./reset";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div>
+      <GlobalStyle />
       <h1>React Shopping Products</h1>
       <ShopPage />
-    </>
+    </div>
   );
 }
 
