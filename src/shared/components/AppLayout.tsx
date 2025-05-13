26e26b511e54a421a8fcd8ad3bb1f0f70f2e555e
keemsebeen <sebin0580@gmail.com>
2025-05-13 16:36:27 +0900

    style: AppLayout relative 지정, padding 제거
    Co-Authored-By: Youjeong Jeong <115888336+jeongyou@users.noreply.github.com>

--- src/shared/components/AppLayout.tsx
+++ src/shared/components/AppLayout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export const AppLayout = ({children}: PropsWithChildren) => {
   return (
-    <div className="h-dvh max-w-[400px] flex flex-col items-center justify-center p-4 mx-auto w-full border-x-[1px] border-gray-300">
+    <div className="h-dvh max-w-[400px] relative flex flex-col items-center justify-center mx-auto w-full border-x-[1px] border-gray-300">
       {children}
     </div>
   )
