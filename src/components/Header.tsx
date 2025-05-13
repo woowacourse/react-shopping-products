50e70d4540c45fd645b626a3528c1340be73397d
hanheel <hanhee630630@gmail.com>
2025-05-13 16:36:29 +0900

    feat : Header 컴포넌트 제작

---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+import styled from '@emotion/styled';
+import Logo from '/public/logo.svg';
+import CartIcon from '/public/cartIcon.svg';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '../constants/routes';
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  return (
+    <HeaderContainer>
+      <HeaderLogoButton>
+        <img src={Logo} alt="헤더 로고" onClick={() => navigate(ROUTES.PRODUCT_LIST_PAGE)} />
+      </HeaderLogoButton>
+      <HeaderCartButton>
+        <img src={CartIcon} alt="장바구니" />
+      </HeaderCartButton>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  background-color: var(--color-black);
+  padding: 16px 24px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const HeaderLogoButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeaderCartButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 32px;
+`;
---
+++ src/pages/ProductListPage.tsx
@@ -0,0 +1,5 @@
+import Header from '../components/Header';
+
+export const ProductListPage = () => {
+  return <Header />;
+};
