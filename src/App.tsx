734b17e6ad4f63f6f67a651b74ab334bf5670bb4
thgml05 <aisohii@naver.com>
2025-05-13 17:42:51 +0900

    feat: Title UI 구현
    Co-authored-by: ShinjungOh <97431021+ShinjungOh@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Layout from './ui/components/Layout/Layout';
 import { Global } from '@emotion/react';
 import GlobalStyle from './ui/styles/globalStyle';
 import Header from './ui/components/Header/Header';
+import Title from './ui/components/Title/Title';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Global styles={GlobalStyle} />
       <Layout>
         <Header title="SHOP" />
+        <Title title="bpple 상품 목록" />
       </Layout>
     </>
   );
