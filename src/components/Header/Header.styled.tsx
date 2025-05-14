9ec98ce62a7722f3cae2ea9e96981c5e67492028
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-05-14 14:39:29 +0900

    refactor: 헤더 컴포넌트 스타일 분리

---
+++ src/components/Header/Header.styled.tsx
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.header`
+  background-color: #000000;
+  width: 100%;
+  height: 64px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 24px;
+  padding-right: 24px;
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export const Title = styled.p`
+  font-weight: 800;
+  font-size: 20px;
+  color: #ffffff;
+`;
+
+export const Button = styled.button`
+  border: 0;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+export const Image = styled.img`
+  width: 20px;
+  height: 24px;
+`;
