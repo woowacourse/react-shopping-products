2d4747092b6da98b9e3ba53e26527259570cde0f
Ko Daeun <drechsu@gmail.com>
2025-05-14 21:18:26 +0900

    test : Header 테스트 추가가

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,8 @@ export default defineConfig({
       jsxImportSource: '@emotion/react', // :white_check_mark: 이것만 있어도 기본적으로 작동함
     }),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 });
