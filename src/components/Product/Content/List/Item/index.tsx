9961104c83b1b57089d6fd43245eee04bb078d4c
wo-o29 <woogur29@gmail.com>
2025-05-14 20:43:36 +0900

    feat: 이미지가 없는 경우 ​​defaultImage 사용
    Co-authored-by: Deokbin Kim <162319857+Db0111@users.noreply.github.com>

--- src/components/Product/Content/List/Item/index.tsx
+++ src/components/Product/Content/List/Item/index.tsx
@@ -3,6 +3,8 @@ import AddCartItemButton from "@/components/Product/Content/List/Item/CardItemBu
 import RemoveCartItemButton from "@/components/Product/Content/List/Item/CardItemButton/Remove";
 import * as S from "./ProductItem.styled";
 import { CartItemType, SetCartItems } from "@/types/cartItem";
+import defaultImage from "@/assets/images/planet-error.png";
+import { SyntheticEvent } from "react";
 
 interface ProductItemProps {
   product: ProductItemType;
@@ -14,10 +16,14 @@ function ProductItem({ product, cartItems, setCartItems }: ProductItemProps) {
   const { id, name, price, imageUrl } = product;
   const findCartItem = cartItems.find(({ product }) => product.id === id);
 
+  const handleImageError = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = defaultImage;
+  };
+
   return (
     <S.Item>
       <S.ImageWrapper>
-        <S.ProductImage src={imageUrl} alt={name} />
+        <S.ProductImage src={imageUrl} alt={name} onError={handleImageError} />
       </S.ImageWrapper>
       <S.Content>
         <S.ProductInfo>
