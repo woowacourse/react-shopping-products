2da0d371ae70c35db0c59d0e1383b39f3bbb824a
keemsebeen <sebin0580@gmail.com>
2025-05-14 22:16:29 +0900

    refactor: PRICE 상수화 수정
    Co-Authored-By: Youjeong Jeong <115888336+jeongyou@users.noreply.github.com>

--- src/features/ProductList/constants/product.ts
+++ src/features/ProductList/constants/product.ts
@@ -4,13 +4,7 @@ export const CATEGORY = {
   FASHION: '패션잡화',
 } as const;
 
-export const PRICE = [
-  {
-    name: '낮은 가격순',
-    value: 'asc',
-  },
-  {
-    name: '높은 가격순',
-    value: 'desc',
-  },
-] as const;
+export const PRICE = {
+  asc: '낮은 가격순',
+  desc: '높은 가격순',
+} as const;
