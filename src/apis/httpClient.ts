85572b9b63014763e925e7d2a1a1ffb13e9a5019
wo-o29 <woogur29@gmail.com>
2025-05-14 15:04:33 +0900

    ​​feat: httpClient 로직 추가
    Co-authored-by: Deokbin Kim <162319857+Db0111@users.noreply.github.com>
    Co-authored-by: Deokbin Kim <Db0111@users.noreply.github.com>

---
+++ src/apis/httpClient.ts
@@ -0,0 +1,34 @@
+class HTTPClient {
+  baseUrl = "";
+  apiKey = "";
+
+  constructor(baseUrl: string, apiKey?: string) {
+    this.baseUrl = baseUrl;
+    this.apiKey = apiKey || "";
+  }
+
+  async get(url: string) {
+    const headers = {
+      "Content-Type": "application/json",
+      ...(this.apiKey && { Authorization: this.apiKey }),
+    };
+
+    const response = await fetch(this.baseUrl + url, {
+      headers,
+    });
+
+    if (!response.ok) {
+      throw new Error("예기치 못한 오류가 발생했습니다.");
+    }
+
+    return response.json();
+  }
+
+  setApiKey(apiKey: string) {
+    this.apiKey = apiKey;
+  }
+}
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+export const httpClient = new HTTPClient(BASE_URL, API_KEY);
