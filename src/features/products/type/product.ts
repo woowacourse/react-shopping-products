7e1dbb59f97b5a4e0a91c2ccd0fbbfeb917ae458
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-14 13:28:58 +0900

    refactor: ProductCard 컴포넌트에 product 타입 적용
    - product 를 매개변수로 넘겨줌

---
+++ src/features/products/type/product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  category: string;
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+}
--- src/features/products/ui/ProductCard.tsx
+++ src/features/products/ui/ProductCard.tsx
@@ -1,29 +1,21 @@
 import CustomButton from '../../../shared/ui/CustomButton';
+import { Product } from '../type/product';
 import * as S from './ProductCard.styles';
 
-const DUMMY_DATA = {
-  image_url: 'https://picsum.photos/seed/picsum/200/300',
-  product_name: '상품 이름',
-  product_price: 35000,
-};
+interface ProductCardProps {
+  product: Product;
+}
 
-export default function ProductCard() {
+export default function ProductCard({ product }: ProductCardProps) {
   return (
     <S.ProductCardContainer>
-      <S.ImageSection
-        src={DUMMY_DATA.image_url}
-        alt={DUMMY_DATA.product_name}
-      ></S.ImageSection>
+      <S.ImageSection src={product.imageUrl} alt={product.name}></S.ImageSection>
       <S.ContentSection>
-        <S.ProductName>{DUMMY_DATA.product_name}</S.ProductName>
-        <S.ProductPrice>{DUMMY_DATA.product_price}</S.ProductPrice>
+        <S.ProductName>{product.name}</S.ProductName>
+        <S.ProductPrice>{product.price}</S.ProductPrice>
       </S.ContentSection>
       <S.ButtonSection>
-        <CustomButton
-          iconUrl="/addCartIcon.svg"
-          title="담기"
-          onClick={() => {}}
-        />
+        <CustomButton iconUrl='/addCartIcon.svg' title='담기' onClick={() => {}} />
       </S.ButtonSection>
     </S.ProductCardContainer>
   );
