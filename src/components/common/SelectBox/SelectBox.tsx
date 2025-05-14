94e55989464a37ca919ae44158071fc4bf7d3f8b
SuHwa Lee <tnghk9611@naver.com>
2025-05-14 15:02:04 +0900

    feat: select 박스에 기본 속성 추가
    Co-authored-by: 이상희 <sang6299@naver.com>

--- src/components/common/SelectBox/SelectBox.tsx
+++ src/components/common/SelectBox/SelectBox.tsx
@@ -6,23 +6,24 @@ type SelectBoxProps = {
 } & React.SelectHTMLAttributes<HTMLElement>;
 
 const selectBoxContainer = css`
-    width:100%;
-    height: 36px;
-    padding: 0 10px;
-    border-radius: 8px;
-    border: 1px solid rgba(0,0,0,0.1);    
-    `
+  width: 100%;
+  height: 36px;
+  padding: 0 10px;
+  border-radius: 8px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+`;
 
-function SelectBox({ placeHolder, options }: SelectBoxProps) {
+function SelectBox({ placeHolder, options, ...props }: SelectBoxProps) {
   return (
     <>
-      <select className={selectBoxContainer}>
+      <select className={selectBoxContainer} {...props}>
         <option hidden>{placeHolder}</option>
-        {
-            options.length > 0 && options.map((option,idx)=>(
-                <option key={idx} value={option}>{option}</option>
-            ))
-        }
+        {options.length > 0 &&
+          options.map((option, idx) => (
+            <option key={idx} value={option}>
+              {option}
+            </option>
+          ))}
       </select>
     </>
   );
