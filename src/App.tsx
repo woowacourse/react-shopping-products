2c0176c05479fe473c740b71f4cf9c3750194755
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-14 13:28:05 +0900

    feat: 메인페이지에 getProducts 함수 로직 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import Navbar from './widgets/navbar/ui/Navbar';
 import * as S from './App.styles';
 import CustomSelect from './shared/ui/CustomSelect';
 import ProductCard from './features/products/ui/ProductCard';
+import { useEffect, useState } from 'react';
+import { getProducts } from './features/products/api/getProducts';
+import { Product } from './features/products/type/product';
 
 const CATEGORY_OPTIONS = [
   { label: '전체', value: 'all' },
@@ -17,6 +20,45 @@ const FILTER_OPTIONS = [
 ];
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [pageInfo, setPageInfo] = useState({
+    totalElements: 0,
+    totalPages: 0,
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const isError = error !== '';
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setIsLoading(true);
+        const response = await getProducts();
+        const products = response.content;
+        const totalElements = response.totalElements;
+        const totalPages = response.totalPages;
+        setPageInfo({ totalElements, totalPages });
+        setProducts(products);
+      } catch (error) {
+        setError('Failed to fetch products');
+        console.error('Error fetching products:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>{error}</div>;
+  }
+
   return (
     <S.ProductListWrapper>
       <Navbar />
@@ -32,10 +74,9 @@ function App() {
         </S.ProductListHeader>
 
         <S.ProductList>
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {products.map((product: Product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
         </S.ProductList>
       </S.ProductListContainer>
     </S.ProductListWrapper>
