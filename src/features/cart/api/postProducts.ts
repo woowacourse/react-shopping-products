e899a25879d421bd0805e59a9cbc19abdbd32519
minji2219 <aabb0219@naver.com>
2025-05-14 14:11:26 +0900

    feat: Post products 함수 구현

---
+++ src/features/cart/api/postProducts.ts
@@ -0,0 +1,30 @@
+export const postProducts = async (productId: number) => {
+  const username = 'minji2219';
+  const password = 'password';
+  const token = btoa(`${username}:${password}`);
+
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/cart-items`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          productId: productId,
+          quantity: 1,
+        }),
+      }
+    );
+
+    if (!response.ok)
+      throw new Error('Network response was not ok for postProducts');
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    throw error;
+  }
+};
