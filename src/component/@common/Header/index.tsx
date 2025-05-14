99b11ba69b4e7ea141d72feb9d6bce017c6b60f7
ohgus <anakin1108@naver.com>
2025-05-14 19:18:24 +0900

    ​feat: 장바구니 수량 UI 구현
    Co-authored-by: 장정안 <126966681+ExceptAnyone@users.noreply.github.com>

--- src/component/@common/Header/index.tsx
+++ src/component/@common/Header/index.tsx
@@ -1,15 +1,43 @@
 import { IconCart, IconLogo } from '../../../asset';
 import { HeaderStyle } from './Header.styles';
 
-const Header = () => {
+import { css } from '@emotion/react';
+
+const CartQuantityStyle = css`
+  position: absolute;
+  top: 70%;
+  right: 0;
+  transform: translateY(-50%);
+  width: 1.9rem;
+  height: 1.9rem;
+  border-radius: 50%;
+  background-color: #fff;
+  color: #000;
+  font-size: 1rem;
+  font-weight: 700;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CardQuantity = ({ children }: { children: React.ReactNode }) => {
+  return <div css={CartQuantityStyle}>{children}</div>;
+};
+
+const CartStyle = css`
+  position: relative;
+`;
+
+const Header = ({ count }: { count: number }) => {
   return (
     <section css={HeaderStyle}>
       <a href="/">
         <img src={IconLogo} alt="logo" />
       </a>
 
-      <a href="/cart">
+      <a href="/cart" css={CartStyle}>
         <img src={IconCart} alt="cart" />
+        {count > 0 && <CardQuantity>{count}</CardQuantity>}
       </a>
     </section>
   );
