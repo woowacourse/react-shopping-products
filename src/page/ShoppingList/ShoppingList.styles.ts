a5af4119f0343c06c169b4f0724dd106ae16e2b3
장정안 <wjddks96@naver.com>
2025-05-14 13:13:41 +0900

    ​​feat: 쇼핑리스트 페이지 스타일 코드 일부 작성
    Co-authored-by: ohgus <123801385+ohgus@users.noreply.github.com>

---
+++ src/page/ShoppingList/ShoppingList.styles.ts
@@ -0,0 +1,23 @@
+import { css } from '@emotion/react';
+
+export const ShoppingListStyle = css`
+  display: flex;
+  padding: 0 2.4rem;
+`;
+
+export const ShoppingListTitleStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  width: 100%;
+  margin-top: 3.6rem;
+  gap: 2.4rem;
+`;
+
+export const ShoppingListFilterStyle = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
