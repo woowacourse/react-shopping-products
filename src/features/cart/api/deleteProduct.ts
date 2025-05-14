0e6de32941c5278afdec5b78f20e1ea2b25147ae
minji2219 <aabb0219@naver.com>
2025-05-14 16:50:12 +0900

    feat: Delete Products 함수 구현

---
+++ src/features/cart/api/deleteProduct.ts
@@ -0,0 +1,22 @@
+export const postProducts = async (productId: number) => {
+  const token = import.meta.env.VITE_API_KEY;
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/cart-items/${productId}`,
+      {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Basic ${token}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    if (!response.ok)
+      throw new Error('Network response was not ok for postProducts');
+
+    return response;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    throw error;
+  }
+};
