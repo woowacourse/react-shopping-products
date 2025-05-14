789d07afde1544c76a2a8d77a9ec1ad05ee3afb8
장정안 <wjddks96@naver.com>
2025-05-14 11:18:58 +0900

    ​​chore: 헤더에 필요한 이미지 다운로드
    Co-authored-by: ohgus <123801385+ohgus@users.noreply.github.com>

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,3 +1,5 @@
 import IconArrowDown from '../../public/icon/ic_arrow_down.svg';
+import IconLogo from '../../public/icon/ic_logo.svg';
+import IconCart from '../../public/icon/ic_cart.svg';
 
-export { IconArrowDown };
+export { IconArrowDown, IconLogo, IconCart };
