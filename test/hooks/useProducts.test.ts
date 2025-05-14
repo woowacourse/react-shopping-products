e324bfe675b827a39f3418d99aa436ef80c76c7b
aydenote <dslgpgh@gmail.com>
2025-05-14 15:48:10 +0900

    test: 상품 목록을 식료품으로 필터링 테스트 작성
    Co-authored-by: Minje Sung <minje813@naver.com>

--- test/hooks/useProducts.test.ts
+++ test/hooks/useProducts.test.ts
@@ -3,11 +3,19 @@ import useProducts from "../../src/hooks/useProducts";
 
 describe("useProducts", () => {
 	describe("상품 목록 조회", () => {
-		it("가장 앞 페이상품 목록을 조회한다.", async () => {
-			const { result } = renderHook(() => useProducts());
+		it("가장 앞 상품 목록을 조회한다.", async () => {
+			const { result } = renderHook(() => useProducts({}));
 
 			await waitFor(() => {
 				expect(result.current.products).toHaveLength(20);
+			});
+		});
+
+		it("가장 앞 상품 목록을 식료품으로 필터링하여 조회한다..", async () => {
+			const { result } = renderHook(() => useProducts({ filterType: "식료품" }));
+
+			await waitFor(() => {
+				expect(result.current.products).toHaveLength(10);
 			});
 		});
 	});
