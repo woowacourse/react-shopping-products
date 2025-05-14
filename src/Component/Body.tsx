d9e9044f6ce899530dbcac10a64c3eb2f365532b
kimyou1102 <yoosk5485@naver.com>
2025-05-14 11:21:16 +0900

    feat: Body 컴포넌트 구현
    Co-authored-by: AHHYUNJU <AHHYUNJU@users.noreply.github.com>

---
+++ src/Component/Body.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps, PropsWithChildren } from 'react';
+import styled from '@emotion/styled';
+
+export default function Body({
+  children,
+  ...props
+}: PropsWithChildren<ComponentProps<'body'>>) {
+  return <StyledBody {...props}>{children}</StyledBody>;
+}
+
+const StyledBody = styled.body`
+  padding: 36px 24px;
+  margin: 0px;
+`;
