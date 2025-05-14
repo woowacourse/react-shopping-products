5d9dfdcd15ae014b7bee13d7e68e7b62e0664761
dev-dino22 <wlsskdud02@naver.com>
2025-05-14 13:31:24 +0900

    feat: ShopPage 리스트 타이틀 작성

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -3,6 +3,7 @@ import Dropdown, { DropdownOptionType } from '../../components/common/Dropdown';
 import ShopHeader from '../../components/features/header/ShopHeader';
 import ProductList from '../../components/features/product/product-list/ProductList';
 import Flex from '../../components/common/Flex';
+import styled from '@emotion/styled';
 
 function ShopPage() {
   const [filterOption, setFilterOption] = useState({
@@ -27,28 +28,47 @@ function ShopPage() {
   return (
     <>
       <ShopHeader />
-      <Flex flexDirection="row" justifyContent="space-between">
-        <Dropdown
-          options={[
-            { label: '전체', value: 'all' },
-            { label: '식료품', value: 'food' },
-            { label: '패션잡화', value: 'fashion' },
-          ]}
-          selectedValue={filterOption.category}
-          onSelectHandler={handleCategoryOption}
-        />
-        <Dropdown
-          options={[
-            { label: '낮은 가격순', value: 'ascending' },
-            { label: '높은 가격순', value: 'descending' },
-          ]}
-          selectedValue={filterOption.sort}
-          onSelectHandler={handleSortOption}
-        />
-      </Flex>
-      <ProductList />
+      <ProductListContainer>
+        <ListTitleBox>
+          <ListTitle>Apple 상품 목록</ListTitle>
+          <Flex flexDirection="row" justifyContent="space-between">
+            <Dropdown
+              options={[
+                { label: '전체', value: 'all' },
+                { label: '식료품', value: 'food' },
+                { label: '패션잡화', value: 'fashion' },
+              ]}
+              selectedValue={filterOption.category}
+              onSelectHandler={handleCategoryOption}
+            />
+            <Dropdown
+              options={[
+                { label: '낮은 가격순', value: 'ascending' },
+                { label: '높은 가격순', value: 'descending' },
+              ]}
+              selectedValue={filterOption.sort}
+              onSelectHandler={handleSortOption}
+            />
+          </Flex>
+        </ListTitleBox>
+        <ProductList />
+      </ProductListContainer>
     </>
   );
 }
+
+const ProductListContainer = styled(Flex)`
+  padding: 36px 24px;
+  gap: 28px;
+`;
+
+const ListTitleBox = styled(Flex)`
+  align-items: flex-start;
+  gap: 24px;
+`;
+
+const ListTitle = styled.h2`
+  ${({ theme }) => theme.heading};
+`;
 
 export default ShopPage;
