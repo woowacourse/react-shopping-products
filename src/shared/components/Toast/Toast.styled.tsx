8b0f10b767a4e3fa18178a64d447f654c5ed4302
keemsebeen <sebin0580@gmail.com>
2025-05-14 22:12:12 +0900

    style: Toast 스타일 수정
    Co-Authored-By: Youjeong Jeong <115888336+jeongyou@users.noreply.github.com>

--- src/shared/components/Toast/Toast.styled.tsx
+++ src/shared/components/Toast/Toast.styled.tsx
@@ -1,28 +1,26 @@
 import styled from '@emotion/styled';
 
-export const StyledBackDrop = styled.div`
-  width: 100%;
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 10;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: rgba(31, 41, 55, 0.2);
-`;
-
 export const StyledModalContainer = styled.div`
   box-sizing: border-box;
-  width: 90%;
-  max-width: 400px;
-  height: auto;
+  width: 100%;
+  max-width: 420px;
+  height: 50px;
   position: fixed;
+  border: 1px solid rgba(0, 0, 0, 0.1);
   background-color: white;
-  padding: 24px 32px;
-  top: 70px;
+  top: 10%;
   left: 50%;
-  border-radius: 16px;
+  transform: translateX(-50%);
+  border-radius: 4px;
+`;
+
+export const StyledModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
 `;
