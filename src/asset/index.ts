7fa36859474812789586abdd9b83a3244e74b169
장정안 <wjddks96@naver.com>
2025-05-14 15:06:45 +0900

    ​​chore: 담기 빼기 버튼에 필요한 svg 다운로드
    Co-authored-by: ohgus <123801385+ohgus@users.noreply.github.com>

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,5 +1,6 @@
 import IconArrowDown from '../../public/icon/ic_arrow_down.svg';
 import IconLogo from '../../public/icon/ic_logo.svg';
 import IconCart from '../../public/icon/ic_cart.svg';
-
-export { IconArrowDown, IconLogo, IconCart };
+import IconAddCart from '../../public/icon/ic_add_cart.svg';
+import IconRemoveCart from '../../public/icon/ic_remove_cart.svg';
+export { IconArrowDown, IconLogo, IconCart, IconAddCart, IconRemoveCart };
