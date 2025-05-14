5eff26b23d2191a67b5c71552f95eb708d6a26ca
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-14 11:29:04 +0900

    feat: Dropdown 컴포넌트 작성

--- src/global/style/reset.ts
+++ src/global/style/reset.ts
@@ -138,14 +138,18 @@ const reset = css`
     cursor: pointer;
     background-color: transparent;
   }
-  p,
-  span,
-  a {
+  @font-face {
     font-family: 'Pretendard-Regular';
     src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
       format('woff');
     font-weight: 400;
     font-style: normal;
+  }
+  p,
+  span,
+  a,
+  div {
+    font-family: 'Pretendard-Regular';
   }
 `;
 
--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,10 +1,51 @@
+import { useState } from 'react';
+import Dropdown, { DropdownOptionType } from '../../components/common/Dropdown';
 import ShopHeader from '../../components/features/header/ShopHeader';
 import ProductList from '../../components/features/product/product-list/ProductList';
+import Flex from '../../components/common/Flex';
 
 function ShopPage() {
+  const [filterOption, setFilterOption] = useState({
+    category: { label: '전체', value: 'all' },
+    sort: { label: '낮은 가격순', value: 'ascending' },
+  });
+
+  const handleCategoryOption = (option: DropdownOptionType) => {
+    setFilterOption((prev) => ({
+      ...prev,
+      category: option,
+    }));
+  };
+
+  const handleSortOption = (option: DropdownOptionType) => {
+    setFilterOption((prev) => ({
+      ...prev,
+      sort: option,
+    }));
+  };
+
   return (
     <>
       <ShopHeader />
+      <Flex flexDirection="row" justifyContent="space-between">
+        <Dropdown
+          options={[
+            { label: '전체', value: 'all' },
+            { label: '식료품', value: 'food' },
+            { label: '패션잡화', value: 'fashion' },
+          ]}
+          selectedValue={filterOption.category}
+          onSelectHandler={handleCategoryOption}
+        />
+        <Dropdown
+          options={[
+            { label: '낮은 가격순', value: 'ascending' },
+            { label: '높은 가격순', value: 'descending' },
+          ]}
+          selectedValue={filterOption.sort}
+          onSelectHandler={handleSortOption}
+        />
+      </Flex>
       <ProductList />
     </>
   );
