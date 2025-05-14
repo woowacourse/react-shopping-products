bb3db5ef7cc7c3ba03c73eec69f4da0303ac20d5
minji2219 <aabb0219@naver.com>
2025-05-14 17:47:59 +0900

    refactor: Get CartProduct 헤더에 토큰값 넣어 요청

--- src/features/cart/api/getCartProduct.ts
+++ src/features/cart/api/getCartProduct.ts
@@ -1,8 +1,17 @@
 export const getCartProduct = async () => {
+  const token = import.meta.env.VITE_API_KEY;
+
   try {
     const response = await fetch(
-      `${import.meta.env.VITE_API_BASE_URL}/cart-items`
+      `${import.meta.env.VITE_API_BASE_URL}/cart-items?page=0&size=20`,
+      {
+        headers: {
+          Authorization: `Basic ${token}`,
+          'Content-Type': 'application/json',
+        },
+      }
     );
+
     if (!response.ok) {
       throw new Error('Network response was not ok for getCartProduct');
     }
