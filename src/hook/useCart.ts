b4fdecb9b00e47dcf999be26bb6c72d7eb843259
ohgus <anakin1108@naver.com>
2025-05-14 20:07:34 +0900

    ​​refactor: 카트 데이터 패치 try catch 추가
    Co-authored-by: 장정안 <126966681+ExceptAnyone@users.noreply.github.com>

--- src/hook/useCart.ts
+++ src/hook/useCart.ts
@@ -15,18 +15,27 @@ const useCart = () => {
   }, []);
 
   const fetchCartData = async () => {
-    const response = await fetch(
-      `${import.meta.env.VITE_API_BASE_URL}/cart-items?page=0&size=20`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Basic ${import.meta.env.VITE_API_KEY}`
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/cart-items?page=0&size=20`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ${import.meta.env.VITE_API_KEY}`
+          }
         }
-      }
-    );
+      );
 
-    const results = await response.json();
-    setCartData(results.content);
+      if (!response.ok) {
+        throw new Error('Failed to fetch cart data');
+      }
+
+      const results = await response.json();
+      setCartData(results.content);
+    } catch (error) {
+      // 예외 처리 로직
+      // Fallback page
+    }
   };
 
   return { cartData, fetchCartData };
