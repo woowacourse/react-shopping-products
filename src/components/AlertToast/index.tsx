7f70e2b573eaced880eed77dfe6c47f855e9a790
wo-o29 <woogur29@gmail.com>
2025-05-14 19:13:57 +0900

    ​​feat: AlertToast 컴포넌트 구현
    Co-authored-by: Deokbin Kim <162319857+Db0111@users.noreply.github.com>

---
+++ src/components/AlertToast/index.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import * as S from "./AlertToast.styled";
+
+interface AlertToastProps {
+  errorMessage: string;
+}
+
+function AlertToast({ errorMessage }: AlertToastProps) {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 3000);
+  }, []);
+
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <S.Container>
+      <S.ErrorMessage>{errorMessage}</S.ErrorMessage>
+    </S.Container>
+  );
+}
+
+export default AlertToast;
