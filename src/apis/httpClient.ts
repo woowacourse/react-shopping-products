9b2cba7d8f4e9edd4c4f626f3f8ba0a381c52ea7
wo-o29 <woogur29@gmail.com>
2025-05-14 16:01:28 +0900

    ​​feat: POST, DELETE 메서드 추가
    Co-authored-by: Deokbin Kim <162319857+Db0111@users.noreply.github.com>

--- src/apis/httpClient.ts
+++ src/apis/httpClient.ts
@@ -24,6 +24,43 @@ class HTTPClient {
     return response.json();
   }
 
+  async post<T>(url: string, data: T) {
+    const headers = {
+      "Content-Type": "application/json",
+      ...(this.apiKey && { Authorization: this.apiKey }),
+    };
+
+    const response = await fetch(this.baseUrl + url, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error("예기치 못한 오류가 발생했습니다.");
+    }
+
+    return response.json();
+  }
+
+  async delete(url: string) {
+    const headers = {
+      "Content-Type": "application/json",
+      ...(this.apiKey && { Authorization: this.apiKey }),
+    };
+
+    const response = await fetch(this.baseUrl + url, {
+      method: "DELETE",
+      headers,
+    });
+
+    if (!response.ok) {
+      throw new Error("예기치 못한 오류가 발생했습니다.");
+    }
+
+    return response.json();
+  }
+
   setApiKey(apiKey: string) {
     this.apiKey = apiKey;
   }
