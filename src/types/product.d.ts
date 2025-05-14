6cf0a3eb90154319c3d1eb5f3dc833bc5e27ca1f
SuHwa Lee <tnghk9611@naver.com>
2025-05-14 19:52:01 +0900

    refactor: 상품 데이터 타입 분리
    Co-authored-by: 이상희 <sang6299@naver.com>

--- src/types/product.d.ts
+++ src/types/product.d.ts
@@ -4,3 +4,11 @@ export type ProductProps = {
   price: number;
   isAdd: boolean;
 };
+
+export type ProductDataType = {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  category: string;
+};
