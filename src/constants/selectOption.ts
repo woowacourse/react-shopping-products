ba6b1fd4deb6ca1c621535557838dfd9361a9275
yeji0214 <1225yj50@naver.com>
2025-05-14 16:11:18 +0900

    feat: 정렬 옵션에 순서 없음 기본값으로 추가
    Co-authored-by: Claire <eunsoa0310@gmail.com>

--- src/constants/selectOption.ts
+++ src/constants/selectOption.ts
@@ -1,2 +1,2 @@
-export const CATEGORY = ["전체", "식료품", "패션잡화"];
-export const SORT = ["낮은 가격순", "높은 가격순"];
+export const CATEGORY = ["전체", "식료품", "패션잡화"] as const;
+export const SORT = ["순서 없음", "낮은 가격순", "높은 가격순"] as const;
