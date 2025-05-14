f45eade0d75824d09f08ab66e41259e720206b75
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-14 16:30:49 +0900

    refactor: Logo 클릭 시 새로고침 적용

--- src/widgets/navBar/ui/Navbar.tsx
+++ src/widgets/navBar/ui/Navbar.tsx
@@ -1,10 +1,17 @@
 import * as S from './Navbar.styles';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate(0);
+  };
+
   return (
     <S.Wrapper>
-      <S.Logo>SHOP</S.Logo>
-      <S.CartIcon src="/cartIcon.svg"></S.CartIcon>
+      <S.Logo onClick={handleLogoClick}>SHOP</S.Logo>
+      <S.CartIcon src='/cartIcon.svg'></S.CartIcon>
     </S.Wrapper>
   );
 }
