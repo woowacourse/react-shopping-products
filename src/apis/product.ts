afc2eba77a5d708a67522247bf91c6b136907cd2
guesung <gueit214@naver.com>
2025-05-14 16:50:55 +0900

    feat: product api추가
    Co-Authored-By: 호이초이 <110888511+hoyychoi@users.noreply.github.com>

---
+++ src/apis/product.ts
@@ -0,0 +1,14 @@
+import { GetProductResponse } from "../types/product";
+
+interface GetProductParams {
+  page: number;
+  size: number;
+  sort?: "asc" | "desc";
+}
+
+export const getProduct = async ({ page, size, sort = "asc" }: GetProductParams): Promise<GetProductResponse> => {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/products?page=${page}&size=${size}&sort=${sort}`);
+  const data = await response.json();
+
+  return data;
+};
