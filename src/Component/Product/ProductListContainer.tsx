1669ff6294e5132312c4e9cff9af1398f1d57801
kimyou1102 <yoosk5485@naver.com>
2025-05-14 20:47:04 +0900

    feat: 카트 상태 반영하여 상품 목록 업데이트
    - getProducts 호출 후 가공하여 업데이트
    
    Co-authored-by: AHHYUNJU <AHHYUNJU@users.noreply.github.com>

--- src/Component/Product/ProductListContainer.tsx
+++ src/Component/Product/ProductListContainer.tsx
@@ -1,17 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ProductTypes } from '../../types/ProductTypes';
 import ProductList from './ProductList';
 import ProductListToolbar from './ProductListToolbar';
+import getProducts from '../../api/getProducts';
 import { CartItemTypes } from '../../types/CartItemType';
 
 interface ProductListContainerProps {
   cartItems: CartItemTypes[];
 }
 
+function markItemsInCart(cartItems: CartItemTypes[], items: ProductTypes[]) {
+  const cartIds = new Set(cartItems.map((cartItem) => cartItem.id));
+
+  return items.map((item) => ({
+    ...item,
+    isItemInCart: cartIds.has(item.id),
+  }));
+}
+
 export default function ProductListContainer({
   cartItems,
 }: ProductListContainerProps) {
   const [products, setProducts] = useState<ProductTypes[]>([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const productsData = await getProducts();
+        const productsContent = productsData.content;
+        setProducts(markItemsInCart(cartItems, productsContent));
+      } catch (e) {
+        //
+      } finally {
+        //
+      }
+    }
+    fetchProducts();
+  }, [cartItems]);
 
   return (
     <>
