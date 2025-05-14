550a07cfc1d21427a293d36edf4c974142148952
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-05-14 14:16:56 +0900

    style: 필터 컴포넌트 스타일 구현

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,17 +1,32 @@
+import styled from "@emotion/styled";
+
 function Filter(props) {
   return (
-    <div>
-      <select>
+    <Container>
+      <Select>
         <option>전체</option>
         <option>식료품</option>
         <option>패션잡화</option>
-      </select>
-      <select>
+      </Select>
+      <Select>
         <option>낮은 가격순</option>
         <option>높은 가격순</option>
-      </select>
-    </div>
+      </Select>
+    </Container>
   );
 }
 
 export default Filter;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Select = styled.select`
+  width: 124px;
+  padding: 8px;
+  border: 1px solid #c4c2c2;
+  border-radius: 8px;
+`;
