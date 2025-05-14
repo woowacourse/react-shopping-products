820b11053e3b4d8f7646bed7737d642bf89ae770
keemsebeen <sebin0580@gmail.com>
2025-05-14 16:24:51 +0900

    feat: ToastContext, ToastProvider 구현
    Co-Authored-By: Youjeong Jeong <115888336+jeongyou@users.noreply.github.com>

---
+++ src/shared/context/ToastProvider.tsx
@@ -0,0 +1,25 @@
+import { createContext, PropsWithChildren, useState } from 'react';
+
+import { Toast } from '../components/Toast';
+
+export const ToastContext = createContext({ showToast(message: string) {} });
+
+export const ToastProvider = ({ children }: PropsWithChildren) => {
+  const [toast, setToast] = useState('');
+
+  const showToast = (message: string) => {
+    setToast(message);
+
+    const timer = setTimeout(() => {
+      setToast('');
+    }, 3000);
+
+    clearTimeout(timer);
+  };
+
+  return (
+    <ToastContext.Provider value={{ showToast }}>
+      {children} && {toast && <Toast message={toast} />}
+    </ToastContext.Provider>
+  );
+};
