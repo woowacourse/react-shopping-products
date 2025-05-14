a5e5a51c0e4e8441466f983831972d904ee50c0a
ohgus <anakin1108@naver.com>
2025-05-14 19:18:47 +0900

    ​​refactor: useCart훅 분리
    Co-authored-by: 장정안 <126966681+ExceptAnyone@users.noreply.github.com>

---
+++ src/hook/useCart.ts
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { Product } from '../types/response';
+
+export type CartItem = {
+  id: number;
+  product: Product;
+  quantity: number;
+};
+
+const useCart = () => {
+  const [cartData, setCartData] = useState<CartItem[]>([]);
+
+  const fetchCartData = async () => {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/cart-items?page=0&size=20`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${import.meta.env.VITE_API_KEY}`
+        }
+      }
+    );
+
+    const results = await response.json();
+    setCartData(results.content);
+  };
+
+  return { cartData, fetchCartData };
+};
+
+export default useCart;
