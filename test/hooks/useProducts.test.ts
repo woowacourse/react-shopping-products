6a2238c36a5463f82e224fe2ce00b0b8362098ee
aydenote <dslgpgh@gmail.com>
2025-05-14 17:45:29 +0900

    test: 상품 목록 오름차순 정렬 테스트 구현
    Co-authored-by: Minje Sung <minje813@naver.com>

--- test/hooks/useProducts.test.ts
+++ test/hooks/useProducts.test.ts
@@ -1,5 +1,7 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import useProducts from "../../src/hooks/useProducts";
+import sortProductList from "../../src/utils/sortProductList";
+import products from "../../src/mocks/data/products.json";
 
 describe("useProducts", () => {
 	describe("상품 목록 조회", () => {
@@ -16,6 +18,14 @@ describe("useProducts", () => {
 
 			await waitFor(() => {
 				expect(result.current.products).toHaveLength(10);
+			});
+		});
+
+		it("가장 앞 상품 목록을 오름차순으로 정렬하여 조회한다.", async () => {
+			const { result } = renderHook(() => useProducts({ sortingType: "asc" }));
+
+			await waitFor(() => {
+				expect(result.current.products).toEqual(sortProductList(products, "asc"));
 			});
 		});
 	});
