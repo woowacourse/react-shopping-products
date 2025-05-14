1bdb8c791a3f12f7090622f473f795687204a0ba
minji2219 <aabb0219@naver.com>
2025-05-14 14:12:15 +0900

    feat: 카트에 product 담기 핸들러 추가

--- src/features/products/ui/ProductCard.tsx
+++ src/features/products/ui/ProductCard.tsx
@@ -1,21 +1,32 @@
 import CustomButton from '../../../shared/ui/CustomButton';
-import { Product } from '../type/product';
+import {postProducts} from '../../cart/api/postProducts';
+import {Product} from '../type/product';
 import * as S from './ProductCard.styles';
 
 interface ProductCardProps {
   product: Product;
 }
 
-export default function ProductCard({ product }: ProductCardProps) {
+export default function ProductCard({product}: ProductCardProps) {
+  const handleProductCart = () => {
+    postProducts(product.id);
+  };
   return (
     <S.ProductCardContainer>
-      <S.ImageSection src={product.imageUrl} alt={product.name}></S.ImageSection>
+      <S.ImageSection
+        src={product.imageUrl}
+        alt={product.name}
+      ></S.ImageSection>
       <S.ContentSection>
         <S.ProductName>{product.name}</S.ProductName>
         <S.ProductPrice>{product.price}</S.ProductPrice>
       </S.ContentSection>
       <S.ButtonSection>
-        <CustomButton iconUrl='/addCartIcon.svg' title='담기' onClick={() => {}} />
+        <CustomButton
+          iconUrl="/addCartIcon.svg"
+          title="담기"
+          onClick={handleProductCart}
+        />
       </S.ButtonSection>
     </S.ProductCardContainer>
   );
