327e2afd38e1f5a091930e88e6fb06eab21b4409
mun-kyeong <horse6953@naver.com>
2025-05-14 11:11:07 +0900

    feat : button reset CSS 적용
    Co-authored-by: ha-kuku <hakukudh@gmail.com>

--- src/reset.tsx
+++ src/reset.tsx
@@ -130,6 +130,15 @@ const reset = css`
     border-collapse: collapse;
     border-spacing: 0;
   }
+  button {
+    background: inherit;
+    border: none;
+    box-shadow: none;
+    border-radius: 0;
+    padding: 0;
+    overflow: visible;
+    cursor: pointer;
+  }
 `;
 
 const GlobalStyle = () => <Global styles={reset} />;
