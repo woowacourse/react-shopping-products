76051099332abedb71d9b279be1ede78cb165fed
wo-o29 <woogur29@gmail.com>
2025-05-15 16:40:07 +0900

    chore: 문서 수정
    Co-authored-by: Deokbin Kim <162319857+Db0111@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "jsdom", // jsdom 환경 설정 (기본값이 jsdom이지만 명시적으로 설정)
-    globals: true, // 전역 변수 활성화
-    setupFiles: "./vitest.setup.ts", // 여기를 통해 위 코드가 실행됨
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./vitest.setup.ts",
   },
   resolve: {
     alias: {
