8625958ceeefacc6fd0fc5597f3e11d9ab1f331a
thgml05 <aisohii@naver.com>
2025-05-15 13:09:41 +0900

    feat: 가격이 동일할 시 id 내림차순 정렬 기능 추가
    Co-authored-by: Luna <ShinjungOh@users.noreply.github.com>

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,9 +1,8 @@
 import { SortKeyType } from '../types/product';
 
-// 기본 상품 정렬 - 오름차순(작은 것 부터)
 export const getProduct = async (sort: SortKeyType) => {
   const response = await fetch(
-    `http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com/products?page=0&size=20&sort=price,${sort}`
+    `http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com/products?page=0&size=20&sort=price,${sort}&sort=id,desc`
   );
   const data = await response.json();
 
