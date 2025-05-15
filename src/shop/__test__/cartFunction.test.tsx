6a8b20ea9ca9b96df0a16703fab45ff705f6f669
dev-dino22 <wlsskdud02@naver.com>
2025-05-15 17:01:07 +0900

    test: cartFunction.test 추가

---
+++ src/shop/__test__/cartFunction.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen, within } from '@testing-library/react';
+import App from '../../App';
+import { productListMockData } from '../__mocks__/productListMockData';
+import { cartMockData } from '../__mocks__/cartData';
+import userEvent from '@testing-library/user-event';
+
+jest.mock('../../api/postCartItem', () => ({
+  postCartItem: jest
+    .fn()
+    .mockImplementation(() => Promise.resolve(cartMockData)),
+}));
+
+jest.mock('../../api/getShoppingCartData', () => ({
+  getShoppingCartData: jest
+    .fn()
+    .mockImplementation(() => Promise.resolve(cartMockData)),
+}));
+
+jest.mock('../../api/getListData', () => ({
+  getListData: jest.fn().mockImplementation(() => {
+    console.log(productListMockData);
+    return Promise.resolve(productListMockData);
+  }),
+}));
+
+jest.mock('../../api/deleteCartItem', () => ({
+  deleteCartItem: jest
+    .fn()
+    .mockImplementation(() => Promise.resolve(cartMockData)),
+}));
+
+jest.mock('../../api/baseAPI', () => ({
+  baseAPI: jest.fn().mockImplementation(() => Promise.resolve(mockData)),
+}));
+describe('SHOP 페이지에 접속 시', () => {
+  it('장바구니 아이콘에 현재 장바구니 아이템 수가 표시된다', async () => {
+    render(<App />);
+
+    const cartButton = await screen.findByTestId('cart-button');
+    expect(cartButton.textContent).toBe('2');
+  });
+});
