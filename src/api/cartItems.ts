8b010c6948a58c491801f7df95b0f4b38e5c0a27
yeji0214 <1225yj50@naver.com>
2025-05-15 11:25:40 +0900

    feat: 상품 목록 정보 가져오는 api 구현
    Co-authored-by: Claire <eunsoa0310@gmail.com>

--- src/api/cartItems.ts
+++ src/api/cartItems.ts
@@ -1,11 +1,29 @@
 import apiRequest from "./utils/apiRequest";
 import { END_POINT } from "./constants/endPoint";
 
-type GetCartItemsCountsResponse = {
+type CartItem = {
+  id: number;
   quantity: number;
+  product: {
+    id: number;
+  };
 };
 
-export const getCartItemsCounts =
-  async (): Promise<GetCartItemsCountsResponse> => {
-    return apiRequest<GetCartItemsCountsResponse>(END_POINT.CART_COUNT, {});
-  };
+type GetCartItemsResponse = {
+  content: CartItem[];
+  totalElements: number;
+  totalPages: number;
+  number: number;
+  size: number;
+};
+
+export const getCartItems = async (): Promise<CartItem[]> => {
+  const response = await apiRequest<GetCartItemsResponse>(END_POINT.CART, {
+    queryParams: {
+      page: 0,
+      size: 50,
+    },
+  });
+
+  return response.content;
+};
