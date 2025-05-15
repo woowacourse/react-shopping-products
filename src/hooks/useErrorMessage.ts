4109fd757edf10cffa085fa6b0a9db9bbfbe478c
dlsxjzld <sinjuk1@naver.com>
2025-05-15 17:35:01 +0900

    refactor: useCallback을 사용하여 에러 메시지 핸들러 최적화

--- src/hooks/useErrorMessage.ts
+++ src/hooks/useErrorMessage.ts
@@ -1,29 +1,29 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react"
 
 const useErrorMessage = () => {
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("")
 
   useEffect(() => {
-    let timer: NodeJS.Timeout | null = null;
+    let timer: NodeJS.Timeout | null = null
 
     if (errorMessage) {
       timer = setTimeout(() => {
-        setErrorMessage("");
-      }, 3000);
+        setErrorMessage("")
+      }, 3000)
     }
 
     return () => {
       if (timer) {
-        clearTimeout(timer);
+        clearTimeout(timer)
       }
-    };
-  }, [errorMessage]);
+    }
+  }, [errorMessage])
 
-  const handleErrorMessage = (newErrorMessage: string) => {
-    setErrorMessage(newErrorMessage);
-  };
+  const handleErrorMessage = useCallback((newErrorMessage: string) => {
+    setErrorMessage(newErrorMessage)
+  }, [])
 
-  return { errorMessage, handleErrorMessage };
-};
+  return { errorMessage, handleErrorMessage }
+}
 
-export default useErrorMessage;
+export default useErrorMessage
