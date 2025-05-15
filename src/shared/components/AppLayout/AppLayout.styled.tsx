d7010866e6b0026a551f4a11b62671b986eb1d51
keemsebeen <sebin0580@gmail.com>
2025-05-15 10:46:41 +0900

    style: AppLayout border 제거, box-shadow 추가
    Co-Authored-By: Youjeong Jeong <115888336+jeongyou@users.noreply.github.com>

--- src/shared/components/AppLayout/AppLayout.styled.tsx
+++ src/shared/components/AppLayout/AppLayout.styled.tsx
@@ -10,7 +10,6 @@ export const StyledAppLayout = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  border-left: 1px solid #000000;
-  border-right: 1px solid #000000;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
   overflow: hidden;
 `;
