903170af016cdf9403c21d9126e448fffd95d703
kimyou1102 <yoosk5485@naver.com>
2025-05-15 13:00:04 +0900

    feat: 상품 목록 api 응답 상태에 따라 UI를 다르게 대응하도록 구현
    Co-authored-by: AHHYUNJU <AHHYUNJU@users.noreply.github.com>

--- src/Component/Product/ProductListContainer.tsx
+++ src/Component/Product/ProductListContainer.tsx
@@ -4,12 +4,17 @@ import ProductList from './ProductList';
 import ProductListToolbar from './ProductListToolbar';
 import getProducts from '../../api/getProducts';
 import { CartItemTypes } from '../../types/CartItemType';
+import ErrorBox from '../Common/ErrorBox';
+import Spinner from '../Common/Spinner';
+import styled from '@emotion/styled';
 
 interface ProductListContainerProps {
   cartItems: CartItemTypes[];
   updateCartItems: () => void;
   getMatchCartItem: (id: number) => CartItemTypes | undefined;
 }
+
+type Status = 'idle' | 'loading' | 'success' | 'error';
 
 export default function ProductListContainer({
   cartItems,
@@ -18,16 +23,18 @@ export default function ProductListContainer({
 }: ProductListContainerProps) {
   const [products, setProducts] = useState<ProductTypes[]>([]);
 
+  const [status, setStatus] = useState<Status>('idle');
+
   useEffect(() => {
     async function fetchProducts() {
       try {
+        setStatus('loading');
         const productsData = await getProducts();
         const productsContent = productsData.content;
         setProducts(productsContent);
+        setStatus('success');
       } catch (e) {
-        //
-      } finally {
-        //
+        setStatus('error');
       }
     }
     fetchProducts();
@@ -36,11 +43,27 @@ export default function ProductListContainer({
   return (
     <>
       <ProductListToolbar setProducts={setProducts} />
-      <ProductList
-        productList={products}
-        updateCartItems={updateCartItems}
-        getMatchCartItem={getMatchCartItem}
-      />
+      {status === 'success' ? (
+        <ProductList
+          productList={products}
+          updateCartItems={updateCartItems}
+          getMatchCartItem={getMatchCartItem}
+        />
+      ) : null}
+      {status === 'loading' ? (
+        <StyledSpinnerWrapper>
+          <Spinner size={100} color={'red'} />
+        </StyledSpinnerWrapper>
+      ) : null}
+      {status === 'error' ? <ErrorBox /> : null}
     </>
   );
 }
+
+const StyledSpinnerWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
