6265a5de3422464bfad03203e0716baaf694a77f
ohgus <anakin1108@naver.com>
2025-05-15 16:10:11 +0900

    fix: 배포 에러 수정
    Co-authored-by: 장정안 <126966681+ExceptAnyone@users.noreply.github.com>

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,5 +1,7 @@
-export const IconArrowDown = './icon/ic_arrow_down.svg';
-export const IconLogo = './icon/ic_logo.svg';
-export const IconCart = './icon/ic_cart.svg';
-export const IconAddCart = './icon/ic_add_cart.svg';
-export const IconRemoveCart = './icon/ic_remove_cart.svg';
+const baseUrl = import.meta.env.BASE_URL;
+
+export const IconArrowDown = `${baseUrl}icon/ic_arrow_down.svg`;
+export const IconLogo = `${baseUrl}icon/ic_logo.svg`;
+export const IconCart = `${baseUrl}icon/ic_cart.svg`;
+export const IconAddCart = `${baseUrl}icon/ic_add_cart.svg`;
+export const IconRemoveCart = `${baseUrl}icon/ic_remove_cart.svg`;
