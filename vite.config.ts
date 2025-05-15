b3f1b6b8a799ff17577fef159c9268e090b516e4
H0ngJu <hongju4385@gmail.com>
2025-05-15 17:15:05 +0900

    chore: config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-shopping-products/",
   plugins: [react()],
   test: {
     globals: true,
