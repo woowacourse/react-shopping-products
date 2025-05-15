8bb8906d1975bb38d613b87a786325019da06523
jin123457 <jinsol9909@gmail.com>
2025-05-15 10:46:22 +0900

    feat: 로딩 아이콘 컴포넌트 추가 및 로딩 상태 처리 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,55 @@
-import * as S from "./App.styled";
-import Header from "./components/Header/Header";
-import ProductControl from "./components/ProductControl/ProductControl";
-import ProductList from "./components/ProductList/ProductList";
-import ErrorBox from "./components/common/ErrorBox/ErrorBox";
-import getProductList from "./api/ProductListApi";
-import { useEffect, useState } from "react";
-import { ResponseCartItem, ResponseProduct } from "./api/types";
-import getCartItemList from "./api/CartItemListApi";
+import * as S from './App.styled';
+import Header from './components/Header/Header';
+import ProductControl from './components/ProductControl/ProductControl';
+import ProductList from './components/ProductList/ProductList';
+import ErrorBox from './components/common/ErrorBox/ErrorBox';
+import getProductList from './api/ProductListApi';
+import { useEffect, useState } from 'react';
+import { ResponseCartItem, ResponseProduct } from './api/types';
+import getCartItemList from './api/CartItemListApi';
+import LoadingIcon from './components/Icon/LoadingIcon';
 
 function App() {
   const [productList, setProductList] = useState<ResponseProduct[]>([]);
   const [cartItemList, setCartItemList] = useState<ResponseCartItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    (async () => {
-      const rawCartItemList = await getCartItemList();
-      const rawProductList = await getProductList({ category: "", sort: "" });
-      setCartItemList(rawCartItemList);
-      setProductList(rawProductList);
-      //FIX: 리팩토링 필요
-      const newProductList = rawProductList.map((product) => {
-        const isInCart = rawCartItemList.some(
-          (cartItem) => cartItem.product.id === product.id
-        );
-        return isInCart ? { ...product, isInCart: true } : product;
-      });
-      setProductList(newProductList);
-    })();
+    try {
+      (async () => {
+        const rawCartItemList = await getCartItemList();
+        const rawProductList = await getProductList({ category: '', sort: '' });
+        setCartItemList(rawCartItemList);
+        //FIX: 리팩토링 필요
+        const newProductList = rawProductList.map((product) => {
+          const isInCart = rawCartItemList.some((cartItem) => cartItem.product.id === product.id);
+          return isInCart ? { ...product, isInCart: true } : product;
+        });
+        setProductList(newProductList);
+      })();
+    } catch (error) {
+      throw new Error('에러 발생');
+    } finally {
+      setIsLoading(false);
+      console.log('로딩 완료', isLoading);
+    }
   }, []);
 
   return (
     <S.Global>
       <S.Wrap>
-        <Header cartItemList={cartItemList} />
-        <ErrorBox backgroundColor="#FFC9C9" text="에러 발생" />
-        <S.MiddleContainer>
-          <ProductControl setProductList={setProductList} />
-          <ProductList productList={productList} cartItemList={cartItemList} />
-        </S.MiddleContainer>
+        {isLoading ? (
+          <LoadingIcon />
+        ) : (
+          <>
+            <Header cartItemList={cartItemList} />
+            <ErrorBox backgroundColor='#FFC9C9' text='에러 발생' />
+            <S.MiddleContainer>
+              <ProductControl setProductList={setProductList} />
+              <ProductList productList={productList} cartItemList={cartItemList} />
+            </S.MiddleContainer>
+          </>
+        )}
       </S.Wrap>
     </S.Global>
   );
