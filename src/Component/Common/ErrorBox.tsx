4224b5c22af082f8dce4f5121a87c88f9154a798
kimyou1102 <yoosk5485@naver.com>
2025-05-15 13:31:03 +0900

    refactor: ErrorBox 리팩토링
    - 텍스트를 외부에서 받을 수 있게 수정
    - position: absolute제거
    
    Co-authored-by: AHHYUNJU <AHHYUNJU@users.noreply.github.com>

--- src/Component/Common/ErrorBox.tsx
+++ src/Component/Common/ErrorBox.tsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled';
 
-export default function ErrorBox() {
+export default function ErrorBox({ children }: { children: React.ReactNode }) {
   return (
     <StyledDiv>
-      <StyledSpan>오류가 발생했습니다. 잠시 후 다시 시도해 주세요.</StyledSpan>
+      <StyledSpan>{children}</StyledSpan>
     </StyledDiv>
   );
 }
@@ -15,9 +15,6 @@ const StyledDiv = styled.div`
   width: 100%;
   height: 40px;
   background-color: #ffc9c9;
-  position: absolute;
-  top: 64px;
-  left: 0px;
 `;
 
 const StyledSpan = styled.span`
