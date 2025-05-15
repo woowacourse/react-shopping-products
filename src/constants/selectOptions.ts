50ec7b6c6e48be5663f871f9bbd091617ee2b02f
jin123457 <jinsol9909@gmail.com>
2025-05-15 14:38:08 +0900

    fix: 카테고리 필터 전체 value 수정

--- src/constants/selectOptions.ts
+++ src/constants/selectOptions.ts
@@ -1,10 +1,10 @@
 export const CategoryOptions = {
-  전체: "전체",
-  식료품: "식료품",
-  패션잡화: "패션잡화",
+  전체: '',
+  식료품: '식료품',
+  패션잡화: '패션잡화',
 };
 
 export const SortOptions = {
-  낮은가격순: "price,asc",
-  높은가격순: "price,desc",
+  낮은가격순: 'price,asc',
+  높은가격순: 'price,desc',
 };
