1e73aed132d86a1fdd121c1997789f9841d9307d
keemsebeen <sebin0580@gmail.com>
2025-05-15 11:12:08 +0900

    feat: useApiRequest 훅 구현
    Co-Authored-By: Youjeong Jeong <115888336+jeongyou@users.noreply.github.com>

---
+++ src/shared/hooks/useApiRequest.ts
@@ -0,0 +1,30 @@
+import { useCallback, useContext, useState } from 'react';
+
+import { ToastContext } from '../context/ToastProvider';
+
+export const useApiRequest = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { showToast } = useContext(ToastContext);
+
+  const handleRequest = useCallback(
+    async <T, R = T>(
+      apiCall: () => Promise<T>,
+      setCallback: (data: T) => R,
+      errorData?: T
+    ): Promise<T | R | undefined> => {
+      try {
+        setIsLoading(true);
+        const data = await apiCall();
+        return setCallback ? setCallback(data) : data;
+      } catch (err) {
+        showToast((err as Error).message);
+        return errorData;
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [showToast]
+  );
+
+  return { isLoading, handleRequest };
+};
