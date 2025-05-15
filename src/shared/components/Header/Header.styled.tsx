37b4402cf1efb99714221aec0ce0172b95edc451
keemsebeen <sebin0580@gmail.com>
2025-05-15 13:16:13 +0900

    style: Header height 지정
    Co-Authored-By: Youjeong Jeong <115888336+jeongyou@users.noreply.github.com>

--- src/shared/components/Header/Header.styled.tsx
+++ src/shared/components/Header/Header.styled.tsx
@@ -3,9 +3,10 @@ import styled from '@emotion/styled';
 import { HeaderProps } from '.';
 
 export const StyledHeader = styled.header<Pick<HeaderProps, 'right'>>`
+  position: sticky;
   top: 0;
   display: flex;
-  min-height: 64px;
+  height: 64px;
   width: 100%;
   padding: 16px;
   background-color: black;
