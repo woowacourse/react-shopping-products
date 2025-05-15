39dd415ecc9f197a4b29da680fec2e29f57137e5
keemsebeen <sebin0580@gmail.com>
2025-05-15 12:38:41 +0900

    style: 애니메이션 추가
    Co-Authored-By: Youjeong Jeong <115888336+jeongyou@users.noreply.github.com>

--- src/shared/components/Toast/Toast.styled.tsx
+++ src/shared/components/Toast/Toast.styled.tsx
@@ -3,15 +3,37 @@ import styled from '@emotion/styled';
 export const StyledModalContainer = styled.div`
   box-sizing: border-box;
   width: 100%;
-  max-width: 420px;
+  max-width: 425px;
   height: 50px;
   position: fixed;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  background-color: white;
-  top: 10%;
+  background-color: rgba(253, 169, 169);
+  top: 8%;
   left: 50%;
   transform: translateX(-50%);
   border-radius: 4px;
+  animation: fadeIn 0.3s ease-in-out;
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      top: 0;
+    }
+    100% {
+      opacity: 1;
+      top: 8%;
+    }
+  }
+
+  @keyframes fadeOut {
+    0% {
+      opacity: 1;
+      top: 8%;
+    }
+    100% {
+      opacity: 0;
+      top: 0;
+    }
+  }
 `;
 
 export const StyledModalContent = styled.div`
@@ -21,6 +43,6 @@ export const StyledModalContent = styled.div`
   justify-content: center;
   width: 100%;
   height: 100%;
-  padding: 0;
+  padding: 10px;
   margin: 0;
 `;
