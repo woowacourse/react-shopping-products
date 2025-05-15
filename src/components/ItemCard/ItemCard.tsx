58d7de6c50a332f8340e4c187a9da53e2a2a3d1b
Minje Sung <minje813@dgu.ac.kr>
2025-05-15 15:17:53 +0900

    chore: 배포 설정

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -1,63 +1,69 @@
-import { CartInfo } from "../../types";
-import addCart from "../../utils/api/addCart";
-import removeCart from "../../utils/api/removeCart";
-import S from "./ItemCard.module.css";
+import { CartInfo } from '../../types';
+import addCart from '../../utils/api/addCart';
+import removeCart from '../../utils/api/removeCart';
+import S from './ItemCard.module.css';
 
 interface ItemCardProps {
-	id: number;
-	imageUrl: string;
-	name: string;
-	price: number;
-	isCart: boolean;
-	cartInfo: CartInfo;
-	fetchCartProducts: () => Promise<void>;
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+  isCart: boolean;
+  cartInfo: CartInfo;
+  fetchCartProducts: () => Promise<void>;
 }
 
-const ItemCard = ({ id, imageUrl, name, price, isCart, cartInfo, fetchCartProducts }: ItemCardProps) => {
-	return (
-		<div className={S.cardContainer}>
-			<img
-				className={S.cardImg}
-				src={imageUrl}
-				alt="상품 이미지"
-				onError={(e) => {
-					const target = e.target as HTMLImageElement;
-					target.src = "./images/default-image.png";
-				}}
-			/>
-			<div className={S.contentContainer}>
-				<div className={S.itemDescription}>
-					<p className={S.cardName}>{name}</p>
-					<p>{price.toLocaleString()}원</p>
-				</div>
-				<div className={S.buttonWrap}>
-					{isCart ? (
-						<button
-							className={S.removeCartButton}
-							onClick={async () => {
-								await removeCart(cartInfo.id);
-								fetchCartProducts();
-							}}
-						>
-							<img className={S.cartImg} src="/images/remove-cart.svg" alt="장바구니 제거" />
-							<p>빼기</p>
-						</button>
-					) : (
-						<button
-							className={S.addCartButton}
-							onClick={async () => {
-								await addCart(id);
-								fetchCartProducts();
-							}}
-						>
-							<img className={S.cartImg} src="/images/add-cart.svg" alt="장바구니 추가" />
-							<p>담기</p>
-						</button>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+const ItemCard = ({
+  id,
+  imageUrl,
+  name,
+  price,
+  isCart,
+  cartInfo,
+  fetchCartProducts,
+}: ItemCardProps) => {
+  return (
+    <div className={S.cardContainer}>
+      <img
+        className={S.cardImg}
+        src={imageUrl}
+        alt="상품 이미지"
+        onError={(e) => {
+          const target = e.target as HTMLImageElement;
+          target.src = './images/default-image.png';
+        }}
+      />
+      <div className={S.contentContainer}>
+        <div className={S.itemDescription}>
+          <p className={S.cardName}>{name}</p>
+          <p>{price.toLocaleString()}원</p>
+        </div>
+        <div className={S.buttonWrap}>
+          {isCart ? (
+            <button
+              className={S.removeCartButton}
+              onClick={async () => {
+                await removeCart(cartInfo.id);
+                fetchCartProducts();
+              }}>
+              <img className={S.cartImg} src="./images/remove-cart.svg" alt="장바구니 제거" />
+              <p>빼기</p>
+            </button>
+          ) : (
+            <button
+              className={S.addCartButton}
+              onClick={async () => {
+                await addCart(id);
+                fetchCartProducts();
+              }}>
+              <img className={S.cartImg} src="./images/add-cart.svg" alt="장바구니 추가" />
+              <p>담기</p>
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ItemCard;
