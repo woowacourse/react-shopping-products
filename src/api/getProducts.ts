a8701db1b1b8976b66c0eac5bca2ffa2842707a6
마 <126178440+shuyeon@users.noreply.github.com>
2025-05-16 21:27:07 +0900

    refactor :  PriceOrder 타입을 단일 출처로 통합

--- src/api/getProducts.ts
+++ src/api/getProducts.ts
@@ -1,36 +1,36 @@
-import { Category } from '../App';
-import fetchWithErrorHandling from './fetchWithErrorHandling';
+import { Category } from "../App";
+import fetchWithErrorHandling from "./fetchWithErrorHandling";
+import { PriceOrder } from "../App";
 
-type PriceOrder = '낮은 가격순' | '높은 가격순';
 type GetProductsProps = {
   category?: Category;
   priceOrder?: PriceOrder;
 };
 const priceOrderQueryString = {
-  '낮은 가격순': 'price%2Casc',
-  '높은 가격순': 'price%2Cdesc',
+  "낮은 가격순": "price%2Casc",
+  "높은 가격순": "price%2Cdesc",
 };
 
 const getProducts = async ({
-  category = '전체',
-  priceOrder = '낮은 가격순',
+  category = "전체",
+  priceOrder = "낮은 가격순",
 }: GetProductsProps = {}) => {
   const searchParams = new URLSearchParams();
   searchParams.toString();
 
-  if (category !== '전체') searchParams.append('category', category);
+  if (category !== "전체") searchParams.append("category", category);
   if (priceOrder)
-    searchParams.append('sort', priceOrderQueryString[priceOrder]);
+    searchParams.append("sort", priceOrderQueryString[priceOrder]);
 
   const queryString = searchParams.toString();
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   };
 
-  let newErrorMessage = '';
+  let newErrorMessage = "";
 
   const { data, status } = await fetchWithErrorHandling(
     `products?${queryString}`,
@@ -38,11 +38,11 @@ const getProducts = async ({
   );
 
   if (status === 400) {
-    newErrorMessage = '상품 목록을 불러오지 못했습니다. 다시 시도해주세요';
+    newErrorMessage = "상품 목록을 불러오지 못했습니다. 다시 시도해주세요";
   } else if (status === 404) {
-    newErrorMessage = 'not found';
+    newErrorMessage = "not found";
   } else if (status === 500) {
-    newErrorMessage = '서버에 문제가 발생했습니다.';
+    newErrorMessage = "서버에 문제가 발생했습니다.";
   }
 
   return { newErrorMessage, data };
