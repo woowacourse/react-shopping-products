4bb3dcd0f476bd1e391fcdf6296375ab06d565c3
마 <126178440+shuyeon@users.noreply.github.com>
2025-05-16 16:41:28 +0900

    refactor : 테스트 이름 의미가 더 명확해지도록 수정

--- __test__/header.test.tsx
+++ __test__/header.test.tsx
@@ -1,16 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import Header from '../src/components/header/Header';
+import { render, screen } from "@testing-library/react";
+import Header from "../src/components/header/Header";
 
-describe('Header 컴포넌트', () => {
-  it('cartItemCount가 전달되어 숫자가 보인다', () => {
+describe("Header 컴포넌트", () => {
+  it("장바구니 상품 개수가 전달되어 숫자가 보인다", () => {
     render(<Header cartItemCount={5} />);
-    const cartCount = screen.queryByText('5');
+    const cartCount = screen.queryByText("5");
     expect(cartCount).toBeTruthy();
   });
 
-  it('cartItemCount가 0이면 숫자가 표시되지 않는다', () => {
+  it("장바구니에 상품이 없으면 숫자가 표시되지 않는다", () => {
     render(<Header cartItemCount={0} />);
-    const badge = screen.queryByText('0');
+    const badge = screen.queryByText("0");
     expect(badge).toBeNull();
   });
 });
