b2dec61cca90a88818d707d299ac20580ae248c6
마 <126178440+shuyeon@users.noreply.github.com>
2025-05-16 15:22:02 +0900

    chore : 배포를 위한 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-shopping-products/",
   plugins: [
     react({
-      jsxImportSource: '@emotion/react', // :white_check_mark: 이것만 있어도 기본적으로 작동함
+      jsxImportSource: "@emotion/react", // :white_check_mark: 이것만 있어도 기본적으로 작동함
     }),
   ],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
 });
