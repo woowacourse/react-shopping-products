6ebbfa0b46dc2b2e153999564592b5e3f591dab6
keemsebeen <sebin0580@gmail.com>
2025-05-17 22:06:46 +0900

    feat: CATEGORY, PRICE의 key 기반 유니언 타입 정의

--- src/features/ProductList/constants/product.ts
+++ src/features/ProductList/constants/product.ts
@@ -4,7 +4,11 @@ export const CATEGORY = {
   FASHION: '패션잡화',
 } as const;
 
+export type CategoryType = keyof typeof CATEGORY;
+
 export const PRICE = {
   asc: '낮은 가격순',
   desc: '높은 가격순',
 } as const;
+
+export type PriceType = keyof typeof PRICE;
