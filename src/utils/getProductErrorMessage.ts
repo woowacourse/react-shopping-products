052783ed22d4d3ba81c8a4c7cb4f83b79da9bfbe
Ko Daeun <drechsu@gmail.com>
2025-05-17 16:12:34 +0900

    feat : 각 도메인에 대해 status에 따라 error메세지를 반환해주는 함수 구현

---
+++ src/utils/getProductErrorMessage.ts
@@ -0,0 +1,14 @@
+export const getProductErrorMessage = (status: number | null) => {
+  console.log('getProductErrorMessage', status);
+  const messages: Record<number, string> = {
+    400: '상품 정보를 불러올 수 없습니다.',
+    404: '상품을 찾을 수 없습니다.',
+    500: '상품 서버에 문제가 발생했습니다.',
+  };
+
+  return status !== null && messages[status]
+    ? messages[status]
+    : '알 수 없는 상품 오류가 발생했습니다.';
+};
+
+export default getProductErrorMessage;
