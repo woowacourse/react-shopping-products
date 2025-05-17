b0589ad755167e3e247cd201d2304bcf6eabe018
guesung <gueit214@naver.com>
2025-05-17 14:01:33 +0900

    design: 전체 높이 설정

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
   position: relative;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   max-width: 430px;
   margin: 0 auto;
   background-color: #fff;
