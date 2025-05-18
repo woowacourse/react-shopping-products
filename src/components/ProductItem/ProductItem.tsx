e98436bdada98edafd554d5194d893b72464c951
Ko Daeun <drechsu@gmail.com>
2025-05-18 14:28:19 +0900

    feat : fallback 이미지 설정

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,4 +1,5 @@
 import { Product } from '../../App';
+import isValidImageUrl from '../../utils/isValidImageUrl';
 import CartActionButton from './button/CartActionButton';
 import styled from '@emotion/styled';
 
@@ -17,9 +18,15 @@ const ProductItem = ({
   addToCart,
   removeFromCart,
 }: ProductItemProps) => {
+  console.log('imgurl', product.imageUrl);
   return (
     <ProductItemContainer>
-      <ProductItemImage src={product.imageUrl} alt={product.name} />
+      <ProductItemImage
+        src={
+          isValidImageUrl(product.imageUrl) ? product.imageUrl : 'fallback.svg'
+        }
+        alt={product.name}
+      />
       <ProductItemInfoContainer>
         <TextContainer>
           <ProductItemName>{product.name}</ProductItemName>
