16084fb0b5f648ba587956096c9f778968e054f7
wo-o29 <woogur29@gmail.com>
2025-05-18 22:19:47 +0900

    ​​refactor: toast 유지 시간 인터페이스 변경
    Co-authored-by: Deokbin Kim <162319857+Db0111@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MobileLayout>
       <App />
-      <Toast limit={5} autoClose={3000} />
+      <Toast limit={5} duration={3000} />
     </MobileLayout>
   </React.StrictMode>
 );
