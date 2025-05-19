65a3784a7e54eae3872259d77decac38170a4d04
Minje Sung <minje813@dgu.ac.kr>
2025-05-19 22:39:42 +0900

    refactor: useCart에 useFetch 적용

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,34 +1,33 @@
 import { useState, useEffect, useCallback } from 'react';
 import { CartProduct } from '../types';
 import { CART_URL } from '../constants/endpoint';
-import handleHttpError from '../utils/handleHTTPError';
 import { USER_TOKEN } from '../constants/env';
 import useError from './useError';
+import useFetch from './useFetch';
 
 export default function useCart() {
   const [cartProducts, setCartProducts] = useState<CartProduct[]>([]);
+  const { fetchData, loading } = useFetch();
   const { showError } = useError();
 
   const fetchCartProducts = useCallback(async () => {
     try {
-      const response = await fetch(CART_URL, {
+      const data = await fetchData(CART_URL, {
         headers: {
           'content-type': 'application/json',
           Authorization: `Basic ${USER_TOKEN}`,
         },
         method: 'GET',
       });
-      handleHttpError(response);
-      const data = await response.json();
       setCartProducts(data.content);
     } catch (error) {
       if (error instanceof Error) showError(error.message);
     }
-  }, [showError]);
+  }, [fetchData, showError]);
 
   useEffect(() => {
     fetchCartProducts();
   }, [fetchCartProducts]);
 
-  return { cartProducts, fetchCartProducts };
+  return { cartProducts, fetchCartProducts, loading };
 }
