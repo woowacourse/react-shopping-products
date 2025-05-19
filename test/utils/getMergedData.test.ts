b6d0a643a3cac9e69ee88019c1e5e78d889e237d
Minje Sung <minje813@dgu.ac.kr>
2025-05-19 23:35:45 +0900

    test: 상품 목록과 장바구니 목록 병합 유틸 함수 테스트 작성

---
+++ test/utils/getMergedData.test.ts
@@ -0,0 +1,54 @@
+import { describe, it, expect } from 'vitest';
+import { CartProduct, Product } from '../../src/types';
+import getMergedData from '../../src/utils/getMergedData';
+
+describe('getMergedData', () => {
+  const products: Product[] = [
+    { id: 1, name: '상품 1', price: 10000, imageUrl: '', category: '식료품' },
+    { id: 2, name: '상품 2', price: 20000, imageUrl: '', category: '패션잡화' },
+    { id: 3, name: '상품 3', price: 30000, imageUrl: '', category: '식료품' },
+  ];
+  it('상품 목록과 장바구니 목록을 병합해 새로운 데이터를 만들어 낸다. 이 때, 장바구니에 없는 경우 -1이 추가된다.', () => {
+    const cartProducts: CartProduct[] = [
+      {
+        id: 101,
+        product: { id: 1, name: '상품 1', price: 10000, imageUrl: '', category: '식료품' },
+        quantity: 2,
+      },
+      {
+        id: 103,
+        product: { id: 3, name: '상품 3', price: 30000, imageUrl: '', category: '식료품' },
+        quantity: 1,
+      },
+    ];
+
+    const result = getMergedData(products, cartProducts);
+
+    expect(result).toEqual([
+      {
+        id: 1,
+        name: '상품 1',
+        price: 10000,
+        imageUrl: '',
+        category: '식료품',
+        cartInfo: { id: 101, quantity: 2 },
+      },
+      {
+        id: 2,
+        name: '상품 2',
+        price: 20000,
+        imageUrl: '',
+        category: '패션잡화',
+        cartInfo: { id: -1, quantity: 0 },
+      },
+      {
+        id: 3,
+        name: '상품 3',
+        price: 30000,
+        imageUrl: '',
+        category: '식료품',
+        cartInfo: { id: 103, quantity: 1 },
+      },
+    ]);
+  });
+});
