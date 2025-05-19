0ca1e62d54eb6c02a1f9a8429ecf993cef3b2f6d
Jeongeun Lee <leeju1009@gmail.com>
2025-05-20 01:14:32 +0900

    refactor: 불필요한 async 제거

--- src/domain/cartItem.ts
+++ src/domain/cartItem.ts
@@ -1,6 +1,6 @@
 import { CartItemType } from '../types/data';
 
-export const getCartId = async (cartItems: CartItemType[], productId: number) => {
+export const getCartId = (cartItems: CartItemType[], productId: number) => {
   const targetItem = cartItems.find((item: CartItemType) => item.product.id === productId);
-  return targetItem && targetItem.id;
+  return targetItem?.id;
 };
