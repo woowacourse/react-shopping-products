e0e826944ec39fe29cd60f1825dd703abb6c909a
youjeong Jeong <wjddbwjd1205@naver.com>
2025-05-20 02:14:16 +0900

    refactor: useCartRequest 훅 분리하여 API 요청 로직 재사용성 및 역할 분리 개선
    - useCart 내부의 add, delete, fetch 로직을 useCartRequest로 분리

--- src/features/ProductList/hooks/useCart.ts
+++ src/features/ProductList/hooks/useCart.ts
@@ -1,57 +1,17 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { addCartItem, deleteCartItem, getCartItemList } from '@/api/cart';
-import { useApiRequest } from '@/shared/hooks/useApiRequest';
+import { useCartRequest } from './useCartRequest';
 
 import { CartItem } from '../types/Cart';
 
 export const useCart = () => {
   const [cartData, setCartData] = useState<CartItem[]>([]);
-  const { handleRequest } = useApiRequest();
 
-  const fetchCartProductData = useCallback(async () => {
-    return handleRequest(
-      () =>
-        getCartItemList({
-          page: 0,
-          size: 50,
-        }),
-      (data) => {
-        setCartData(data);
-        return data;
-      }
-    );
-  }, [handleRequest]);
+  const handleCartData = (cartData: CartItem[]) => {
+    setCartData(cartData);
+  };
 
-  const addToCart = useCallback(
-    async (productId: number, quantity: number = 1) => {
-      return handleRequest(
-        () =>
-          addCartItem({
-            productId,
-            quantity,
-          }),
-        (data) => {
-          setCartData(data);
-          return data.length;
-        }
-      );
-    },
-    [handleRequest]
-  );
-
-  const deleteFromCart = useCallback(
-    async (cartItemId: number) => {
-      return handleRequest(
-        () => deleteCartItem(cartItemId),
-        (data) => {
-          setCartData(data);
-          return data.length;
-        }
-      );
-    },
-    [handleRequest]
-  );
+  const { fetchCartProductData, addToCart, deleteFromCart } = useCartRequest(handleCartData);
 
   useEffect(() => {
     fetchCartProductData();
