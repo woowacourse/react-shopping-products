39242a156113e3ffbd6f76bd3ad69169b6bbe68e
장정안 <wjddks96@naver.com>
2025-05-19 15:15:37 +0900

    ​​chore: 필요없는 fragment 제거
    Co-authored-by: ohgus <123801385+ohgus@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,13 @@ export type SortOption = '높은 가격순' | '낮은 가격순';
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Global styles={GlobalStyle} />
-        <ToastProvider>
-          <ShoppingList />
-          <Toast />
-        </ToastProvider>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyle} />
+      <ToastProvider>
+        <ShoppingList />
+        <Toast />
+      </ToastProvider>
+    </ThemeProvider>
   );
 }
 
