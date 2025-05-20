59be8995dbe556617fe19441586174a0c10316b3
ohgus <anakin1108@naver.com>
2025-05-20 15:35:40 +0900

    refactor: CartQuantity 다른 컴포넌트와 일관성있게 변경

--- src/component/@common/Header/Header.tsx
+++ src/component/@common/Header/Header.tsx
@@ -1,32 +1,5 @@
 import { IconCart, IconLogo } from '../../../asset';
-import { HeaderStyle } from './Header.styles';
-
-import { css } from '@emotion/react';
-
-const CartQuantityStyle = css`
-  position: absolute;
-  top: 70%;
-  right: 0;
-  transform: translateY(-50%);
-  width: 1.9rem;
-  height: 1.9rem;
-  border-radius: 50%;
-  background-color: #fff;
-  color: #000;
-  font-size: 1rem;
-  font-weight: 700;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CardQuantity = ({ children }: { children: React.ReactNode }) => {
-  return <div css={CartQuantityStyle}>{children}</div>;
-};
-
-const CartStyle = css`
-  position: relative;
-`;
+import { CartQuantityStyle, CartStyle, HeaderStyle } from './Header.styles';
 
 const Header = ({ count }: { count: number }) => {
   return (
@@ -37,7 +10,7 @@ const Header = ({ count }: { count: number }) => {
 
       <a href="/cart" css={CartStyle}>
         <img src={IconCart} alt="cart" />
-        {count > 0 && <CardQuantity>{count}</CardQuantity>}
+        {count > 0 && <div css={CartQuantityStyle}>{count}</div>}
       </a>
     </section>
   );
