7940b374044ff817cb6aed96a37fa36be99c0bb6
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-05-20 21:29:18 +0900

    refactor: fetchRemoveProduct 함수에서 apiRequest 사용으로 변경

--- src/apis/product/fetchRemoveProduct.ts
+++ src/apis/product/fetchRemoveProduct.ts
@@ -1,27 +1,16 @@
+import { apiRequest } from "../ApiRequest";
+
 type fetchRemoveProductParams = {
   params: {
     productId: number;
   };
 };
 
-const BASE_URL = `${import.meta.env.VITE_BASE_URL}/cart-items`;
-const TOKEN = import.meta.env.VITE_TOKEN;
-
 const fetchRemoveProduct = async ({ params }: fetchRemoveProductParams) => {
-  const url = new URL(`${BASE_URL}/${params.productId}`);
-
-  const options = {
-    method: "DELETE",
-    headers: {
-      accept: "application/json",
-      Authorization: `Basic ${TOKEN}`,
-    },
-  };
-
-  const response = await fetch(url, options);
-  if (!response.ok) {
-    throw new Error("상품을 빼는데 실패했습니다.");
-  }
+  await apiRequest.DELETE({
+    endpoint: "/cart-items/",
+    searchParams: params,
+  });
 };
 
 export default fetchRemoveProduct;
