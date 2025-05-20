c5b9bb7efa45a1858487d58618887480c5304d89
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-05-20 21:13:54 +0900

    refactor: fetchCartItems 함수에서 apiRequest 사용으로 변경

--- src/apis/product/fetchCartItems.ts
+++ src/apis/product/fetchCartItems.ts
@@ -1,4 +1,5 @@
 import { FetchCartItemsResult } from "../../types/FetchCartItemsResult";
+import { apiRequest } from "../ApiRequest";
 
 type fetchCartItemsParams = {
   params?: {
@@ -7,30 +8,14 @@ type fetchCartItemsParams = {
   };
 };
 
-const BASE_URL = `${import.meta.env.VITE_BASE_URL}/cart-items`;
-const TOKEN = import.meta.env.VITE_TOKEN;
-
 const fetchCartItems = async ({
   params = { page: "0", size: "50" },
-}: fetchCartItemsParams): Promise<FetchCartItemsResult> => {
-  const url = new URL(BASE_URL);
-
-  url.search = new URLSearchParams(params).toString();
-
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Basic ${TOKEN}`,
-    },
-  };
-
-  const response = await fetch(url, options);
-  if (!response.ok) {
-    throw new Error("에러 발생!");
-  }
-
-  const data = await response.json();
+}: fetchCartItemsParams) => {
+  const data = await apiRequest.GET<FetchCartItemsResult>({
+    endpoint: "/cart-items",
+    searchParams: params,
+    useToken: true,
+  });
 
   return data;
 };
