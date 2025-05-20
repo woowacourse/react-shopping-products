2b41c89097f8f508038cd62ce937d36b14e10ecd
Minje Sung <minje813@dgu.ac.kr>
2025-05-20 22:28:19 +0900

    chore: Skeleton prettier 적용

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -1,14 +1,14 @@
-import SkeletonCard from "./SkeletonCard";
-import S from "./Skeleton.module.css";
+import SkeletonCard from './SkeletonCard';
+import S from './Skeleton.module.css';
 
 const Skeleton = ({ length }: { length: number }) => {
-	return (
-		<div className={S.itemContainer}>
-			{Array.from({ length: length }).map((_, i) => (
-				<SkeletonCard key={i} />
-			))}
-		</div>
-	);
+  return (
+    <div className={S.itemContainer}>
+      {Array.from({ length: length }).map((_, i) => (
+        <SkeletonCard key={i} />
+      ))}
+    </div>
+  );
 };
 
 export default Skeleton;
