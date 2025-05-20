7c7e13bb5aa09381dfc2713e9c39d0e0fc833901
Oliy <eunsoa0310@gmail.com>
2025-05-20 22:24:09 +0900

    refactor: LoadingContext 제거 및 App.tsx에서 LoadingProvider 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { ErrorProvider } from './context/ErrorContext';
-import { LoadingProvider } from './context/LoadingContext';
 import ProductPage from './pages/ProductPage';
 import './styles/reset.css';
 
 function App() {
   return (
     <ErrorProvider>
-      <LoadingProvider>
-        <ProductPage />
-      </LoadingProvider>
+      <ProductPage />
     </ErrorProvider>
   );
 }
