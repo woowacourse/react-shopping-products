51fb8df4766cc4d345536d2edade1a15fb346123
Jeongeun Lee <leeju1009@gmail.com>
2025-05-20 15:37:02 +0900

    refactor: 컴포넌트가 언마운트 된 후에 setTimeout 내부 상태 변경이 이루어지지 않도록 클린업 추가

--- src/context/ErrorMessageContext.tsx
+++ src/context/ErrorMessageContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useRef, useState } from 'react';
 
 interface ErrorMessageContextProps {
   children: React.ReactNode;
@@ -13,17 +13,31 @@ export const ErrorMessageContext = createContext<ErrorMessageContextType | null>
 
 export const ErrorMessageProvider = ({ children }: ErrorMessageContextProps) => {
   const [errorMessage, setErrorMessage] = useState('');
+  const timerIdRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const isShowingMessageRef = useRef(false);
+
   const handleErrorMessage = (newErrorMessage: string) => {
-    if (errorMessage.length !== 0) {
+    if (isShowingMessageRef.current) {
       return;
     }
 
     setErrorMessage(newErrorMessage);
+    isShowingMessageRef.current = true;
 
-    setTimeout(() => {
+    timerIdRef.current = setTimeout(() => {
       setErrorMessage('');
+      timerIdRef.current = null;
+      isShowingMessageRef.current = false;
     }, 3000);
   };
+
+  useEffect(() => {
+    return () => {
+      if (timerIdRef.current) {
+        clearTimeout(timerIdRef.current);
+      }
+    };
+  }, []);
 
   return (
     <ErrorMessageContext.Provider
