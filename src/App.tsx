cac68dbdcd242259010be94bb509f36b5bd3f44a
mun-kyeong <horse6953@naver.com>
2025-05-20 19:53:27 +0900

    feat : App 컴포넌트에 flex 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { css } from "@emotion/react";
 import ShopPage from "./page/ShopPage";
 import GlobalStyle from "./reset";
 
 function App() {
   return (
-    <div>
+    <div
+      css={css`
+        display: flex;
+        justify-content: center;
+      `}
+    >
       <GlobalStyle />
       <ShopPage />
     </div>
