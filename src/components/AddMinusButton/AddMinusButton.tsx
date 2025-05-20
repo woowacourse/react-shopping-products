6f0e4d9394e6c299e90a4ca10de0cdd50cca94fd
guesung <gueit214@naver.com>
2025-05-20 14:08:31 +0900

    feat: 더하고 빼는 버튼

---
+++ src/components/AddMinusButton/AddMinusButton.tsx
@@ -0,0 +1,47 @@
+import { css } from "@emotion/react";
+import Text from "../Text/Text";
+import Button from "../Button/Button";
+
+interface AddMinusButtonProps {
+  onAddButtonClick: () => void;
+  onMinusButtonClick: () => void;
+  quantity: number;
+}
+
+export default function AddMinusButton({ onAddButtonClick, onMinusButtonClick, quantity }: AddMinusButtonProps) {
+  return (
+    <div style={{ display: "flex", alignItems: "center", gap: "16px" }}>
+      <Button css={controlButtonStyle} onClick={onMinusButtonClick}>
+        <Text css={controlButtonTextStyle} variant="body-0">
+          -
+        </Text>
+      </Button>
+      <Text variant="body-2">{quantity}</Text>
+      <Button css={controlButtonStyle} onClick={onAddButtonClick}>
+        <Text css={controlButtonTextStyle} variant="body-0">
+          +
+        </Text>
+      </Button>
+    </div>
+  );
+}
+
+const controlButtonStyle = css`
+  padding: 0;
+  border-radius: 8px;
+  border: 1.5px solid #e5e5e5;
+  width: 20px;
+  height: 20px;
+  background: #fff;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const controlButtonTextStyle = css`
+  width: 100%;
+  font-size: 24px;
+  color: #222;
+  text-align: center;
+`;
