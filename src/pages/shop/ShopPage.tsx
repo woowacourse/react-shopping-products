27a28a6c77ff63cd314d3e6e888f6730af88fcdc
dev-dino22 <wlsskdud02@naver.com>
2025-05-20 15:58:29 +0900

    refactor: ShopPage 상태 제거

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,13 +1,10 @@
-import { useCartContext } from '../../context/useCartContext';
 import ShopHeader from '../../shop/components/header/ShopHeader';
 import ProductFilterList from './product-filter-list/ProductFilterList';
 
 function ShopPage() {
-  const { cartList } = useCartContext();
-
   return (
     <>
-      <ShopHeader itemsCount={cartList.length} />
+      <ShopHeader />
       <ProductFilterList />
     </>
   );
--- src/shop/components/header/ShopHeader.tsx
+++ src/shop/components/header/ShopHeader.tsx
@@ -1,7 +1,11 @@
 import Header from '../../../components/common/Header';
+import { useCartContext } from '../../../context/useCartContext';
 import CartButton from './CartButton';
 
-function ShopHeader({ itemsCount }: { itemsCount: number }) {
-  return <Header title="SHOP" right={<CartButton itemsCount={itemsCount} />} />;
+function ShopHeader() {
+  const { cartList } = useCartContext();
+  return (
+    <Header title="SHOP" right={<CartButton itemsCount={cartList.length} />} />
+  );
 }
 export default ShopHeader;
