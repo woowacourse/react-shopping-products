c5a11cba4f479321b5b4c86224292e30c3eb637b
yeongipark <yeongi0111@naver.com>
2025-05-20 18:07:03 +0900

    test: 반복되는 도메인 로직 커스텀 훅을 사용하도록 변경

--- src/__test__/cartToggleButton.test.tsx
+++ src/__test__/cartToggleButton.test.tsx
@@ -1,59 +1,44 @@
 // @vitest-environment jsdom
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import CartToggleButton from "../components/cartToggleButton/CartToggleButton";
 import "@testing-library/jest-dom";
+import useFetchCartProducts from "../hooks/useFetchCartProducts/useFetchCartProducts";
+import { ToastProvider } from "../components/toastProvider/ToastProvider";
 const TEST_PRODUCT_ID = 4;
 
-interface CartItem {
-  id: number;
-  product: { id: number };
-}
-
-const fetchCartItems = async (): Promise<
-  { cartId: number; productId: number }[]
-> => {
-  const response = await fetch(`${import.meta.env.VITE_BASE_URL}/cart-items`);
-
-  const data = await response.json();
-  return data.content.map((item: CartItem) => ({
-    cartId: item.id,
-    productId: item.product.id,
-  }));
-};
-
-describe("CartToggleButton 통합 테스트 (실제 API)", () => {
+describe("CartToggleButton 통합 테스트", () => {
   it("처음에는 '담기' 버튼이 보이고 클릭 시 장바구니에 추가되며 '빼기' 버튼으로 전환된다", async () => {
     const Wrapper = () => {
-      const [cartItems, setCartItems] = useState<
-        { productId: number; cartId: number }[]
-      >([]);
-
-      const updateCart = async () => {
-        const items = await fetchCartItems();
-        setCartItems(items);
-      };
+      const { cartItemIds, setCartItemIds, fetchCartProducts } =
+        useFetchCartProducts();
 
       useEffect(() => {
-        updateCart();
-      }, []);
+        fetchCartProducts();
+      }, [fetchCartProducts]);
 
-      const item = cartItems.find((item) => item.productId === TEST_PRODUCT_ID);
+      const item = cartItemIds.find(
+        (item) => item.productId === TEST_PRODUCT_ID
+      );
 
       return (
         <CartToggleButton
           productId={TEST_PRODUCT_ID}
           cartId={item?.cartId}
           isAdded={!!item}
-          cartAmount={cartItems.length}
-          setCartItemIds={setCartItems}
-          fetchCartProducts={updateCart}
+          cartAmount={cartItemIds.length}
+          setCartItemIds={setCartItemIds}
+          fetchCartProducts={fetchCartProducts}
         />
       );
     };
 
-    render(<Wrapper />);
+    render(
+      <ToastProvider>
+        <Wrapper />
+      </ToastProvider>
+    );
 
     const addBtn = await screen.findByText("담기");
     expect(addBtn).toBeInTheDocument();
