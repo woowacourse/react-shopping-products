9d7f0761b6346b6b234a09aec5f5e9471e5ff1e4
aydenote <dslgpgh@gmail.com>
2025-05-20 21:07:59 +0900

    feat: Select 컴포넌트 구현

--- src/components/ItemCard/ItemCardFilterSort.tsx
+++ src/components/ItemCard/ItemCardFilterSort.tsx
@@ -1,21 +1,26 @@
 import { filterType, sortingType } from "../../types";
 import { useAppContext } from "../Context/AppProvider";
+import Select from "../Select/Select";
 import S from "./ItemCardFilterSort.module.css";
+
+const filterOptions = [
+	{ value: "", label: "전체" },
+	{ value: "식료품", label: "식료품" },
+	{ value: "패션잡화", label: "패션잡화" },
+];
+
+const sortOptions = [
+	{ value: "asc", label: "낮은 가격순" },
+	{ value: "desc", label: "높은 가격순" },
+];
 
 const ItemCardFilterSort = () => {
 	const { filter, sort, setFilter, setSort } = useAppContext();
 
 	return (
 		<div className={S.dropdownContainer}>
-			<select value={filter} onChange={(e) => setFilter(e.target.value as filterType)}>
-				<option value="">전체</option>
-				<option value="식료품">식료품</option>
-				<option value="패션잡화">패션잡화</option>
-			</select>
-			<select value={sort} onChange={(e) => setSort(e.target.value as sortingType)}>
-				<option value="asc">낮은 가격순</option>
-				<option value="desc">높은 가격순</option>
-			</select>
+			<Select value={filter} options={filterOptions} onChange={(value) => setFilter(value as filterType)} />
+			<Select value={sort} options={sortOptions} onChange={(value) => setSort(value as sortingType)} />
 		</div>
 	);
 };
