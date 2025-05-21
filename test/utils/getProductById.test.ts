6db82ec967cf3e9e60e42017b16582b4a14fea24
Minje Sung <minje813@dgu.ac.kr>
2025-05-21 20:02:00 +0900

    test: Product에 quantity 추가에 따른 테스트 코드 변경

--- test/utils/getProductById.test.ts
+++ test/utils/getProductById.test.ts
@@ -4,14 +4,14 @@ import getProductById from '../../src/utils/getProductsById';
 
 describe('getProductById 유틸 함수 테스트', () => {
   const products: Product[] = [
-    { id: 1, name: '첫째', price: 7000, imageUrl: '', category: '식료품' },
-    { id: 2, name: '둘째', price: 3000, imageUrl: '', category: '식료품' },
-    { id: 3, name: '셋째', price: 5000, imageUrl: '', category: '식료품' },
+    { id: 1, name: '첫째', price: 7000, imageUrl: '', category: '식료품', quantity: 1 },
+    { id: 2, name: '둘째', price: 3000, imageUrl: '', category: '식료품', quantity: 5 },
+    { id: 3, name: '셋째', price: 5000, imageUrl: '', category: '식료품', quantity: 10 },
   ];
   it.each([
-    [1, { id: 1, name: '첫째', price: 7000, imageUrl: '', category: '식료품' }],
-    [2, { id: 2, name: '둘째', price: 3000, imageUrl: '', category: '식료품' }],
-    [3, { id: 3, name: '셋째', price: 5000, imageUrl: '', category: '식료품' }],
+    [1, { id: 1, name: '첫째', price: 7000, imageUrl: '', category: '식료품', quantity: 1 }],
+    [2, { id: 2, name: '둘째', price: 3000, imageUrl: '', category: '식료품', quantity: 5 }],
+    [3, { id: 3, name: '셋째', price: 5000, imageUrl: '', category: '식료품', quantity: 10 }],
   ])('상품 목록과 ID(%s)를 주면 해당하는 ID를 가지고 있는 상품 배열을 반환한다.', (id, result) => {
     expect(getProductById(products, id)).toEqual([result]);
   });
