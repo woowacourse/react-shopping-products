bf4fb3fb2703767fd7dd1ba036d8d1b5c0efaea7
dev-dino22 <wlsskdud02@naver.com>
2025-05-22 01:10:21 +0900

    refactor: MobileLayout 도 theme 적용 받도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import MobileLayout from './components/common/MobileLayout';
+import { APIDataProvider } from './context/APIDataProvider';
 import reset from './global/style/reset';
 import { theme } from './global/style/theme';
 import ShopPage from './pages/shop/ShopPage';
-import CartProvider from './context/CartProvider';
-import ShopErrorProvider from './shop/context/ShopErrorProvider';
 
 function App() {
   return (
-    <MobileLayout>
+    <>
       <Global styles={reset} />
       <ThemeProvider theme={theme}>
-        <ShopErrorProvider>
-          <CartProvider>
+        <MobileLayout>
+          <APIDataProvider>
             <ShopPage />
-          </CartProvider>
-        </ShopErrorProvider>
+          </APIDataProvider>
+        </MobileLayout>
       </ThemeProvider>
-    </MobileLayout>
+    </>
   );
 }
 
