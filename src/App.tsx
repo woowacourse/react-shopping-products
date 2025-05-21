8b04903bbd8b0367237bf49706f41117821e9270
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-21 17:22:39 +0900

    feat: MSW 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import MobileLayout from './components/common/MobileLayout';
+import CartProvider from './context/CartProvider';
 import reset from './global/style/reset';
 import { theme } from './global/style/theme';
 import ShopPage from './pages/shop/ShopPage';
-import CartProvider from './context/CartProvider';
 import ShopErrorProvider from './shop/context/ShopErrorProvider';
 
 function App() {
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,61 @@
+// src/mocks/handlers.js
+import { http, HttpResponse } from 'msw';
+
+const baseURL = import.meta.env.VITE_BASE_URL;
+
+export const handlers = [
+  http.get(`${baseURL}/products`, () => {
+    return HttpResponse.json({
+      content: [
+        {
+          id: '26',
+          name: '기세',
+          price: 100,
+          imageUrl: '33',
+          category: '식료품',
+        },
+        {
+          id: '42',
+          name: '프린세스 미용놀이',
+          price: 1010,
+          imageUrl:
+            'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202204/19/ed8eddd4-0edd-40ad-af7d-44a171577c92.jpg',
+          category: '패션잡화',
+        },
+        {
+          id: '34',
+          name: '코카콜라 제로 1.5L',
+          price: 2100,
+          imageUrl:
+            'https://sitem.ssgcdn.com/88/19/87/item/0000006871988_i1_750.jpg',
+          category: '식료품',
+        },
+      ],
+      pageable: {
+        pageNumber: 0,
+        pageSize: 20,
+        sort: {
+          empty: false,
+          sorted: true,
+          unsorted: false,
+        },
+        offset: 0,
+        paged: true,
+        unpaged: false,
+      },
+      last: false,
+      totalElements: 3,
+      totalPages: 1,
+      size: 20,
+      number: 0,
+      sort: {
+        empty: false,
+        sorted: true,
+        unsorted: false,
+      },
+      first: true,
+      numberOfElements: 20,
+      empty: false,
+    });
+  }),
+];
