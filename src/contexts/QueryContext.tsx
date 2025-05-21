aa2c0d04885416118383e48015b85f91f6b0f0b0
spoyodevelop <kjy9505@gmail.com>
2025-05-22 00:01:43 +0900

    feat: QueryContext 및 useQueryContext 훅 추가하여 데이터 관리 기능 구현

---
+++ src/contexts/QueryContext.tsx
@@ -0,0 +1,61 @@
+import {
+  createContext,
+  useContext,
+  useRef,
+  useState,
+  ReactNode,
+  useMemo,
+  useCallback,
+} from "react";
+import { OrderByOptionType } from "../types/categoryOption";
+import type { DataKey, DataPoolMap } from "../types/data-types";
+
+type DataPool = Partial<DataPoolMap>;
+
+interface QueryCtx {
+  dataPool: DataPool;
+  setData: <K extends DataKey>(k: K, v: DataPoolMap[K]) => void;
+  controllers: React.MutableRefObject<Record<string, AbortController | null>>;
+  productsQuery: OrderByOptionType;
+  setProductsQuery: (q: OrderByOptionType) => void;
+}
+
+const QueryContext = createContext<QueryCtx | undefined>(undefined);
+
+export const QueryContextProvider = ({ children }: { children: ReactNode }) => {
+  const [dataPool, setDataPool] = useState<DataPool>({});
+
+  const controllers = useRef<Record<string, AbortController | null>>({});
+
+  const [productsQuery, _setProductsQuery] =
+    useState<OrderByOptionType>("낮은 가격순");
+
+  const setData = useCallback(
+    <K extends DataKey>(key: K, value: DataPoolMap[K]) =>
+      setDataPool((prev) => ({ ...prev, [key]: value })),
+    []
+  );
+  const setProductsQuery = useCallback(
+    (query: OrderByOptionType) => {
+      _setProductsQuery(query);
+    },
+    [_setProductsQuery]
+  );
+
+  const value = useMemo(
+    () => ({ dataPool, setData, controllers, productsQuery, setProductsQuery }),
+    [dataPool, setData, controllers, productsQuery, setProductsQuery]
+  );
+
+  return (
+    <QueryContext.Provider value={{ ...value }}>
+      {children}
+    </QueryContext.Provider>
+  );
+};
+
+export const useQueryContext = () => {
+  const ctx = useContext(QueryContext);
+  if (!ctx) throw new Error("useQueryContext must be inside QueryProvider");
+  return ctx;
+};
