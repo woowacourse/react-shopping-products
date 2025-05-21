22f173db18e17c23a0d00620c9bd88ebceda8efd
AHHYUNJU <wndkgus0225@naver.com>
2025-05-21 15:52:44 +0900

    fix: cart에 지정했던 반환타입 제거

--- src/domain/cart/cart.tsx
+++ src/domain/cart/cart.tsx
@@ -1,15 +1,9 @@
 import { CartItemTypes } from "../../types/CartItemType";
 
-export function findIsCartItem(
-  items: CartItemTypes[],
-  productId: number
-): CartItemTypes | undefined {
+export function findIsCartItem(items: CartItemTypes[], productId: number) {
   return items.find((item) => item.product.id === productId);
 }
 
-export function isCartFull(
-  cartItems: CartItemTypes[],
-  maxCount: number = 50
-): boolean {
+export function isCartFull(cartItems: CartItemTypes[], maxCount: number = 50) {
   return cartItems.length >= maxCount;
 }
