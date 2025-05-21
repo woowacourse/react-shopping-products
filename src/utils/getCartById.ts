1f2a9ecebac2bf845dce27f2457a65d71db6f8d6
Minje Sung <minje813@dgu.ac.kr>
2025-05-21 20:05:45 +0900

    remove: 미사용 유틸 함수 제거

--- src/utils/getCartById.ts
+++
@@ -1,7 +0,0 @@
-import { CartProduct } from '../types';
-
-const getCartById = (carts: CartProduct[], id: number) => {
-  return carts.filter((cart) => cart.id === id);
-};
-
-export default getCartById;
--- src/utils/getProductsById.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from '../types';
-
-const getProductsById = (products: Product[], id: number) => {
-  return products.filter((product) => product.id === id);
-};
-
-export default getProductsById;
