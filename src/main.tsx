6b4e8108d960a2aef278425d0addb2cfb7065469
guesung <gueit214@naver.com>
2025-05-21 10:53:02 +0900

    feat: MSW 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/reset.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") return;
+
+  const { worker } = await import("./mocks/browser");
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
