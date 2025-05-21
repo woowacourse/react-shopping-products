c61abfcce7dac07734466c47c7705dd06d736ee8
aydenote <dslgpgh@gmail.com>
2025-05-21 19:59:19 +0900

    test: useProducts 테스트 개선 및 필터링, 정렬 로직 추가

--- test/hooks/useProducts.test.ts
+++ test/hooks/useProducts.test.ts
@@ -1,23 +1,23 @@
 import { renderHook, waitFor, act } from "@testing-library/react";
+import { server } from "../../src/mocks/server";
+import { http, HttpResponse } from "msw";
+import { PRODUCT_URL } from "../../src/constants/endpoint";
 import useProducts from "../../src/hooks/useProducts";
 import products from "../../src/mocks/data/products.json";
 import sortProductList from "../utils/sortProductList";
 import filterProductList from "../utils/filterProductList";
-import { server } from "../../src/mocks/server";
-import { http, HttpResponse } from "msw";
-import { PRODUCT_URL } from "../../src/constants/endpoint";
 
 describe("useProducts", () => {
 	describe("상품 목록 조회", () => {
 		it("가장 앞 상품 목록을 조회한다.", async () => {
-			const { result } = renderHook(() => useProducts({}));
+			const { result } = renderHook(() => useProducts());
 
 			await waitFor(() => {
 				expect(result.current.products).toHaveLength(products.length);
 			});
 		});
 
-		it("가장 앞 상품 목록을 식료품으로 필터링하여 조회한다.", async () => {
+		it("상품 목록을 '식료품'으로 필터링하여 조회한다.", async () => {
 			const FILTER_TYPE = "식료품";
 			const filteredProducts = filterProductList(products, FILTER_TYPE);
 
@@ -34,15 +34,18 @@ describe("useProducts", () => {
 				})
 			);
 
-			const { result } = renderHook(() => useProducts({}));
+			const { result, rerender } = renderHook(() => useProducts());
 
+			// 필터 적용
 			act(() => {
 				result.current.setFilter(FILTER_TYPE);
 			});
 
+			// 상태 변경 후 rerender 필요
+			rerender();
+
 			await waitFor(() => {
-				const filteredProducts = result.current.products.every((product) => product.category === FILTER_TYPE);
-				expect(filteredProducts).toBe(true);
+				expect(result.current.products.every((p) => p.category === FILTER_TYPE)).toBe(true);
 			});
 		});
 
