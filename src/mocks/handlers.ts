034b78d4b68cad83114dcf41ae32c45e05e0d0df
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-21 16:44:11 +0900

    refactor: GET production handler 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,30 @@
-import { HttpResponse, http } from 'msw';
-import dummy from './dummy.json';
+import { http, HttpResponse } from 'msw';
+import products from './products.json';
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const handlers = [
-  http.get('/api/user', () => {
-    return HttpResponse.json(dummy);
+  http.get(`${BASE_URL}/products`, ({ request }) => {
+    const url = new URL(request.url);
+    const sortParam = url.searchParams.get('sort');
+
+    const sorted = [...products];
+
+    if (sortParam) {
+      const [key, direction] = sortParam.split(',');
+
+      sorted.sort((a, b) => {
+        const isAsc = direction === 'asc';
+        if (key === 'price') {
+          return isAsc ? a.price - b.price : b.price - a.price;
+        }
+        if (key === 'name') {
+          return isAsc ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+        }
+        return 0;
+      });
+    }
+
+    return HttpResponse.json({ content: sorted });
   }),
 ];
