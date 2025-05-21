19a8db31d1fe27ca85b10320b6094693f2acb2f1
minji2219 <aabb0219@naver.com>
2025-05-21 13:40:48 +0900

    feat: 모달 컴포넌트 구현

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -1,6 +1,8 @@
 import ErrorToast from '../../../shared/ui/ErrorToast';
 import * as S from './Navbar.styles';
 import {useErrorToast} from '../../../shared/provider/errorProvider';
+import {useState} from 'react';
+import Modal from '../../../shared/ui/Modal';
 
 interface NavbarProps {
   cartQuantity: number;
@@ -8,16 +10,23 @@ interface NavbarProps {
 
 export default function Navbar({cartQuantity}: NavbarProps) {
   const error = useErrorToast();
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <S.NavbarWrapper>
       <S.NavbarContainer>
         <S.Logo>SHOP</S.Logo>
-        <S.CartIconContainer>
+        <S.CartIconContainer onClick={() => setIsOpen(true)}>
           <S.CartQuantity>{cartQuantity}</S.CartQuantity>
           <S.CartIcon src="./cartIcon.svg" />
         </S.CartIconContainer>
       </S.NavbarContainer>
       {error && <ErrorToast errorMessage={error} />}
+      <Modal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        position="bottom"
+        title={{text: '장바구니', size: 20}}
+      />
     </S.NavbarWrapper>
   );
 }
