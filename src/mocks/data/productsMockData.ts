e39e93840745a2d053e3918f6cba2102dba3e3e0
Minje Sung <minje813@dgu.ac.kr>
2025-05-21 16:35:11 +0900

    chore: MSW products에 quantity 추가

--- src/mocks/data/productsMockData.ts
+++ src/mocks/data/productsMockData.ts
@@ -7,6 +7,7 @@ const productsMockData = {
       imageUrl:
         'https://kream-phinf.pstatic.net/MjAyNTA1MTNfMjI5/MDAxNzQ3MTA4MjUzOTg4.106G0-WfVU8g8ziNKgKJjc1_UXvF-2IatsA-Cz5mG1og.etXRFVPYqcs5J9HAfXpaHFPFHorGnZU4Nl7k4368rfog.PNG/a_090d2310040b4f9ca922f2498ae8ae3a.png?type=l',
       category: '패션잡화',
+      quantity: 4,
     },
     {
       id: 2,
@@ -15,6 +16,7 @@ const productsMockData = {
       imageUrl:
         'https://kream-phinf.pstatic.net/MjAyNTA1MTNfMjI5/MDAxNzQ3MTA4MjUzOTg4.106G0-WfVU8g8ziNKgKJjc1_UXvF-2IatsA-Cz5mG1og.etXRFVPYqcs5J9HAfXpaHFPFHorGnZU4Nl7k4368rfog.PNG/a_090d2310040b4f9ca922f2498ae8ae3a.png?type=l',
       category: '패션잡화',
+      quantity: 4,
     },
     {
       id: 3,
@@ -23,6 +25,7 @@ const productsMockData = {
       imageUrl:
         'https://kream-phinf.pstatic.net/MjAyNTA1MTNfMjI5/MDAxNzQ3MTA4MjUzOTg4.106G0-WfVU8g8ziNKgKJjc1_UXvF-2IatsA-Cz5mG1og.etXRFVPYqcs5J9HAfXpaHFPFHorGnZU4Nl7k4368rfog.PNG/a_090d2310040b4f9ca922f2498ae8ae3a.png?type=l',
       category: '패션잡화',
+      quantity: 4,
     },
     {
       id: 4,
@@ -31,6 +34,7 @@ const productsMockData = {
       imageUrl:
         'https://thumbnail6.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/794f/cecbea5bdc654a11ae02d28b4d1f4bd2a03a7389eb2b8cc4a45c1c9f7d9b.jpg',
       category: '패션잡화',
+      quantity: 2,
     },
     {
       id: 5,
@@ -39,6 +43,7 @@ const productsMockData = {
       imageUrl:
         'https://m.cocosocks.com/web/product/medium/202503/940897aced51144109baa4d145def01f.jpg',
       category: '패션잡화',
+      quantity: 10,
     },
     {
       id: 6,
@@ -47,6 +52,7 @@ const productsMockData = {
       imageUrl:
         'https://velog.velcdn.com/images/minsungje/post/c27c57cb-fcbb-4641-b72d-0e2030739ae7/image.jpg',
       category: '식료품',
+      quantity: 1,
     },
     {
       id: 7,
@@ -55,6 +61,7 @@ const productsMockData = {
       imageUrl:
         'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb5H3cg%2FbtsMRVqcfYF%2FvbKfazkKNY7I5CGkF1Ye9k%2Fimg.png',
       category: '식료품',
+      quantity: 1,
     },
     {
       id: 8,
@@ -62,6 +69,7 @@ const productsMockData = {
       price: 50000,
       imageUrl: 'https://media.bunjang.co.kr/product/223522208_%7Bcnt%7D_1683581287_w%7Bres%7D.jpg',
       category: '패션잡화',
+      quantity: 1,
     },
     {
       id: 9,
@@ -69,6 +77,7 @@ const productsMockData = {
       price: 200000,
       imageUrl: 'https://blog.kakaocdn.net/dn/qCz9R/btrmYEn7tZV/Uxh60wpS69qCFymU4WKOy0/img.jpg',
       category: '패션잡화',
+      quantity: 0,
     },
     {
       id: 22,
@@ -77,6 +86,7 @@ const productsMockData = {
       imageUrl:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZeoCnBP_VbQ4pLozKbZOIu6B0A9FB3gaeQA&s',
       category: '패션잡화',
+      quantity: 15,
     },
     {
       id: 23,
@@ -85,6 +95,7 @@ const productsMockData = {
       imageUrl:
         'https://image.zeta-ai.io/profile-image/793bf4d3-03de-4ac3-afe1-95be8a9bc62c/29cd5c72-f872-4dba-8be1-21ba51e4487f.jpeg?w=1080&q=90&f=webp',
       category: '패션잡화',
+      quantity: 1,
     },
     {
       id: 24,
@@ -93,6 +104,7 @@ const productsMockData = {
       imageUrl:
         'https://cafe24.poxo.com/ec01/dmswo9075/HOvhRhvOk+Cp2KY4JuusAqBst4wtnsfbyXcejHyxMmXKvNELh5kEAFzUfK9ehG6ogDMwTwYJTLHHXeYVBq809g==/_/web/product/big/202408/19deee5e9d060d80a4180e2b2ecb6ce8.jpg',
       category: '패션잡화',
+      quantity: 3,
     },
     {
       id: 25,
@@ -101,6 +113,7 @@ const productsMockData = {
       imageUrl:
         'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20171018_6%2F1508253136417Dlrjh_PNG%2FCdq22zpVpr92_XHROlHbxjJ0.png&type=sc960_832',
       category: '식료품',
+      quantity: 100,
     },
     {
       id: 26,
@@ -108,6 +121,7 @@ const productsMockData = {
       price: 100,
       imageUrl: '33',
       category: '식료품',
+      quantity: 0,
     },
     {
       id: 27,
@@ -116,6 +130,7 @@ const productsMockData = {
       imageUrl:
         'https://image.ohousecdn.com/i/bucketplace-v2-development/uploads/cards/snapshots/171653801239329270.jpeg?w=256&h=366&c=c',
       category: '식료품',
+      quantity: 30,
     },
     {
       id: 28,
@@ -124,6 +139,7 @@ const productsMockData = {
       imageUrl:
         'https://d2afncas1tel3t.cloudfront.net/wp-content/uploads/2023/12/%EC%95%84%EC%83%B7%EC%B6%94%EC%95%84%EC%9D%B4%EC%8A%A4%ED%8B%B0%EC%83%B7%EC%B6%94%EC%B9%B4%EB%94%94%EC%B9%B4%ED%8E%98%EC%9D%B8_1.png',
       category: '식료품',
+      quantity: 20,
     },
     {
       id: 29,
@@ -132,6 +148,7 @@ const productsMockData = {
       imageUrl:
         'https://i.namu.wiki/i/kQCwKHpwjePBTPXPTIizJSE0alohKKRlsGOJSrPhAdsODckkF05KNDV27xdydVqHLEdgM7yQu6NSUL-gE0t9SZH_cmaY8tMquJnfLQv5shH_pSdvsRc87hCcO5V3WBZrTwR23NYzoJJEoQIHWqAM4Q.webp',
       category: '패션잡화',
+      quantity: 10,
     },
     {
       id: 30,
@@ -140,6 +157,7 @@ const productsMockData = {
       imageUrl:
         'https://www.esquirekorea.co.kr/resources_old/online/org_thumnail_image/eq/322f1c2e-fdd0-4b84-97ec-cfc8ee88c9d7.jpg',
       category: '식료품',
+      quantity: 1,
     },
     {
       id: 31,
@@ -148,6 +166,7 @@ const productsMockData = {
       imageUrl:
         'https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/218/20dddd283cca0cb01c9ac7285f20b704_res.jpeg',
       category: '식료품',
+      quantity: 0,
     },
     {
       id: 32,
@@ -156,6 +175,7 @@ const productsMockData = {
       imageUrl:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1bvoBgTtG0L-FBnZBsCOl5O-WcelpPH24IQ&s',
       category: '패션 잡화',
+      quantity: 2,
     },
   ],
 };
