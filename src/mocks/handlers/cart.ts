5a20bad7c5aab597705c19c76627f4f0058e7161
Jeongeun Lee <leeju1009@gmail.com>
2025-05-22 01:19:56 +0900

    refactor: 장바구니에 아이템을 추가하는 모킹 api가 모킹 데이터를 업데이트하도록 수정

--- src/mocks/handlers/cart.ts
+++ src/mocks/handlers/cart.ts
@@ -8,22 +8,32 @@ interface AddCartItemsParams {
 
 const handlers = [
   http.post<never, AddCartItemsProps>('/cart-items', async ({ request }) => {
-    const { productId, quantity } = await request.json();
-    const targetProduct = MOCK_PRODUCTS.find((product) => product.id === productId);
-
-    if (!targetProduct) {
+    const { productId, quantity: addQuantity } = await request.json();
+    const targetIndex = MOCK_PRODUCTS.findIndex((product) => product.id === productId);
+    if (targetIndex === -1) {
       return HttpResponse.json(
         { errorCode: 'NOT_FOUND', message: '상품을 찾을 수 없습니다. 상품 id를 확인해주세요.' },
         { status: 404 },
       );
     }
 
-    if (quantity > targetProduct.quantity) {
+    const targetProduct = MOCK_PRODUCTS[targetIndex];
+    if (addQuantity > targetProduct.quantity) {
       return HttpResponse.json(
         { errorCode: 'OUT_OF_STOCK', message: '재고 수량을 초과하여 담을 수 없습니다.' },
         { status: 400 },
       );
     }
+
+    MOCK_CART_ITEMS.push({
+      id: MOCK_CART_ITEMS.length + 1,
+      quantity: addQuantity,
+      product: targetProduct,
+    });
+    MOCK_PRODUCTS[targetIndex] = {
+      ...targetProduct,
+      quantity: targetProduct.quantity - addQuantity,
+    };
 
     return HttpResponse.json({ message: '장바구니 담기에 성공했습니다.' }, { status: 201 });
   }),
