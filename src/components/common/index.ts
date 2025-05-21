32c77848770a6333a3d6fdc972b895bbb57d44da
dev-dino22 <wlsskdud02@naver.com>
2025-05-21 23:44:54 +0900

    fix: 삭제된 ErrorToast 파일 임포트 삭제

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,6 +1,5 @@
 export { default as Arrow } from './Arrow';
 export { default as Dropdown } from './Dropdown';
-export { default as ErrorToastMessage } from './ErrorToastMessage';
 export { default as Flex } from './Flex';
 export { default as Header } from './Header';
 export { default as Loading } from './Loading';
