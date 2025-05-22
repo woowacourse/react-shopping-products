40c9f0671bc25975457d7fb5ecb09ae17c4dccf3
keemsebeen <sebin0580@gmail.com>
2025-05-22 19:15:30 +0900

    chore: mocking에 사용할 데이터 구조화

---
+++ src/shared/mocks/handlers/cart.data.ts
@@ -0,0 +1,30 @@
+import { CartResponse } from '@/features/ProductList/types/Cart';
+
+export const cartData: CartResponse = {
+  content: [],
+  pageable: {
+    pageNumber: 0,
+    pageSize: 50,
+    sort: {
+      empty: true,
+      sorted: false,
+      unsorted: true,
+    },
+    offset: 0,
+    paged: true,
+    unpaged: false,
+  },
+  last: true,
+  totalElements: 8,
+  totalPages: 1,
+  size: 50,
+  number: 0,
+  sort: {
+    empty: true,
+    sorted: false,
+    unsorted: true,
+  },
+  first: true,
+  numberOfElements: 8,
+  empty: false,
+};
