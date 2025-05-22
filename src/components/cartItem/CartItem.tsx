8fd148263e9bada8d7432c171ffb8a02291c89c4
hanheel <hanhee630630@gmail.com>
2025-05-22 17:18:19 +0900

    refactor : cartItem 스타일 수정

--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -1,8 +1,7 @@
-import styled from '@emotion/styled';
+import * as S from './CardItem.styles';
 import CounterControl from '../common/counterControl/CounterControl';
 import type { CartItemType } from '../../types/data';
 import useCartCount from '../../hooks/useCartCount';
-import useErrorMessageContext from '../../hooks/useErrorMessageContext';
 
 interface CartItemProps {
   cartItem: CartItemType;
@@ -26,52 +25,20 @@ const CartItem = ({
   });
 
   return (
-    <CartItemContainer>
-      <CartItemImage src="./default-product.png" />
-      <CartItemInfoContainer>
-        <CartItemName>{cartItem.product.name}</CartItemName>
-        <CartItemPrice>{cartItem.product.price}</CartItemPrice>
+    <S.CartItemContainer>
+      <S.CartItemImage src="./default-product.png" />
+      <S.CartItemInfoContainer>
+        <S.CartItemName>{cartItem.product.name}</S.CartItemName>
+        <S.CartItemPrice>{cartItem.product.price}</S.CartItemPrice>
         <CounterControl
           count={cartItem.quantity}
           maxCount={cartItem.product.quantity}
           handlePlusCount={handlePlusCount}
           handleMinusCount={handleMinusCount}
         />
-      </CartItemInfoContainer>
-    </CartItemContainer>
+      </S.CartItemInfoContainer>
+    </S.CartItemContainer>
   );
 };
-
-const CartItemContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 16px;
-  border-top: 1px solid var(--color-light-grey);
-  border-bottom: 1px solid var(--color-light-grey);
-  padding: 8px 0;
-`;
-
-const CartItemImage = styled.img`
-  width: 80px;
-  height: 80px;
-`;
-
-const CartItemInfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  align-items: start;
-  width: 100%;
-`;
-
-const CartItemName = styled.span`
-  font-size: var(--font-size-subtitle);
-  font-weight: var(--font-weight-subtitle);
-`;
-
-const CartItemPrice = styled.span`
-  font-size: var(--font-size-body);
-  font-weight: var(--font-weight-body);
-`;
 
 export default CartItem;
