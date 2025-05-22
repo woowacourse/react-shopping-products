a293fb430abfd94740d853ec2831ab40bbacee06
Jeongeun Lee <leeju1009@gmail.com>
2025-05-22 21:46:16 +0900

    feat: 기존 상태 데이터를 수정하는 핸들러 추가

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,37 +1,55 @@
 import { useCallback, useState } from 'react';
 
+type UpdaterType<T> = (prev: T | undefined) => T;
 interface useFetchDataProps<T> {
   apiCall: () => Promise<T>;
   dataName: string;
-  onSuccess: (data: T, updateHandler: (name: string, value: T) => void) => void;
+  onSuccess: (data: T, dataHandler: (value: T) => void) => void;
   onError: (error: Error | unknown) => void;
 }
 
 const useFetchData = <T>({ apiCall, dataName, onSuccess, onError }: useFetchDataProps<T>) => {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState<Map<string, T>>(new Map());
+  const replaceHandler = useCallback(
+    (newData: T) => {
+      setData((prev) => new Map(prev).set(dataName, newData));
+    },
+    [dataName],
+  );
 
-  const updateHandler = useCallback((name: string, newData: T) => {
-    setData((prev) => new Map(prev).set(name, newData));
-  }, []);
+  const updateHandler = useCallback(
+    (updater: UpdaterType<T>) => {
+      setData((prev) => {
+        const prevData = prev.get(dataName);
+        const newData = updater(prevData);
+        return new Map(prev).set(dataName, newData);
+      });
+    },
+    [dataName],
+  );
 
-  const fetchData = useCallback(async () => {
-    try {
-      setIsLoading(true);
+  const fetchData = useCallback(
+    async (dataHandler = replaceHandler) => {
+      try {
+        setIsLoading(true);
 
-      const fetchedData = await apiCall();
-      if (fetchedData) {
-        onSuccess(fetchedData, updateHandler);
+        const fetchedData = await apiCall();
+        if (fetchedData && onSuccess) {
+          onSuccess(fetchedData, dataHandler);
+        }
+      } catch (error) {
+        onError(error);
+      } finally {
+        setIsLoading(false);
       }
-    } catch (error) {
-      onError(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [apiCall, dataName, onSuccess, onError]);
+    },
+    [apiCall, onSuccess, onError],
+  );
 
   return {
     data,
+    replaceHandler,
     updateHandler,
     isLoading,
     fetchData,
