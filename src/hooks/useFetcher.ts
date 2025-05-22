a20ab4deb97e7fce20ba2353fae51b71a55dd263
Jeongeun Lee <leeju1009@gmail.com>
2025-05-22 15:40:58 +0900

    refactor: 커스텀 훅 내부에서 비동기 api 통신 함수를 선언하고 반환하도록 수정
    - 훅은 사이드 이펙트가 없고 렌더링마다 호출되기 때문에 동기 함수여야 하므로, 비동기 키워드를 제거함

--- src/hooks/useFetcher.ts
+++ src/hooks/useFetcher.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 interface useFetcherProps<T> {
   apiCall: () => Promise<T>;
@@ -6,26 +6,30 @@ interface useFetcherProps<T> {
   onError: (error: Error | unknown) => void;
 }
 
-const useFetcher = async <T>({ apiCall, dataName, onError }: useFetcherProps<T>) => {
+const useFetcher = <T>({ apiCall, dataName, onError }: useFetcherProps<T>) => {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState<Map<string, T>>(new Map());
 
-  try {
-    setIsLoading(true);
-    const fetchedData = await apiCall();
-    if (fetchedData) {
-      setData((prev) => prev.set(dataName, fetchedData));
+  const fetchData = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      const fetchedData = await apiCall();
+      if (fetchedData) {
+        setData((prev) => prev.set(dataName, fetchedData));
+      }
+    } catch (error) {
+      onError(error);
+    } finally {
+      setIsLoading(false);
     }
-  } catch (error) {
-    onError(error);
-  } finally {
-    setIsLoading(false);
-  }
+  }, [apiCall, dataName, onError]);
 
   return {
     data,
     setData,
     isLoading,
+    fetchData,
   };
 };
 
