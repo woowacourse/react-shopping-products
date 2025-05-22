93cfc8d4b4bc84842b83ca665cccdd07fa2032e5
Jeongeun Lee <leeju1009@gmail.com>
2025-05-22 17:20:03 +0900

    refactor: 데이터 패칭 성공 시 수행될 onSuccess 콜백을 인자로 받도록 useFetcher 수정

--- src/hooks/useFetcher.ts
+++ src/hooks/useFetcher.ts
@@ -3,12 +3,17 @@ import { useCallback, useState } from 'react';
 interface useFetcherProps<T> {
   apiCall: () => Promise<T>;
   dataName: string;
+  onSuccess: (data: T, updateHandler: (name: string, value: T) => void) => void;
   onError: (error: Error | unknown) => void;
 }
 
-const useFetcher = <T>({ apiCall, dataName, onError }: useFetcherProps<T>) => {
+const useFetcher = <T>({ apiCall, dataName, onSuccess, onError }: useFetcherProps<T>) => {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState<Map<string, T>>(new Map());
+
+  const updateHandler = useCallback((name: string, newData: T) => {
+    setData((prev) => new Map(prev).set(name, newData));
+  }, []);
 
   const fetchData = useCallback(async () => {
     try {
@@ -16,18 +21,18 @@ const useFetcher = <T>({ apiCall, dataName, onError }: useFetcherProps<T>) => {
 
       const fetchedData = await apiCall();
       if (fetchedData) {
-        setData((prev) => prev.set(dataName, fetchedData));
+        onSuccess(fetchedData, updateHandler);
       }
     } catch (error) {
       onError(error);
     } finally {
       setIsLoading(false);
     }
-  }, [apiCall, dataName, onError]);
+  }, [apiCall, dataName, onSuccess, onError]);
 
   return {
     data,
-    setData,
+    updateHandler,
     isLoading,
     fetchData,
   };
