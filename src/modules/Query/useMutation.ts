cca843bca1d6c2eee45cb00e897353ab489c6b1c
guesung <gueit214@naver.com>
2025-05-22 17:42:47 +0900

    feat: `useMutation` 훅

---
+++ src/modules/Query/useMutation.ts
@@ -0,0 +1,26 @@
+import { useQueryClient } from "./QueryProvider";
+
+interface UseMutationProps<T, V> {
+  mutationFn: (variables: V) => Promise<T>;
+  queryKey: string;
+}
+
+export default function useMutation<T, V>({ mutationFn, queryKey }: UseMutationProps<T, V>) {
+  const { getQueryData, setQueryData, setQueryStatus } = useQueryClient();
+
+  const mutate = async (variables: V, optimisticUpdate?: (prev: T) => T) => {
+    const prevData = getQueryData(queryKey) as T;
+
+    if (optimisticUpdate) {
+      setQueryData(queryKey, optimisticUpdate(prevData));
+      setQueryStatus(queryKey, "success");
+
+    } else {
+      const response = await mutationFn(variables);
+      setQueryData(queryKey, response);
+      setQueryStatus(queryKey, "success");
+    }
+  };
+
+  return { mutate };
+}
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,3 +1,4 @@
 export { default as QueryProvider } from "./Query/QueryProvider";
 export { default as useQuery } from "./Query/useQuery";
+export { default as useMutation } from "./Query/useMutation";
 export * from "./Query/QueryPromises";
