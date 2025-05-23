962c05364091626a230143000ceb2fdb709645bc
Jeongeun Lee <leeju1009@gmail.com>
2025-05-23 15:15:07 +0900

    refactor: cart, product 핸들러들이 useFetchData 함수를 활용하도록 수정

--- src/hooks/useCartHandler.ts
+++ src/hooks/useCartHandler.ts
@@ -1,71 +1,78 @@
-import { useEffect } from 'react';
-import { getCartId } from '../domain/cartItem';
+import { useCallback, useEffect } from 'react';
 import { addCartItems, getCartItems, removeCartItems } from '../services/cartItemServices';
 import { CartItemType } from '../types/data';
 import useFetchData from './useFetchData';
 import { DEFAULT_ERROR_MESSAGE } from '../constants/errorMessages';
+import { getCartId } from '../domain/cartItem';
 
 interface CartHandlerProps {
   handleErrorMessage: (errorMessage: string) => void;
 }
 
 const useCartHandler = ({ handleErrorMessage }: CartHandlerProps) => {
-  const { data: cartItems, fetchData: fetchCartItems } = useFetchData<CartItemType[]>({
-    apiCall: getCartItems,
+  const {
+    data: cartItems,
+    setDataMap,
+    fetchData: fetchCartItems,
+  } = useFetchData<CartItemType[]>({
     dataName: 'cartItems',
-    onSuccess: (items, dataHandler) => {
-      dataHandler(items);
-    },
-    onError: (error) => {
-      const message = error instanceof Error ? error.message : DEFAULT_ERROR_MESSAGE;
-      handleErrorMessage(message);
-    },
   });
 
+  const fetchTotalCartItems = useCallback(async () => {
+    await fetchCartItems({
+      apiCall: getCartItems,
+      onSuccess: (newData) => {
+        if (newData) {
+          setDataMap((prev) => new Map(prev).set('cartItems', newData));
+        }
+      },
+      onError: (error) => {
+        const message = error instanceof Error ? error.message : DEFAULT_ERROR_MESSAGE;
+        handleErrorMessage(message);
+      },
+    });
+  }, [fetchCartItems, handleErrorMessage, setDataMap]);
+
   useEffect(() => {
-    (async () => await fetchCartItems())();
-  }, []);
+    (async () => await fetchTotalCartItems())();
+  }, [fetchTotalCartItems]);
 
-  const handleAddCartItems = (id: number, quantity: number) => {
-    const addItemInfo = {
-      productId: id,
-      quantity,
-    };
+  const handleAddCartItems = useCallback(
+    async (productId: number, quantity: number) => {
+      await fetchCartItems({
+        apiCall: () => addCartItems({ productId, quantity }),
+        onSuccess: () => {
+          fetchTotalCartItems();
+        },
+        onError: (error) => {
+          const message = error instanceof Error ? error.message : DEFAULT_ERROR_MESSAGE;
+          handleErrorMessage(message);
+        },
+      });
+    },
+    [fetchCartItems, fetchTotalCartItems, handleErrorMessage],
+  );
 
-    const { fetchData: fetchAddCartItems } = useFetchData<void>({
-      apiCall: () => addCartItems(addItemInfo),
-      dataName: 'cartItems',
-      onSuccess: () => {
-        fetchCartItems();
-      },
-      onError: (error) => {
-        const message = error instanceof Error ? error.message : DEFAULT_ERROR_MESSAGE;
-        handleErrorMessage(message);
-      },
-    });
+  const handleRemoveCartItems = useCallback(
+    async (id: number) => {
+      const cartId = getCartId(cartItems, id) as number;
 
-    fetchAddCartItems();
-  };
-
-  const handleRemoveCartItems = (id: number) => {
-    const cartId = getCartId(cartItems, id);
-
-    const { fetchData: fetchRemoveCartItems } = useFetchData<CartItemType[]>({
-      apiCall: () => removeCartItems(cartId),
-      dataName: 'cartItems',
-      onSuccess: () => {
-        fetchCartItems();
-      },
-      onError: (error) => {
-        const message = error instanceof Error ? error.message : DEFAULT_ERROR_MESSAGE;
-        handleErrorMessage(message);
-      },
-    });
-
-    fetchRemoveCartItems();
-  };
+      await fetchCartItems({
+        apiCall: () => removeCartItems(cartId),
+        onSuccess: () => {
+          fetchTotalCartItems();
+        },
+        onError: (error) => {
+          const message = error instanceof Error ? error.message : DEFAULT_ERROR_MESSAGE;
+          handleErrorMessage(message);
+        },
+      });
+    },
+    [fetchCartItems, cartItems, fetchTotalCartItems, handleErrorMessage],
+  );
 
   return {
+    cartItems,
     handleAddCartItems,
     handleRemoveCartItems,
   };
--- src/hooks/useProductHandler.ts
+++ src/hooks/useProductHandler.ts
@@ -1,44 +1,44 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { getProducts } from '../services/productServices';
-import tryApiCall from '../util/tryApiCall';
-import { CATEGORY_OPTIONS, SORT_OPTIONS, SELECT_SORT_OPTIONS } from '../constants/systemConstants';
+import { CATEGORY_OPTIONS, SELECT_SORT_OPTIONS, SORT_OPTIONS } from '../constants/systemConstants';
 import { CategoryType, ProductItemType, SelectedSortType } from '../types/data';
+import useFetchData from './useFetchData';
+import { DEFAULT_ERROR_MESSAGE } from '../constants/errorMessages';
 
 interface ProductListProps {
   handleErrorMessage: (errorMessage: string) => void;
 }
 
 const useProductHandler = ({ handleErrorMessage }: ProductListProps) => {
+  const {
+    data: products,
+    setDataMap,
+    fetchData: fetchProducts,
+    isLoading,
+  } = useFetchData<ProductItemType[]>({
+    dataName: 'products',
+  });
   const [categoryOption, setCategoryOption] = useState(CATEGORY_OPTIONS[0]);
   const [sortOption, setSortOption] = useState(SELECT_SORT_OPTIONS[0]);
 
-  const [products, setProducts] = useState<ProductItemType[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const isMountedRef = useRef(false);
+  const fetchTotalProducts = useCallback(async () => {
+    await fetchProducts({
+      apiCall: () => getProducts(categoryOption, SORT_OPTIONS.get(sortOption)),
+      onSuccess: (newData) => {
+        if (newData) {
+          setDataMap((prev) => new Map(prev).set('products', newData));
+        }
+      },
+      onError: (error) => {
+        const message = error instanceof Error ? error.message : DEFAULT_ERROR_MESSAGE;
+        handleErrorMessage(message);
+      },
+    });
+  }, [fetchProducts, handleErrorMessage, setDataMap, categoryOption, sortOption]);
 
   useEffect(() => {
-    isMountedRef.current = true;
-    setIsLoading(true);
-
-    (async () => {
-      try {
-        const productsData = await tryApiCall(
-          () => getProducts(categoryOption, SORT_OPTIONS.get(sortOption)),
-          handleErrorMessage,
-        );
-
-        if (productsData && isMountedRef.current) {
-          setProducts(productsData);
-        }
-      } finally {
-        setIsLoading(false);
-      }
-
-      return () => {
-        isMountedRef.current = false;
-      };
-    })();
-  }, [categoryOption, sortOption]);
+    fetchTotalProducts();
+  }, [fetchTotalProducts]);
 
   const handleCategoryOption = (value: CategoryType) => {
     setCategoryOption(value);
