fe8d42331cb07a8d9c341157fa0bac058c5abc08
spoyodevelop <kjy9505@gmail.com>
2025-05-23 18:20:12 +0900

    chore: update dev script to enable MSW and fix BASE_URL condition check

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,5 +1,5 @@
 const BASE_URL =
-  import.meta.env.VITE_APP_USE_MSW === true
+  import.meta.env.VITE_APP_USE_MSW === "true"
     ? "/api" // MSW가 활성화된 경우 상대 경로 사용
     : "http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com";
 
