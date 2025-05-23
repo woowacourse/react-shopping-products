77d6673d76a5700beda29a138f806194ece153d4
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-24 01:43:48 +0900

    feat: cart item 관련 api handler 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,4 @@
+import { CartItemContent } from '@/components/features/product/api/type';
 import { http, HttpResponse } from 'msw';
 import products from './data/mock-products.json';
 
@@ -7,15 +8,13 @@ export const handlers = [
   http.get(`${baseURL}/products`, ({ request }) => {
     const url = new URL(request.url);
     const category = url.searchParams.get('category');
-    const sort = url.searchParams.get('sort'); // e.g., "price,desc"
+    const sort = url.searchParams.get('sort');
 
-    // 1. 필터링
     let filtered = products;
     if (category) {
       filtered = filtered.filter((item) => item.category === category);
     }
 
-    // 2. 정렬
     if (sort) {
       const [, direction] = sort.split(',');
       filtered = filtered.sort((a, b) => {
@@ -55,5 +54,86 @@ export const handlers = [
       numberOfElements: 20,
       empty: false,
     });
+  }),
+  http.get(`${baseURL}/cart-items`, () => {
+    // localStorage에서 장바구니 데이터 가져오기
+    const raw = localStorage.getItem('cartItems');
+    const cartItems = raw ? JSON.parse(raw) : [];
+
+    const totalElements = cartItems.length;
+    const totalPages = Math.ceil(cartItems.length / 20);
+
+    return HttpResponse.json({
+      content: cartItems,
+      pageable: {
+        pageNumber: 0,
+        pageSize: 20,
+        sort: {
+          empty: false,
+          sorted: true,
+          unsorted: false,
+        },
+        offset: 0,
+        paged: true,
+        unpaged: false,
+      },
+      last: true,
+      totalElements,
+      totalPages,
+      size: 20,
+      number: 0,
+      sort: {
+        empty: false,
+        sorted: true,
+        unsorted: false,
+      },
+      first: true,
+      numberOfElements: 20,
+      empty: false,
+    });
+  }),
+  http.post(`${baseURL}/cart-items`, async ({ request }) => {
+    const { productId, quantity } = (await request.json()) as {
+      productId: string;
+      quantity: number;
+    };
+    const raw = localStorage.getItem('cartItems');
+    const cartItems: CartItemContent[] = raw ? JSON.parse(raw) : [];
+
+    const addingProduct = products.find(
+      (item) => item.id === Number(productId)
+    );
+    if (!addingProduct) {
+      return HttpResponse.json(
+        { message: '상품을 찾을 수 없습니다.' },
+        { status: 404 }
+      );
+    }
+
+    const newCartItems = [
+      ...cartItems,
+      {
+        id: Date.now(),
+        quantity,
+        product: addingProduct,
+      },
+    ];
+    localStorage.setItem('cartItems', JSON.stringify(newCartItems));
+
+    return HttpResponse.json(null, { status: 201 });
+  }),
+  http.delete(`${baseURL}/cart-items/:id`, ({ params }) => {
+    const { id } = params;
+    const raw = localStorage.getItem('cartItems');
+    const cartItems: CartItemContent[] = raw ? JSON.parse(raw) : [];
+
+    console.log(cartItems);
+
+    const updatedCartItems = cartItems.filter(
+      (item) => String(item.id) !== String(id)
+    );
+    localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+
+    return new HttpResponse(null, { status: 204 });
   }),
 ];
