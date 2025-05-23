dc87c97c5953bab0e664e1ede721d419d1f48a45
hoyyChoi <hoyeon8165@gmail.com>
2025-05-23 17:12:31 +0900

    feat: msw 초기 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/reset.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+  const { worker } = await import("./mocks/browser");
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
