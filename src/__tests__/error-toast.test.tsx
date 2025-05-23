60b6b3833b4facb4694daaec4d4a377c94877e4c
spoyodevelop <kjy9505@gmail.com>
2025-05-23 18:28:23 +0900

    test: 테스트 스펙 약간 변경

--- src/__tests__/error-toast.test.tsx
+++ src/__tests__/error-toast.test.tsx
@@ -46,12 +46,12 @@ describe("ErrorToast 컴포넌트 테스트", () => {
     //장바구니가 후출되므로, 이것이 표시되게 됨.
     await waitFor(() => {
       expect(
-        screen.getByText("장바구니 정보를 가져오는 중 오류가 발생했습니다.")
+        screen.getByText("제품 정보를 가져오는 중 오류가 발생했습니다.")
       ).toBeInTheDocument();
     });
   });
 
-  test("여러 개의 에러가 동시에 발생했을 때 마지막 에러 토스트가 표시되어야 함", async () => {
+  test("여러 개의 에러가 동시에 발생했을 때 첫번째 에러 토스트가 표시되어야 함", async () => {
     testStateStore.shouldFailProducts = true;
     testStateStore.shouldFailCart = true;
 
@@ -59,7 +59,7 @@ describe("ErrorToast 컴포넌트 테스트", () => {
     // 이것도 동일.
     await waitFor(() => {
       expect(
-        screen.getByText("장바구니 정보를 가져오는 중 오류가 발생했습니다.")
+        screen.getByText("제품 정보를 가져오는 중 오류가 발생했습니다.")
       ).toBeInTheDocument();
     });
   });
