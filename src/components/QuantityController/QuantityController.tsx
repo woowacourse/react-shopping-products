28d290a3109b998961a155b1b94636a5235112d2
yeji0214 <1225yj50@naver.com>
2025-05-24 17:14:41 +0900

    feat: 상품 수량 컨트롤러에 이벤트 핸들러를 달아 숫자 감소/증가 기능 구현

--- src/components/QuantityController/QuantityController.tsx
+++ src/components/QuantityController/QuantityController.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IMAGE_PATH } from "../../constants/imagePath";
 import {
   Button,
@@ -6,14 +7,23 @@ import {
 } from "./QuantityController.styled";
 
 const QuantityController = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  const increase = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  const decrease = () => {
+    if (quantity > 1) setQuantity((prev) => prev - 1);
+  };
 
   return (
     <QuantityControllerWrapper>
-      <Button>
+      <Button onClick={decrease}>
         <img src={IMAGE_PATH.MINUS_ICON} alt="minus-icon" />
       </Button>
-      <Count>1</Count>
-      <Button>
+      <Count>{quantity}</Count>
+      <Button onClick={increase}>
         <img src={IMAGE_PATH.PLUS_ICON} alt="plus-icon" />
       </Button>
     </QuantityControllerWrapper>
