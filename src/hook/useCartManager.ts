bc6b9ac009b5218d9156fab8bc07eb3b622217a9
장정안 <wjddks96@naver.com>
2025-05-24 17:36:42 +0900

    feat: 장바구니 삭제 시 filter => find로 변경

--- src/hook/useCartManager.ts
+++ src/hook/useCartManager.ts
@@ -10,7 +10,7 @@ const useCartManager = () => {
   const handleAddCart = async (productId: number) => {
     try {
       await cartApi.addToCart(productId);
-      fetchCartData();
+      await fetchCartData();
       openToast('상품이 장바구니에 추가되었습니다.', true);
     } catch (error) {
       openToast('장바구니 담기에 실패했어요...', false);
@@ -19,17 +19,17 @@ const useCartManager = () => {
 
   const handleRemoveCart = async (productId: number) => {
     try {
-      const cartItem = cartData.filter(
+      const cartItem = cartData.find(
         (item: CartItem) => item.product.id === productId
       );
 
-      if (cartItem.length === 0) {
+      if (!cartItem) {
         console.error('장바구니에서 해당 상품을 찾을 수 없습니다:', productId);
         openToast('장바구니에서 상품을 찾을 수 없습니다.', false);
         return;
       }
 
-      const targetId = cartItem[0].id;
+      const targetId = cartItem.id;
       await cartApi.removeFromCart(targetId);
       await fetchCartData();
       openToast('상품이 장바구니에서 제거되었습니다.', true);
@@ -43,6 +43,7 @@ const useCartManager = () => {
     cartData,
     handleAddCart,
     handleRemoveCart,
+    fetchCartData,
   };
 };
 
