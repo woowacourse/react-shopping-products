f8c7071d096a6e76e9f757205aacd18b271b3540
eunoia-jaxson <eunoia@handong.ac.kr>
2025-05-24 20:58:24 +0900

    feat: 최초 접속 및 필터링 구현

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,25 +1,23 @@
 import ErrorToast from '../components/ErrorToast';
 import ProductCardList from '../components/ProductCardList';
 import ProductListToolBar from '../components/ProductListToolBar';
-import { useProductsContext } from '../contexts/useProductsContext';
-import { useShoppingCartContext } from '../contexts/useShoppingCartContext';
 import OrbitSpinner from '../components/OrbitSpinner/index';
+import { useProducts } from '../hooks/useProducts';
+// import { useShoppingCart } from '../hooks/useShoppingCart';
 
 const ProductListPage = () => {
-  const products = useProductsContext();
-  const shoppingCart = useShoppingCartContext();
+  const products = useProducts();
+  // const shoppingCart = useShoppingCart();
 
   return (
     <>
-      {products.error.is && <ErrorToast message={products.error.message} />}
-      {shoppingCart.error.is && (
-        <ErrorToast message={shoppingCart.error.message} />
-      )}
+      {products.error && <ErrorToast message={products.error} />}
+      {/* {shoppingCart.error && <ErrorToast message={shoppingCart.error} />} */}
       <ProductListToolBar />
       {products.loading ? (
         <OrbitSpinner />
       ) : (
-        <ProductCardList products={products.items} />
+        <ProductCardList products={products.data} />
       )}
     </>
   );
