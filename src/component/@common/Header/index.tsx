4bdcd38feb2f7d1a515dd2cba8f87bff8f8bd4a7
장정안 <wjddks96@naver.com>
2025-05-24 17:43:07 +0900

    feat: 헤더 내 아이콘 클릭 시 바텀 시트 열리게 구현

--- src/component/@common/Header/index.tsx
+++ src/component/@common/Header/index.tsx
@@ -1,19 +1,34 @@
 import { IconCart, IconLogo } from '../../../asset';
 import CardQuantity from '../CardQuantity';
 import { CartStyle, HeaderStyle } from './Header.styles';
+import * as Dialog from '../Dialog/Dialog';
+import CartSheet from '../../feature/CartSheet';
+import useCartManager from '../../../hook/useCartManager';
 
 const Header = ({ count }: { count: number }) => {
-  return (
-    <section css={HeaderStyle}>
-      <a href="/">
-        <img src={IconLogo} alt="logo" />
-      </a>
+  const { cartData, handleRemoveCart, fetchCartData } = useCartManager();
 
-      <a href="/cart" css={CartStyle}>
-        <img src={IconCart} alt="cart" />
-        {count > 0 && <CardQuantity>{count}</CardQuantity>}
-      </a>
-    </section>
+  const handleBeforeOpen = async () => {
+    await fetchCartData();
+  };
+
+  return (
+    <Dialog.Root>
+      <section css={HeaderStyle}>
+        <a href="/">
+          <img src={IconLogo} alt="logo" />
+        </a>
+
+        <Dialog.Trigger css={CartStyle} onBeforeOpen={handleBeforeOpen}>
+          <img src={IconCart} alt="cart" />
+          {count > 0 && <CardQuantity>{count}</CardQuantity>}
+        </Dialog.Trigger>
+      </section>
+
+      <Dialog.Portal>
+        <CartSheet cartData={cartData} handleRemoveCart={handleRemoveCart} />
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
 
