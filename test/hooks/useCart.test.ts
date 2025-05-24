62c1e686a0835cbe730689b8741d55f14e14e3ab
aydenote <dslgpgh@gmail.com>
2025-05-24 17:19:32 +0900

    refactor: 장바구니 불필요한 테스트 제거

--- test/hooks/useCart.test.ts
+++ test/hooks/useCart.test.ts
@@ -1,14 +1,37 @@
-import { renderHook, waitFor } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 import useCart from "../../src/hooks/useCart";
+import { getCartState } from "../../src/mocks/handlers/cartHandler";
 
 describe("useCart", () => {
-	describe("장바구니 목록 조회", () => {
-		it("장바구니에 등록된 아이템 목록을 조회한다.", async () => {
-			const { result } = renderHook(() => useCart());
+	it("장바구니에 등록된 아이템 목록을 조회한다.", async () => {
+		const { result } = renderHook(() => useCart());
 
-			await waitFor(() => {
-				expect(result.current.cartProducts).toHaveLength(5);
-			});
+		await waitFor(() => {
+			expect(result.current.cartProducts).toEqual(getCartState());
+		});
+	});
+
+	it("장바구니에 아이템을 등록한다.", async () => {
+		const { result } = renderHook(() => useCart());
+
+		act(() => {
+			result.current.updateCartItem("add", 20);
+		});
+
+		await waitFor(() => {
+			expect(result.current.cartProducts).toEqual(getCartState());
+		});
+	});
+
+	it("장바구니에 등록된 아이템을 제거한다.", async () => {
+		const { result } = renderHook(() => useCart());
+
+		act(() => {
+			result.current.updateCartItem("remove", 1);
+		});
+
+		await waitFor(() => {
+			expect(result.current.cartProducts).toEqual(getCartState());
 		});
 	});
 });
