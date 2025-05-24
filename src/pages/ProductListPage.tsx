de43b941595365f6cbd2003440d16c2c886a3f32
H0ngJu <hongju4385@gmail.com>
2025-05-25 02:49:38 +0900

    refactor(ProductListPage): useAPIContext를 사용하도록 수정 및 sorting, category 핸들러 정의

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,28 +1,60 @@
 import ErrorToast from "../components/ErrorToast";
 import ProductCardList from "../components/ProductCardList";
 import ProductListToolBar from "../components/ProductListToolBar";
-import { useProductsContext } from "../contexts/products/useProductsContext";
-import { useShoppingCartContext } from "../contexts/shoppingCart/useShoppingCartContext";
 import OrbitSpinner from "../components/OrbitSpinner/index";
+import { useAPIContext } from "../contexts/APIProvider/useAPIContext";
+import getProducts from "../APIs/products/getProducts";
+import { Category, Product, SortOption } from "../types/product.type";
+import { useMemo, useState } from "react";
 
 const ProductListPage = () => {
-  const { products, productsError, isProductsLoading } = useProductsContext();
-  const { isShoppingLoading } = useShoppingCartContext();
-  const { shoppingCartError } = useShoppingCartContext();
+  const [category, setCategory] = useState<Category>("전체");
+  const [sort, setSort] = useState<SortOption>("낮은 가격순");
+
+  const handleCategoryChange = (newCategory: Category) => {
+    setCategory(newCategory);
+    refetch();
+  };
+
+  const handleSortChange = (newSort: SortOption) => {
+    setSort(newSort);
+    refetch();
+  };
+
+  const endpoint = useMemo(() => {
+    const params = new URLSearchParams({
+      page: "0",
+      size: "20",
+      sort: sort === "낮은 가격순" ? "price,asc" : "price,desc",
+    });
+
+    if (category !== "전체") {
+      params.append("category", category);
+    }
+
+    return `/products?${params.toString()}`;
+  }, [category, sort]);
+
+  const name = `products-${category}-${sort}`;
+
+  const { data, isLoading, error, refetch } = useAPIContext<Product[]>({
+    name,
+    fetcher: () => getProducts({ endpoint }),
+  });
 
   return (
     <>
-      {productsError.isError && (
-        <ErrorToast errorMessage={productsError.errorMessage} />
-      )}
-      {shoppingCartError.isError && (
-        <ErrorToast errorMessage={shoppingCartError.errorMessage} />
-      )}
-      <ProductListToolBar />
-      {isProductsLoading && isShoppingLoading ? (
+      <ProductListToolBar
+        category={category}
+        sort={sort}
+        setCategory={handleCategoryChange}
+        setSort={handleSortChange}
+      />
+      {error.isError && <ErrorToast errorMessage={error.errorMessage} />}
+      {isLoading ? (
         <OrbitSpinner />
       ) : (
-        <ProductCardList products={products} />
+        data && <ProductCardList products={data} />
       )}
     </>
   );
