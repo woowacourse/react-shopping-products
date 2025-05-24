96582669a5a730ec412e5bdfbc74bf45534fa09f
wo-o29 <woogur29@gmail.com>
2025-05-24 21:22:46 +0900

    ​​feat: 장바구니 개수 업데이트 관련 모킹 추가
    Co-authored-by: Deokbin Kim <162319857+Db0111@users.noreply.github.com>

--- src/apis/mocks/cartItemsData/cartItemsHandler.ts
+++ src/apis/mocks/cartItemsData/cartItemsHandler.ts
@@ -2,21 +2,21 @@ import { http, HttpResponse } from "msw";
 import { cartItemsMockData } from "./cartItemsMockData";
 import { productsMockData } from "../productsData/productsMockData";
 import { ProductItemType } from "@/apis/products/product.type";
+import {
+  AddCartItems,
+  UpdateCartItemQuantity,
+} from "@/apis/cartItems/cartItem.type";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const CART_ITEMS_URL = `${BASE_URL}/cart-items`;
 
-interface AddCartItemsBody {
-  productId: number;
-  quantity: number;
-}
 let cartItemSequence = 1;
 
 export const cartItemsHandlers = [
   http.get(CART_ITEMS_URL, () => {
     return HttpResponse.json(cartItemsMockData, { status: 200 });
   }),
-  http.post<never, AddCartItemsBody>(CART_ITEMS_URL, async ({ request }) => {
+  http.post<never, AddCartItems>(CART_ITEMS_URL, async ({ request }) => {
     const { productId, quantity } = await request.json();
     const product = productsMockData.content.find(
       ({ id }) => id === productId
@@ -44,4 +44,20 @@ export const cartItemsHandlers = [
       message: "장바구니에서 상품을 제거했습니다.",
     });
   }),
+  http.patch<{ id: string }, UpdateCartItemQuantity>(
+    `${CART_ITEMS_URL}/:id`,
+    async ({ request, params }) => {
+      const { quantity } = await request.json();
+      const cartItemId = params.id;
+      const cartItemIndex = cartItemsMockData.content.findIndex(
+        ({ id }) => id === Number(cartItemId)
+      );
+
+      cartItemsMockData.content[cartItemIndex].quantity = Number(quantity);
+      return HttpResponse.json({
+        status: 200,
+        message: "장바구니 상품 수량을 변경했습니다.",
+      });
+    }
+  ),
 ];
