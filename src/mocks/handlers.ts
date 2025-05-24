7c553ad92023d33376d48f7c3f3364c6bc8caeff
thgml05 <aisohii@naver.com>
2025-05-25 07:37:55 +0900

    feat: 장바구니 상품 목록 msw handler 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,14 @@
 import { http, HttpResponse } from 'msw';
 // import { productList } from './productData';
 import productData from './productData.json';
+import CartData from './cartData.json';
 
 export const handlers = [
   http.get(`${import.meta.env.VITE_API_BASE_URL}/products`, () => {
     return HttpResponse.json(productData);
+  }),
+
+  http.get(`${import.meta.env.VITE_API_BASE_URL}/cart-items`, () => {
+    return HttpResponse.json(CartData);
   }),
 ];
