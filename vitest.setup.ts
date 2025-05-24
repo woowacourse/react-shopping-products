a6129c9ecdd7dc0371b346b834a3ad889d94e187
guesung <gueit214@naver.com>
2025-05-24 21:39:40 +0900

    feat: 각 테스트 후 초기화

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,8 +1,11 @@
 import { setupServer } from "msw/node";
-import { handlers } from "./src/mocks/handlers";
+import { handlers, resetCartItems } from "./src/mocks/handlers";
 
 const server = setupServer(...handlers);
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 afterAll(() => server.close());
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+  server.resetHandlers();
+  resetCartItems();
+});
