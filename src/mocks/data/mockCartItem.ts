80a535346409495c19b78b62ba1bf16f6e0e751e
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-05-24 16:53:26 +0900

    feat: 장바구니 아이템 mockCartItem 데이터 추가

---
+++ src/mocks/data/mockCartItem.ts
@@ -0,0 +1,119 @@
+export const cartItems = [
+  {
+    id: 6614,
+    quantity: 1,
+    product: {
+      id: 34,
+      name: "코카콜라 제로 1.5L",
+      price: 2100,
+      imageUrl:
+        "https://sitem.ssgcdn.com/88/19/87/item/0000006871988_i1_750.jpg",
+      category: "식료품",
+      quantity: 10,
+    },
+  },
+  {
+    id: 6615,
+    quantity: 1,
+    product: {
+      id: 28,
+      name: "아샷추",
+      price: 3800,
+      imageUrl:
+        "https://d2afncas1tel3t.cloudfront.net/wp-content/uploads/2023/12/%EC%95%84%EC%83%B7%EC%B6%94%EC%95%84%EC%9D%B4%EC%8A%A4%ED%8B%B0%EC%83%B7%EC%B6%94%EC%B9%B4%EB%94%94%EC%B9%B4%ED%8E%98%EC%9D%B8_1.png",
+      category: "식료품",
+      quantity: 10,
+    },
+  },
+  {
+    id: 6616,
+    quantity: 1,
+    product: {
+      id: 27,
+      name: "아바라",
+      price: 4800,
+      imageUrl:
+        "https://image.ohousecdn.com/i/bucketplace-v2-development/uploads/cards/snapshots/171653801239329270.jpeg?w=256&h=366&c=c",
+      category: "식료품",
+      quantity: 10,
+    },
+  },
+  {
+    id: 6618,
+    quantity: 1,
+    product: {
+      id: 25,
+      name: "얌샘김밥",
+      price: 5000,
+      imageUrl:
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20171018_6%2F1508253136417Dlrjh_PNG%2FCdq22zpVpr92_XHROlHbxjJ0.png&type=sc960_832",
+      category: "식료품",
+      quantity: 10,
+    },
+  },
+  {
+    id: 6619,
+    quantity: 1,
+    product: {
+      id: 42,
+      name: "프린세스 미용놀이",
+      price: 1010,
+      imageUrl:
+        "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202204/19/ed8eddd4-0edd-40ad-af7d-44a171577c92.jpg",
+      category: "패션잡화",
+      quantity: 10,
+    },
+  },
+  {
+    id: 6620,
+    quantity: 1,
+    product: {
+      id: 5,
+      name: "동물 양말",
+      price: 20000,
+      imageUrl:
+        "https://m.cocosocks.com/web/product/medium/202503/940897aced51144109baa4d145def01f.jpg",
+      category: "패션잡화",
+      quantity: 10,
+    },
+  },
+  {
+    id: 6621,
+    quantity: 1,
+    product: {
+      id: 57,
+      name: "후추",
+      price: 23000,
+      imageUrl:
+        "https://i.namu.wiki/i/t4M8eo-01JpLUZLIrpTD5vqBnquZLvQrGZJ4Dl3lcXtbk5AOlyK2k3k-VOQQNhFyor-zEHGhlEn60FisBPIqjF8i2xRq10Dbc_Hgg5IbSGM0ROgmychWXYmJzU95XhFmpLMhgUyUGPMv7S9-6Jh6PQ.webp",
+      category: "패션잡화",
+      quantity: 10,
+    },
+  },
+  {
+    id: 6622,
+    quantity: 1,
+    product: {
+      id: 4,
+      name: "달 무드등",
+      price: 28000,
+      imageUrl:
+        "https://thumbnail6.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/794f/cecbea5bdc654a11ae02d28b4d1f4bd2a03a7389eb2b8cc4a45c1c9f7d9b.jpg",
+      category: "패션잡화",
+      quantity: 10,
+    },
+  },
+  {
+    id: 6623,
+    quantity: 1,
+    product: {
+      id: 8,
+      name: "앵그리버드",
+      price: 50000,
+      imageUrl:
+        "https://media.bunjang.co.kr/product/223522208_%7Bcnt%7D_1683581287_w%7Bres%7D.jpg",
+      category: "패션잡화",
+      quantity: 10,
+    },
+  },
+];
