5c6895acd5ace8ad97bc53e076c418e9ff04657a
Jeongeun Lee <leeju1009@gmail.com>
2025-05-24 14:49:57 +0900

    feat: 장바구니 api를 모킹하도록 코드 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,5 +1,6 @@
 import { default as productHandlers } from './products';
+import { default as cartHandlers } from './cart';
 
-const handlers = [...productHandlers];
+const handlers = [...productHandlers, ...cartHandlers];
 
 export default handlers;
