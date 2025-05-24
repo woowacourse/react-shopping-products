abbab7f22883bac5513c52b06aea8344f52a9618
H0ngJu <hongju4385@gmail.com>
2025-05-24 16:13:32 +0900

    feat(CartModal): 장바구니 모달 구현

---
+++ src/components/CartModal/CartModal.tsx
@@ -0,0 +1,37 @@
+import { Modal } from "pongju-modal-component";
+import CartModalItem from "./CartModalItem";
+import CartTotalPrice from "./CartTotalPrice";
+
+interface CartModalProps {
+  isOpen: boolean;
+  onModalClose: () => void;
+}
+
+const CartModal = ({ isOpen, onModalClose }: CartModalProps) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onModalClose} position="bottom">
+      <Modal.Backdrop>
+        <Modal.Frame styled={{ width: "429px" }}>
+          <Modal.Title title={"장바구니"} />
+          <Modal.Body>
+            <CartModalItem
+              name="테스트"
+              imgUrl={"./sample.png"}
+              quantity={1}
+              price={5000}
+            />
+            <CartTotalPrice totalPrice={5000} />
+          </Modal.Body>
+          <Modal.Button
+            title={"닫기"}
+            onClick={() => {}}
+            size="large"
+            styled={{ height: "44px" }}
+          />
+        </Modal.Frame>
+      </Modal.Backdrop>
+    </Modal>
+  );
+};
+
+export default CartModal;
