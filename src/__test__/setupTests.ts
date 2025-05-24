bed6521e12fbd91a57db14276e3b100391d43a92
sooyeoniya <tndus1306403@naver.com>
2025-05-24 18:42:06 +0900

    test: MSW 테스트를 위한 설정

--- src/__test__/setupTests.ts
+++ src/__test__/setupTests.ts
@@ -1,1 +1,11 @@
 import "@testing-library/jest-dom";
+import { afterAll, afterEach, beforeAll } from "vitest";
+import { server } from "../mocks/server";
+import { cleanup } from "@testing-library/react";
+
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => server.close());
