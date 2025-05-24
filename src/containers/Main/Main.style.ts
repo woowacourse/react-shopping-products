88206489820b125edaa0a8baddf0fa99e623c8fc
mun-kyeong <horse6953@naver.com>
2025-05-25 05:06:28 +0900

    feat: Main 스타일 수정

--- src/containers/Main/Main.style.ts
+++ src/containers/Main/Main.style.ts
@@ -3,7 +3,6 @@ import { css } from "@emotion/react";
 const bodyLayout = css`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   padding: 36px 25px;
   box-sizing: border-box;
   gap: 28px;
