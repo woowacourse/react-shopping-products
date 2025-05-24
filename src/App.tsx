793ec660cc38b7ef6c071ad25ee32dc04fb7da97
keemsebeen <sebin0580@gmail.com>
2025-05-24 22:49:11 +0900

    feat: App에 DataProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ProductListPage } from './features/ProductList/pages/ProductList';
+import { DataProvider } from './shared/context/DataProvider';
 import { ToastProvider } from './shared/context/ToastProvider';
 
 export const App = () => {
   return (
     <ToastProvider>
-      <ProductListPage />
+      <DataProvider>
+        <ProductListPage />
+      </DataProvider>
     </ToastProvider>
   );
 };
