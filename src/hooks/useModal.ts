abd4f5709c3f604e75f52c55fd476e14201c0f1a
eunoia-jaxson <eunoia@handong.ac.kr>
2025-05-25 21:37:45 +0900

    feat: 모달 커스텀 훅 구현

---
+++ src/hooks/useModal.ts
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export const useModal = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return {
+    isOpen,
+    handleOpen,
+    handleClose,
+  };
+};
