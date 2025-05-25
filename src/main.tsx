e86fe320c240c0ae23d213bd48c34a5b79f20bd5
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-05-26 00:49:48 +0900

    🔨 chore: 배포 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
 
-  const { worker } = await import("./mocks/browser");
-  return worker.start();
+import "./index.css";
+import App from "./App";
+
+async function enableMocking() {
+  const { worker } = await import("./mocks/browser"); //Dynamic import
+  await worker.start({
+    serviceWorker: {
+      url: "/react-shopping-products/mockServiceWorker.js",
+    },
+  });
 }
 
 enableMocking().then(() => {
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/react-shopping-products/",
-
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
     }),
   ],
+  base: "/react-shopping-products/",
+
   test: {
     environment: "jsdom",
     globals: true,
