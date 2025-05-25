a261b5013d6df08c1125af584ab9b38104abf329
youjeong Jeong <wjddbwjd1205@naver.com>
2025-05-25 13:18:02 +0900

    refactor: useShopping 훅을 context 기반 전역 상태 구조로 리팩터링

--- src/features/ProductList/hooks/useShopping.ts
+++ src/features/ProductList/hooks/useShopping.ts
@@ -1,20 +1,25 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback, useContext, useMemo } from 'react';
 
 import { useCart } from './useCart';
 import { useProductList } from './useProductList';
+import { APIContext } from '@/shared/context/APIContext';
+import { Product } from '../types/Product';
+import { CartItem } from '../types/Cart';
 
 export const useShopping = () => {
-  const { cartData, addToCart, increaseQuantity, decreaseQuantity, deleteFromCart } = useCart();
+  const { data } = useContext(APIContext);
+  const product = Object.values(data['product'] ?? {}) as Product[];
+  const cartData = Object.values(data['cartItem'] ?? {}) as CartItem[];
+  const { addToCart, increaseQuantity, decreaseQuantity, deleteFromCart } = useCart();
   const {
     isLoading: isProductLoading,
-    product,
     categorySelect,
     priceSelect,
     handleCategorySelect,
     handlePriceSelect,
   } = useProductList();
 
-  const data = useMemo(() => {
+  const filteredData = useMemo(() => {
     return product.map((item) => {
       const isInCart = cartData
         .map((item) => item.product)
@@ -24,7 +29,7 @@ export const useShopping = () => {
         isChecked: isInCart,
       };
     });
-  }, [cartData, product]);
+  }, [product, cartData]);
 
   const addCartItem = useCallback(
     async (id: number) => {
@@ -35,24 +40,27 @@ export const useShopping = () => {
 
   const updateCartQuantity = useCallback(
     async (cartItemId: number, delta: number, currentQuantity: number) => {
+      const latestCartItem = cartData.find((item) => item.id === cartItemId);
+      if (!latestCartItem) return;
+
       const newQuantity = currentQuantity + delta;
 
       if (newQuantity < 1) {
         await deleteFromCart(cartItemId);
       } else {
         if (delta > 0) {
-          await increaseQuantity(cartItemId, currentQuantity);
+          await increaseQuantity(cartItemId, latestCartItem.quantity);
         } else {
-          await decreaseQuantity(cartItemId, currentQuantity);
+          await decreaseQuantity(cartItemId, latestCartItem.quantity);
         }
       }
     },
-    [deleteFromCart, increaseQuantity, decreaseQuantity]
+    [cartData, deleteFromCart, increaseQuantity, decreaseQuantity]
   );
 
   return {
     cartData,
-    filteredData: data,
+    filteredData,
     isLoading: isProductLoading,
     addCartItem,
     updateCartQuantity,
