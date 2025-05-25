12f965b3169e828796012130e2ff0ecd41058128
hanheel <hanhee630630@gmail.com>
2025-05-25 12:47:23 +0900

    refactor: 배포 환경에서도 msw가 작동할 수 있도록 변경

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 async function enableMocking() {
-  if (import.meta.env.MODE !== 'development') {
+  if (import.meta.env.MODE === 'development' || import.meta.env.VITE_APP_ENABLE_MSW === 'true') {
     return;
   }
 
@@ -13,6 +13,7 @@ async function enableMocking() {
 }
 
 enableMocking().then(() => {
+  // biome-ignore lint/style/noNonNullAssertion: 테스트 환경에서는 무조건 렌더링되어야 하기 때문에 무조건 존재하는 요소로 처리
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <App />
