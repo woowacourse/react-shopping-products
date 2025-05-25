0216d912f96561df53a5e21a4b0bdb0abc1ef6f8
AHHYUNJU <wndkgus0225@naver.com>
2025-05-26 00:38:28 +0900

    fix: 불필요한 코드 삭제

--- src/domain/cart/cart.tsx
+++
@@ -1,9 +0,0 @@
-import { CartItemTypes } from "../../types/CartItemType";
-
-export function findIsCartItem(items: CartItemTypes[], productId: number) {
-  return items.find((item) => item.product.id === productId);
-}
-
-export function isCartFull(cartItems: CartItemTypes[], maxCount: number = 50) {
-  return cartItems.length >= maxCount;
-}
