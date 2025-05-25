3b759f196f3aa8f1d1d67ec42e823d8dc22d6df4
wo-o29 <woogur29@gmail.com>
2025-05-25 21:15:11 +0900

    refactor: 주석 제거​​
    Co-authored-by: Deokbin Kim <162319857+Db0111@users.noreply.github.com>

--- src/pages/Product/ProductPage.tsx
+++ src/pages/Product/ProductPage.tsx
@@ -1,15 +1,7 @@
 import Header from "@/components/Header";
 import ProductContent from "@/components/Product/Content";
 
-// const cartItemResource = getCartItems();
 function ProductPage() {
-  // const cartItemData = use(cartItemResource);
-  // const [cartItems, setCartItems] = useState<CartItemType[]>(cartItemData);
-
-  // const updateCartItems = (newCartItems: CartItemType[]) => {
-  //   setCartItems(newCartItems);
-  // };
-
   return (
     <>
       <Header />
