197da9f355f02c94369a19cfb5ee4df891b41568
AHHYUNJU <wndkgus0225@naver.com>
2025-05-26 06:06:23 +0900

    feat: mockData에 stock 항목 추가

--- src/mocks/shoppingCart/shoppingCartMockData.ts
+++ src/mocks/shoppingCart/shoppingCartMockData.ts
@@ -10,6 +10,7 @@ export const SHOPPING_CART_MOCK_DATA = {
         imageUrl:
           "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20171018_6%2F1508253136417Dlrjh_PNG%2FCdq22zpVpr92_XHROlHbxjJ0.png&type=sc960_832",
         category: "식료품",
+        stock: 1,
       },
     },
     {
@@ -22,6 +23,7 @@ export const SHOPPING_CART_MOCK_DATA = {
         imageUrl:
           "https://image.zeta-ai.io/profile-image/793bf4d3-03de-4ac3-afe1-95be8a9bc62c/29cd5c72-f872-4dba-8be1-21ba51e4487f.jpeg?w=1080&q=90&f=webp",
         category: "패션잡화",
+        stock: 2,
       },
     },
     {
@@ -34,6 +36,7 @@ export const SHOPPING_CART_MOCK_DATA = {
         imageUrl:
           "https://kream-phinf.pstatic.net/MjAyNTA1MTNfMjI5/MDAxNzQ3MTA4MjUzOTg4.106G0-WfVU8g8ziNKgKJjc1_UXvF-2IatsA-Cz5mG1og.etXRFVPYqcs5J9HAfXpaHFPFHorGnZU4Nl7k4368rfog.PNG/a_090d2310040b4f9ca922f2498ae8ae3a.png?type=l",
         category: "패션잡화",
+        stock: 3,
       },
     },
     {
@@ -46,6 +49,7 @@ export const SHOPPING_CART_MOCK_DATA = {
         imageUrl:
           "https://d2afncas1tel3t.cloudfront.net/wp-content/uploads/2023/12/%EC%95%84%EC%83%B7%EC%B6%94%EC%95%84%EC%9D%B4%EC%8A%A4%ED%8B%B0%EC%83%B7%EC%B6%94%EC%B9%B4%EB%94%94%EC%B9%B4%ED%8E%98%EC%9D%B8_1.png",
         category: "식료품",
+        stock: 4,
       },
     },
     {
@@ -57,6 +61,7 @@ export const SHOPPING_CART_MOCK_DATA = {
         price: 100,
         imageUrl: "33",
         category: "식료품",
+        stock: 5,
       },
     },
     {
@@ -69,6 +74,7 @@ export const SHOPPING_CART_MOCK_DATA = {
         imageUrl:
           "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202204/19/ed8eddd4-0edd-40ad-af7d-44a171577c92.jpg",
         category: "패션잡화",
+        stock: 6,
       },
     },
     {
@@ -81,6 +87,7 @@ export const SHOPPING_CART_MOCK_DATA = {
         imageUrl:
           "https://sitem.ssgcdn.com/88/19/87/item/0000006871988_i1_750.jpg",
         category: "식료품",
+        stock: 7,
       },
     },
     {
@@ -93,6 +100,7 @@ export const SHOPPING_CART_MOCK_DATA = {
         imageUrl:
           "https://image.ohousecdn.com/i/bucketplace-v2-development/uploads/cards/snapshots/171653801239329270.jpeg?w=256&h=366&c=c",
         category: "식료품",
+        stock: 8,
       },
     },
     {
@@ -105,6 +113,7 @@ export const SHOPPING_CART_MOCK_DATA = {
         imageUrl:
           "https://m.cocosocks.com/web/product/medium/202503/940897aced51144109baa4d145def01f.jpg",
         category: "패션잡화",
+        stock: 9,
       },
     },
   ],
