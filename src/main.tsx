ca414524a386444e869c04efb0181c539984dd7e
keemsebeen <sebin0580@gmail.com>
2025-05-25 19:29:17 +0900

    chore: seviceWorker url 지정

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,11 @@ import { App } from './App.tsx';
 
 async function enableMocking() {
   const { worker } = await import('./shared/mocks/browser.ts');
-  return worker.start();
+  return worker.start({
+    serviceWorker: {
+      url: '/react-shopping-products/mockServiceWorker.js',
+    },
+  });
 }
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import { defineConfig } from 'vite';
 // eslint-disable-next-line no-restricted-exports
 export default defineConfig({
   plugins: [react({ jsxImportSource: '@emotion/react' })],
+  publicDir: 'public',
+  base: 'https://keemsebin.github.io/react-shopping-products/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
