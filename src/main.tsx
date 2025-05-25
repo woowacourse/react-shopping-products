3efa751fbd90abcd24b5a82505e04bc8ebaf7e50
mun-kyeong <horse6953@naver.com>
2025-05-25 19:44:36 +0900

    feat: enableMocks 함수로 MSW 서비스 워커 초기화 로직 분리

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-if (
-  import.meta.env.MODE !== "production" ||
-  import.meta.env.VITE_ENABLE_MSW === "true"
-) {
-  const { worker } = await import("./mocks/browser");
-  await worker.start({
-    serviceWorker: {
-      url: `${import.meta.env.BASE_URL}mockServiceWorker.js`,
-    },
-  });
+async function enableMocks() {
+  if (
+    import.meta.env.MODE !== "production" ||
+    import.meta.env.VITE_ENABLE_MSW === "true"
+  ) {
+    const { worker } = await import("./mocks/browser");
+    await worker.start({
+      serviceWorker: {
+        url: `${import.meta.env.BASE_URL}mockServiceWorker.js`,
+      },
+    });
+  }
 }
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+enableMocks().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
