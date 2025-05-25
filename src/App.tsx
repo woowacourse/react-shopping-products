753366dd527485a414cfa45bc851511e02b69697
youjeong Jeong <wjddbwjd1205@naver.com>
2025-05-25 13:05:03 +0900

    feat: App에 APIProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ProductListPage } from './features/ProductList/pages/ProductList';
+import { APIProvider } from './shared/context/APIContext';
 import { ToastProvider } from './shared/context/ToastProvider';
 
 export const App = () => {
   return (
     <ToastProvider>
-      <ProductListPage />
+      <APIProvider>
+        <ProductListPage />
+      </APIProvider>
     </ToastProvider>
   );
 };
