964fabe59dc2d7d51f4d7a69dd6750d68855e7e3
AHHYUNJU <wndkgus0225@naver.com>
2025-05-25 17:20:53 +0900

    fix: AppContent 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import AppContent from "./AppContent";
-import { AppProvider } from "./domain/contexts/AppProvider";
+import { useState } from "react";
+import Modal from "./Component/Common/Modal/Modal";
+import Body from "./Component/Layout/Body/Body";
+import Header from "./Component/Layout/Header/Header";
+import ProductListContainer from "./Component/Product/ProductListContainer/ProductListContainer";
+import { APIProvider } from "./domain/contexts/APIContext";
 
 function App() {
   return (
-    <>
-      <AppProvider>
-        <AppContent />
-      </AppProvider>
-    </>
+    <APIProvider>
+      <AppContent />
+    </APIProvider>
   );
 }
 
 export default App;
+
+function AppContent() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <div>
+        <Header onOpenModal={() => setIsModalOpen(true)} />
+        <Body>
+          <ProductListContainer />
+        </Body>
+      </div>
+      {/* <Modal isModalOpen={isModalOpen} onClose={() => setIsModalOpen(false)} /> */}
+    </>
+  );
+}
