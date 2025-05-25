2276c161856d08764d8917f9824e112a63846b24
youjeong Jeong <wjddbwjd1205@naver.com>
2025-05-26 00:00:16 +0900

    chore: NODE_ENV와 상관없이 무조건 mocking 활성화

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,6 @@ import './index.css';
 import { App } from './App.tsx';
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
 
   const { worker } = await import('./mocks/browser'); //Dynamic import
   return worker.start();
