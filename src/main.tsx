47ef7a8b124f3fa4a4d477c002bd682d66987691
Ko Daeun <drechsu@gmail.com>
2025-05-25 23:49:01 +0900

    refactor : production환경에서 msw 사용하도록 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-if (import.meta.env.DEV) {
+if (import.meta.env.DEV || import.meta.env.PROD) {
   const { worker } = await import('./mocks/browser');
   await worker.start({
     onUnhandledRequest: 'bypass',
