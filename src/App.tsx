baeb7ce5c3dc84bcab436341ed5ee081ff2585c6
ohgus <anakin1108@naver.com>
2025-05-25 14:21:24 +0900

    refactor: Header를 feature로 이동
    내부에 카드 관련 로직이 포함되어 있음

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import { GlobalStyle, theme } from './style';
-import Header from './component/@common/Header/Header';
+import Header from './component/feature/Header/Header';
 import ShoppingList from './page/ShoppingList/ShoppingList';
 import ToastProvider from './component/@common/Toast/context/toastProvider';
 import Toast from './component/@common/Toast/Toast';
