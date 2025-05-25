08a3fb0246ea69cd8f8cfc71ee0344e2af5cd8b7
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-05-26 01:37:05 +0900

    refactor: 리뷰어가 쉽게 확인할 수 있도록, 개발환경 뿐만 아니라, 프로덕션 환경에서도 MSW 를 start

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,8 @@ import { worker } from "./mocks/browser";
 import { ErrorMessageProvider } from "./context/ErrorMessageContext.tsx";
 import { CartItemsIdProvider } from "./context/CartItemsContext.tsx";
 
+// 리뷰어가 쉽게 확인할 수 있도록, 개발환경 뿐만 아니라, 프로덕션 환경에서도 MSW 를 start 한다.
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
-
   return worker.start();
 }
 
