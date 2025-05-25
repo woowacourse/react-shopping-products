7a33b76cccbac877f62a519b864c7139055236bb
Ko Daeun <drechsu@gmail.com>
2025-05-26 00:39:23 +0900

    fix : 프로덕션 환경에서 배포 가능하도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 const startWorker = async () => {
-  const { worker } = await import('./mocks/browser');
-  await worker.start({
-    onUnhandledRequest: 'bypass',
-  });
+  if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
+    const { worker } = await import('./mocks/browser');
+    await worker.start({
+      onUnhandledRequest: 'bypass',
+    });
+  } else if (navigator.serviceWorker) {
+    const { worker } = await import('./mocks/browser');
+    await worker.start({
+      serviceWorker: {
+        url: '/mockServiceWorker.js',
+      },
+      onUnhandledRequest: 'bypass',
+    });
+  }
 };
 
 if (import.meta.env.DEV || import.meta.env.PROD) {
