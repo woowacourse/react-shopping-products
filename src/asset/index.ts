1fa64d2ecafe1f85dae6290b17ecb80e840aba20
ohgus <anakin1108@naver.com>
2025-05-25 17:50:51 +0900

    chore: 수량 조절 아이콘 추가

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -5,3 +5,5 @@ export const IconLogo = `${baseUrl}icon/ic_logo.svg`;
 export const IconCart = `${baseUrl}icon/ic_cart.svg`;
 export const IconAddCart = `${baseUrl}icon/ic_add_cart.svg`;
 export const IconRemoveCart = `${baseUrl}icon/ic_remove_cart.svg`;
+export const IconPlus = `${baseUrl}icon/ic_plus.svg`;
+export const IconMinus = `${baseUrl}icon/ic_minus.svg`;
