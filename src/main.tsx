0491fe03daed533937bd7e53b9c520242f2dabb7
Ko Daeun <drechsu@gmail.com>
2025-05-26 00:37:06 +0900

    fix : top-level await 제거

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-if (import.meta.env.DEV || import.meta.env.PROD) {
+const startWorker = async () => {
   const { worker } = await import('./mocks/browser');
   await worker.start({
     onUnhandledRequest: 'bypass',
   });
+};
+
+if (import.meta.env.DEV || import.meta.env.PROD) {
+  startWorker();
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
