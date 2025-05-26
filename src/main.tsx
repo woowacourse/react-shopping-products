d9890fa3e264bf34ac9c1473813836b7224ce19d
sanghee01 <sang6299@naver.com>
2025-05-26 13:57:54 +0900

    fix: github pages 환경일때 서비스워커  base path 경로 설정

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,15 @@ async function enableMocking() {
 
   const { worker } = await import('./mocks/browser');
 
+  const isGitHubPages = window.location.hostname.includes('github.io');
+  const serviceWorkerUrl = isGitHubPages
+    ? '/react-shopping-products/mockServiceWorker.js'
+    : '/mockServiceWorker.js';
+
   return worker.start({
+    serviceWorker: {
+      url: serviceWorkerUrl,
+    },
     onUnhandledRequest: 'bypass',
   });
 }
