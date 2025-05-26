37ef6b4228734921346f83cbf48de90b6df334e4
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-05-26 14:31:02 +0900

    refactor: App 컴포넌트에서 ProductListPage로 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import Header from "./components/product/Header/Header";
-import ProductListContainer from "./components/product/ProductListContainer/ProductListContainer";
-
-import styled from "@emotion/styled";
-
 import { ApiProvider } from "./context/ApiContext/ApiContext";
+import ProductListPage from "./page/ProductListPage";
 
 function App() {
   return (
     <ApiProvider>
-      <Container>
-        <Wrapper>
-          <Header />
-          <ProductListContainer />
-        </Wrapper>
-      </Container>
+      <ProductListPage />
     </ApiProvider>
   );
 }
 
 export default App;
-
-const Container = styled.main`
-  display: flex;
-  max-width: 430px;
-  min-height: 100vh;
-  margin: 0 auto;
-`;
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
