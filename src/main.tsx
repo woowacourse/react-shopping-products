d76bdbbba0721cd8b443c936133b9fa5b32d49ad
eunoia-jaxson <eunoia@handong.ac.kr>
2025-05-26 11:43:28 +0900

    build: msw 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./styles/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './styles/index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = await import('./mocks/browser.ts');
+  worker.start();
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
