c5fbc4e43c87b807688a2366b6de0e286fdbe0d1
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-26 16:46:18 +0900

    chore: 수정

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,7 @@ import App from './App.tsx';
 async function enableMocking() {
   const { worker } = await import('./mocks/browser'); //Dynamic import하는 것이 눈에 띄였다.
 
-  const isLocalhost = location.hostname === 'localhost';
   return worker.start({
-    serviceWorker: {
-      url: isLocalhost
-        ? '/mockServiceWorker.js'
-        : '/react-shopping-products/mockServiceWorker.js',
-    },
     onUnhandledRequest: 'bypass',
   });
 }
