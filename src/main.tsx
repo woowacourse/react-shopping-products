5c9813e118ba2811c931cca3aa39a9aae6648333
jin123457 <jinsol9909@gmail.com>
2025-05-26 16:26:03 +0900

    feat: 모킹 기능 설정 개선 및 Vite 설정 파일 코드 수정

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { Global } from '@emotion/react';
 import { resetCss } from './styles/reset.ts';
 
 async function enableMocking() {
-  const { worker } = await import('./api/mock/browser.ts');
-
-  return worker.start();
+  const { worker } = await import('./api/mock/browser.ts'); //Dynamic import
+  await worker.start({
+    serviceWorker: {
+      url: '/react-shopping-products/mockServiceWorker.js',
+    },
+  });
 }
-
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
