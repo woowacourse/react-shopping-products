83b9ddca7bfdb211918a2897a692df6255e73541
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-05-26 16:59:35 +0900

    chore: 테스트 환경을 위해 MSW server 세팅

--- setupTests.ts
+++ setupTests.ts
@@ -1,1 +1,12 @@
 import "@testing-library/jest-dom";
+import { server } from "./src/mocks/server.ts";
+
+beforeAll(() => {
+  server.listen();
+});
+afterEach(() => {
+  server.resetHandlers();
+});
+afterAll(() => {
+  server.close();
+});
