b8229677063c5ed8f4a8ebbf1c7fa3cd35d9be22
Minje Sung <minje813@dgu.ac.kr>
2025-05-26 12:54:48 +0900

    fix: Top-level await is not available 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import App from './App.tsx';
 import './index.css';
 import { worker } from './mocks/browser.ts';
 
-await worker.start();
+async function prepare() {
+  if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
+    await worker.start();
+  }
+}
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+prepare().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
