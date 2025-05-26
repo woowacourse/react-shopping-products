535bc537d671533a1cb27374cb6f6414f2bdd860
SuHwa Lee <tnghk9611@naver.com>
2025-05-26 16:39:28 +0900

    chore: msw 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,19 @@ import { worker } from './mocks/browser.ts';
 import React from 'react';
 
 if (process.env.NODE_ENV === 'development' || import.meta.env.VITE_USE_MSW === 'true') {
-  worker.start().then(() => {
-    createRoot(document.getElementById('root')!).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-    );
-  });
+  worker
+    .start({
+      serviceWorker: {
+        url: '/react-shopping-products/mockServiceWorker.js',
+      },
+    })
+    .then(() => {
+      createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>,
+      );
+    });
 } else {
   createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
