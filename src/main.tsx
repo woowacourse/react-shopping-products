27854a7eef18d88bab12022041b71e7a2fb10325
eunoia-jaxson <eunoia@handong.ac.kr>
2025-05-26 13:45:52 +0900

    build: MSW 배포 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/index.css';
 
-if (import.meta.env.DEV) {
-  const { worker } = await import('./mocks/browser.ts');
-  await worker.start({ onUnhandledRequest: 'bypass' });
+async function enableMocking() {
+  const isLocalhost = location.hostname === 'localhost';
+  const { worker } = await import('./mocks/browser');
+  return worker.start({
+    serviceWorker: {
+      url: isLocalhost
+        ? '/mockServiceWorker.js'
+        : '/react-shopping-products/mockServiceWorker.js',
+    },
+    onUnhandledRequest: 'bypass',
+  });
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
