1f9eb23250bc51521cbd030e496878bc751369a4
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-26 16:11:34 +0900

    chore: 배포 테스트

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+//fasdkfjasldkfjasldfkj
 async function enableMocking() {
   const { worker } = await import('./mocks/browser'); //Dynamic import하는 것이 눈에 띄였다.
 
