92f41893e12a9a24153cc1edbe68876538c9e280
kimyou1102 <yoosk5485@naver.com>
2025-05-26 14:40:35 +0900

    chore: 불필요한 브라우저 워커 초기화

--- src/mock/browser.ts
+++ src/mock/browser.ts
@@ -2,12 +2,3 @@ import { setupWorker } from 'msw/browser';
 import { handlers } from './handlers';
 
 export const worker = setupWorker(...handlers);
-
-await worker.start({
-  serviceWorker: {
-    url: `${import.meta.env.BASE_URL}mockServiceWorker.js`,
-    options: {
-      scope: import.meta.env.BASE_URL,
-    },
-  },
-});
