330a5b99b577b1869ee57077c726f01d0eeb89e9
장정안 <wjddks96@naver.com>
2025-05-26 12:43:51 +0900

    refactor: Text 내 className prop 추가

--- src/component/@common/Text/index.tsx
+++ src/component/@common/Text/index.tsx
@@ -1,12 +1,18 @@
+/** @jsxImportSource @emotion/react */
 import { textStyle } from './Text.styles';
 
 interface TextProps {
   children: React.ReactNode;
   variant: 'title' | 'productName' | 'body' | 'description';
+  className?: string;
 }
 
-const Text = ({ children, variant }: TextProps) => {
-  return <p css={textStyle[variant]}>{children}</p>;
+const Text = ({ children, variant, className }: TextProps) => {
+  return (
+    <p css={textStyle[variant]} className={className}>
+      {children}
+    </p>
+  );
 };
 
 export default Text;
