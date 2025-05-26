e43d5bd09a075db9b404e51f3b337a0e16437502
aydenote <dslgpgh@gmail.com>
2025-05-26 16:54:07 +0900

    feat: 이미지가 에러인 경우 기본 이미지 렌더링 구현

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -8,7 +8,15 @@ const CartItem = ({ product }: { product: MergedProduct }) => {
 
 	return (
 		<div className={S.cartContainer}>
-			<img src={product.imageUrl} alt="상품 이미지" className={S.cartImg} />
+			<img
+				src={product.imageUrl}
+				alt="상품 이미지"
+				className={S.cartImg}
+				onError={(e) => {
+					const target = e.target as HTMLImageElement;
+					target.src = "./images/default-image.png";
+				}}
+			/>
 			<div className={S.cartInfo}>
 				<p className={S.productName}>{product.name}</p>
 				<p className={S.productPrice}>{product.price.toLocaleString()}원</p>
