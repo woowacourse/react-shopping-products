db512838f61693d6bbb2590079092556578ffb85
aydenote <dslgpgh@gmail.com>
2025-05-26 16:45:50 +0900

    feat: 이미지 URL 동적 입력

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -6,9 +6,11 @@ import CartItemCount from "./CartItemCount";
 const CartItem = ({ product }: { product: MergedProduct }) => {
 	const { updateCartItem } = useCartState();
 
+	console.log(product);
+
 	return (
 		<div className={S.cartContainer}>
-			<img src="https://velog.velcdn.com/images/minsungje/post/c27c57cb-fcbb-4641-b72d-0e2030739ae7/image.jpg" alt="상품 이미지" className={S.cartImg} />
+			<img src={product.imageUrl} alt="상품 이미지" className={S.cartImg} />
 			<div className={S.cartInfo}>
 				<p className={S.productName}>{product.name}</p>
 				<p className={S.productPrice}>{product.price.toLocaleString()}원</p>
