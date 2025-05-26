5dbb4836efb38deed8ae41ef031a47822ee623d0
ha-kuku <hakukudh@gmail.com>
2025-05-26 17:31:01 +0900

    feat: ErrorBoundary 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { APIProvider } from './hook/APIContext.tsx';
+import ErrorBoundary from './ErrorBoundary.tsx';
 
 if (import.meta.env.DEV) {
   const { worker } = await import('./mocks/browser');
@@ -11,7 +12,9 @@ if (import.meta.env.DEV) {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <APIProvider>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </APIProvider>
   </React.StrictMode>
 );
