5b9d3dc0be04853592bb4df112457fab69ecff2f
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-26 16:31:34 +0900

    chore: 배포 설정 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-//fasdkfjasldkfjasldfkj
 async function enableMocking() {
   const { worker } = await import('./mocks/browser'); //Dynamic import하는 것이 눈에 띄였다.
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  publicDir: 'public',
-  base: '/react-shopping-products/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
