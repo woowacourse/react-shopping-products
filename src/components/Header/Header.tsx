52fb062c382f9d2d30e8a54682d736f2f3313f0e
jin123457 <jinsol9909@gmail.com>
2025-05-26 13:24:43 +0900

    feat: 모달 컴포넌트 라이브러리 적용

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,12 +2,13 @@ import * as S from './Header.styled';
 import BagIcon from '../Icon/BagIcon';
 import { useCartItemList } from '../../pages/productListPage/context/useCartContext';
 
-function Header() {
+function Header({ setIsOpen }: { setIsOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
   const { cartItemList } = useCartItemList();
+
   return (
     <S.HeaderContainer>
       <S.HeaderTitle>SHOP</S.HeaderTitle>
-      <S.HeaderIconContainer>
+      <S.HeaderIconContainer onClick={() => setIsOpen((prev) => !prev)}>
         <BagIcon />
         {cartItemList.length > 0 && <S.CartBadge>{cartItemList.length}</S.CartBadge>}
       </S.HeaderIconContainer>
