3dfa144acebe4922119dd039a276ce2aad764da3
aydenote <dslgpgh@gmail.com>
2025-05-26 16:30:49 +0900

    refactor: 장바구니 상품 수량 로직 컴포넌트로 분리

--- src/components/Cart/CartItemCount.tsx
+++ src/components/Cart/CartItemCount.tsx
@@ -1,42 +1,16 @@
 import { MergedProduct } from "../../types";
-import { useStoreContext } from "../Context/StoreContext";
+import CartCountButton from "../Button/CartCountButton";
+import { useCartState } from "../Context/StoreContext";
 import S from "./CartItemCount.module.css";
 
 const CartItemCount = ({ mergedProduct }: { mergedProduct: MergedProduct }) => {
-	const { updateCartItem } = useStoreContext();
+	const { updateCartItem } = useCartState();
 
 	return (
 		<div className={S.container}>
-			<button
-				className={S.countButton}
-				disabled={mergedProduct.cartInfo?.quantity === 0}
-				onClick={() => {
-					if (mergedProduct.cartInfo?.quantity === 1) {
-						return updateCartItem("remove", mergedProduct.cartInfo.id);
-					}
-					return mergedProduct.cartInfo && updateCartItem("change", mergedProduct.cartInfo.id, mergedProduct.cartInfo.quantity - 1);
-				}}
-			>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-					<rect x="0.5" y="0.5" width="24" height="24" rx="8" fill="white" />
-					<rect x="1" y="1" width="23" height="23" rx="7.5" stroke="black" strokeOpacity="0.1" />
-					<path d="M6.5 12.5C11.1863 12.5 13.8137 12.5 18.5 12.5" stroke="#363636" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-				</svg>
-			</button>
+			<CartCountButton type="minus" mergedProduct={mergedProduct} onUpdate={updateCartItem} />
 			<p className={S.cartQuantity}>{mergedProduct.cartInfo?.quantity}</p>
-			<button
-				className={S.countButton}
-				disabled={mergedProduct.quantity === mergedProduct.cartInfo?.quantity}
-				onClick={() => {
-					return mergedProduct.cartInfo && updateCartItem("change", mergedProduct.cartInfo.id, mergedProduct.cartInfo.quantity + 1);
-				}}
-			>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-					<rect x="0.5" y="0.5" width="24" height="24" rx="8" fill="white" />
-					<rect x="1" y="1" width="23" height="23" rx="7.5" stroke="black" strokeOpacity="0.1" />
-					<path d="M6.5 12.5H18.5M12.5 18.5V6.5" stroke="#363636" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-				</svg>
-			</button>
+			<CartCountButton type="plus" mergedProduct={mergedProduct} onUpdate={updateCartItem} />
 		</div>
 	);
 };
