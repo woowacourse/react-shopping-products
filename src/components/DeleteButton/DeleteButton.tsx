51f7dce2fe1cb1092e0c8f25a3b50281bb522b23
sanghee01 <sang6299@naver.com>
2025-05-26 12:06:09 +0900

    feat: 빼기 버튼을 삭제 버튼으로 만듦

--- src/components/DeleteButton/DeleteButton.tsx
+++ src/components/DeleteButton/DeleteButton.tsx
@@ -5,8 +5,8 @@ function DeleteButton({ onClick }: { onClick: () => void }) {
   return (
     <Button backgroundColor="#eaeaea" color="#000000" radius="4px" onClick={onClick}>
       <div className={deleteButton}>
-        <img src="./images/deleteCart.png" alt="deleteCart" />
-        <span>빼기</span>
+        {/* <img src="./images/deleteCart.png" alt="deleteCart" /> */}
+        <span>삭제</span>
       </div>
     </Button>
   );
