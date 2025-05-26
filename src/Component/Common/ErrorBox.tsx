094e48fac06d66712d78c580d020a8ef6d05965d
AHHYUNJU <wndkgus0225@naver.com>
2025-05-26 14:52:33 +0900

    feat: ErrorBox 3초 뒤에 사라지도록 구현

--- src/Component/Common/ErrorBox.tsx
+++ src/Component/Common/ErrorBox.tsx
@@ -1,9 +1,23 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { StyledDiv, StyledSpan } from "../../styles/Common/ErrorBox.styles";
 
 type ErrorBoxProps = PropsWithChildren<object>;
 
 export default function ErrorBox({ children }: ErrorBoxProps) {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
     <StyledDiv>
       <StyledSpan>{children}</StyledSpan>
