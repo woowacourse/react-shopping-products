e54d5563a92fc61c86a7b51953a5bb1bd1a9c379
kimyou1102 <yoosk5485@naver.com>
2025-05-26 13:19:00 +0900

    chore: msw 테스트 세팅

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,3 +1,9 @@
 // tests/setup.ts
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/vitest';
+
+import { beforeAll, afterAll, afterEach } from 'vitest';
+import { server } from '../src/mock/server';
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
