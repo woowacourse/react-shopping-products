46908cae4c5ad77ec4b359c56dbf740751622cc4
minji2219 <aabb0219@naver.com>
2025-05-28 01:48:52 +0900

    refactor: cartModal 컴포넌트로 구현

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -2,13 +2,7 @@ import ErrorToast from '../../../shared/ui/ErrorToast';
 import * as S from './Navbar.styles';
 import {useErrorToast} from '../../../shared/provider/errorProvider';
 import {useState} from 'react';
-import Modal from '../../../shared/ui/Modal';
-import CartCard from '../../../features/cart/ui/CartCard';
-import {useApi} from '../../../features/products/provider/apiProvider';
-import {getCartProduct} from '../../../features/cart/api/getCartProduct';
-import CustomButton from '../../../shared/ui/CustomButton';
-import {css} from '@emotion/react';
-import {formatPrice} from '../../../shared/utils/formatPrice';
+import CartModal from '../../../features/cart/ui/CartModal';
 
 interface NavbarProps {
   cartQuantity: number;
@@ -17,13 +11,6 @@ interface NavbarProps {
 export default function Navbar({cartQuantity}: NavbarProps) {
   const error = useErrorToast();
   const [isOpen, setIsOpen] = useState(false);
-
-  const {data: cartItems} = useApi(getCartProduct, 'cartItems');
-  const totalPrice =
-    cartItems?.content.reduce(
-      (acc, item) => acc + item.product.price * item.quantity,
-      0
-    ) || 0;
 
   return (
     <S.NavbarWrapper>
@@ -37,33 +24,7 @@ export default function Navbar({cartQuantity}: NavbarProps) {
         </S.CartIconContainer>
       </S.NavbarContainer>
       {error && <ErrorToast errorMessage={error} />}
-
-      <Modal
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        position="bottom"
-        title={{text: '장바구니', size: 20}}
-      >
-        {cartItems?.content.map((item) => (
-          <CartCard
-            key={item.id}
-            cartId={item.id}
-            product={item.product}
-            quantity={item.quantity}
-          />
-        ))}
-        <S.TotalPriceSection>
-          <S.Description>총 결제 금액</S.Description>
-          <S.TotalPrice>{formatPrice(totalPrice)}원</S.TotalPrice>
-        </S.TotalPriceSection>
-        <CustomButton
-          title="닫기"
-          onClick={() => setIsOpen(false)}
-          css={css`
-            padding: 10px;
-          `}
-        />
-      </Modal>
+      <CartModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
     </S.NavbarWrapper>
   );
 }
