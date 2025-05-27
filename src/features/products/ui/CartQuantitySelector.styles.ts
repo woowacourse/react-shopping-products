bfdf91e3382fdf0dedbc7a8a2fcc819522c986b8
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-28 00:39:06 +0900

    style: 불필요한 styles 제거

--- src/features/products/ui/CartQuantitySelector.styles.ts
+++ src/features/products/ui/CartQuantitySelector.styles.ts
@@ -1,8 +1,6 @@
 import styled from '@emotion/styled';
 
 export const CartQuantityContainer = styled.div`
-  width: 24px;
-  height: 24px;
   display: flex;
   justify-content: end;
   align-items: center;
