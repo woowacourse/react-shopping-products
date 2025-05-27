7d82c2334886d5edd359dc9093e50aa53d678da2
minji2219 <aabb0219@naver.com>
2025-05-28 00:59:33 +0900

    type: env type 선언

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_MOCK_MODE: 'true' | 'false';
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
