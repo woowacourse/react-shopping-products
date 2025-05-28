abdc4fd73f738a74d3b4a92f5b6f68783b082414
keemsebeen <sebin0580@gmail.com>
2025-05-28 21:23:12 +0900

    refactor: 장바구니 수량 삭제 로직 보호구문 추가 및 분기 처리 리팩토링

--- src/features/CartList/hooks/useControlQuantity.ts
+++ src/features/CartList/hooks/useControlQuantity.ts
@@ -47,15 +47,25 @@ export const useControlQuantity = (productId: number) => {
 
   const decreaseQuantity = async () => {
     try {
-      if (cartItem && currentQuantity !== 1) {
-        await cartData.mutate(
-          () => updateCartItem({ cartId: cartItem.id, newQuantity: currentQuantity - 1 }),
-          getCartItemList
-        );
+      if (!cartItem) {
+        showToast('장바구니에 담긴 상품이 없습니다.');
+        return;
       }
+
+      if (currentQuantity <= 0) {
+        showToast('수량이 올바르지 않습니다.');
+        return;
+      }
+
       if (currentQuantity === 1) {
         await removeCartItem();
+        return;
       }
+
+      await cartData.mutate(
+        () => updateCartItem({ cartId: cartItem.id, newQuantity: currentQuantity - 1 }),
+        getCartItemList
+      );
     } catch (error) {
       showToast(`장바구니에서 ${cartItem?.product.name} 상품을 삭제할 수 없습니다.`);
     }
