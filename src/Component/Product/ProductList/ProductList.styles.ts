32ac7daa5b3756f880463208a65d2fbc3c898d54
AHHYUNJU <wndkgus0225@naver.com>
2025-05-28 22:54:09 +0900

    fix: 불필요한 파일 삭제

--- src/Component/Product/ProductList/ProductList.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const StyledUl = styled.ul`
-  display: grid;
-  list-style-type: none;
-  padding: 0;
-  grid-template-columns: 1fr 1fr;
-  gap: 16px;
-`;
--- src/contexts/CartProvider.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { CartContext } from "./CartContext";
-import useShoppingCart from "../hooks/useShoppingCart";
-
-export function CartProvider({ children }: { children: React.ReactNode }) {
-  const cart = useShoppingCart();
-
-  return <CartContext.Provider value={cart}>{children}</CartContext.Provider>;
-}
