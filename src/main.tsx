05e35e09047eb22b32c5d976314c782270716e9e
yeji0214 <1225yj50@naver.com>
2025-05-30 16:35:03 +0900

    chore: mock 실행용 스크립트(dev:mock) 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import App from "./App.tsx";
 import { DataProvider } from "./contexts/DataContext.tsx";
 
 async function main() {
-  if (import.meta.env.MODE === "development") {
+  if (import.meta.env.MODE === "mock") {
     const { worker } = await import("./mocks/browser");
-    await worker.start({
-      onUnhandledRequest: "bypass",
-    });
+    await worker.start();
   }
 
   ReactDOM.createRoot(document.getElementById("root")!).render(
