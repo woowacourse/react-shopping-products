af2f4df479eff47767f931857e1f39adf42c6bc9
mun-kyeong <horse6953@naver.com>
2025-05-31 22:17:32 +0900

    fix: vite.config.ts에서 불필요한 define 설정 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 /// <reference types="vitest" />
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
+export default defineConfig(() => {
   return {
     base: "/react-shopping-products/",
     plugins: [
@@ -12,10 +10,6 @@ export default defineConfig(({ mode }) => {
         jsxImportSource: "@emotion/react",
       }),
     ],
-    define: {
-      "import.meta.env.VITE_BASE_URL": JSON.stringify(env.VITE_BASE_URL || ""),
-      "import.meta.env.VITE_USER_TOKEN": JSON.stringify(env.VITE_USER_TOKEN),
-    },
     test: {
       globals: true,
       environment: "jsdom",
