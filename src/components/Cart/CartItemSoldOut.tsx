71a66660dc07327da8491f88dc8e24fb3ab729ab
aydenote <dslgpgh@gmail.com>
2025-05-31 22:33:24 +0900

    feat: 상품 품절 컴포넌트 구현

---
+++ src/components/Cart/CartItemSoldOut.tsx
@@ -0,0 +1,7 @@
+import S from "./CartItemSoldOut.module.css";
+
+const CartItemSoldOut = () => {
+	return <div className={S.soldOut}>품절</div>;
+};
+
+export default CartItemSoldOut;
--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -1,15 +1,15 @@
 import { MergedProduct } from "../../types";
-
 import ItemCardInfo from "./ItemCardInfo";
 import ItemCardButton from "./ItemCardButton";
-import S from "./ItemCard.module.css";
 import Image from "../Image/Image";
+import CartItemSoldOut from "../Cart/CartItemSoldOut";
+import S from "./ItemCard.module.css";
 
 const ItemCard = ({ mergedProduct }: { mergedProduct: MergedProduct }) => {
 	return (
 		<div className={S.cardContainer}>
 			<Image src={mergedProduct.imageUrl} alt="상품 이미지" className="card" />
-			{mergedProduct.quantity === 0 && <div className={S.backGround}>품절</div>}
+			{mergedProduct.quantity === 0 && <CartItemSoldOut />}
 			<div className={S.contentContainer}>
 				<ItemCardInfo name={mergedProduct.name} price={mergedProduct.price} />
 				<div className={S.buttonWrap}>
