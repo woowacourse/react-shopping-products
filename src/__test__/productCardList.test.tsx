31ff3c6f9782088f86133dbaa5384e718631be3a
yeongipark <yeongi0111@naver.com>
2025-05-31 19:02:21 +0900

    test: ProductList -> ProductContainer로 렌더링 컴포넌트 변경

--- src/__test__/productCardList.test.tsx
+++ src/__test__/productCardList.test.tsx
@@ -1,16 +1,16 @@
 // @vitest-environment jsdom
 import { render, screen } from "@testing-library/react";
-import ProductCardList from "../components/productCardList/ProductCardList";
 import { expect, describe, it } from "vitest";
 import { ToastProvider } from "../provider/ToastProvider";
 import { DataProvider } from "../provider/DataProvider";
+import ProductContainer from "../components/productContainer/ProductContainer";
 
 describe("ProductCardList 컴포넌트", () => {
   it("상품 데이터를 받아오면 상품 목록을 렌더링한다", async () => {
     render(
       <DataProvider>
         <ToastProvider>
-          <ProductCardList />
+          <ProductContainer />
         </ToastProvider>
       </DataProvider>
     );
