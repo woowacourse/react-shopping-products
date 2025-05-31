7c6e27721144952a041bd7c80ed15932cd1f71de
장정안 <wjddks96@naver.com>
2025-06-01 05:28:48 +0900

    refactor: 필요없는 상태 제거

--- src/component/feature/ProductCard/index.tsx
+++ src/component/feature/ProductCard/index.tsx
@@ -9,13 +9,11 @@ import {
   productCardImageContainerStyle,
   productCardImageStyle,
   productCardStyle,
-  soldOutTextClass,
 } from './ProductCard.styles';
 import { IconAddCart } from '../../../asset';
 import { Product, CartItem } from '../../../types/common';
 import CountButton from '../CountButton';
 import SoldOutOverlay from '../SoldOutOverlay';
-import { useToast } from '../../@common/Toast/context';
 
 interface ProductCardProps extends Omit<Product, 'category'> {
   isInCart: boolean;
@@ -35,10 +33,8 @@ const ProductCard = ({
   handleRemoveCart,
   cartData,
 }: ProductCardProps) => {
-  const [remainingStock, setRemainingStock] = useState(quantity);
   const [showCountButton, setShowCountButton] = useState(isInCart);
   const [currentQuantity, setCurrentQuantity] = useState(0);
-  const { openToast } = useToast();
   const [isSoldOut, setIsSoldOut] = useState(quantity <= 0);
 
   useEffect(() => {
@@ -47,7 +43,6 @@ const ProductCard = ({
     setCurrentQuantity(cartQuantity);
 
     const newRemainingStock = quantity - cartQuantity;
-    setRemainingStock(newRemainingStock);
     setIsSoldOut(newRemainingStock <= 0);
 
     if (cartQuantity > 0 && !showCountButton) {
@@ -61,11 +56,6 @@ const ProductCard = ({
     const prevCount = currentQuantity;
 
     if (count > prevCount) {
-      if (remainingStock <= 0) {
-        openToast('재고가 부족합니다.', false);
-        return;
-      }
-
       handleAddCart(id);
     } else if (count < prevCount) {
       handleRemoveCart(id);
@@ -77,11 +67,6 @@ const ProductCard = ({
   };
 
   const handleAddButtonClick = () => {
-    if (remainingStock <= 0) {
-      openToast('재고가 부족합니다.', false);
-      return;
-    }
-
     setShowCountButton(true);
     handleAddCart(id);
   };
@@ -97,13 +82,6 @@ const ProductCard = ({
         <div css={productCardContentHeaderStyle}>
           <Text variant="productName">{name}</Text>
           <Text variant="body">{price.toLocaleString()}원</Text>
-          {remainingStock > 0 ? (
-            <Text variant="description">남은 수량: {remainingStock}개</Text>
-          ) : (
-            <Text variant="description" className={soldOutTextClass.toString()}>
-              품절
-            </Text>
-          )}
         </div>
         <div css={productCardButtonContainerStyle}>
           {showCountButton ? (
