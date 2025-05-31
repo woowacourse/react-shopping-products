51175b37d1980a8667c1ffe341a769fa5d4a4d90
ohgus <anakin1108@naver.com>
2025-05-31 21:42:25 +0900

    test: open, close에 대한 테스트 추가

--- src/hook/__test__/useDisclosure.test.ts
+++ src/hook/__test__/useDisclosure.test.ts
@@ -16,4 +16,24 @@ describe('useDisclosure', () => {
 
     expect(result.current.isOpen).toBe(true);
   });
+
+  it('isOpen 상태를 닫을 수 있다.', () => {
+    const { result } = renderHook(() => useDisclosure());
+
+    act(() => {
+      result.current.close();
+    });
+
+    expect(result.current.isOpen).toBe(false);
+  });
+
+  it('isOpen 상태를 열 수 있다.', () => {
+    const { result } = renderHook(() => useDisclosure());
+
+    act(() => {
+      result.current.open();
+    });
+
+    expect(result.current.isOpen).toBe(true);
+  });
 });
