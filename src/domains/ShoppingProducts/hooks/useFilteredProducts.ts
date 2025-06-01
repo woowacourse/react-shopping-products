147088c59383448c8edd16bf70761c1bc34bf414
mun-kyeong <horse6953@naver.com>
2025-06-01 21:39:09 +0900

    refactor: useFilteredProducts에서 필터링 로직 간소화

--- src/domains/ShoppingProducts/hooks/useFilteredProducts.ts
+++ src/domains/ShoppingProducts/hooks/useFilteredProducts.ts
@@ -5,11 +5,10 @@ export function useFilteredProducts() {
   const { product, filter, category } = useShoppingContext();
 
   return useMemo(() => {
-    let result = [...product.item];
-
-    if (category !== "전체") {
-      result = result.filter((p) => p.category === category);
-    }
+    const result = [...product.item].filter((p) => {
+      if (category !== "전체") return p.category === category;
+      return true;
+    });
 
     if (filter === "낮은 가격순") {
       result.sort((a, b) => a.price - b.price);
