4842dcba2434e098d60c7883837345a1e13fed38
aydenote <dslgpgh@gmail.com>
2025-06-02 04:13:08 +0900

    refactor: 상품 데이터 관리 훅 개선
    - useProducts 훅 데이터 패칭 로직 개선

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,46 +1,46 @@
 import { PRODUCT_URL } from "../constants/endpoint";
-import getQueryURL from "../utils/getQueryURL";
-import { FilterType, Product, SortType } from "../types";
-import { useState, useEffect } from "react";
+import { FilterType, SortType } from "../types";
+import { useData } from "../components/Context/DataContext";
+import { useEffect, useState } from "react";
 import fetchData from "../utils/api/fetchData";
+import getQueryURL from "../utils/getQueryURL";
 
-interface UseProductsProps {
-	page?: string;
-	size?: string;
-}
-
-export default function useProducts({ page = "0", size = "20" }: UseProductsProps = {}) {
+export const useProducts = () => {
 	const [filter, setFilter] = useState<FilterType>("");
 	const [sort, setSort] = useState<SortType>("asc");
-	const [products, setProducts] = useState<Product[]>([]);
-	const [loading, setLoading] = useState<boolean>(true);
-	const [productError, setProductError] = useState<string>("");
-
 	const query = {
-		page,
-		size,
+		page: "0",
+		size: "20",
 		...(sort && { sort: `price,${sort}` }),
 		...(filter && { category: filter }),
 	};
 	const url = getQueryURL(PRODUCT_URL, query);
 
-	const fetchProducts = async () => {
-		try {
-			setLoading(true);
-			const data = await fetchData({ url });
-			setProducts(data);
-		} catch (error) {
-			if (error instanceof Error) {
-				setProductError(error.message);
-			}
-		} finally {
-			setLoading(false);
-		}
-	};
+	const {
+		data: products,
+		refetch,
+		loading,
+		setLoading,
+	} = useData({
+		fetcher: () =>
+			fetchData({
+				url,
+			}),
+		name: "products",
+	});
 
 	useEffect(() => {
-		fetchProducts();
+		setLoading(true);
+		refetch();
+		setLoading(false);
 	}, [url]);
 
-	return { products, loading, filter, setFilter, sort, setSort, productError };
-}
+	return {
+		products: products || [],
+		loading,
+		filter,
+		setFilter,
+		sort,
+		setSort,
+	};
+};
