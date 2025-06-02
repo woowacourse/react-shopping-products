95704982405f9a2aa45f6a3de3e9c59039156389
장정안 <wjddks96@naver.com>
2025-06-02 13:07:19 +0900

    refactor: 초기 데이터 구조 유틸 함수 분리

--- src/hook/useProductData.ts
+++ src/hook/useProductData.ts
@@ -5,6 +5,35 @@ import useResource from './useResource';
 
 const PRODUCTS_URL = '/products';
 
+const getInitialProductData = (): ProductListResponse => ({
+  content: [],
+  pageable: {
+    pageNumber: 0,
+    pageSize: 20,
+    sort: {
+      empty: true,
+      sorted: false,
+      unsorted: true,
+    },
+    offset: 0,
+    paged: true,
+    unpaged: false,
+  },
+  last: true,
+  totalElements: 0,
+  totalPages: 0,
+  size: 0,
+  number: 0,
+  sort: {
+    empty: true,
+    sorted: false,
+    unsorted: true,
+  },
+  first: true,
+  numberOfElements: 0,
+  empty: true,
+});
+
 const useProductData = (
   sortOption: SortOption,
   categoryOption: CategoryOption
@@ -37,34 +66,7 @@ const useProductData = (
     refetch,
   } = useResource<ProductListResponse>(PRODUCTS_URL, {
     params: getParams(),
-    initialData: {
-      content: [],
-      pageable: {
-        pageNumber: 0,
-        pageSize: 20,
-        sort: {
-          empty: true,
-          sorted: false,
-          unsorted: true,
-        },
-        offset: 0,
-        paged: true,
-        unpaged: false,
-      },
-      last: true,
-      totalElements: 0,
-      totalPages: 0,
-      size: 0,
-      number: 0,
-      sort: {
-        empty: true,
-        sorted: false,
-        unsorted: true,
-      },
-      first: true,
-      numberOfElements: 0,
-      empty: true,
-    },
+    initialData: getInitialProductData(),
     autoFetch: true,
   });
 
