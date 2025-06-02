18333f9ca8c68bc02d09ab33c6b7df998188a921
장정안 <wjddks96@naver.com>
2025-06-02 13:08:36 +0900

    refactor: 초기 데이터 구조 유틸 코드로 분리

--- src/hook/useProductData.ts
+++ src/hook/useProductData.ts
@@ -2,37 +2,9 @@ import { useCallback, useEffect, useRef } from 'react';
 import { CategoryOption, SortOption } from '../types/common';
 import { ProductListResponse } from '../types/response';
 import useResource from './useResource';
+import { getInitialProductData } from '@/util/productUtil';
 
 const PRODUCTS_URL = '/products';
-
-const getInitialProductData = (): ProductListResponse => ({
-  content: [],
-  pageable: {
-    pageNumber: 0,
-    pageSize: 20,
-    sort: {
-      empty: true,
-      sorted: false,
-      unsorted: true,
-    },
-    offset: 0,
-    paged: true,
-    unpaged: false,
-  },
-  last: true,
-  totalElements: 0,
-  totalPages: 0,
-  size: 0,
-  number: 0,
-  sort: {
-    empty: true,
-    sorted: false,
-    unsorted: true,
-  },
-  first: true,
-  numberOfElements: 0,
-  empty: true,
-});
 
 const useProductData = (
   sortOption: SortOption,
