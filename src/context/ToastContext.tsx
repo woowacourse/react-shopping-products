e9c43609122c7b421886974d3da686f02f108090
ShinjungOh <tlswjd9260@gmail.com>
2025-06-03 21:32:57 +0900

    feat: 일정 시간이 지나면 Toast가 사라지도록 duration 추가

--- src/context/ToastContext.tsx
+++ src/context/ToastContext.tsx
@@ -7,17 +7,23 @@ interface ToastMessage {
 
 interface ToastContextType {
   toasts: ToastMessage[];
-  showToast: (message: string) => void;
+  showToast: (message: string, duration?: number) => void;
 }
 
 const ToastContext = createContext<ToastContextType | undefined>(undefined);
 
+const DEFAULT_DURATION = 3000;
+
 export function ToastProvider({ children }: { children: ReactNode }) {
   const [toasts, setToasts] = useState<ToastMessage[]>([]);
 
-  const showToast = (message: string) => {
+  const showToast = (message: string, duration: number = DEFAULT_DURATION) => {
     const id = Date.now();
     setToasts((prevToasts) => [...prevToasts, { message, id }]);
+
+    setTimeout(() => {
+      setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
+    }, duration);
   };
 
   return <ToastContext.Provider value={{ toasts, showToast }}>{children}</ToastContext.Provider>;
