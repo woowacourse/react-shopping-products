41edd00e4211f292cc1a557c93c31ab6ba715828
bunju20 <ghkd2271@naver.com>
2025-06-03 14:52:28 +0900

    chore: 불필요한 로깅 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,8 @@ import { Global } from "@emotion/react";
 import { resetCss } from "./styles/reset";
 
 async function startApp() {
-  console.log("MSW 초기화 중...");
-
   try {
     const { worker } = await import("./mocks/browser");
-
-    console.log("MSW 워커 로드됨:", !!worker);
 
     await worker.start({
       serviceWorker: {
@@ -18,8 +14,6 @@ async function startApp() {
       },
       onUnhandledRequest: "warn",
     });
-
-    console.log("🔶 MSW 모의 서버가 성공적으로 시작되었습니다.");
   } catch (error) {
     console.error("MSW 로드 실패:", error);
   }
